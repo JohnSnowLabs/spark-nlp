e308e954297b6fa488a49a92fdce2ecf84d99aaf
saifjsl <saif@johnsnowlabs.com>
2017-05-18 21:18:48 -0300

    NON-FUNCTIONAL port -> Style code improvements

--- src/main/scala/com/jsl/nlp/annotators/pos/perceptron/AveragedPerceptron.scala
+++ src/main/scala/com/jsl/nlp/annotators/pos/perceptron/AveragedPerceptron.scala
@@ -20,24 +20,24 @@ class AveragedPerceptron {
     features
       .filter{case (feature, value) => featureWeights.contains(feature) && value != 0}
       .map{case (feature, value ) => (featureWeights(feature), value)}
-      .foreach{case (categoryWeights, value) => {
-        categoryWeights.foreach{case (category, weight) => {
+      .foreach{case (categoryWeights, value) =>
+        categoryWeights.foreach{case (category, weight) =>
           scores(category) += (value * weight)
-        }}
-      }}
+        }
+      }
     classes.maxBy((category) => (scores(category),category))
   }
 
-  def averageWeights = {
-    featureWeights.foreach{case (feature, weights) => {
+  def averageWeights(): Unit = {
+    featureWeights.foreach{case (feature, weights) =>
       val newWeights: MMap[String, Double] = MMap()
-      weights.foreach{case (category, weight) => {
+      weights.foreach{case (category, weight) =>
         val param = (feature, category)
         val total = totals(param) + ((nIteration - timestamps(param)) * weight)
         newWeights(category) = total / nIteration.toDouble
         featureWeights(feature) = newWeights
-      }}
-    }}
+      }
+    }
   }
 
   def update(truth: String, guess: String, features: Map[String, Int]): Unit = {
@@ -49,11 +49,11 @@ class AveragedPerceptron {
     }
     nIteration += 1
     if (truth != guess) {
-      features.foreach{case (category, feature) => {
+      features.foreach{case (category, _) =>
         val weights = featureWeights.getOrElseUpdate(category, MMap())
         updateFeature(truth, category, weights.getOrElse(truth, 0.0), 1.0)
         updateFeature(guess, category, weights.getOrElse(guess, 0.0), -1.0)
-      }}
+      }
     }
   }
 
@@ -62,14 +62,14 @@ object AveragedPerceptron {
   def train(nIterations: Int, examples: Map[Map[String, Int], String]): AveragedPerceptron = {
     val model = new AveragedPerceptron()
     (1 to nIterations).foreach { _ =>
-      Random.shuffle(examples).foreach{case (features, answer) => {
+      Random.shuffle(examples).foreach{case (features, answer) =>
         val guess = model.predict(features)
         if (guess != answer) {
           model.update(answer, guess, features)
         }
-      }}
+      }
     }
-    model.averageWeights
+    model.averageWeights()
     model
   }
 }
--- src/main/scala/com/jsl/nlp/annotators/pos/perceptron/PerceptronApproach.scala
+++ src/main/scala/com/jsl/nlp/annotators/pos/perceptron/PerceptronApproach.scala
@@ -79,7 +79,7 @@ class PerceptronApproach extends POSApproach {
     val tokens = ArrayBuffer[(String, String)]()
     tokenize(sentences).foreach{words => {
       val context = START ++: words.map(dataPreProcess) ++: END
-      words.zipWithIndex.foreach{case (word, i) => {
+      words.zipWithIndex.foreach{case (word, i) =>
         val tag = tagdict.getOrElse(
           word,
           {
@@ -91,28 +91,28 @@ class PerceptronApproach extends POSApproach {
         tokens.append((word, tag))
         prev2 = prev
         prev = tag
-      }}
+      }
     }}
     tokens.toArray.map(t => TaggedWord(t._1, t._2))
   }
 
   private def makeTagDict(sentences: List[(List[String], List[String])]): Unit = {
     val counts: MMap[String, MMap[String, Int]] = MMap().withDefaultValue(MMap().withDefaultValue(0))
-    sentences.foreach{case (words, tags) => {
-      words.zip(tags).foreach{case (word, tag) => {
+    sentences.foreach{case (words, tags) =>
+      words.zip(tags).foreach{case (word, tag) =>
         counts(word)(tag) += 1
         classes.add(tag)
-      }}
-    }}
+      }
+    }
     val freqThreshold = 20
     val ambiguityThreshold = 0.97
-    counts.foreach{case (word, tagFreqs) => {
+    counts.foreach{case (word, tagFreqs) =>
       val (tag, mode) = tagFreqs.maxBy(_._2)
       val n = tagFreqs.values.sum
       if (n >= freqThreshold && (mode / n.toDouble) >= ambiguityThreshold) {
         tagdict(word) = tag
       }
-    }}
+    }
   }
 
   def train(sentences: List[(List[String], List[String])], nIterations: Int = 5): Unit = {
@@ -123,9 +123,9 @@ class PerceptronApproach extends POSApproach {
     (1 to nIterations).foreach{_ => {
       var c = 0
       var n = 0
-      Random.shuffle(sentences).foreach{case (words, tags) => {
+      Random.shuffle(sentences).foreach{case (words, tags) =>
         val context = START ++: words.map(dataPreProcess) ++: END
-        words.zipWithIndex.foreach{case (word, i) => {
+        words.zipWithIndex.foreach{case (word, i) =>
           val guess = tagdict.getOrElseUpdate(word, {
             val features = getFeatures(i, word, context, prev, prev2)
             val guess = model.predict(features.toMap)
@@ -136,10 +136,10 @@ class PerceptronApproach extends POSApproach {
           prev = guess
           c = if (guess == tags(i)) c + 1 else c
           n += 1
-        }}
-      }}
+        }
+      }
     }}
-    model.averageWeights
+    model.averageWeights()
   }
 
 }
