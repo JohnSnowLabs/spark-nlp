b62b0206b619e25bc15e10b914036ee561ef2589
saifjsl <saif@johnsnowlabs.com>
2017-06-01 04:12:54 -0300

    Few precision improvements and naming changes

--- src/test/scala/com/jsl/nlp/ContentProvider.scala
+++ src/test/scala/com/jsl/nlp/ContentProvider.scala
@@ -24,7 +24,7 @@ object ContentProvider {
     " he was doing. Freemasonry, at any rate as he saw it here, sometimes seemed to him based merely on externals." +
     "\nHe did not think of doubting Freemasonry itself, but suspected that Russian Masonry had taken a wrong path" +
     " and deviated from its original principles. And so toward the end of the year he went abroad to be initiated" +
-    " into the higher secrets of the order.What is to be done in these circumstances? To favor revolutions, overthrow" +
+    " into the higher secrets of the order. What is to be done in these circumstances? To favor revolutions, overthrow" +
     " everything, repel force by force?No! We are very far from that. Every violent reform deserves censure, for it" +
     " quite fails to remedy evil while men remain what they are, and also because wisdom needs no violence. \"But" +
     " what is there in running across it like that?\" said Ilagin's groom. \"Once she had missed it and turned it" +
