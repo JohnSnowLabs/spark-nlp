0f54ae66ecda6b94d09873072402ce94d2895dee
saifjsl <saif@johnsnowlabs.com>
2017-06-16 04:44:50 -0300

    Trivial optimization. Immutable TaggedSentence doesnt need to map on request.

--- src/main/scala/com/jsl/nlp/annotators/pos/TaggedSentence.scala
+++ src/main/scala/com/jsl/nlp/annotators/pos/TaggedSentence.scala
@@ -4,6 +4,6 @@ package com.jsl.nlp.annotators.pos
   * Created by Saif Addin on 5/20/2017.
   */
 case class TaggedSentence(taggedWords: List[TaggedWord]) {
-  def words = taggedWords.map(_.word)
-  def tags = taggedWords.map(_.tag)
+  val words: List[String] = taggedWords.map(_.word)
+  val tags: List[String] = taggedWords.map(_.tag)
 }
