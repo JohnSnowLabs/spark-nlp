60cff1d9916fbd5a9197536d099896b0789fda7f
Saif Addin <saif@johnsnowlabs.com>
2018-01-06 00:45:26 -0300

    - Removed bad comments

--- src/main/scala/com/johnsnowlabs/nlp/annotators/Lemmatizer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/Lemmatizer.scala
@@ -6,7 +6,7 @@ import com.johnsnowlabs.nlp.{Annotation, AnnotatorModel}
 import com.typesafe.config.Config
 import com.johnsnowlabs.nlp.util.ConfigHelper
 import org.apache.spark.ml.param.Param
-import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable, MLReader, MLWriter}
+import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
 
 import scala.collection.JavaConverters._
 
@@ -88,27 +88,6 @@ class Lemmatizer(override val uid: String) extends AnnotatorModel[Lemmatizer] {
 }
 
 object Lemmatizer extends DefaultParamsReadable[Lemmatizer] {
-/*
-  private val config: Config = ConfigHelper.retrieve
-  private val legacyRead = config.getBoolean("settings.legacyRead")
-
-  private val lemmaDictKey = "lemmaDict"
-  private val lemmaFormatKey = "lemmaFormat"
-  private val lemmaKeySepKey = "lemmmaKeySep"
-  private val lemmaValSepKey = "lemmaValSep"
-
-  override def read: MLReader[Lemmatizer] = {
-    if (legacyRead)
-      super.read
-    else
-      new Reader(super.read)
-  }
-
-  /**
-    * Retrieves Lemma dictionary from configured compiled source set in configuration
-    * @return a Dictionary for lemmas
-    */
-*/
   protected def retrieveLemmaDict(
                          lemmaFilePath: String,
                          lemmaFormat: String,
@@ -117,41 +96,4 @@ object Lemmatizer extends DefaultParamsReadable[Lemmatizer] {
                        ): Map[String, String] = {
     ResourceHelper.flattenRevertValuesAsKeys(lemmaFilePath, lemmaFormat.toUpperCase, lemmaKeySep, lemmaValSep)
   }
-/*
-  class Reader(baseReader: MLReader[Lemmatizer]) extends MLReader[Lemmatizer] {
-
-    override def load(path: String): Lemmatizer = {
-      val helper = SerializationHelper(sparkSession, path)
-      val instance = baseReader.load(path)
-
-      val lemmaDict = helper.deserializeMap[String, String](lemmaDictKey)
-      val lemmaFormat = helper.deserializeScalar[String](lemmaFormatKey)
-        .getOrElse(config.getString("nlp.lemmaDict.format"))
-      val lemmaKeySep = helper.deserializeScalar[String](lemmaKeySepKey)
-        .getOrElse(config.getString("nlp.lemmaDict.kvSeparator"))
-      val lemmaValSep = helper.deserializeScalar[String](lemmaValSepKey)
-        .getOrElse(config.getString("nlp.lemmaDict.vSeparator"))
-
-      instance
-        .setLemmaDictMap(lemmaDict)
-        .setLemmaFormat(lemmaFormat)
-        .setLemmaKeySep(lemmaKeySep)
-        .setLemmaValSep(lemmaValSep)
-    }
-  }
-
-  class Writer(model: Lemmatizer, baseWriter: MLWriter) extends MLWriter {
-
-    override protected def saveImpl(path: String): Unit = {
-      baseWriter.save(path)
-      val helper = SerializationHelper(sparkSession, path)
-
-      helper.serializeMap[String, String](lemmaDictKey, model.getLemmaDict)
-      helper.serializeScalar[String](lemmaFormatKey, model.getLemmaFormat)
-      helper.serializeScalar[String](lemmaKeySepKey, model.getLemmaKeySep)
-      helper.serializeScalar[String](lemmaValSepKey, model.getLemmaValSep)
-
-    }
-  }
-*/
 }
