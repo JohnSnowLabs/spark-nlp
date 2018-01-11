35c57e57703294d8078df725c1d8c31c77477900
Alberto <albertoandreotti@gmail.com>
2018-01-11 13:30:36 -0300

    implemented simple version of the regex tokenizer

--- src/main/scala/com/johnsnowlabs/nlp/annotators/assertion/logreg/RegexTokenizer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/assertion/logreg/RegexTokenizer.scala
@@ -9,9 +9,21 @@ class RegexTokenizer extends Tokenizer{
   val punctuation = Seq(".", ":", ";", ",", "?", "!", "+", "-", "_", "(", ")", "{",
     "}", "#", "mg/kg", "ml", "m2", "cm", "/", "\\", "\"", "'", "[", "]", "%", "<", ">", "&", "=")
 
-  val percent_regex = """([0-9]{1,2}\.[0-9]{1,2}%|[0-9]{1,3}%)"""
-  val number_regex = """([0-9]{1,6})"""
+  val percent_regex = """([0-9]{1,2}\.[0-9]{1,2}%|[0-9]{1,3}%)""".r
+  val number_regex = """([0-9]{1,6})""".r
 
-  override def tokenize(sent: String): Array[String] = ???
+  override def tokenize(sent: String): Array[String] = {
+    // replace percentage
+    var tmp = percent_regex.replaceAllIn(sent, " percentnum ")
+
+    // unbind special chars
+    for (c <- punctuation) {
+      tmp = tmp.replaceAllLiterally(c, " " + c + " ")
+    }
+
+    // replace any num
+    val result = number_regex.replaceAllIn(tmp, " digitnum ").toLowerCase.split(" ").filter(_!="")
+    result
+  }
 
 }
