01eee65c0ff2244ef486e60a069631b5524e8310
Alberto <albertoandreotti@gmail.com>
2018-01-15 10:57:59 -0300

    reorg reader level annotations

---
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/datasets/I2B2Annotations.scala
@@ -0,0 +1,6 @@
+package com.johnsnowlabs.nlp.annotators.datasets
+
+/**
+  * Created by jose on 12/01/18.
+  */
+case class I2b2AnnotationAndText(text: String, target: String, label: String, start:Int, end:Int)
