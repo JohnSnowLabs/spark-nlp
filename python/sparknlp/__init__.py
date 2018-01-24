67ad615044f48f2524d5acbae323f76a0e5f887b
Alberto <albertoandreotti@gmail.com>
2018-01-24 11:26:03 -0300

    added jupyter notebook for assertion status

--- python/sparknlp/__init__.py
+++ python/sparknlp/__init__.py
@@ -6,6 +6,7 @@ sys.modules['com.johnsnowlabs.nlp.annotators'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.ner'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.ner.regex'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.ner.crf'] = annotator
+sys.modules['com.johnsnowlabs.nlp.annotators.assertion.logreg'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.pos'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.pos.perceptron'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.sbd'] = annotator
