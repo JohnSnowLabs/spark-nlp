b36e5b39abfac080f5c2b53a2c39c414d5bc5490
Alberto <albertoandreotti@gmail.com>
2018-02-25 12:03:51 -0300

    completed code for predefined pipelines

--- python/pretrained/en/pipelines.py
+++ python/pretrained/en/pipelines.py
@@ -2,12 +2,29 @@ import sparknlp.internal as _internal
 from sparknlp.base import DocumentAssembler, TokenAssembler
 from pyspark.ml.wrapper import JavaModel
 
-# standard parameters available out of the box 
+# standard pipelines with parameters available out of the box 
 
 class SentenceDetector(object):
     def std():
-        j_obj = _internal._DownloadSentenceDetector().apply()
-        jmodel = JavaModel()
-        jmodel._java_obj = j_obj
+        j_obj = _internal._DownloadPredefinedPipeline("com.johnsnowlabs.pretrained.en.pipelines.SentenceDetector.std").apply()
+        jmodel = JavaModel(j_obj)
+        return jmodel 
+
+class Tokenizer(object):
+    def std():
+        j_obj = _internal._DownloadPredefinedPipeline("com.johnsnowlabs.pretrained.en.pipelines.Tokenizer.std").apply()
+        jmodel = JavaModel(j_obj)
+        return jmodel 
+
+class Pos(object):
+    def fast():
+        j_obj = _internal._DownloadPredefinedPipeline("com.johnsnowlabs.pretrained.en.pipelines.Pos.fast").apply()
+        jmodel = JavaModel(j_obj)
+        return jmodel
+
+class Ner(object):
+    def fast():
+        j_obj = _internal._DownloadPredefinedPipeline("com.johnsnowlabs.pretrained.en.pipelines.Ner.fast").apply()
+        jmodel = JavaModel(j_obj)
         return jmodel 
 
