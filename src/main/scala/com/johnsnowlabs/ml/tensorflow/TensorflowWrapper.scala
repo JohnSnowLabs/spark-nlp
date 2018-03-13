36125b9a9be2418a68e715ae86ee7f302fb532db
Aleksei Alekseev <alekseev.aleksei@gmail.com>
2018-03-13 14:12:15 +0300

    More stable tensorflow model saving

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowWrapper.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowWrapper.scala
@@ -1,8 +1,10 @@
 package com.johnsnowlabs.ml.tensorflow
 
 import java.io.{File, IOException, ObjectOutputStream}
+import java.nio.file.attribute.{BasicFileAttributeView, FileTime}
 import java.nio.file.{Files, Paths}
 import java.util.UUID
+
 import org.apache.commons.io.FileUtils
 import org.tensorflow.{Graph, SavedModelBundle, Session}
 
@@ -18,17 +20,36 @@ class TensorflowWrapper
     this(null, null)
   }
 
+  private def getCreationTime(file: File): Long = {
+    val path = Paths.get(file.getAbsolutePath())
+    val view = Files.getFileAttributeView(path, classOf[BasicFileAttributeView]).readAttributes()
+    view.creationTime().toMillis
+  }
+
+  private def findSavedFile(folder: String): String = {
+    val folderFile = new File(folder)
+    require(folderFile.exists, s"folder $folder should exists")
+    require(folderFile.isDirectory, s"folder $folder should be a directory")
+
+    val fName = folderFile.listFiles.filter(f => f.isDirectory && f.getName.startsWith("model_temp_"))
+          .sortBy(f => -getCreationTime(f)).headOption
+    require(fName.isDefined, s"File model_temp_* hasn't found in folder $folder. " +
+      s"Probably something changed in Tensorflow Save operation behaviour")
+
+    fName.get.getAbsolutePath
+  }
+
   def saveToFile(file: String): Unit = {
     // 1. Create tmp director
     val folder = Files.createTempDirectory(UUID.randomUUID().toString.takeRight(12) + "_ner")
       .toAbsolutePath.toString
 
-    val modelName = "model_temp_e08d7ffea7a143eeabde4444e8d81456"
+    val modelFileName = findSavedFile("./")
 
     // 2. Save variables
     session.runner.addTarget("save/SaveV2").run()
     val variablesFile = Paths.get(folder, "variables").toString
-    FileUtils.moveDirectory(new File(modelName), new File(variablesFile))
+    FileUtils.moveDirectory(new File(modelFileName), new File(variablesFile))
 
     // 3. Save Graph
     val graphDef = graph.toGraphDef
