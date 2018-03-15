071b1bf61f8e0621aafa92c4d8c55738e0fc2ba6
Aleksei Alekseev <alekseev.aleksei@gmail.com>
2018-03-15 23:28:00 +0300

    Saving annotations as csv file

---
+++ src/test/scala/com/johnsnowlabs/benchmarks/spark/NerHelper.scala
@@ -0,0 +1,34 @@
+package com.johnsnowlabs.benchmarks.spark
+
+import java.io.{BufferedWriter, File, FileWriter}
+
+import com.johnsnowlabs.nlp.Annotation
+
+
+object NerHelper {
+
+  /**
+    * Print top n Named Entity annotations
+    */
+  def print(annotations: Seq[Annotation], n: Int): Unit = {
+    for (a <- annotations.take(n)) {
+      System.out.println(s"${a.start}, ${a.end}, ${a.result}, ${a.metadata("text")}")
+    }
+  }
+
+  /**
+    * Saves ner results to csv file
+    * @param annotations
+    * @param file
+    */
+  def saveNerSpanTags(annotations: Array[Array[Annotation]], file: String): Unit = {
+    val bw = new BufferedWriter(new FileWriter(new File(file)))
+
+    bw.write(s"start\tend\ttag\ttext\n")
+    for (i <- 0 until annotations.length) {
+      for (a <- annotations(i))
+        bw.write(s"${a.start}\t${a.end}\t${a.result}\t${a.metadata("text")}\n")
+    }
+    bw.close()
+  }
+}
