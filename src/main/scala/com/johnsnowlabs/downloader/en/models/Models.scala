d5a6900b4ac19fa52dcff24df79ad6ce74ca61ad
Saif Addin <saif@johnsnowlabs.com>
2018-03-19 18:54:21 -0300

    - Moved annotator object code to trait - Created base importers - Created easy methods - Cloud models for further annotators

--- src/main/scala/com/johnsnowlabs/downloader/en/models/Models.scala
+++ src/main/scala/com/johnsnowlabs/downloader/en/models/Models.scala
@@ -5,6 +5,8 @@ import com.johnsnowlabs.nlp.annotators
 import com.johnsnowlabs.nlp.annotators.ner.crf.NerCrfModel
 import com.johnsnowlabs.nlp.annotators.pos.perceptron.PerceptronModel
 import com.johnsnowlabs.downloader.ResourceDownloader
+import com.johnsnowlabs.nlp.annotator.{NorvigSweetingModel, ViveknSentimentModel}
+import com.johnsnowlabs.nlp.annotators.Lemmatizer
 
 object CloudDocumentAssembler {
   def retrieveStandard = ResourceDownloader.downloadModel(nlp.DocumentAssembler, "document_std", Some("en"))
@@ -24,5 +26,17 @@ object CloudPerceptronModel {
 
 object CloudNerCrfModel {
   def retrieveSmall = ResourceDownloader.downloadModel(NerCrfModel, "ner_fast", Some("en"))
+}
+
+object CloudLemmatizer {
+  def retrieveSmall = ResourceDownloader.downloadModel(Lemmatizer, "lemma_fast", Some("en"))
+}
+
+object CloudSpellChecker {
+  def retrieveSmall = ResourceDownloader.downloadModel(NorvigSweetingModel, "spell_fast", Some("en"))
+}
+
+object CloudViveknSentiment {
+  def retrieveSmall = ResourceDownloader.downloadModel(ViveknSentimentModel, "vivekn_fast", Some("en"))
 }
 
