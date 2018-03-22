b3ff0a5913411c178ba433b868e6628d1437daef
Saif Addin <saif@johnsnowlabs.com>
2018-03-22 18:38:44 -0300

    - Adaptations to master

--- src/test/scala/com/johnsnowlabs/nlp/annotators/UniversalImportTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/UniversalImportTestSpec.scala
@@ -5,10 +5,13 @@ import org.scalatest.FlatSpec
 class UniversalImportTestSpec extends FlatSpec {
 
   "A SentenceDetector" should "be imported automatically when accessing annotator pseudo package" in {
+    /** Now you can access all annotators by using this import here */
     import com.johnsnowlabs.nlp.annotator._
-    val sd = new SentenceDetector()
+
+    /** For example */
+    val sentenceDetector = new SentenceDetector()
     val SSD_PATH = "./tst_shortcut_sd"
-    sd.write.overwrite().save(SSD_PATH)
+    sentenceDetector.write.overwrite().save(SSD_PATH)
     SentenceDetector.read.load(SSD_PATH)
   }
 
