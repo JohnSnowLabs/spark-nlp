3b9bc79f1a9c3564f1dab27414f24e7a445db39c
Ashok Venkatesan <ashok@mix.com>
2018-03-24 22:15:50 -0700

    Fix IndexOutOfBoundsException when bulding pi and last_leaf functions

--- src/test/scala/com/johnsnowlabs/collections/SearchTrieSpec.scala
+++ src/test/scala/com/johnsnowlabs/collections/SearchTrieSpec.scala
@@ -2,12 +2,16 @@ package com.johnsnowlabs.collections
 
 import org.scalatest.FlatSpec
 
-
 class SearchTrieSpec extends FlatSpec {
-  val trie = SearchTrie(
+  val wikiTrie = SearchTrie(
     Array(
-      Array("a", "b", "a", "b", "a"),
-      Array("a", "a", "a")
+      Array("a"),
+      Array("a", "b"),
+      Array("b", "a", "b"),
+      Array("b", "c"),
+      Array("b", "c", "a"),
+      Array("c"),
+      Array("c", "a", "a")
     )
   )
 
@@ -20,47 +24,51 @@ class SearchTrieSpec extends FlatSpec {
     )
   )
 
-  "SearchTrie" should "create correct encode words" in {
-    assert(trie.vocabulary.size == 2)
-    assert(trie.vocabulary("a") == 0)
-    assert(trie.vocabulary("b") == 1)
+  "SearchTrie" should "create a vocabulary from unique words in phrases" in {
+    assert(wikiTrie.vocabulary.size == 3)
+    assert(wikiTrie.vocabulary("a") == 0)
+    assert(wikiTrie.vocabulary("b") == 1)
+    assert(wikiTrie.vocabulary("c") == 2)
   }
 
   "SearchTrie" should "create correct number of nodes" in {
-    assert(trie.nodes.size == 8)
+    assert(wikiTrie.nodes.size == 11)
   }
 
-  "SearchTrie" should "correct fill nodes info" in {
-    val isLeaf = Seq(false, false, false, false, false, true, false, true)
-    val trieIsLeaf = trie.nodes.map(n => n._2)
+  "SearchTrie" should "correctly fill nodes info" in {
+    val isLeaf = Seq(false, true, true, false, false, true, true, true, true, false, true)
+    val trieIsLeaf = wikiTrie.nodes.map(n => n._2)
     assert(trieIsLeaf == isLeaf)
 
-    val length = Seq(0, 1, 2, 3, 4, 5, 2, 3)
-    val trieLength = trie.nodes.map(n => n._3)
+    val length = Seq(0, 1, 2, 1, 2, 3, 2, 3, 1, 2, 3)
+    val trieLength = wikiTrie.nodes.map(n => n._3)
     assert(trieLength == length)
 
-    val pi = Seq(0, 0, 0, 1, 2, 3, 1, 6)
-    val triePi = trie.nodes.map(n => n._1)
+    val pi = Seq(0, 0, 3, 0, 1, 2, 8, 9, 0, 1, 1)
+    val triePi = wikiTrie.nodes.map(n => n._1)
     assert(triePi == pi)
   }
 
-  "SearchTrie" should "correct search" in {
-    val text = "a b a a a b a b a a a a".split(" ")
-    val result = trie.search(text)
+  "SearchTrie" should "search correctly" in {
+    val text = "a a b a b c a".split(" ")
+    val result = wikiTrie.search(text)
 
-    assert(result == Seq((2, 4), (4, 8), (8, 10), (9, 11)))
+    val expectedResult = Seq(
+      (0, 0), (1, 1), (1, 2), (3, 3), (2, 4), (3, 4), (4, 5), (5, 5), (4, 6), (6, 6))
+
+    assert(result == expectedResult)
   }
 
-  "SearchTrie" should "correct handle out of vocabulary words" in {
-    val text = "a b a c a b a b a c a a a".split(" ")
-    val result = trie.search(text)
+  "SearchTrie" should "correctly handle out of vocabulary words" in {
+    val text = "d b e b a c b".split(" ")
+    val result = wikiTrie.search(text)
 
-    assert(result == Seq((4, 8), (10, 12)))
+    assert(result == Seq((4, 4), (5, 5)))
   }
 
   "SearchTrie" should "correctly calculate lastLeaf" in {
-    val lastLeafs = aTrie.nodes.map(n => n._4)
-    val expected = Seq(-1, -1, 1, 2, 3, 3)
+    val lastLeafs = wikiTrie.nodes.map(n => n._4)
+    val expected = Seq(-1, -1, -1, -1, 1, 2, 8, 1, -1, 1, 1)
 
     assert(lastLeafs == expected)
   }
@@ -69,7 +77,7 @@ class SearchTrieSpec extends FlatSpec {
     val text = "a a a a c a a a a a a".split(" ")
     val result = aTrie.search(text)
     val shouldFound =
-        Seq((0, 0), (1, 1), (2, 2), (3, 3), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)) ++
+      Seq((0, 0), (1, 1), (2, 2), (3, 3), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10)) ++
         Seq((0, 1), (1, 2), (2, 3), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10)) ++
         Seq((0, 2), (1, 3), (5, 7), (6, 8), (7, 9), (8, 10)) ++
         Seq((5, 9), (6, 10))
