d03b0751db0db9133509e7f716197e5d1c4b4dcb
ShOwY <eagmunoz@gmail.com>
2018-03-30 11:12:42 +0200

    Updated sparknlp.pretrained's module path in the test/annotators.py to pass tests

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -4,7 +4,7 @@ import re
 from sparknlp.annotator import *
 from sparknlp.base import *
 from test.util import SparkContextForTest
-from pretrained.pipeline.en import BasicPipeline
+from sparknlp.pretrained.pipeline.en import BasicPipeline
 
 
 class BasicAnnotatorsTestSpec(unittest.TestCase):
