fc768814da092f63e3fa9a8c96364586c9b9a535
danilojsl <danilo@johnsnowlabs.com>
2018-05-09 21:23:34 -0500

    Adding slang dictionary functionality to normalizer

--- src/main/scala/com/johnsnowlabs/nlp/annotators/Normalizer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/Normalizer.scala
@@ -1,23 +1,28 @@
 package com.johnsnowlabs.nlp.annotators
 
-import com.johnsnowlabs.nlp.{Annotation, AnnotatorModel}
+import com.johnsnowlabs.nlp.AnnotatorType.TOKEN
+import com.johnsnowlabs.nlp.annotators.param.ExternalResourceParam
+import com.johnsnowlabs.nlp.util.io.{ExternalResource, ReadAs, ResourceHelper}
+import com.johnsnowlabs.nlp.{Annotation, AnnotatorApproach, AnnotatorModel}
+import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.param.{BooleanParam, Param}
 import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
+import org.apache.spark.sql.Dataset
 
 /**
   * Annotator that cleans out tokens. Requires stems, hence tokens
   * @param uid required internal uid for saving annotator
   */
-class Normalizer(override val uid: String) extends AnnotatorModel[Normalizer] {
+class Normalizer(override val uid: String) extends AnnotatorApproach[NormalizerModel] {
 
-  import com.johnsnowlabs.nlp.AnnotatorType._
-
+  override val description: String = "Cleans out tokens"
   override val annotatorType: AnnotatorType = TOKEN
-
-  override val requiredAnnotatorTypes: Array[AnnotatorType] = Array(TOKEN)
+  /** Annotator reference id. Used to identify elements in metadata or to refer to this annotator type */
+  override val requiredAnnotatorTypes: Array[String] = Array(TOKEN)
 
   val pattern = new Param[String](this, "pattern", "normalization regex pattern which match will be replaced with a space")
   val lowercase = new BooleanParam(this, "lowercase", "whether to convert strings to lowercase")
+  val slangDictionary = new ExternalResourceParam(this, "slangDictionary", "delimited file with list of custom words to be manually corrected")
 
   setDefault(pattern, "[^\\pL+]")
   setDefault(lowercase, false)
@@ -30,27 +35,32 @@ class Normalizer(override val uid: String) extends AnnotatorModel[Normalizer] {
 
   def setLowercase(value: Boolean): this.type = set(lowercase, value)
 
+  def setSlangDictionary(value: ExternalResource): this.type = {
+    require(value.options.contains("delimiter"), "slang dictionary is a delimited text. needs 'delimiter' in options")
+    set(slangDictionary, value)
+  }
+
+  def setSlangDictionary(path: String,
+                         delimiter: String,
+                         readAs: ReadAs.Format = ReadAs.LINE_BY_LINE,
+                         options: Map[String, String] = Map("format" -> "text")): this.type =
+    set(slangDictionary, ExternalResource(path, readAs, options ++ Map("delimiter" -> delimiter)))
+
   def this() = this(Identifiable.randomUID("NORMALIZER"))
 
-  /** ToDo: Review implementation, Current implementation generates spaces between non-words, potentially breaking tokens*/
-  override def annotate(annotations: Seq[Annotation]): Seq[Annotation] =
-    annotations.map { token =>
-      val cased =
-        if ($(lowercase)) token.result.toLowerCase
-        else token.result
+  override def train(dataset: Dataset[_], recursivePipeline: Option[PipelineModel]): NormalizerModel = {
 
-      val nToken = cased
-        .replaceAll($(pattern), "")
-        .trim
-      Annotation(
-        annotatorType,
-        token.begin,
-        token.end,
-        nToken,
-        token.metadata
-      )
-    }.filter(_.result.nonEmpty)
+    val loadSlangs = if (get(slangDictionary).isDefined)
+      ResourceHelper.parseKeyValueText($(slangDictionary))
+    else
+      Map.empty[String, String]
 
+    new NormalizerModel()
+      .setPattern($(pattern))
+      .setLowerCase($(lowercase))
+      .setSlangDict(loadSlangs)
+  }
+  _
 }
 
 object Normalizer extends DefaultParamsReadable[Normalizer]
