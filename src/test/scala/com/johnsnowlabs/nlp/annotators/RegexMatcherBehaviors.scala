05ee41f3063173264529219e7abc79b047d9da2b
Saif Addin <saif@johnsnowlabs.com>
2018-05-10 13:46:44 -0300

    RegexMatcher includes identifier

--- src/test/scala/com/johnsnowlabs/nlp/annotators/RegexMatcherBehaviors.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/RegexMatcherBehaviors.scala
@@ -8,7 +8,7 @@ import scala.language.reflectiveCalls
 
 trait RegexMatcherBehaviors { this: FlatSpec =>
   def fixture(dataset: Dataset[Row], rules: Array[(String, String)], strategy: String) = new {
-    val annotationDataset: Dataset[_] = AnnotatorBuilder.withRegexMatcher(dataset, rules, strategy)
+    val annotationDataset: Dataset[_] = AnnotatorBuilder.withRegexMatcher(dataset, strategy)
     val regexAnnotations: Array[Annotation] = annotationDataset.select("regex")
       .collect
       .flatMap { _.getSeq[Row](0) }
@@ -17,33 +17,14 @@ trait RegexMatcherBehaviors { this: FlatSpec =>
     annotationDataset.show()
   }
 
-  def predefinedRulesRegexMatcher(dataset: => Dataset[Row], strategy: String): Unit = {
-    val rules = Array.empty[(String, String)]
-    "A RegexMatcher Annotator with predefined rules" should s"successfuly match" in {
-      val f = fixture(dataset, rules, strategy)
-      f.regexAnnotations.foreach { a =>
-        assert(a.metadata.toArray.isEmpty)
-      }
-    }
-
-    it should "create annotations" in {
-      val f = fixture(dataset, rules, strategy)
-      assert(f.regexAnnotations.nonEmpty)
-    }
-
-    it should "create annotations with the correct tag" in {
-      val f = fixture(dataset, rules, strategy)
-      f.regexAnnotations.foreach { a =>
-        assert(a.annotatorType == REGEX)
-      }
-    }
-  }
-
   def customizedRulesRegexMatcher(dataset: => Dataset[Row], rules: Array[(String, String)], strategy: String): Unit = {
     "A RegexMatcher Annotator with custom rules" should s"successfuly match ${rules.map(_._1).mkString(",")}" in {
       val f = fixture(dataset, rules, strategy)
       f.regexAnnotations.foreach { a =>
-        assert(a.metadata.toArray.isEmpty)
+        assert(a.metadata.values.forall(m => {
+          println(m)
+          Seq("followed by 'the'", "ceremony").contains(m)
+        }))
       }
     }
 
