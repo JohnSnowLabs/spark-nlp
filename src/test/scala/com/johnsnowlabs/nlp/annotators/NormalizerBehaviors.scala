8c542cf4b7f50a23874263679eced6ee6a6b2f2c
danilojsl <danilo@johnsnowlabs.com>
2018-05-14 06:01:36 -0500

    Minor changes

--- src/test/scala/com/johnsnowlabs/nlp/annotators/NormalizerBehaviors.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/NormalizerBehaviors.scala
@@ -137,24 +137,11 @@ trait NormalizerBehaviors { this: FlatSpec =>
       val data = Seq("gr8").toDS.toDF("token")
       data.show()
       //val pretrainedPipeline = BasicPipeline().pretrained() //download from S3, thus it has outdated version
-      //val pdata = pretrainedPipeline.transform(data)
-      //pdata.show()
-      val homePath = "HOMEPATH/spark-nlp-models/models/"
-      //val lemmaModel = LemmatizerModel.load(homePath+"lemma_fast_en_1.5.3_2_1526218239169")
+      val homePath = "/Users/dburbano/IdeaProjects/spark-nlp-models/models/"
       val normModel = NormalizerModel.load(homePath+"norm_fast_en_1.5.3_2_1526218675158")
 
-      //val lemmaModel = PipelineModel.read.load(homePath+"lemma_fast_en_1.5.3_2_1526213726756/")
-      //val pdata = lemmaModel.transform(data)
-      //pdata.show()
-      //println("Lemmatizer Model")
-      //lemmaModel.transform(data).show()
       println("Normalizer Model")
       normModel.transform(data).show()
-      println("Done")
-      /*val tempNormalizer = normalizer.fit(pdata)
-      tempNormalizer.write.overwrite.save("./tmp_symspell")
-      val modelNormalizer = NormalizerModel.load("./tmp_symspell")
-      modelNormalizer.transform(pdata).show(5)*/
 
     }
   }
