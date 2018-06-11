8a0dd2a69c2cb1f90d2a4d5af5fc3aa5f99f991b
danilojsl <danilo@johnsnowlabs.com>
2018-06-11 17:38:33 -0500

    Refactoring Chunker: - Added multiple regex parsers support

--- src/main/scala/com/johnsnowlabs/nlp/annotators/Chunker.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/Chunker.scala
@@ -2,27 +2,37 @@ package com.johnsnowlabs.nlp.annotators
 
 import scala.util.matching.Regex
 import com.johnsnowlabs.nlp.{Annotation, AnnotatorModel, ParamsAndFeaturesReadable}
-import org.apache.spark.ml.param.Param
+import org.apache.spark.ml.param.{Param, StringArrayParam}
 import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
 
 class Chunker(override val uid: String) extends AnnotatorModel[Chunker] {
 
   import com.johnsnowlabs.nlp.AnnotatorType._
 
-  val regexParser: Param[String] = new Param(this, "regexParser",
-                                        "A grammar based chunk parser")
+  val regexParsers = new StringArrayParam(this, "regexParsers", "An array of grammar based chunk parsers")
 
   override val annotatorType: AnnotatorType = DOCUMENT
   override val requiredAnnotatorTypes: Array[AnnotatorType] = Array(POS)
 
-  def setRegexParser(value: String): Chunker = set(regexParser, value)
-  def getRegexParser: String = $(regexParser)
+  def setRegexParser(value: Array[String]): Chunker = set(regexParsers, value)
+  def getRegexParser: Array[String] = $(regexParsers)
 
   def this() = this(Identifiable.randomUID("CHUNKER"))
 
+  private lazy val replacements = Map( "<" -> "(<", ">" -> ">)", "|" -> ">)|(<")
 
-  def patternMatchIndexes(pattern: Regex, text: String): List[(Int, Int)] =
+  private lazy val POSTagPatterns: Array[Regex] = {
+    getRegexParser.map(regexParser => replaceRegexParser(regexParser))
+  }
+
+  def replaceRegexParser(regexParser: String): Regex = {
+    replacements.foldLeft(regexParser)((accumulatedParser, keyValueReplace) =>
+      accumulatedParser.replaceAllLiterally(keyValueReplace._1, keyValueReplace._2)).r
+  }
+
+  def patternMatchIndexes(pattern: Regex, text: String): List[(Int, Int)] = {
     pattern.findAllMatchIn(text).map(index => (index.start, index.end )).toList
+  }
 
   def patternMatchFirstIndex(pattern: Regex, text: String): List[Int] =
     pattern.findAllMatchIn(text).map(_.start).toList
@@ -39,22 +49,26 @@ class Chunker(override val uid: String) extends AnnotatorModel[Chunker] {
     annotation
   }
 
-  private lazy val POSTagPattern: Regex = {
-    val replacements = Map( "<" -> "(<", ">" -> ">)", "|" -> ">)|(<")
-    replacements.foldLeft($(regexParser))((accumulatedParser, keyValueReplace) =>
-      accumulatedParser.replaceAllLiterally(keyValueReplace._1, keyValueReplace._2)).r
+  def getChunkPhrases(POSTagPattern: Regex, POSFormatSentence: String, annotations: Seq[Annotation]):
+  Option[Array[Seq[Annotation]]] = {
+    val rangeMatches = patternMatchIndexes(POSTagPattern, POSFormatSentence)
+    if (rangeMatches.isEmpty){
+      None
+    }
+    val indexLeftTags = patternMatchFirstIndex("<".r, POSFormatSentence)
+    val indexRightTags = patternMatchFirstIndex(">".r, POSFormatSentence)
+    val indexTags = indexLeftTags zip indexRightTags //merge two sequential collections
+    val indexAnnotations = rangeMatches.map(range => getIndexAnnotation(range, indexTags))
+    Some(indexAnnotations.map(indexAnnotation => getPhrase(indexAnnotation, annotations)).toArray)
   }
 
   override def annotate(annotations: Seq[Annotation]): Seq[Annotation] = {
 
     val POSFormatSentence = annotations.map(annotation => "<"+annotation.result+">")
                                         .mkString(" ").replaceAll("\\s","")
-    val rangeMatches = patternMatchIndexes(POSTagPattern, POSFormatSentence)
-    val indexLeftTags = patternMatchFirstIndex("<".r, POSFormatSentence)
-    val indexRightTags = patternMatchFirstIndex(">".r, POSFormatSentence)
-    val indexTags = indexLeftTags zip indexRightTags //merge two sequential collections
-    val indexAnnotations = rangeMatches.map(range => getIndexAnnotation(range, indexTags))
-    val chunkPhrases = indexAnnotations.map(indexAnnotation => getPhrase(indexAnnotation, annotations)).toArray
+
+    val chunkPhrases = POSTagPatterns.flatMap(POSTagPattern =>
+      getChunkPhrases(POSTagPattern, POSFormatSentence, annotations)).flatten
 
     val chunkAnnotations = chunkPhrases.zipWithIndex.map{ case (phrase, index) =>
       val result = phrase.map(annotation => annotation.metadata.head._2).mkString(" ")
