8dca9a89b54956911ef4793f34ba8ad954e372c1
Saif Addin <saif@johnsnowlabs.com>
2018-06-20 19:29:11 -0300

    Updated Light Pipeline, content trimming, merged tensorflow reader improvements

--- src/test/scala/com/johnsnowlabs/example/OcrExample.scala
+++ src/test/scala/com/johnsnowlabs/example/OcrExample.scala
@@ -1,8 +1,7 @@
 package com.johnsnowlabs.example
 
-import com.johnsnowlabs.nlp.SparkAccessor
+import com.johnsnowlabs.nlp.{OcrAssembler, SparkAccessor}
 import com.johnsnowlabs.nlp.base.Finisher
-import ocr.tesseract.OcrAnnotator
 import org.apache.spark.ml.Pipeline
 
 object OcrExample extends App {
@@ -10,7 +9,7 @@ object OcrExample extends App {
   val spark = SparkAccessor.spark
   import spark.implicits._
 
-  val ocrAnnotator = new OcrAnnotator().
+  val ocrAnnotator = new OcrAssembler().
     setInputPath("./files"). // these are your files, possibly in HDFS
     //setPageSegmentationMode(3). // this causes different regions on each page be treated separately
     setOutputCol("text_regions") // this is where annotations end up being
