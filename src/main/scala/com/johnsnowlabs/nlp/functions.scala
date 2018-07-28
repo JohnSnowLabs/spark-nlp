df1a0afdc40b318c49adf38bbdc7c94bc98fe419
Alberto <albertoandreotti@gmail.com>
2018-07-28 11:28:11 -0300

    added metadata

--- src/main/scala/com/johnsnowlabs/nlp/functions.scala
+++ src/main/scala/com/johnsnowlabs/nlp/functions.scala
@@ -7,20 +7,25 @@ import scala.reflect.runtime.universe._
 
 object functions {
 
+
+
   def filterByAnnotations(dataset: DataFrame, column: String, function: Seq[Annotation] => Boolean): DataFrame = {
+    val meta = dataset.schema(column).metadata
     val func = udf {
       annotatorProperties: Seq[Row] =>
         function(annotatorProperties.map(Annotation(_)))
     }
-    dataset.filter(func(col(column)))
+    dataset.filter(func(col(column)).as(column, meta))
   }
 
   def mapAnnotations[T: TypeTag](dataset: DataFrame, column: String, outputCol: String, function: Seq[Annotation] => T): DataFrame = {
+    val meta = dataset.schema(column).metadata
     val func = udf {
       annotatorProperties: Seq[Row] =>
         function(annotatorProperties.map(Annotation(_)))
     }
-    dataset.withColumn(outputCol, func(col(column)))
+    dataset.withColumn(outputCol, func(col(column)).as(outputCol, meta))
   }
+
 
 }
