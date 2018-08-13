ddf0be50787319b6287eceb71bc184f8f5aab04b
Saif Addin <saif@johnsnowlabs.com>
2018-08-13 16:14:55 -0300

    Updated performance slightly on spell checkers

--- src/main/scala/com/johnsnowlabs/util/Benchmark.scala
+++ src/main/scala/com/johnsnowlabs/util/Benchmark.scala
@@ -1,23 +1,28 @@
 package com.johnsnowlabs.util
 
 object Benchmark {
-  def time[R](description: String)(block: => R): R = {
+
+  private var print = true
+
+  def setPrint(v: Boolean) = print = v
+  def getPrint: Boolean = print
+
+  def time[R](description: String, forcePrint: Boolean = false)(block: => R): R = {
     val t0 = System.nanoTime()
     val result = block
     val t1 = System.nanoTime()
-    println(description + ": " + ((t1 - t0)/1000000000.0) + "sec")
+    if (print || forcePrint) println(description + ": " + ((t1 - t0)/1000000000.0) + "sec")
     result
   }
 
-  def measure(iterations: Integer = 3, msg: Boolean = true, description: String = "Took")(f: => Any): Double = {
+  def measure(iterations: Integer = 3, forcePrint: Boolean = false, description: String = "Took")(f: => Any): Double = {
     val time = (0 until iterations).map { _ =>
       val t0 = System.nanoTime()
       f
       System.nanoTime() - t0
     }.sum.toDouble / iterations
 
-    if (msg)
-      println(s"$description (Avg for $iterations iterations): ${time / 1000000000} sec")
+    if (print || forcePrint) println(s"$description (Avg for $iterations iterations): ${time / 1000000000} sec")
 
     time
   }
