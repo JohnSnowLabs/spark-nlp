9220d948dacab54c7b19c244ea0c617aee2bee14
Alberto <albertoandreotti@gmail.com>
2018-10-03 14:51:24 -0300

    wip-ocr spell checker

--- python/sparknlp/spellchecker/tests.py
+++ python/sparknlp/spellchecker/tests.py
@@ -1,4 +1,4 @@
-from data_prepare import ComplexToken
+from data_prepare import ComplexToken, Percentage, SuffixedToken
 
 # test complex token handling
 ct = ComplexToken(set(['ac', 'dc', '/', '-', '110v']))
@@ -6,3 +6,15 @@ assert(ct.belongs('ac/dc'))
 assert(ct.belongs('ac-dc'))
 
 assert(ct.parse('ac/dc-110v') == ['ac', '/', 'dc', '-', '110v'])
+
+# test percentage handling
+per = Percentage()
+assert(per.belongs('55%'))
+assert(per.belongs('55.5%'))
+assert(per.belongs('55.15%'))
+
+# test suffixes handling
+suff = SuffixedToken()
+assert(suff.belongs('Patient:'))
+assert(suff.belongs('12,'))
+assert(suff.get_rep('pain"') == ['pain', '"'])
--- src/main/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/OcrSpellCheck.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/OcrSpellCheck.scala
@@ -1,21 +1,48 @@
 package com.johnsnowlabs.nlp.annotators.spell.ocr
 
 import com.johnsnowlabs.ml.tensorflow.TensorflowWrapper
+import com.johnsnowlabs.nlp.annotators.spell.ocr.parser._
 import com.johnsnowlabs.nlp.{AnnotatorApproach, AnnotatorType}
 import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.Dataset
 import org.tensorflow.{Graph, Session}
 
+import scala.collection.mutable.HashSet
+
 class OcrSpellCheckApproach(override val uid: String) extends AnnotatorApproach[OcrSpellCheckModel]{
   override val description: String = "Ocr specific Spell Checking"
 
   override def train(dataset: Dataset[_], recursivePipeline: Option[PipelineModel]): OcrSpellCheckModel = {
 
+    // TODO: finish loading the language model
     val graph = new Graph()
     val config = Array[Byte](56, 1)
     val session = new Session(graph, config)
     val tf = new TensorflowWrapper(session, graph)
+
+    // extract vocabulary
+    val rawText = "../auxdata/spell_dataset/vocab/spell_corpus.txt" //TODO:should go to param
+    val source = scala.io.Source.fromFile(rawText)
+
+    val vocab = HashSet[String]()
+    val firstPass = Seq(SuffixedToken, RoundBrackets, DoubleQuotes)
+
+    source.getLines.foreach { line =>
+
+      // first pass separate things
+      var tmp = line
+      firstPass.foreach{parser =>
+        tmp = parser.separate(tmp)
+      }
+
+      // second pass identify things, and replace
+      tmp.split(" ").filter(_!=" ").foreach { token =>
+        vocab += NumberToken.separate(DateToken.separate(token))
+      }
+    }
+
+    // Create Levenshtein Automata
 
     // TODO: replace hard coded stuff for development only
     new OcrSpellCheckModel().
