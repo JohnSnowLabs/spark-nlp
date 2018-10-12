dbd786ee0e6fea11d106fc1e769635ba2d81d374
Saif Addin <saif@johnsnowlabs.com>
2018-10-12 09:42:07 -0300

    Lazy embeddings unification. clusterEmbeddings trait. WORK IN PROGRESS.

--- src/main/scala/com/johnsnowlabs/nlp/ModelWithWordEmbeddings.scala
+++ src/main/scala/com/johnsnowlabs/nlp/ModelWithWordEmbeddings.scala
@@ -1,9 +1,11 @@
 package com.johnsnowlabs.nlp
 
-import java.io.{File, FileNotFoundException}
+import java.io.File
 import java.nio.file.{Files, Paths}
 
 import com.johnsnowlabs.nlp.embeddings._
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.SparkFiles
 import org.apache.spark.ml.param.Param
 import org.apache.spark.sql.SparkSession
 
@@ -20,17 +22,25 @@ trait ModelWithWordEmbeddings extends HasLazyEmbeddings with ParamsAndFeaturesWr
 
   def setIndexPath(path: String): this.type = set(this.indexPath, path)
 
+  private def getEmbeddingsSerializedPath(path: String): Path =
+    Path.mergePaths(new Path(path), new Path("/embeddings"))
+
+  private def updateAvailableEmbeddings: Unit = {
+    val currentEmbeddings = clusterEmbeddings
+      .orElse(get(includedEmbeddingsRef).flatMap(EmbeddingsHelper.embeddingsCache.get))
+      .orElse(get(indexPath).map(new SparkWordEmbeddings(_, $(embeddingsDim), $(caseSensitiveEmbeddings))))
+      .getOrElse(throw new NoSuchElementException(
+        s"Word embeddings missing. " +
+          s"Not in cache ${get(includedEmbeddingsRef).getOrElse("")} " +
+          s"or index path not found ${get(indexPath).getOrElse("")}")
+      )
+
+    setEmbeddingsIfFNotSet(currentEmbeddings)
+  }
+
   def embeddings: WordEmbeddings = {
-    if (clusterEmbeddings.isEmpty) {
-      if ($(includeEmbeddings) && isDefined(indexPath)) {
-        clusterEmbeddings = Some(new SparkWordEmbeddings($(indexPath), $(embeddingsDim), $(caseSensitiveEmbeddings)))
-        clusterEmbeddings.get.wordEmbeddings
-      } else if (isDefined(includedEmbeddingsRef)) {
-        clusterEmbeddings = Some(EmbeddingsHelper.embeddingsCache($(includedEmbeddingsRef)))
-        clusterEmbeddings.get.wordEmbeddings
-      } else throw new FileNotFoundException("Word embeddings missing. Not provided by source, index or instance")
-    } else
-      clusterEmbeddings.get.wordEmbeddings
+    updateAvailableEmbeddings
+    getEmbeddings.wordEmbeddings
   }
 
   def moveFolderFiles(folderSrc: String, folderDst: String): Unit = {
@@ -43,23 +53,33 @@ trait ModelWithWordEmbeddings extends HasLazyEmbeddings with ParamsAndFeaturesWr
 
   def deserializeEmbeddings(path: String, spark: SparkSession): Unit = {
     val src = EmbeddingsHelper.getEmbeddingsSerializedPath(path)
-    val embeddings: SparkWordEmbeddings = if ($(includeEmbeddings)) {
-      EmbeddingsHelper.loadEmbeddings(
-        src.toUri.toString,
-        spark,
-        WordEmbeddingsFormat.SPARKNLP,
-        $(embeddingsDim),
-        $(caseSensitiveEmbeddings))
-    } else if (isDefined(includedEmbeddingsRef)) {
-      EmbeddingsHelper.embeddingsCache.getOrElse($(includedEmbeddingsRef), throw new IndexOutOfBoundsException("EmbeddingsRef not found in cache"))
-    } else throw new IllegalArgumentException("Embeddings not found. Either they were not included or embeddingsRef not provided")
+    val embeddings: Option[SparkWordEmbeddings] =
+      get(includedEmbeddingsRef)
+        .flatMap(EmbeddingsHelper.embeddingsCache.get)
+        .orElse(EmbeddingsHelper.loadEmbeddings(
+          src.toUri.toString,
+          spark,
+          WordEmbeddingsFormat.SPARKNLP,
+          $(embeddingsDim),
+          $(caseSensitiveEmbeddings)))
 
-    setEmbeddings(embeddings)
+    embeddings.foreach(e => {
+      setIndexPath(e.clusterFilePath)
+      setEmbeddings(e)
+    })
   }
 
   def serializeEmbeddings(path: String, spark: SparkSession): Unit = {
     if ($(includeEmbeddings) && isDefined(indexPath)) {
-      EmbeddingsHelper.saveEmbeddings(path, spark, $(indexPath))
+      updateAvailableEmbeddings
+
+      val index = new Path(SparkFiles.get(getEmbeddings.clusterFilePath))
+
+      val uri = new java.net.URI(path)
+      val fs = FileSystem.get(uri, spark.sparkContext.hadoopConfiguration)
+      val dst = getEmbeddingsSerializedPath(path)
+
+      EmbeddingsHelper.saveEmbeddings(fs, index, dst)
     }
   }
 
