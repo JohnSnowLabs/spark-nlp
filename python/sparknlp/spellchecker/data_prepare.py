070b4c7473e28bcc5ef8d906d40f61e51c61abfb
Alberto <albertoandreotti@gmail.com>
2018-10-17 10:44:46 -0300

    fixed paths

--- python/sparknlp/spellchecker/data_prepare.py
+++ python/sparknlp/spellchecker/data_prepare.py
@@ -26,18 +26,18 @@ def gen_vocab(file_name):
             vocab_file.write(word + "\n")
 
 
-def gen_id_seqs(file_path):
+def gen_id_seqs(file_path, vocab):
 
     def word_to_id(word, word_dict):
         id = word_dict.get(word)
         return id if id is not None else word_dict.get("_UNK_")
 
-    with open("data/vocab", "r") as vocab_file:
+    with open(vocab, "r") as vocab_file:
         lines = [line.strip() for line in vocab_file.readlines()]
         word_dict = dict([(b, a) for (a, b) in enumerate(lines)])
 
     with open(file_path, "r") as raw_file:
-        with open("data/"+file_path.split("/")[-1]+".ids", "w") as current_file:
+        with open(file_path+".ids", "w") as current_file:
             for line in raw_file.readlines():
                 line = [word_to_id(word, word_dict) for word in line.strip().replace("<unk>", "_UNK_").split()]
                 # each sentence has the start and the end.
--- python/sparknlp/spellchecker/run.py
+++ python/sparknlp/spellchecker/run.py
@@ -16,33 +16,39 @@ TRAIN = True
 # are testing.
 VERBOSE = True
 
-# To indicate your test corpus
-test_file = "./gap_filling_exercise/gap_filling_exercise"
+# this is the path where all data files live
+data_path= '../../../../auxdata/spell_dataset/vocab/'
 
+# To indicate your test/train corpora
+test_file = "gap_filling_exercise"
+train_file = "spell_corpus_400.ids"
+valid_file = "valid.ids"
 #if not os.path.isfile("../../../../auxdata/spell_dataset/vocab/spell_corpus.txt.ids"):
 #    gen_vocab("ptb/train")
 #if not os.path.isfile("data/train.ids"):
 #    gen_id_seqs("ptb/train")
 #    gen_id_seqs("ptb/valid")
 
-with open("../../../../auxdata/spell_dataset/vocab/spell_corpus.txt.ids") as fp:
+
+with open(data_path + train_file) as fp:
     num_train_samples = len(fp.readlines())
-with open("../../../../auxdata/spell_dataset/vocab/valid.ids") as fp:
+with open(data_path + "valid.ids") as fp:
     num_valid_samples = len(fp.readlines())
 
-with open("../../../../auxdata/spell_dataset/vocab/vocab") as vocab:
+vocab_path = data_path + "vocab"
+with open(vocab_path) as vocab:
     vocab_size = len(vocab.readlines())
 
 def create_model(sess):
     model = RNNLM(vocab_size=vocab_size,
-                  batch_size=1,
-                  num_epochs=6,
+                  batch_size=4,
+                  num_epochs=1,
                   check_point_step=100,
                   num_train_samples=num_train_samples,
                   num_valid_samples=num_valid_samples,
-                  num_layers=2,
+                  num_layers=1,
                   num_hidden_units=100,
-                  initial_learning_rate=.6,
+                  initial_learning_rate=.7,
                   final_learning_rate=0.0005,
                   max_gradient_norm=5.0,
                   )
@@ -54,7 +60,9 @@ if TRAIN:
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=True, device_count = {'GPU': 0})) as sess:
         model = create_model(sess)
         saver = tf.train.Saver()
-        model.batch_train(sess, saver)
+        train_ids = data_path + train_file
+        valid_ids = data_path + valid_file
+        model.batch_train(sess, saver, train_ids, valid_ids)
 
 tf.reset_default_graph()
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.99)
@@ -67,8 +75,8 @@ with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
 
     model.save('bundle', sess)
 
-    predict_id_file = os.path.join("data", test_file.split("/")[-1]+".ids")
+    predict_id_file = os.path.join(data_path, test_file.split("/")[-1]+".ids")
     if not os.path.isfile(predict_id_file):
-        gen_id_seqs(test_file)
-    model.predict(sess, predict_id_file, test_file, verbose=VERBOSE)
+        gen_id_seqs(data_path + test_file, vocab_path)
+    model.predict(sess, predict_id_file, data_path + test_file, verbose=VERBOSE)
 
