f29887fff48bcbdabd6c05902d38e6ee23ea1c60
danilojsl <danilo@johnsnowlabs.com>
2018-10-23 21:20:14 -0300

    Refactoring Dependency Parser

--- src/test/scala/com/johnsnowlabs/nlp/AnnotatorBuilder.scala
+++ src/test/scala/com/johnsnowlabs/nlp/AnnotatorBuilder.scala
@@ -161,6 +161,8 @@ object AnnotatorBuilder extends FlatSpec { this: Suite =>
     new DependencyParserApproach()
       .setInputCols(Array("sentence", "pos", "token"))
       .setOutputCol("dependency")
+      //.setDependencyTreeBank("/Users/dburbano/tmp/dependency_treebank_small")
+      .setDependencyTreeBank("/src/test/resources/parser/dependency_treebank")
       .fit(df)
       .transform(df)
   }
