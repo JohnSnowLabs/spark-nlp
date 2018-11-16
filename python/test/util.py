bb09788071d66c851fd7654e783e668b7984ebcc
danilojsl <danilo@johnsnowlabs.com>
2018-11-16 20:18:42 -0300

    Python Unit Tests for Dependency Parser and Typed Dependency Parser

--- python/test/util.py
+++ python/test/util.py
@@ -7,43 +7,27 @@ class SparkContextForTest:
     spark = SparkSession.builder \
         .master("local[4]") \
         .config("spark.jars", 'lib/sparknlp.jar,lib/sparknlp-ocr.jar') \
-        .getOrCreate()
-    data = spark. \
-        read \
-        .parquet("file:///" + os.getcwd() + "/../../src/test/resources/sentiment.parquet") \
-        .limit(100)
-    data.cache()
-    data.count()
-
-
-class SparkContextForNER:
-    spark = SparkSession.builder \
-        .master("local[4]") \
-        .config("spark.jars", 'lib/sparknlp.jar') \
-        .getOrCreate()
-    data = spark. \
-        read \
-        .csv("file:///" + os.getcwd() + "/../../src/test/resources/ner-corpus/icdtest.txt") \
-        .limit(100)
-    data.cache()
-    data.count()
-
-
-class SparkContextForTDP:
-    spark = SparkSession.builder \
-        .master("local[1]") \
         .config("spark.driver.memory", "4G") \
-        .config("spark.driver.maxResultSize", "1G") \
-        .config("spark.executor.memory", "4G") \
-        .config("spark.driver.extraJavaOptions", "-Xms1G -Xss1M") \
-        .config("spark.executor.extraJavaOptions", "-Xms1G -Xss1M") \
         .getOrCreate()
 
-    data = spark.sparkContext.parallelize([["I saw a girl with a telescope"]]).toDF().toDF("text")
 
-# --conf spark.driver.extraJavaOptions="-Xss10m -XX:MaxPermSize=1024M "
-# --conf spark.executor.extraJavaOptions="-Xss10m -XX:MaxPermSize=512M"
-# .config("spark.driver.extraJavaOptions=",
-#         "-Xms1G -Xmx4G -Xss1M -XX:+CMSClassUnloadingEnabled") \
-# .config("spark.executor.extraJavaOptions=",
-#         "-Xms1G -Xmx4G -Xss1M -XX:+CMSClassUnloadingEnabled") \
+class DataForTest:
+
+    data = SparkContextForTest.spark. \
+        read \
+        .parquet("file:///" + os.getcwd() + "/../src/test/resources/sentiment.parquet") \
+        .limit(100)
+    data.cache()
+    data.count()
+
+    data_ner = SparkContextForTest.spark. \
+        read \
+        .csv("file:///" + os.getcwd() + "/../src/test/resources/ner-corpus/icdtest.txt") \
+        .limit(100)
+    data.cache()
+    data.count()
+
+    data_tdp = SparkContextForTest.spark.\
+        sparkContext.parallelize([["I saw a girl with a telescope"]]).toDF().toDF("text")
+
+
