7fe25011005239adb8a831f7d26a82c94a2d880f
danilojsl <danilo@johnsnowlabs.com>
2018-11-22 06:33:59 -0300

    Comments spark.driver.memory for Travis

--- python/test/util.py
+++ python/test/util.py
@@ -7,12 +7,11 @@ class SparkContextForTest:
     spark = SparkSession.builder \
         .master("local[4]") \
         .config("spark.jars", 'lib/sparknlp.jar,lib/sparknlp-ocr.jar') \
-        .config("spark.driver.memory", "4G") \
         .getOrCreate()
+    # .config("spark.driver.memory", "3500M") \
 
 
 class DataForTest:
-
     data = SparkContextForTest.spark. \
         read \
         .parquet("file:///" + os.getcwd() + "/../src/test/resources/sentiment.parquet") \
@@ -27,7 +26,5 @@ class DataForTest:
     data.cache()
     data.count()
 
-    data_tdp = SparkContextForTest.spark.\
+    data_tdp = SparkContextForTest.spark. \
         sparkContext.parallelize([["I saw a girl with a telescope"]]).toDF().toDF("text")
-
-
