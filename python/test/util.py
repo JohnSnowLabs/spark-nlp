083dea0c608f6a9a325b1a15a849bf63c925c55a
danilojsl <danilo@johnsnowlabs.com>
2018-11-22 21:03:06 -0300

    Adds spark.driver.memory

--- python/test/util.py
+++ python/test/util.py
@@ -7,6 +7,7 @@ class SparkContextForTest:
     spark = SparkSession.builder \
         .master("local[4]") \
         .config("spark.jars", 'lib/sparknlp.jar,lib/sparknlp-ocr.jar') \
+        .config("spark.driver.memory", "3500M") \
         .getOrCreate()
     data = spark. \
         read \
