d62f99c7da6e4063f2ce7cf89a01e2d4ec564810
Alberto <albertoandreotti@gmail.com>
2018-11-23 00:57:07 -0300

    wip

--- src/test/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/MeasureHP.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/MeasureHP.scala
@@ -1,5 +1,60 @@
 package com.johnsnowlabs.nlp.annotators.spell.ocr
 
-class MeasureHP {
+import com.johnsnowlabs.nlp.annotators.spell.ocr.parser.{DateToken, NumberToken}
+import com.johnsnowlabs.nlp.{Annotation, SparkAccessor}
+
+object MeasureHP extends App with OcrTestData {
+  override val correctFile = "/home/jose/auxdata/spell_dataset/correct_text.txt"
+  override val rawFile = "/home/jose/auxdata/spell_dataset/raw_text.txt"
+
+  val (correct, raw) = loadDataSets()
+
+  val trainCorpusPath = "../auxdata/spell_dataset/vocab/bigone.txt"
+
+  import SparkAccessor.spark.implicits._
+  val ocrspellModel = new OcrSpellCheckApproach().
+    setInputCols("text").
+    setTrainCorpusPath(trainCorpusPath).
+    setSpecialClasses(List(DateToken, NumberToken, AgeToken, UnitToken, MedicationClass)).
+    fit(Seq.empty[String].toDF("text"))
+
+  val time = System.nanoTime()
+  val corrected = raw.map { tokens =>
+    ocrspellModel.annotate(Seq(
+      Annotation(tokens.mkString(" ")))).head.result.split(" ")
+  }
+  print(s"Done, ${(System.nanoTime() - time) / 1e9}")
+
+  var destroyed = 0
+  var totalErrorFree = 0
+  var succeeded = 0
+  var totalErroneous = 0
+
+  (correct, raw, corrected).zipped.foreach { case (c, r, cd) =>
+    (c, r, cd).zipped.foreach{ case (correctWord, rawWord, correctedWord) =>
+
+      // the corrected word was ok at the output, and it was misspelled
+      if (correctedWord.equals(correctWord) && !correctWord.equals(rawWord))
+          succeeded += 1
+
+      // we received a good word, destroyed it
+      if (!correctedWord.equals(correctWord) && correctWord.equals(rawWord))
+          destroyed += 1
+
+      if (correctWord.equals(rawWord))
+        totalErrorFree += 1
+
+      if (!correctWord.equals(rawWord))
+        totalErroneous += 1
+
+      // words that we couldn't correct
+      if(!correctedWord.equals(correctWord))
+        println(rawWord, correctedWord, correctWord)
+    }
+  }
+
+  println(s"accuracy % : ${(succeeded + totalErrorFree - destroyed).toDouble / (totalErroneous + totalErrorFree)}")
+  println(s"total % of good corrections: ${succeeded.toFloat / totalErroneous}")
+  println(s"total % of destructive corrections: ${destroyed.toFloat / totalErrorFree}")
 
 }
--- src/test/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/OcrSpellCheckerTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/spell/ocr/OcrSpellCheckerTestSpec.scala
@@ -1,16 +1,55 @@
 package com.johnsnowlabs.nlp.annotators.spell.ocr
+import com.github.liblevenshtein.transducer.{Candidate, ITransducer}
+import com.johnsnowlabs.nlp.annotators.spell.ocr.parser._
 import com.johnsnowlabs.nlp.{Annotation, SparkAccessor}
 import org.scalatest._
 
+//import scala.collection.mutable.Map
+
+
+object MedicationClass extends VocabParser {
+
+  override val vocab = loadCSV("meds_wcase.txt")
+
+  override val transducer: ITransducer[Candidate] = generateTransducer
+
+  override val label: String = "_MED_"
+
+}
+
+object AgeToken extends RegexParser {
+
+  override val regex: String = "1?[0-9]{0,2}-(year|month|day)(s)?(-old)?"
+
+  override val transducer: ITransducer[Candidate] = generateTransducer
+
+  override val label: String = "_AGE_"
+
+}
+
+
+object UnitToken extends VocabParser {
+
+  override val vocab: Set[String] = Set("MG=", "MEQ=", "TAB",
+    "tablet", "mmHg", "TMIN", "TMAX", "mg/dL", "MMOL/L", "mmol/l", "mEq/L", "mmol/L",
+    "mg", "ml", "mL", "mcg", "mcg/", "gram", "unit", "units", "DROP", "intl", "KG")
+
+  override val transducer: ITransducer[Candidate] = generateTransducer
+
+  override val label: String = "_UNIT_"
+
+}
+
 
 class OcrSpellCheckerTestSpec extends FlatSpec {
+
+  /* TODO  Note some test cases should be moved to internal repo */
 
   trait Scope extends TokenClasses {
     weights += ('l' -> Map('1' -> 0.5f, '!' -> 0.2f), 'P' -> Map('F' -> 0.2f))
   }
 
-  // TODO: come back to this when scoring the final list with weighted distance
-  "weighted Levenshtein distance" should "produce weighted results" ignore new Scope {
+  "weighted Levenshtein distance" should "produce weighted results" in new Scope {
     assert(wLevenshteinDist("c1ean", "clean") > wLevenshteinDist("c!ean", "clean"))
     assert(wLevenshteinDist("crean", "clean") > wLevenshteinDist("c!ean", "clean"))
     assert(wLevenshteinDist("Fatient", "Patient") < wLevenshteinDist("Aatient", "Patient"))
@@ -20,28 +59,52 @@ class OcrSpellCheckerTestSpec extends FlatSpec {
     assert(wLevenshteinDateDist("10/0772018") == 1.0f)
   }
 
+
+  "a model" should "serialize properly" ignore {
+
+    val trainCorpusPath = "../auxdata/spell_dataset/vocab/bigone.txt"
+
+    import SparkAccessor.spark.implicits._
+    val ocrSpellModel = new OcrSpellCheckApproach().
+      setInputCols("text").
+      setTrainCorpusPath(trainCorpusPath).
+      setSpecialClasses(List(DateToken, NumberToken, AgeToken, UnitToken, MedicationClass)).
+      train(Seq.empty[String].toDF("text"))
+
+    ocrSpellModel.write.overwrite.save("./test_ner_dl")
+    val loadedNer = OcrSpellCheckModel.read.load("./test_ner_dl")
+  }
+
+
   "a model" should "train and predict" in {
 
-    val trainCorpusPath = "../auxdata/spell_dataset/vocab/spell_corpus.txt"
-    val vocabPath =  "../auxdata/spell_dataset/vocab/vocab"
+    val trainCorpusPath = "../auxdata/spell_dataset/vocab/bigone.txt"
 
     import SparkAccessor.spark.implicits._
     val ocrspell = new OcrSpellCheckApproach().
                     setInputCols("text").
                     setTrainCorpusPath(trainCorpusPath).
-
-
-      train(Seq.empty[String].toDF("text"))
-
-    val result = ocrspell.annotate(Seq(
+                    setSpecialClasses(List(DateToken, NumberToken, AgeToken, UnitToken, MedicationClass)).
+                    train(Seq.empty[String].toDF("text"))
+/*
+    val result1 = ocrspell.annotate(Seq(
       Annotation("swelling than his baseline . he states ha's teen compliant with his medications ." +
                 " although he stales he ran out of his Eliquis & few wesks ago . he denies having any blood" +
                 " in his stools or meiena , although he does 1ake iron pills and states his stools arc" +
-                " frequently black his hemoglobin is a1 baseline .")))
+                " frequently black his hemoglobin is a1 baseline ."))) */
+
+    val result2 = ocrspell.annotate(Seq(
+      Annotation("fibrillation on Aliquis , history of Gi bleed , CR-M3 , and anemia who presents with ull wegks oi" +
+                 " generalized fatigue and fecling umvell . He also notes some shortness of breath and worsening" +
+                 " dyspnea wilh minimal éxerlion . His major complaints are shoulder and joint pains , diffusely ." +
+                 " He also complains of \" bene pain ’ . He denies having any fevers or chills . He deries having" +
+                 " any chest pain , palpitalicns , He denies any worse sxtramity")))
 
     //model.predict(Array(Array("frequently", "black" ,"his", "hemoglobin", "is", "a1",  "baseline", ".").map(vocabIds.get).map(_.get)))
 
-    result.map(_.result).foreach(println)
+    //result1.map(_.result).foreach(println)
+
+    result2.map(_.result).foreach(println)
 
   }
 
@@ -53,13 +116,56 @@ class OcrSpellCheckerTestSpec extends FlatSpec {
   }
 
   "a spell checker" should "correclty parse training data" ignore {
-
     val ocrspell = new OcrSpellCheckApproach().
       setMinCount(1.0)
     val vocab = ocrspell.genVocab("src/main/resources/spell_corpus.txt")
     assert(vocab._1.size == 10)
+  }
+
+
+  "age classes" should "recognize different age patterns" in {
+    import scala.collection.JavaConversions._
+    val transducer = AgeToken.generateTransducer
+
+    assert(transducer.transduce("102-year-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("102-years-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("24-year-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("24-months-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("24-month-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("4-days-old").toList.exists(_.distance == 0))
+    assert(transducer.transduce("14-year").toList.exists(_.distance == 0))
+    assert(transducer.transduce("14-years").toList.exists(_.distance == 0))
+
 
   }
+
+  "medication classes" should "recognize different medications patterns" in {
+    import scala.collection.JavaConversions._
+    val transducer = MedicationClass.generateTransducer
+
+    assert(transducer.transduce("Eliquis").toList.exists(_.distance == 0))
+    assert(transducer.transduce("Percocet").toList.exists(_.distance == 0))
+
+  }
+
+  "number classes" should "recognize different medications patterns" in {
+    import scala.collection.JavaConversions._
+    val transducer = NumberToken.generateTransducer
+
+    assert(transducer.transduce("100.3").toList.exists(_.distance == 0))
+    assert(NumberToken.separate("$40,000").equals(NumberToken.label))
+  }
+
+
+  "date classes" should "recognize different date and time formats" in {
+    import scala.collection.JavaConversions._
+    val transducer = DateToken.generateTransducer
+
+    assert(transducer.transduce("10/25/1982").toList.exists(_.distance == 0))
+    assert(DateToken.separate("10/25/1982").equals(DateToken.label))
+  }
+
+
 
   /*
     "double quotes" should "be parsed correctly" in {
