db51e6c53fe491619e4f6c3a816958436c1a0edb
Saif Addin <saif@johnsnowlabs.com>
2018-12-10 17:17:27 -0300

    Disable RocksDB compression for windows compatibility

--- src/main/scala/com/johnsnowlabs/nlp/embeddings/RocksDbIndexer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/embeddings/RocksDbIndexer.scala
@@ -8,7 +8,7 @@ import org.rocksdb._
 private [embeddings] case class RocksDbIndexer(dbFile: String, autoFlashAfter: Option[Integer] = None) extends Closeable{
   val options = new Options()
   options.setCreateIfMissing(true)
-  options.setCompressionType(CompressionType.LZ4_COMPRESSION)
+  options.setCompressionType(CompressionType.NO_COMPRESSION)
   options.setWriteBufferSize(20 * 1 << 20)
 
   RocksDB.loadLibrary()
