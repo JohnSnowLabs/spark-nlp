daa6cdcfb7f2647aa86141c65e87d74689683918
Alberto <albertoandreotti@gmail.com>
2019-01-23 18:25:09 -0300

    adaptive rotation - first working version

--- ocr/src/main/scala/com/johnsnowlabs/nlp/util/io/ImageProcessing.scala
+++ ocr/src/main/scala/com/johnsnowlabs/nlp/util/io/ImageProcessing.scala
@@ -16,11 +16,49 @@ trait ImageProcessing {
 
   /*
   * angle is in degrees
+  *
+  * adapted from https://stackoverflow.com/questions/30204114/rotating-an-image-object
   * */
   def rotate(image:BufferedImage, angle:Double):BufferedImage = {
-    val g2d = image.createGraphics()
-    g2d.rotate(Math.toRadians(angle))
-    image
+    import java.awt.Graphics2D
+    import java.awt.geom.AffineTransform
+    import java.awt.image.BufferedImage
+
+    // The size of the original image
+    val w = image.getWidth
+    val h = image.getHeight
+
+    // The angle of the rotation in radians
+    val rads = Math.toRadians(angle)
+
+    // Calculate the amount of space the image will need in
+    // order not be clipped when it's rotated
+    val sin = Math.abs(Math.sin(rads))
+    val cos = Math.abs(Math.cos(rads))
+    val newWidth = Math.floor(w * cos + h * sin).toInt
+    val newHeight = Math.floor(h * cos + w * sin).toInt
+
+    // A new image, into which the original will be painted
+    val rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY)
+    val g2d = rotated.createGraphics
+
+    // The transformation which will be used to actually rotate the image
+    // The translation, actually makes sure that the image is positioned onto
+    // the viewable area of the image
+    val at = new AffineTransform
+    at.translate((newWidth - w) / 2, (newHeight - h) / 2)
+
+    // Rotate about the center of the image
+    val x = w / 2
+    val y = h / 2
+    at.rotate(rads, x, y)
+    g2d.setTransform(at)
+
+    // And we paint the original image onto the new image
+    g2d.drawImage(image, 0, 0, null)
+    g2d.dispose()
+    rotated
+
   }
 
   /*
@@ -40,7 +78,7 @@ trait ImageProcessing {
     val outputData = dest.getRaster().getDataBuffer().asInstanceOf[DataBufferByte].getData
     val inputData = gray.getRaster().getDataBuffer().asInstanceOf[DataBufferByte].getData
 
-    // handle the unsigned type signedByte2UnsignedInt =fromUnsigned
+    // handle the unsigned type
     val converted = inputData.map(signedByte2UnsignedInt)
 
     outputData.indices.par.foreach { idx =>
@@ -75,7 +113,7 @@ trait ImageProcessing {
   def criterionFunc(projections: Array[Int]): Double =
     projections.map(col => Math.pow(col, 2)).sum
 
-  def minAreaRect(pointList: Array[(Int, Int)]): (Int, Int) = {
+  def minAreaRect(pointList: List[(Int, Int)]): (Int, Int) = {
     val maxX = pointList.maxBy(_._2)._2
     val minX = pointList.minBy(_._2)._2
 
@@ -85,31 +123,30 @@ trait ImageProcessing {
   }
 
   def detectSkewAngle(image: BufferedImage, halfAngle:Int): Int = {
-    var angle_score = Map[Int, Double]()
-
-    Range(-halfAngle, halfAngle + 1).foreach { angle =>
-        var pointList: Array[(Int, Int)] = Array.empty
+    val angle_score = Range(-halfAngle, halfAngle + 1).par.map { angle =>
+        var pointList: List[(Int, Int)] = List.empty
         val rotImage = rotate(image, angle)
         val projections: Array[Int] = Array.fill(rotImage.getWidth)(0)
         val rotImageData = rotImage.getRaster().getDataBuffer().asInstanceOf[DataBufferByte].getData
         val (imgW, imgH) = (rotImage.getWidth, rotImage.getHeight)
 
-
+        val l = Range(0, imgH).toList
         dumpImage(rotImage, angle.toString + ".png")
-        Range(0, imgW).par.foreach { i =>
+        Range(0, imgW).foreach { i =>
           var j: Int = 0
-          Range(0, imgH).foreach { j =>
+          l.foreach { j =>
             val pixVal = rotImageData(j * imgW + i) // check best way to access data here
-            if (pixVal == 255.toByte) {
-              pointList = pointList :+ (j, i)
+            if (pixVal == -1) {
+              pointList =  (j, i) :: pointList
               projections(i) += 1
             }
           }
         }
-      val (w, h) = minAreaRect(pointList)
-      val score = criterionFunc(projections) / (w * h).toDouble
-      angle_score = angle_score + (angle -> score)
-    }
+        val (w, h) = minAreaRect(pointList)
+        val score = criterionFunc(projections) / (w * h).toDouble
+        (angle, score)
+    }.toMap
+
   angle_score.maxBy(_._2)._1
   }
 }
