b4ff1ee1e6c902bed8f9e029a1688752c64bcc4c
Saif Addin <saif@johnsnowlabs.com>
2019-02-07 02:58:00 -0300

    ChunkTokenizer annotator

---
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/ChunkTokenizer.scala
@@ -0,0 +1,23 @@
+package com.johnsnowlabs.nlp.annotators
+
+import com.johnsnowlabs.nlp.Annotation
+import com.johnsnowlabs.nlp.AnnotatorType.CHUNK
+import com.johnsnowlabs.nlp.annotators.common.{ChunkSplit, TokenizedWithSentence}
+import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
+
+class ChunkTokenizer(override val uid: String) extends Tokenizer {
+
+  def this() = this(Identifiable.randomUID("CHUNK_TOKENIZER"))
+
+  override val requiredAnnotatorTypes: Array[AnnotatorType] = Array[AnnotatorType](CHUNK)
+
+  /** one to many annotation */
+  override def annotate(annotations: Seq[Annotation]): Seq[Annotation] = {
+    val sentences = ChunkSplit.unpack(annotations)
+    val tokenized = tag(sentences)
+    TokenizedWithSentence.pack(tokenized)
+  }
+
+}
+
+object ChunkTokenizer extends DefaultParamsReadable[ChunkTokenizer]
