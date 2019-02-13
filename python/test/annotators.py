167d073d8eaf410c452782704bcf589676ea3d09
Saif Addin <saif@johnsowlabs.com>
2019-02-12 22:40:41 -0300

    Added pyspark doc2chunk and chunk2doc as to sparknlp module

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -627,3 +627,39 @@ class TypedDependencyParserTestSpec(unittest.TestCase):
         dependency_parsed = dependency_parser.fit(pos_tagged).transform(pos_tagged)
         typed_dependency_parsed = typed_dependency_parser.fit(dependency_parsed).transform(dependency_parsed)
         typed_dependency_parsed.show()
+
+
+class ChunkDocSerializingTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkContextForTest.spark \
+            .createDataFrame([["I saw a girl with a telescope"]]).toDF("text")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+        tokenizer = Tokenizer() \
+            .setInputCols(["document"]) \
+            .setOutputCol("token")
+        entity_extractor = TextMatcher() \
+            .setOutputCol("entity") \
+            .setEntities(path="file:///" + os.getcwd() + "/../src/test/resources/entity-extractor/test-chunks.txt")
+        chunk2doc = Chunk2Doc() \
+            .setInputCols(['entity']) \
+            .setOutputCol('entity_doc')
+        doc2chunk = Doc2Chunk() \
+            .setInputCols(['entity_doc']) \
+            .setOutputCol('entity_rechunk')
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            tokenizer,
+            entity_extractor,
+            chunk2doc,
+            doc2chunk
+        ])
+
+        model = pipeline.fit(self.data)
+        pipe_path = "file:///" + os.getcwd() + "/tmp_chunkdoc"
+        model.write().overwrite().save(pipe_path)
+        PipelineModel.load(pipe_path)
