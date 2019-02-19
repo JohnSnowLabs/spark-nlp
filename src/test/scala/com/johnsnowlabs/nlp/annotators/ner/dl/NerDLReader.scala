c35eda20b80d7fd03216c6c13943f5116dd081d6
Saif Addin <saif@johnsnowlabs.com>
2019-02-18 17:07:33 -0800

    Simplified pipeline resolution, made sparknlp easy to use

--- src/test/scala/com/johnsnowlabs/nlp/annotators/ner/dl/NerDLReader.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/ner/dl/NerDLReader.scala
@@ -3,7 +3,7 @@ package com.johnsnowlabs.nlp.annotators.ner.dl
 import com.johnsnowlabs.nlp.LightPipeline
 import com.johnsnowlabs.nlp.annotators.NormalizerModel
 import com.johnsnowlabs.nlp.embeddings.WordEmbeddingsFormat
-import com.johnsnowlabs.nlp.pretrained.pipelines.en.BasicPipeline
+import com.johnsnowlabs.nlp.pretrained.PretrainedPipeline
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
 import org.apache.spark.ml.Pipeline
 import org.scalatest._
@@ -28,13 +28,13 @@ class NerDLReaderTestSpec extends FlatSpec {
     val spark = ResourceHelper.spark
     import spark.implicits._
 
-    val bp = BasicPipeline().pretrained()
+    val bp = PretrainedPipeline("pipeline_basic")
 
-    bp.stages(2).asInstanceOf[NormalizerModel]
+    bp.model.stages(2).asInstanceOf[NormalizerModel]
 
     val ner = NerDLModel.load("./some_model").setInputCols("document", "normal").setOutputCol("ner")
 
-    val np = new Pipeline().setStages(Array(bp, ner))
+    val np = new Pipeline().setStages(Array(bp.model, ner))
 
     val target = Array(
       "With regard to the patient's chronic obstructive pulmonary disease, the patient's respiratory status improved throughout the remainder of her hospital course.")
