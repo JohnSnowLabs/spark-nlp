a44ab642e23139c6125c671e9f6b57aac7810c6b
Alberto <albertoandreotti@gmail.com>
2019-03-16 19:22:13 -0300

    applied stash

--- src/main/scala/com/johnsnowlabs/nlp/annotators/spell/context/ContextSpellCheckerModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/spell/context/ContextSpellCheckerModel.scala
@@ -69,7 +69,8 @@ class ContextSpellCheckerModel(override val uid: String) extends AnnotatorModel[
     " correction is applied on paragraphs as defined by newline characters.")
   def setUseNewLines(useIt: Boolean):this.type = set(useNewLines, useIt)
 
-  setDefault(tradeoff -> 18.0f, gamma -> 120.0f, useNewLines -> false)
+  setDefault(tradeoff -> 18.0f, gamma -> 120.0f, useNewLines -> false, maxCandidates -> 6)
+
 
   // the scores for the EOS (end of sentence), and BOS (beginning of sentence)
   private val eosScore = .01
