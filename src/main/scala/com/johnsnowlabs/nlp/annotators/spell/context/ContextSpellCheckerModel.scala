c21d1d2b61e3c919eca75ffaafca39fd0e366241
Saif Addin <saif@johnsnowlabs.com>
2019-04-29 03:06:04 -0300

    Optionally get sentence metadata

--- src/main/scala/com/johnsnowlabs/nlp/annotators/spell/context/ContextSpellCheckerModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/spell/context/ContextSpellCheckerModel.scala
@@ -219,8 +219,7 @@ class ContextSpellCheckerModel(override val uid: String) extends AnnotatorModel[
     * @return any number of annotations processed for every input annotation. Not necessary one to one relationship
     */
   override def annotate(annotations: Seq[Annotation]): Seq[Annotation] = {
-    // TODO still don't like the .apply() here
-    val decodedSentPaths = annotations.groupBy(_.metadata.apply("sentence")).mapValues{ sentTokens =>
+    val decodedSentPaths = annotations.groupBy(_.metadata.getOrElse("sentence", "0")).mapValues{ sentTokens =>
       val (decodedPath, cost) = toOption(getOrDefault(useNewLines)).map { _ =>
         val idxs = Seq(-1) ++ sentTokens.zipWithIndex.filter { case (a, _) => a.result.equals(System.lineSeparator) || a.result.equals(System.lineSeparator*2) }.
           map(_._2) ++ Seq(annotations.length)
