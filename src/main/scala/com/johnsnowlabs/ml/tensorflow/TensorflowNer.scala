178c422596015dae8160e3ece6149335ec697348
Saif Addin <saif@johnsnowlabs.com>
2019-04-29 17:24:56 -0300

    load contrib if needed on graph retrieval

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowNer.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowNer.scala
@@ -60,7 +60,7 @@ class TensorflowNer
       else {
         val tensors = new TensorResources()
 
-        val calculated = tensorflow.getSession.runner
+        val calculated = tensorflow.getSession(true).runner
           .feed(sentenceLengthsKey, tensors.createTensor(batchInput.sentenceLengths))
           .feed(wordEmbeddingsKey, tensors.createTensor(batchInput.wordEmbeddings))
 
@@ -120,7 +120,7 @@ class TensorflowNer
 
     // Initialize
     if (startEpoch == 0)
-      tensorflow.createSession.runner.addTarget(initKey).run()
+      tensorflow.createSession(true).runner.addTarget(initKey).run()
 
     val trainDatasetSeq = trainDataset.toSeq
     // Train
@@ -142,7 +142,7 @@ class TensorflowNer
         val batchTags = encoder.encodeTags(tags)
 
         val tensors = new TensorResources()
-        val calculated = tensorflow.getSession.runner
+        val calculated = tensorflow.getSession(true).runner
           .feed(sentenceLengthsKey, tensors.createTensor(batchInput.sentenceLengths))
           .feed(wordEmbeddingsKey, tensors.createTensor(batchInput.wordEmbeddings))
 
