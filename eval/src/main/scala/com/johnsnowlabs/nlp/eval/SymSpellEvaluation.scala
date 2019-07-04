cfb6a2aee5d28eb67a3c08d539ba9f124e07557d
danilojsl <danilo@johnsnowlabs.com>
2019-07-04 06:57:07 -0500

    Refactor Symmetric Spell Evaluation Adds unit test for Symmetric Spell Evaluation

--- eval/src/main/scala/com/johnsnowlabs/nlp/eval/SymSpellEvaluation.scala
+++ eval/src/main/scala/com/johnsnowlabs/nlp/eval/SymSpellEvaluation.scala
@@ -1,75 +1,56 @@
 package com.johnsnowlabs.nlp.eval
 
-import java.io.File
-import scala.collection.mutable
-
-import com.johnsnowlabs.nlp.base._
 import com.johnsnowlabs.nlp.annotator._
-
 import com.johnsnowlabs.nlp.annotators._
+import com.johnsnowlabs.nlp.base._
+import com.johnsnowlabs.nlp.eval.NorvigSpellEvaluation.loggingData
+import com.johnsnowlabs.nlp.eval.util.LoggingData
 import com.johnsnowlabs.util.{Benchmark, PipelineModels}
-
 import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+import scala.collection.mutable
 
 object SymSpellEvaluation extends App {
 
-  private val spark = SparkSession.builder()
-    .appName("benchmark")
-    .master("local[1]")
-    .config("spark.driver.memory", "8G")
-    .config("spark.kryoserializer.buffer.max", "200M")
-    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    .getOrCreate()
+  var loggingData: LoggingData = _
 
-  import spark.implicits._
-
-  private val trainingDataSet = getDataSetFromFile("src/main/resources/spell/coca2017.txt")
-
-  private val documentAssembler = new DocumentAssembler()
-    .setInputCol("text")
-    .setOutputCol("document")
-
-  private val tokenizer = new Tokenizer()
-    .setInputCols(Array("document"))
-    .setOutputCol("token")
-
-  private val spell = new SymmetricDeleteApproach()
-    .setInputCols(Array("token"))
-    .setOutputCol("spell")
-    .setDictionary("src/main/resources/spell/words.txt")
-
-  private val finisher = new Finisher()
-    .setInputCols("spell")
-    .setOutputCols("prediction")
-
-  private val spellCheckerPipeline = new Pipeline()
-    .setStages(Array(
-      documentAssembler,
-      tokenizer,
-      spell,
-      finisher
-    ))
-
-  val testDataSet = getDataSetFromFile("./misspell.txt")
-  var spellCheckerModel: PipelineModel = _
-  Benchmark.measure("[Symmetric Spell Checker] Time to train") {
-    spellCheckerModel = spellCheckerPipeline.fit(trainingDataSet)
+  def apply(trainFile: String, spell: SymmetricDeleteApproach, testFile: String, groundTruthFile: String): Unit = {
+    loggingData = new LoggingData("LOCAL", this.getClass.getSimpleName)
+    loggingData.logSymSpellParams(spell)
+    computeAccuracy(trainFile, spell, testFile, groundTruthFile)
+    loggingData.closeLog()
   }
-  println("Prediction DataSet")
-  val predictionDataSet = spellCheckerModel.transform(testDataSet).select("prediction")
-  Benchmark.measure("[Symmetric Spell Checker] Time to show") {
-    predictionDataSet.show()
-  }
-  val groundTruthDataSet = getGroundTruthDataSet("./ground_truth.txt")
 
-  println("Evaluation DataSet")
-  val evaluationDataSet = getEvaluationDataSet(predictionDataSet, groundTruthDataSet)
-  evaluationDataSet.show(5, false)
-  evaluationDataSet.select(avg(col("accuracy"))).show()
+  private def computeAccuracy(trainFile: String, spell: SymmetricDeleteApproach,
+                              testFile: String, groundTruthFile: String): Unit = {
+    val spellCheckerModel = trainSpellChecker(trainFile, spell)
+    val predictionDataSet = correctMisspells(spellCheckerModel, testFile)
+    evaluateSpellChecker(groundTruthFile, predictionDataSet)
+  }
+
+  private def trainSpellChecker(trainFile: String, spell: SymmetricDeleteApproach): PipelineModel = {
+    val trainingDataSet = getDataSetFromFile(trainFile)
+    var spellCheckerModel: PipelineModel = null
+    val spellCheckerPipeline = getSpellCheckerPipeline(spell)
+    Benchmark.measure("[Symmetric Spell Checker] Time to train") {
+      spellCheckerModel = spellCheckerPipeline.fit(trainingDataSet)
+    }
+    spellCheckerModel
+  }
 
   def getDataSetFromFile(textFile: String): Dataset[_] = {
+
+    val spark = SparkSession.builder()
+      .appName("benchmark")
+      .master("local[1]")
+      .config("spark.kryoserializer.buffer.max", "200M")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+
+    import spark.implicits._
+
     if (textFile == "") {
       Seq("Simple data set").toDF.withColumnRenamed("value", "text")
     } else {
@@ -79,7 +60,49 @@ object SymSpellEvaluation extends App {
     }
   }
 
-  def getGroundTruthDataSet(textFile: String): Dataset[_] = {
+  private def getSpellCheckerPipeline(spell: SymmetricDeleteApproach): Pipeline = {
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val tokenizer = new Tokenizer()
+      .setInputCols(Array("document"))
+      .setOutputCol("token")
+
+    val finisher = new Finisher()
+      .setInputCols("spell")
+      .setOutputCols("prediction")
+
+    new Pipeline()
+      .setStages(Array(
+        documentAssembler,
+        tokenizer,
+        spell,
+        finisher
+      ))
+  }
+
+  private def correctMisspells(spellCheckerModel: PipelineModel, testFile: String): Dataset[_] = {
+    println("Prediction DataSet")
+    val testDataSet = getDataSetFromFile(testFile)
+    val predictionDataSet = spellCheckerModel.transform(testDataSet).select("prediction")
+    Benchmark.measure("[Symmetric Spell Checker] Time to show") {
+      predictionDataSet.show()
+    }
+    predictionDataSet
+  }
+
+  private def evaluateSpellChecker(groundTruthFile: String, predictionDataSet: Dataset[_]): Unit = {
+    println("Evaluation DataSet")
+    val groundTruthDataSet = getGroundTruthDataSet(groundTruthFile)
+    val evaluationDataSet = getEvaluationDataSet(predictionDataSet, groundTruthDataSet)
+    evaluationDataSet.show(5, false)
+    val accuracyDataSet = evaluationDataSet.select(avg(col("accuracy")))
+    val accuracy = accuracyDataSet.collect.flatMap(_.toSeq).headOption.getOrElse(-1).toString
+    loggingData.logMetric( "accuracy", accuracy.toDouble)
+  }
+
+  private def getGroundTruthDataSet(textFile: String): Dataset[_] = {
 
     val documentAssembler = new DocumentAssembler()
       .setInputCol("text")
@@ -108,21 +131,22 @@ object SymSpellEvaluation extends App {
 
   }
 
+
   def getEvaluationDataSet(predictionDataSet: Dataset[_], groundTruthDataSet: Dataset[_]): Dataset[_] = {
     val evaluationDataSet = predictionDataSet.withColumn("id", monotonically_increasing_id())
       .join(groundTruthDataSet.withColumn("id", monotonically_increasing_id()), Seq("id"))
       .drop("id")
     evaluationDataSet.withColumn("accuracy",
-      getAccuracy(col("prediction"), col("ground_truth")))
+      getAccuracyByRow(col("prediction"), col("ground_truth")))
   }
 
-  private def getAccuracy = udf { (prediction: mutable.WrappedArray[String],
-                                   groundTruth: mutable.WrappedArray[String]) =>
-    val accuracy = computeAccuracy(prediction.toSet, groundTruth.toSet)
+  private def getAccuracyByRow = udf { (prediction: mutable.WrappedArray[String],
+                                        groundTruth: mutable.WrappedArray[String]) =>
+    val accuracy = computeAccuracyByRow(prediction.toSet, groundTruth.toSet)
     accuracy
   }
 
-  def computeAccuracy(prediction: Set[String], groundTruth: Set[String]): Float = {
+  def computeAccuracyByRow(prediction: Set[String], groundTruth: Set[String]): Float = {
     val numberOfCorrectWords = prediction.intersect(groundTruth).size.toFloat
     val accuracy: Float = numberOfCorrectWords / groundTruth.size.toFloat
     accuracy
--- eval/src/test/scala/com/johnsnowlabs/nlp/eval/NorvigSpellEvalTestSpec.scala
+++ eval/src/test/scala/com/johnsnowlabs/nlp/eval/NorvigSpellEvalTestSpec.scala
@@ -7,10 +7,10 @@ class NorvigSpellEvalTestSpec extends FlatSpec {
 
   "A Norvig Spell Evaluation" should "display accuracy results" in {
 
-    val trainFile = "./coca2017.txt"
+    val trainFile = "src/test/resources/spell/sherlockholmes.txt"
     val dictionaryFile = "src/test/resources/spell/words.txt"
-    val testFile = "./misspell.txt"
     val groundTruthFile = "./ground_truth.txt"
+    val testFile = "./misspell.txt"
 
     val spell = new NorvigSweetingApproach()
       .setInputCols(Array("token"))
