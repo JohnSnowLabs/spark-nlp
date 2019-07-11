913e0dad38c01e7857503622900f9e8f0ec7f1a8
Saif Addin <saif@johnsnowlabs.com>
2019-07-11 15:30:20 -0300

    Version bump, python tokenizer

--- src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
+++ src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 
 object SparkNLP {
 
-  val currentVersion = "2.1.0-rc3"
+  val currentVersion = "2.1.0-rc4"
 
   def start(includeOcr: Boolean = false): SparkSession = {
     val build = SparkSession.builder()
@@ -15,11 +15,11 @@ object SparkNLP {
 
     if (includeOcr) {
       build
-        .config("spark.jars.packages", "JohnSnowLabs:spark-nlp:2.1.0-rc3,com.johnsnowlabs.nlp:spark-nlp-ocr_2.11:2.1.0-rc3,javax.media.jai:com.springsource.javax.media.jai.core:1.1.3")
+        .config("spark.jars.packages", "JohnSnowLabs:spark-nlp:2.1.0-rc4,com.johnsnowlabs.nlp:spark-nlp-ocr_2.11:2.1.0-rc4,javax.media.jai:com.springsource.javax.media.jai.core:1.1.3")
         .config("spark.jars.repositories", "http://repo.spring.io/plugins-release")
     } else {
       build
-        .config("spark.jars.packages", "JohnSnowLabs:spark-nlp:2.1.0-rc3")
+        .config("spark.jars.packages", "JohnSnowLabs:spark-nlp:2.1.0-rc4")
     }
 
     build.getOrCreate()
