80d51b259da4e2c144cda76a71e32f1ed8fd2e96
danilojsl <danilo@johnsnowlabs.com>
2019-07-22 22:10:44 -0500

    Modifies spell argument as java object

--- python/sparknlp/eval.py
+++ python/sparknlp/eval.py
@@ -4,11 +4,11 @@ from sparknlp.internal import ExtendedJavaWrapper
 class NorvigSpellEvaluation(ExtendedJavaWrapper):
 
     def __init__(self, test_file, ground_truth_file):
-        super(NorvigSpellEvaluation, self).__init__("com.johnsnowlabs.nlp.eval.NorvigSpellEvaluation")
+        super(NorvigSpellEvaluation, self).__init__("com.johnsnowlabs.nlp.eval.spell.NorvigSpellEvaluation")
         self._java_obj = self._new_java_obj(self._java_obj, test_file, ground_truth_file)
 
     def computeAccuracyAnnotator(self, train_file, spell):
-        return self._java_obj.computeAccuracyAnnotator(train_file, spell)
+        return self._java_obj.computeAccuracyAnnotator(train_file, spell._java_obj)
 
     def testMethod(self, test_parameter):
         return self._java_obj.testMethod(test_parameter)
