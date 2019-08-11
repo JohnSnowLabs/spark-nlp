5b71de11ff1b83b7f058b2046596cf974f323670
fernandrez <fernandrez@gmail.com>
2019-08-10 21:24:40 -0300

    WordEmbeddingsRetriever.scala: Adjusted getEmbeddingsFromDb to return an Option. Subsequently adjust getEmbeddingsVector function and the LRU definition. TokenPieceEmbeddings function in SentenceWithEmbeddings.scala now has an "isOOV" property in its definition and also an apply in its companion that receives the option returned by the embeddings retriever and also receives a default vector to use in case the Option value is None. This is aiming at giving the annotator the power to choose the embeddings when there is no match in the DB without having to process the option at all. Also adjusted two calls to getEmbeddingsVector function, one in CoNLL2003NerReader.scala and another one in NerDLCoNLL2003.scala. On the other hand we included two helper functions that aid with embeddings analysis and debugging: One that calculates a new column with the embeddings coverage per row and another one that returns the overall coverage.

--- src/main/scala/com/johnsnowlabs/nlp/annotators/common/SentenceWithEmbeddings.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/common/SentenceWithEmbeddings.scala
@@ -12,7 +12,7 @@ case class WordpieceEmbeddingsSentence
 )
 
 case class TokenPieceEmbeddings(wordpiece: String, token: String, pieceId: Int,
-                                isWordStart: Boolean,
+                                isWordStart: Boolean, isOOV: Boolean,
                                 embeddings: Array[Float], begin: Int, end: Int)
 
 object TokenPieceEmbeddings {
@@ -22,13 +22,30 @@ object TokenPieceEmbeddings {
       token = piece.token,
       pieceId = piece.pieceId,
       isWordStart = piece.isWordStart,
+      isOOV = false, // FIXME: I think BERT wont have OOV, this "constructor" is called from TensorFlowBert
       embeddings = embeddings,
       begin = piece.begin,
       end = piece.end)
   }
+  def apply(wordpiece: String, token: String, pieceId: Int,
+            isWordStart: Boolean,
+            embeddings: Option[Array[Float]], zeroArray: Array[Float], begin: Int, end: Int): TokenPieceEmbeddings = {
+
+    val vector = embeddings.getOrElse(zeroArray)
+    val oov = embeddings match { case Some(_) => false; case default => true; }
+    TokenPieceEmbeddings(
+      wordpiece = wordpiece,
+      token = token,
+      pieceId = pieceId,
+      isWordStart = isWordStart,
+      isOOV = oov,
+      embeddings = vector,
+      begin = begin,
+      end = end)
+  }
 }
 
-object WordpieceEmbeddingsSentence extends Annotated[WordpieceEmbeddingsSentence] {
+object  WordpieceEmbeddingsSentence extends Annotated[WordpieceEmbeddingsSentence] {
   override def annotatorType: String = AnnotatorType.WORD_EMBEDDINGS
 
   override def unpack(annotations: Seq[Annotation]): Seq[WordpieceEmbeddingsSentence] = {
@@ -44,6 +61,7 @@ object WordpieceEmbeddingsSentence extends Annotated[WordpieceEmbeddingsSentence
           token = token.metadata("token"),
           pieceId = token.metadata("pieceId").toInt,
           isWordStart = token.metadata("isWordStart").toBoolean,
+          isOOV = token.metadata("isOOV").toBoolean,
           embeddings = token.embeddings,
           begin = token.begin,
           end = token.end
@@ -73,7 +91,8 @@ object WordpieceEmbeddingsSentence extends Annotated[WordpieceEmbeddingsSentence
           Map("sentence" -> sentenceIndex.toString,
             "token" -> token.token,
             "pieceId" -> token.pieceId.toString,
-            "isWordStart" -> token.isWordStart.toString
+            "isWordStart" -> token.isWordStart.toString,
+            "isOOV" -> token.isOOV.toString
           ),
           embeddings,
           sentenceEmbeddings
