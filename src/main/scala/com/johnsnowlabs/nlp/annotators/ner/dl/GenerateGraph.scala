5adc7795a81dab2bd1c7b02622c494b73812d13b
danilojsl <danilo@johnsnowlabs.com>
2019-08-21 21:43:00 -0500

    Adds load model method

--- src/main/scala/com/johnsnowlabs/nlp/annotators/ner/dl/GenerateGraph.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/ner/dl/GenerateGraph.scala
@@ -1,22 +1,38 @@
 package com.johnsnowlabs.nlp.annotators.ner.dl
 
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.SparkSession
 
-case class GraphData(graphFilePath: String, numberOfTags: Int, embeddingsDimension: Int,
-                     numberOfChars: Int, lstmSize: Int=128)
+case class GraphParams(numberOfTags: Int, embeddingsDimension: Int, numberOfChars: Int, lstmSize: Int=128)
 
-class GenerateGraph(graphData: GraphData) {
+class GenerateGraph(graphParams: GraphParams, graphFilePath: String, sparkSession: SparkSession) {
 
   private val useContrib: Boolean = { if (ResourceHelper.getOsName == "Windows") false else true }
   private val pythonFile = "python/sparknlp/graph.py"
+  private val defaultPath = "src/main/resources/ner-dl"
 
   def getModelName: String = {
     val namePrefix = getNamePrefix
-    s"${namePrefix}_${graphData.numberOfTags}_${graphData.embeddingsDimension}_${graphData.lstmSize}_${graphData.numberOfChars}"
+    s"${namePrefix}_${graphParams.numberOfTags}_${graphParams.embeddingsDimension}_${graphParams.lstmSize}_${graphParams.numberOfChars}"
   }
 
   private def getNamePrefix: String = {
     if (useContrib) "blstm" else "blstm-noncontrib"
+  }
+
+  def loadModel(): Unit = {
+    val modelName = getModelName
+    if (fileExists(graphFilePath + "/" + modelName + ".pb")) {
+      println("Load Model")
+    } else {
+      if (fileExists(defaultPath + "/" + modelName + ".pb")) {
+        println("Load model from default Path")
+      } else {
+        create
+        println("Load model after creation")
+      }
+    }
   }
 
   def create: String = {
@@ -33,15 +49,27 @@ class GenerateGraph(graphData: GraphData) {
    }
 
   private def getArguments: String = {
-    val fileName = graphData.graphFilePath + "/" + getModelName
-    " " + fileName + " " + useContrib + " " + graphData.numberOfTags + " " + graphData.embeddingsDimension + " " +
-      graphData.numberOfChars
+    val fileName = graphFilePath + "/" + getModelName
+    " " + fileName + " " + useContrib + " " + graphParams.numberOfTags + " " + graphParams.embeddingsDimension +
+      " " + graphParams.numberOfChars
   }
 
   private def getErrorMessage(fullErrorMessage: String): String = {
-    val pattern = "Exception:[\\sa-zA-Z\\d_.-]*".r
-    val errorMessage = pattern findFirstIn fullErrorMessage
+    var pattern = "Exception:[\\sa-zA-Z\\d_.-]*".r
+    var errorMessage = pattern findFirstIn fullErrorMessage
+    if (errorMessage.isEmpty ) {
+      pattern = "Error:[\\sa-zA-Z\\d=>_.-]*".r
+      errorMessage = pattern findFirstIn fullErrorMessage
+    }
     errorMessage.getOrElse("")
   }
+
+  private def fileExists(fullPath: String): Boolean = {
+    val uri = new java.net.URI(fullPath.replaceAllLiterally("\\", "/"))
+    val fs: FileSystem = FileSystem.get(uri, sparkSession.sparkContext.hadoopConfiguration)
+    val dataPath = new Path(fullPath)
+    fs.exists(dataPath)
+  }
+
 
 }
--- src/test/scala/com/johnsnowlabs/nlp/annotators/ner/dl/GenerateGraphTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/ner/dl/GenerateGraphTestSpec.scala
@@ -1,6 +1,7 @@
 package com.johnsnowlabs.nlp.annotators.ner.dl
 
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import org.apache.spark.sql.SparkSession
 import org.scalatest.FlatSpec
 
 class GenerateGraphTestSpec extends FlatSpec {
@@ -8,10 +9,17 @@ class GenerateGraphTestSpec extends FlatSpec {
   private val numberOfTags = 80
   private val embeddingsDimension = 200
   private val numberOfChars = 125
+  private val spark = SparkSession.builder()
+    .appName("benchmark")
+    .master("local[*]")
+    .config("spark.driver.memory", "1G")
+    .config("spark.kryoserializer.buffer.max", "200M")
+    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    .getOrCreate()
 
   "GenerateGraph" should "get model file name" in {
-    val graphData = GraphData("", numberOfTags, embeddingsDimension, numberOfChars)
-    val generateGraph = new GenerateGraph(graphData)
+    val graphData = GraphParams(numberOfTags, embeddingsDimension, numberOfChars)
+    val generateGraph = new GenerateGraph(graphData, "", spark)
     val graphFileName = generateGraph.getModelName
 
     if (ResourceHelper.getOsName == "Linux") {
@@ -22,9 +30,9 @@ class GenerateGraphTestSpec extends FlatSpec {
   }
 
   "GenerateGraph" should "create a graph" in {
-    val graphFilePath = "src/main/resources/ner-dl"
-    val graphData = GraphData(graphFilePath, numberOfTags, embeddingsDimension, numberOfChars)
-    val generateGraph = new GenerateGraph(graphData)
+    val graphFilePath = "./tmp"
+    val graphParams = GraphParams(numberOfTags, embeddingsDimension, numberOfChars)
+    val generateGraph = new GenerateGraph(graphParams, graphFilePath, spark)
     val expectedMessage = "Graph created successfully"
 
     val message = generateGraph.create
@@ -32,13 +40,26 @@ class GenerateGraphTestSpec extends FlatSpec {
     assert(message==expectedMessage)
   }
 
-  "GenerateGraph with wrong argument" should "return an error message" ignore {
-    //val generateGraph = new GenerateGraph()
-    val pythonScript = "python ./graph.py -1 200 125"
-    val expectedMessage = "Exception: Error message"
+  "GenerateGraph" should "load a graph from default path" in {
+    val numberOfTags = 10
+    val embeddingsDimension = 100
+    val numberOfChars = 100
+    val graphFilePath = "./tmp"
+    val graphParams = GraphParams(numberOfTags, embeddingsDimension, numberOfChars)
+    val generateGraph = new GenerateGraph(graphParams, graphFilePath, spark)
 
-//    val message = generateGraph.create(pythonScript)
-//    assert(message==expectedMessage)
+    generateGraph.loadModel()
+
+  }
+
+  "GenerateGraph with wrong argument" should "return an error message" in {
+    val graphFilePath = "./tmp"
+    val graphParams = GraphParams(-1, embeddingsDimension, numberOfChars)
+    val generateGraph = new GenerateGraph(graphParams, graphFilePath, spark)
+    val expectedString = "Error:"
+
+    val message = generateGraph.create
+    assert(message contains expectedString)
   }
 
 
