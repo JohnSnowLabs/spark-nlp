3b5acb9528fd5b121a9c76b51cbf118f602f54b8
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-09-24 15:44:14 +0200

    [skip travis] Add unit tests for SentenceEmbeddings

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -4,6 +4,7 @@ from sparknlp.annotator import *
 from sparknlp.base import *
 from sparknlp.embeddings import *
 from test.util import SparkContextForTest
+from test.util import SparkSessionWithoutOCRForTest
 from sparknlp.ocr import OcrHelper
 
 
@@ -816,3 +817,38 @@ class ChunkDocSerializingTestSpec(unittest.TestCase):
         pipe_path = "file:///" + os.getcwd() + "/tmp_chunkdoc"
         model.write().overwrite().save(pipe_path)
         PipelineModel.load(pipe_path)
+
+
+class SentenceEmbeddingsTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkSessionWithoutOCRForTest.spark.read.option("header", "true")\
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/embeddings/sentence_embeddings.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+        sentence_detector = SentenceDetector() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
+        tokenizer = Tokenizer() \
+            .setInputCols(["sentence"]) \
+            .setOutputCol("token")
+        glove = WordEmbeddingsModel.pretrained() \
+            .setInputCols(["sentence", "token"]) \
+            .setOutputCol("embeddings")
+        sentence_embeddings = SentenceEmbeddings() \
+            .setInputCols(["sentence", "embeddings"]) \
+            .setOutputCol("sentence_embeddings") \
+            .setPoolingStrategy("AVERAGE")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_detector,
+            tokenizer,
+            glove,
+            sentence_embeddings
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
