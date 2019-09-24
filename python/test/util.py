d08561397a941c6abd868f3ac184a82071dc801f
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-09-24 15:43:52 +0200

    [skip travis] Add extra clean SparkSessions for unit test in Python

--- python/test/util.py
+++ python/test/util.py
@@ -16,3 +16,21 @@ class SparkContextForTest:
         .limit(100)
     data.cache()
     data.count()
+
+
+class SparkSessionWithoutOCRForTest:
+    spark = SparkSession.builder \
+        .master("local[4]") \
+        .config("spark.driver.memory", "6500M") \
+        .config("spark.jars", 'lib/sparknlp.jar') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+
+class SparkSessionWithOCRForTest:
+    spark = SparkSession.builder \
+        .master("local[4]") \
+        .config("spark.driver.memory", "6500M") \
+        .config("spark.jars", 'lib/sparknlp.jar,lib/sparknlp-ocr.jar') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
