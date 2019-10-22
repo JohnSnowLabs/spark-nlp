b5eb33ab90f52e5e0fec58bfb2d19c6605aacc88
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-10-22 13:03:44 +0200

    [skip travis] Add a new ChunkEmbeddings annotator

--- src/main/scala/com/johnsnowlabs/nlp/AnnotatorType.scala
+++ src/main/scala/com/johnsnowlabs/nlp/AnnotatorType.scala
@@ -6,6 +6,7 @@ object AnnotatorType {
   val WORDPIECE = "wordpiece"
   val WORD_EMBEDDINGS = "word_embeddings"
   val SENTENCE_EMBEDDINGS = "sentence_embeddings"
+  val CHUNK_EMBEDDINGS = "chunk_embeddings"
   val DATE = "date"
   val ENTITY = "entity"
   val SENTIMENT = "sentiment"
