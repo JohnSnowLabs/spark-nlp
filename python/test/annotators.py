bd16ae451d24ffbb5e6e99e7221558c218364800
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-10-25 14:58:08 +0200

    [skip travis] Add Python unit test for ChunkEmbeddings

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -864,3 +864,46 @@ class NGramGeneratorTestSpec(unittest.TestCase):
         assert transformed_data.select("ngrams_cum.result").rdd.flatMap(lambda x: x).collect() == \
                [['This', 'is', 'my', 'first', 'sentence', '.', 'This is', 'is my', 'my first', 'first sentence', 'sentence .', 'This', 'is', 'my', 'second', '.', 'This is', 'is my', 'my second', 'second .'], ['This', 'is', 'my', 'third', 'sentence', '.', 'This is', 'is my', 'my third', 'third sentence', 'sentence .', 'This', 'is', 'my', 'forth', '.', 'This is', 'is my', 'my forth', 'forth .']]
 
+
+class ChunkEmbeddingsTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkContextForTest.spark.read.option("header", "true") \
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/embeddings/sentence_embeddings.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+        sentence_detector = SentenceDetector() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
+        tokenizer = Tokenizer() \
+            .setInputCols(["sentence"]) \
+            .setOutputCol("token")
+        pos_tagger = PerceptronModel.pretrained() \
+            .setInputCols(["token", "sentence"]) \
+            .setOutputCol("pos")
+        chunker = Chunker() \
+            .setInputCols(["sentence", "pos"]) \
+            .setOutputCol("chunk") \
+            .setRegexParsers(["<DT>?<JJ>*<NN>+"])
+        glove = WordEmbeddingsModel.pretrained() \
+            .setInputCols(["sentence", "token"]) \
+            .setOutputCol("embeddings")
+        chunk_embeddings = ChunkEmbeddings() \
+            .setInputCols(["chunk", "embeddings"]) \
+            .setOutputCol("chunk_embeddings") \
+            .setPoolingStrategy("AVERAGE")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_detector,
+            tokenizer,
+            pos_tagger,
+            chunker,
+            glove,
+            chunk_embeddings
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
