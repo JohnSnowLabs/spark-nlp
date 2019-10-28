9c071950073fcf85b2a316d68fa9f62c5b2b3fab
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-10-28 20:19:08 +0100

    Adding Scala and Python unit tests to EmbeddingsFinisher transformer

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -4,6 +4,8 @@ from sparknlp.annotator import *
 from sparknlp.base import *
 from sparknlp.embeddings import *
 from test.util import SparkContextForTest
+from pyspark.ml.feature import SQLTransformer
+from pyspark.ml.clustering import KMeans
 
 
 class BasicAnnotatorsTestSpec(unittest.TestCase):
@@ -907,3 +909,49 @@ class ChunkEmbeddingsTestSpec(unittest.TestCase):
 
         model = pipeline.fit(self.data)
         model.transform(self.data).show()
+
+
+class EmbeddingsFinisherTestSpec(unittest.TestCase):
+
+    def setUp(self):
+        self.data = SparkContextForTest.spark.read.option("header", "true") \
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/embeddings/sentence_embeddings.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+        sentence_detector = SentenceDetector() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
+        tokenizer = Tokenizer() \
+            .setInputCols(["sentence"]) \
+            .setOutputCol("token")
+        glove = WordEmbeddingsModel.pretrained() \
+            .setInputCols(["sentence", "token"]) \
+            .setOutputCol("embeddings")
+        sentence_embeddings = SentenceEmbeddings() \
+            .setInputCols(["sentence", "embeddings"]) \
+            .setOutputCol("sentence_embeddings") \
+            .setPoolingStrategy("AVERAGE")
+        embeddings_finisher = EmbeddingsFinisher() \
+            .setInputCols("sentence_embeddings") \
+            .setOutputCols("sentence_embeddings_vectors") \
+            .setOutputAsVector(True)
+        explode_vectors = SQLTransformer(statement="SELECT EXPLODE(sentence_embeddings_vectors) AS features, * FROM __THIS__")
+        kmeans = KMeans().setK(2).setSeed(1).setFeaturesCol("features")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_detector,
+            tokenizer,
+            glove,
+            sentence_embeddings,
+            embeddings_finisher,
+            explode_vectors,
+            kmeans
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
+
