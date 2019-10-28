b99ed74863be9e8c938f1a34e3db88f90d150c1d
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2019-10-28 20:19:08 +0100

    Adding Scala and Python unit tests to EmbeddingsFinisher transformer

---
+++ src/test/scala/com/johnsnowlabs/nlp/EmbeddingsFinisherTestSpec.scala
@@ -0,0 +1,169 @@
+package com.johnsnowlabs.nlp
+
+import com.johnsnowlabs.nlp.annotators.Tokenizer
+import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
+import com.johnsnowlabs.nlp.embeddings.{SentenceEmbeddings, WordEmbeddingsModel}
+import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+import org.apache.spark.ml.feature.{Normalizer, SQLTransformer}
+import org.apache.spark.sql.functions.{explode, size}
+import org.scalatest._
+
+class EmbeddingsFinisherTestSpec extends FlatSpec {
+
+  "EmbeddingsFinisher" should "correctly transform embeddings into array of floats for K-Mean in Spark ML" in {
+
+    import ResourceHelper.spark.implicits._
+
+    val smallCorpus = ResourceHelper.spark.read.option("header","true").csv("src/test/resources/embeddings/sentence_embeddings.csv")
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val sentence = new SentenceDetector()
+      .setInputCols("document")
+      .setOutputCol("sentence")
+      .setExplodeSentences(false)
+
+    val tokenizer = new Tokenizer()
+      .setInputCols(Array("sentence"))
+      .setOutputCol("token")
+
+    val embeddings = WordEmbeddingsModel.pretrained()
+      .setInputCols("sentence", "token")
+      .setOutputCol("embeddings")
+      .setCaseSensitive(false)
+
+    val embeddingsSentence = new SentenceEmbeddings()
+      .setInputCols(Array("sentence", "embeddings"))
+      .setOutputCol("sentence_embeddings")
+      .setPoolingStrategy("AVERAGE")
+
+    val embeddingsFinisher = new EmbeddingsFinisher()
+      .setInputCols("sentence_embeddings", "embeddings")
+      .setOutputCols("finished_sentence_embeddings", "finished_embeddings")
+      .setOutputAsVector(false)
+      .setCleanAnnotations(false)
+
+    val pipeline = new RecursivePipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentence,
+        tokenizer,
+        embeddings,
+        embeddingsSentence,
+        embeddingsFinisher
+      ))
+
+    val pipelineDF = pipeline.fit(smallCorpus).transform(smallCorpus)
+
+    pipelineDF.select(size(pipelineDF("finished_embeddings")).as("sentence_embeddings_size")).show
+    pipelineDF.select("finished_embeddings").show(2 ,false)
+
+
+    pipelineDF.select(size(pipelineDF("finished_sentence_embeddings")).as("sentence_embeddings_size")).show
+    pipelineDF.select("finished_sentence_embeddings").show(2 ,false)
+
+    val explodedVectors = pipelineDF.select($"sentence", explode($"finished_sentence_embeddings").as("features"))
+
+    explodedVectors.select("features").show
+
+    pipelineDF.printSchema()
+    explodedVectors.printSchema()
+
+    // Trains a k-means model.
+    val kmeans = new KMeans().setK(2).setSeed(1L).setMaxIter(1).setFeaturesCol("features")
+    val model = kmeans.fit(explodedVectors)
+
+    // Make predictions
+    val predictions = model.transform(explodedVectors)
+
+    // Evaluate clustering by computing Silhouette score
+    val evaluator = new ClusteringEvaluator()
+
+    val silhouette = evaluator.evaluate(predictions)
+    println(s"Silhouette with squared euclidean distance = $silhouette")
+
+    // Shows the result.
+    println("Cluster Centers: ")
+    model.clusterCenters.foreach(println)
+
+    predictions.select("sentence.result", "prediction").show(false)
+  }
+
+  "EmbeddingsFinisher" should "correctly transform embeddings into Vectors and normalize it by Spark ML" in {
+
+    val smallCorpus = ResourceHelper.spark.read.option("header","true").csv("src/test/resources/embeddings/sentence_embeddings.csv")
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val sentence = new SentenceDetector()
+      .setInputCols("document")
+      .setOutputCol("sentence")
+      .setExplodeSentences(false)
+
+    val tokenizer = new Tokenizer()
+      .setInputCols(Array("sentence"))
+      .setOutputCol("token")
+
+    val embeddings = WordEmbeddingsModel.pretrained()
+      .setInputCols("sentence", "token")
+      .setOutputCol("embeddings")
+      .setCaseSensitive(false)
+
+    val embeddingsSentence = new SentenceEmbeddings()
+      .setInputCols(Array("sentence", "embeddings"))
+      .setOutputCol("sentence_embeddings")
+      .setPoolingStrategy("AVERAGE")
+
+    val embeddingsFinisher = new EmbeddingsFinisher()
+      .setInputCols("sentence_embeddings", "embeddings")
+      .setOutputCols("sentence_embeddings_vectors", "embeddings_vectors")
+      .setOutputAsVector(true)
+      .setCleanAnnotations(false)
+
+    val explodeVectors = new SQLTransformer().setStatement(
+      "SELECT EXPLODE(sentence_embeddings_vectors) AS features, * FROM __THIS__")
+
+    // Normalize each Vector using $L^1$ norm.
+    val vectorNormalizer = new Normalizer()
+      .setInputCol("features")
+      .setOutputCol("normFeatures")
+      .setP(1.0)
+
+    val pipeline = new RecursivePipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentence,
+        tokenizer,
+        embeddings,
+        embeddingsSentence,
+        embeddingsFinisher,
+        explodeVectors,
+        vectorNormalizer
+      ))
+
+    val pipelineModel = pipeline.fit(smallCorpus)
+    val pielineDF = pipelineModel.transform(smallCorpus)
+
+    pielineDF.show()
+    pielineDF.printSchema()
+
+    pielineDF.select(size(pielineDF("finished_embeddings")).as("sentence_embeddings_size")).show
+    pielineDF.select("finished_embeddings").show(2 ,false)
+
+
+    pielineDF.select(size(pielineDF("finished_sentence_embeddings")).as("sentence_embeddings_size")).show
+    pielineDF.select("finished_sentence_embeddings").show(2 ,false)
+
+    pielineDF.select("features").show(2 ,false)
+
+    pielineDF.select("normFeatures").show(2 ,false)
+
+  }
+
+}
