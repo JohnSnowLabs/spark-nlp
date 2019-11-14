ae58ac7edd5a7aab78bd200ccb63e596f6e80735
jfernandrezj <andres@johnsnowlabs.com>
2019-11-14 16:04:49 -0300

    [skip travis] Merge Overlapping Matches Feature for TextMatcher

--- src/main/scala/com/johnsnowlabs/nlp/annotators/TextMatcherModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/TextMatcherModel.scala
@@ -6,6 +6,7 @@ import org.apache.spark.ml.util.Identifiable
 import com.johnsnowlabs.nlp.AnnotatorType._
 import com.johnsnowlabs.nlp.serialization.ArrayFeature
 import org.apache.spark.ml.param.BooleanParam
+import annotation.{tailrec => tco}
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -22,8 +23,8 @@ class TextMatcherModel(override val uid: String) extends AnnotatorModel[TextMatc
   override val inputAnnotatorTypes: Array[AnnotatorType] = Array(DOCUMENT, TOKEN)
 
   val parsedEntities = new ArrayFeature[Array[String]](this, "parsedEntities")
-
   val caseSensitive = new BooleanParam(this, "caseSensitive", "whether to match regardless of case. Defaults true")
+  val mergeOverlapping = new BooleanParam(this, "mergeOverlapping", "whether to merge overlapping matched chunks. Defaults false")
 
   def setEntities(value: Array[Array[String]]): this.type = set(parsedEntities, value)
 
@@ -33,10 +34,23 @@ class TextMatcherModel(override val uid: String) extends AnnotatorModel[TextMatc
   def getCaseSensitive: Boolean =
     $(caseSensitive)
 
+  def setMergeOverlapping(v: Boolean): this.type = set(mergeOverlapping, v)
+
+  def getMergeOverlapping: Boolean = $(mergeOverlapping)
+
   lazy val searchTrie = SearchTrie.apply($$(parsedEntities), $(caseSensitive))
 
   /** internal constructor for writabale annotator */
   def this() = this(Identifiable.randomUID("ENTITY_EXTRACTOR"))
+
+  @tco final def collapse(rs: List[(Int,Int)], sep: List[(Int,Int)] = Nil): List[(Int,Int)] = rs match {
+    case x :: y :: rest =>
+      if (y._1 > x._2) collapse(y :: rest, x :: sep)
+      else collapse( (x._1, x._2 max y._2) :: rest, sep)
+    case _ =>
+      (rs ::: sep).reverse
+  }
+  def merge(rs: List[(Int,Int)]): List[(Int,Int)] = collapse(rs.sortBy(_._1))
 
   /**
     * Searches entities and stores them in the annotation
@@ -56,7 +70,11 @@ class TextMatcherModel(override val uid: String) extends AnnotatorModel[TextMatc
           token.begin >= sentence.begin &&
             token.end <= sentence.end)
 
-      for ((begin, end) <- searchTrie.search(tokens.map(_.result))) {
+      val foundTokens = searchTrie.search(tokens.map(_.result)).toList
+
+      val finalTokens = if($(mergeOverlapping)) merge(foundTokens) else foundTokens
+
+      for ((begin, end) <- finalTokens) {
 
         val firstTokenBegin = tokens(begin).begin
         val lastTokenEnd = tokens(end).end
