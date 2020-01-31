c437b8579bdb5a2c518dd2e00f4a9daae7281db8
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-01-31 18:25:44 +0100

    [skip travis] Clean up ElmoEmbeddings

--- src/main/scala/com/johnsnowlabs/nlp/embeddings/ElmoEmbeddings.scala
+++ src/main/scala/com/johnsnowlabs/nlp/embeddings/ElmoEmbeddings.scala
@@ -1,17 +1,22 @@
 package com.johnsnowlabs.nlp.embeddings
 
+import java.io.File
+
 import com.johnsnowlabs.ml.tensorflow._
 import com.johnsnowlabs.nlp._
 import com.johnsnowlabs.nlp.annotators.common._
-import com.johnsnowlabs.nlp.serialization.MapFeature
-import com.johnsnowlabs.nlp.util.io.{ExternalResource, ReadAs, ResourceHelper}
 import com.johnsnowlabs.storage.HasStorageRef
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.ml.param.{IntArrayParam, IntParam}
+import org.apache.spark.ml.param.{IntArrayParam, IntParam, Param}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-
+/** Embeddings from a language model trained on the 1 Billion Word Benchmark.
+  *
+  * Note that this is a very computationally expensive module compared to word embedding modules that only perform embedding lookups.
+  * The use of an accelerator is recommended.
+  *
+  */
 class ElmoEmbeddings(override val uid: String) extends
   AnnotatorModel[ElmoEmbeddings]
   with WriteTensorflowModel
@@ -20,13 +25,11 @@ class ElmoEmbeddings(override val uid: String) extends
   with HasCaseSensitiveProperties {
 
   /** Annotator reference id. Used to identify elements in metadata or to refer to this annotator type */
-  override val inputAnnotatorTypes = Array(AnnotatorType.DOCUMENT, AnnotatorType.TOKEN)
+  override val inputAnnotatorTypes: Array[String] = Array(AnnotatorType.DOCUMENT, AnnotatorType.TOKEN)
   override val outputAnnotatorType: AnnotatorType = AnnotatorType.WORD_EMBEDDINGS
   val batchSize = new IntParam(this, "batchSize", "Batch size. Large values allows faster processing but requires more memory.")
-  val vocabulary: MapFeature[String, Int] = new MapFeature(this, "vocabulary")
   val configProtoBytes = new IntArrayParam(this, "configProtoBytes", "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()")
-  val maxSentenceLength = new IntParam(this, "maxSentenceLength", "Max sentence length to process")
-  val poolingLayer = new IntParam(this, "poolingLayer", "Set ELMO pooling layer to: -1 for for outputting only the 512 dim word embeddings, -2 for only outputting 1024 sentence embeddings.  Set 0 For both, word and sentence embeddings")
+  val poolingLayer = new Param[String](this, "poolingLayer", "Set ELMO pooling layer to: word_emb, lstm_outputs1, lstm_outputs2, or elmo")
   private var _model: Option[Broadcast[TensorflowElmo]] = None
 
   def this() = this(Identifiable.randomUID("ELMO_EMBEDDINGS"))
@@ -37,44 +40,47 @@ class ElmoEmbeddings(override val uid: String) extends
     this
   }
 
-  def setVocabulary(value: Map[String, Int]): this.type = set(vocabulary, value)
+  override def setDimension(value: Int): this.type = {
+    if(get(dimension).isEmpty)
+      set(this.dimension, value)
+    this
+
+  }
 
   def setConfigProtoBytes(bytes: Array[Int]): ElmoEmbeddings.this.type = set(this.configProtoBytes, bytes)
 
-  def setMaxSentenceLength(value: Int): this.type = {
-    if (get(maxSentenceLength).isEmpty)
-      set(maxSentenceLength, value)
-    this
-  }
-
   /** Function used to set the embedding output layer of the ELMO model
-    * word_emb: the character-based word representations with shape [batch_size, max_length, 512].  == -3
-    * lstm_outputs1: the first LSTM hidden state with shape [batch_size, max_length, 1024]. === -2
-    * lstm_outputs2: the second LSTM hidden state with shape [batch_size, max_length, 1024]. === -1
-    * elmo: the weighted sum of the 3 layers, where the weights are trainable. This tensor has shape [batch_size, max_length, 1024]  == 0
+    * word_emb: the character-based word representations with shape [batch_size, max_length, 512].  == word_emb
+    * lstm_outputs1: the first LSTM hidden state with shape [batch_size, max_length, 1024]. === lstm_outputs1
+    * lstm_outputs2: the second LSTM hidden state with shape [batch_size, max_length, 1024]. === lstm_outputs2
+    * elmo: the weighted sum of the 3 layers, where the weights are trainable. This tensor has shape [batch_size, max_length, 1024]  == elmo
     *
     * @param layer Layer specification
     */
-  def setPoolingLayer(layer: Int): this.type = {
+  def setPoolingLayer(layer: String): this.type = {
     layer match {
-      case 0 => set(poolingLayer, 0)
-      case -1 => set(poolingLayer, -1)
-      case -2 => set(poolingLayer, -2)
-      case -2 => set(poolingLayer, -3)
+      case "word_emb" => set(poolingLayer, "word_emb")
+      case "lstm_outputs1" => set(poolingLayer, "lstm_outputs1")
+      case "lstm_outputs2" => set(poolingLayer, "lstm_outputs2")
+      case "elmo" => set(poolingLayer, "elmo")
 
-      case _ => throw new MatchError("poolingLayer must be either 0, -1, -2 or -3  Definitions : -3 = word_emb, -2 = lstm1, -1=lmst2 , 0=elmo")
+      case _ => throw new MatchError("poolingLayer must be either word_emb, lstm_outputs1, lstm_outputs2, or elmo")
     }
   }
 
-  def getMaxSentenceLength: Int = $(maxSentenceLength)
-
-  def getPoolingLayer: Int = $(poolingLayer)
+  def getPoolingLayer: String = $(poolingLayer)
 
   setDefault(
     batchSize -> 32,
-    maxSentenceLength -> 64,
-    poolingLayer -> 0
+    poolingLayer -> "word_emb",
+    dimension -> 512
   )
+
+  private var tfHubPath: String = ""
+  def setTFhubPath(value: String): Unit = {
+    tfHubPath = value
+  }
+  def getTFhubPath: String = tfHubPath
 
   def setModelIfNotSet(spark: SparkSession, tensorflow: TensorflowWrapper): this.type = {
     if (_model.isEmpty) {
@@ -83,9 +89,6 @@ class ElmoEmbeddings(override val uid: String) extends
         spark.sparkContext.broadcast(
           new TensorflowElmo(
             tensorflow,
-            sentenceStartTokenId,
-            sentenceEndTokenId,
-            maxSentenceLength = $(maxSentenceLength),
             batchSize = $(batchSize),
             configProtoBytes = getConfigProtoBytes
           )
@@ -94,14 +97,6 @@ class ElmoEmbeddings(override val uid: String) extends
     }
 
     this
-  }
-
-  def sentenceStartTokenId: Int = {
-    $$(vocabulary)("<S>")
-  }
-
-  def sentenceEndTokenId: Int = {
-    $$(vocabulary)("</S>")
   }
 
   /**
@@ -130,8 +125,8 @@ class ElmoEmbeddings(override val uid: String) extends
 
   def getConfigProtoBytes: Option[Array[Byte]] = get(this.configProtoBytes).map(_.map(_.toByte))
 
-  override protected def afterAnnotate(dataset: DataFrame): DataFrame = { // TODO change embedding depending if word or sentence
-    wrapEmbeddingsMetadata(dataset.col(getOutputCol), 512, get(storageRef))
+  override protected def afterAnnotate(dataset: DataFrame): DataFrame = {
+    wrapEmbeddingsMetadata(dataset.col(getOutputCol), $(dimension), get(storageRef))
     dataset
   }
 
@@ -142,11 +137,8 @@ trait ReadablePretrainedElmoModel extends ParamsAndFeaturesReadable[ElmoEmbeddin
 
   /** Java compliant-overrides */
   override def pretrained(): ElmoEmbeddings = super.pretrained()
-
   override def pretrained(name: String): ElmoEmbeddings = super.pretrained(name)
-
   override def pretrained(name: String, lang: String): ElmoEmbeddings = super.pretrained(name, lang)
-
   override def pretrained(name: String, lang: String, remoteLoc: String): ElmoEmbeddings = super.pretrained(name, lang, remoteLoc)
 }
 
@@ -160,16 +152,27 @@ trait ReadElmoTensorflowModel extends ReadTensorflowModel {
     instance.setModelIfNotSet(spark, tf)
   }
 
+  addReader(readTensorflow)
 
-  def loadFromPython(folder: String, spark: SparkSession): ElmoEmbeddings = {
+  def loadSavedModel(folder: String, spark: SparkSession): ElmoEmbeddings = {
+
+    val f = new File(folder)
+    val savedModel = new File(folder, "saved_model.pb")
+    require(f.exists, s"Folder $folder not found")
+    require(f.isDirectory, s"File $folder is not folder")
+    require(
+      savedModel.exists(),
+      s"savedModel file saved_model.pb not found in folder $folder"
+    )
 
     val wrapper = TensorflowWrapper.read(folder, zipped = false, useBundle = true, tags = Array("serve"))
-    val vocab = folder + "/vocab-2016-09-10.txt"
-    val vocabResource = new ExternalResource(vocab, ReadAs.TEXT, Map("format" -> "text"))
-    val words = ResourceHelper.parseLines(vocabResource).zipWithIndex.toMap
-    new ElmoEmbeddings()
-      .setVocabulary(words)
+
+    val Elmo = new ElmoEmbeddings()
       .setModelIfNotSet(spark, wrapper)
+
+    Elmo.setTFhubPath(folder)
+
+    Elmo
   }
 }
 
