d609397c4c598b1dd291cbf64e1917dae89d62ac
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-01-31 18:34:01 +0100

    Update Scala unit test for ElmoEmbeddings

--- src/test/scala/com/johnsnowlabs/nlp/embeddings/ElmoEmbeddingsTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/embeddings/ElmoEmbeddingsTestSpec.scala
@@ -1,17 +1,18 @@
 package com.johnsnowlabs.nlp.embeddings
 
 import com.johnsnowlabs.nlp.annotators.Tokenizer
+import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
 import com.johnsnowlabs.nlp.base.DocumentAssembler
-import com.johnsnowlabs.nlp.util.io.ResourceHelper
 import com.johnsnowlabs.nlp.util.io.ResourceHelper.spark.implicits._
 import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.functions.{size, explode}
 import org.scalatest._
 
 class ElmoEmbeddingsTestSpec extends FlatSpec {
-  "Elmo Embeddings" should "generate annotations" in {
+  "Elmo Embeddings" should "generate annotations" ignore {
     System.out.println("Working Directory = " + System.getProperty("user.dir"))
     val data = Seq(
-      "i like pancakes in the summer",
+      "I like pancakes in the summer. I hate ice cream in winter.",
       "If I had asked people what they wanted, they would have said faster horses"
     ).toDF("text")
 
@@ -19,19 +20,38 @@ class ElmoEmbeddingsTestSpec extends FlatSpec {
       .setInputCol("text")
       .setOutputCol("document")
 
+    val sentence = new SentenceDetector()
+      .setInputCols("document")
+      .setOutputCol("sentence")
+
     val tokenizer = new Tokenizer()
       .setInputCols(Array("document"))
       .setOutputCol("token")
-    val embeddings = ElmoEmbeddings.loadFromPython("src/test/resources/embeddings/elmo", ResourceHelper.spark).setPoolingLayer(0)
-      .setInputCols(Array("token", "document"))
-      .setOutputCol("elmo")
 
-    val pipeline = new Pipeline().setStages(Array(document, tokenizer, embeddings))
+    val elmoSavedModel = ElmoEmbeddings.pretrained()
+      .setPoolingLayer("word_emb")
+      .setInputCols(Array("token", "document"))
+      .setOutputCol("embeddings")
+
+    elmoSavedModel.write.overwrite().save("./tmp_elmo_tf")
+
+    val embeddings = ElmoEmbeddings.load("./tmp_elmo_tf")
+
+    val pipeline = new Pipeline().setStages(Array(
+      document,
+      sentence,
+      tokenizer,
+      embeddings
+    ))
 
     val elmoDDD = pipeline.fit(data).transform(data)
 
+    elmoDDD.select("embeddings.result").show(false)
+    elmoDDD.select("embeddings.metadata").show(false)
+    val explodeEmbds = elmoDDD.select(explode($"embeddings.embeddings").as("embedding"))
+    elmoDDD.select(size(elmoDDD("embeddings.embeddings")).as("embeddings_size")).show
+    explodeEmbds.select(size($"embedding").as("embeddings_size")).show
 
-    elmoDDD.show(false)
 
   }
 
