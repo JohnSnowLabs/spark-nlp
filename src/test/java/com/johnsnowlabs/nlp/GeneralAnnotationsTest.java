4274748785a3e6eee39db03b59f9f4a57d770174
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-03-02 15:58:16 +0100

    Add argument to Java unit test

--- src/test/java/com/johnsnowlabs/nlp/GeneralAnnotationsTest.java
+++ src/test/java/com/johnsnowlabs/nlp/GeneralAnnotationsTest.java
@@ -30,7 +30,7 @@ public class GeneralAnnotationsTest {
         Pipeline pipeline = new Pipeline();
         pipeline.setStages(new PipelineStage[] {document, tokenizer});
 
-        SparkSession spark = com.johnsnowlabs.nlp.SparkNLP.start();
+        SparkSession spark = com.johnsnowlabs.nlp.SparkNLP.start(true);
 
         LinkedList<String> text = new java.util.LinkedList<String>();
 
