2ac1db9e6c4006619b77219cf4c5f47e5ccb2960
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-03-12 10:40:32 +0100

    [skip travis] Add unit test with StopWordsCleaner

--- src/test/scala/com/johnsnowlabs/nlp/TokenAssemblerTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/TokenAssemblerTestSpec.scala
@@ -44,4 +44,49 @@ class TokenAssemblerTestSpec extends FlatSpec {
     pipelineDF.select("tokens").show(2, false)
   }
 
+  "TokenAssembler" should "correctly merge tokens after StopWordsCleaner" in {
+
+    val smallCorpus = ResourceHelper.spark.read.option("header","true")
+      .csv("src/test/resources/embeddings/sentence_embeddings.csv")
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val sentence = new SentenceDetector()
+      .setInputCols("document")
+      .setOutputCol("sentence")
+
+    val token = new Tokenizer()
+      .setInputCols("document")
+      .setOutputCol("tokens")
+
+    val stop = new StopWordsCleaner()
+      .setInputCols("tokens")
+      .setOutputCol("cleaned")
+
+    val tokenAssem = new TokenAssembler()
+      .setInputCols("cleaned")
+      .setOutputCol("newDocs")
+
+    val pipeline = new RecursivePipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentence,
+        token,
+        stop,
+        tokenAssem
+      ))
+
+    val pipelineDF = pipeline.fit(smallCorpus).transform(smallCorpus)
+
+    pipelineDF.show()
+    pipelineDF.select("document").show(2, false)
+    pipelineDF.select("sentence").show(2, false)
+    pipelineDF.select("tokens").show(2, false)
+    pipelineDF.select("cleaned").show(2, false)
+    pipelineDF.select("newDocs").show(2, false)
+
+  }
+
 }
