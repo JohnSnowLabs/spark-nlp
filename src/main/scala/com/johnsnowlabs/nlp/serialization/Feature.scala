f74053b953f12cad6760867efb0a51c47d09e80f
rohit kumar <rohit@johnsnowlabs.com>
2020-03-19 10:35:19 +0100

    removed configLoader

--- src/main/scala/com/johnsnowlabs/nlp/serialization/Feature.scala
+++ src/main/scala/com/johnsnowlabs/nlp/serialization/Feature.scala
@@ -5,7 +5,6 @@ import com.github.liblevenshtein.serialization.PlainTextSerializer
 import com.github.liblevenshtein.transducer.{Candidate, ITransducer, Transducer}
 import com.johnsnowlabs.nlp.HasFeatures
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.util.ConfigLoader
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.{Encoder, Encoders, SparkSession}
@@ -15,11 +14,12 @@ import scala.reflect.ClassTag
 abstract class Feature[Serializable1, Serializable2, TComplete: ClassTag](model: HasFeatures, val name: String) extends Serializable {
   model.features.append(this)
 
-  private val config = ConfigLoader.retrieve
+  // private val config = ConfigLoader.retrieve
   private val spark = ResourceHelper.spark
 
-  val serializationMode: String = config.getString("sparknlp.settings.annotatorSerializationFormat")
-  val useBroadcast: Boolean = config.getBoolean("sparknlp.settings.useBroadcastForFeatures")
+  val serializationMode: String = "object"
+  //config.getString("sparknlp.settings.annotatorSerializationFormat")
+  val useBroadcast: Boolean = true //config.getBoolean("sparknlp.settings.useBroadcastForFeatures")
 
   final protected var broadcastValue: Option[Broadcast[TComplete]] = None
 
