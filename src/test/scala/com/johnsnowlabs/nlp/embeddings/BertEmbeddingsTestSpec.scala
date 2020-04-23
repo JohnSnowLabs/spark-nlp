bb40151c1e1cfb16f566f493f002621aea99bd9d
bvannah <brian.s.vannah@gmail.com>
2020-04-22 20:42:53 -0400

    debugged

--- src/test/scala/com/johnsnowlabs/nlp/embeddings/BertEmbeddingsTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/embeddings/BertEmbeddingsTestSpec.scala
@@ -57,7 +57,7 @@ class BertEmbeddingsTestSpec extends FlatSpec {
 
   }
 
-  "Bert Embeddings" should "correctly work in a pipeline" ignore {
+  "Bert Embeddings" should "correctly work in a pipeline" in {
 
     val conll = CoNLL()
     val training_data = conll.readDataset(ResourceHelper.spark, "src/test/resources/conll2003/eng.train")
