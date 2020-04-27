178191d89d011dfeccef38ede64f44e9ed469800
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-04-27 12:34:15 +0200

    [skip travis] Add SEP CLS tokens
    Also fix mapping ids

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowXlnet.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowXlnet.scala
@@ -7,9 +7,8 @@ import scala.collection.JavaConverters._
 
 class TensorflowXlnet(val tensorflow: TensorflowWrapper,
                       val spp: SentencePieceWrapper,
-                      batchSize: Int,
                       configProtoBytes: Option[Array[Byte]] = None
-                      ) extends Serializable {
+                     ) extends Serializable {
 
   // keys representing the input and output tensors of the ALBERT model
   private val tokenIdsKey = "input_ids"
@@ -17,8 +16,7 @@ class TensorflowXlnet(val tensorflow: TensorflowWrapper,
   private val segmentIdsKey = "segment_ids"
   private val outputSequenceKey = "module/seq_out"
 
-  private val specialTokenSEPId = Array(4145, 83, 8186, 3158)
-  private val specialTokenCLSId = Array(4145, 7416, 83, 3158)
+  private val tokenSEPCLSIds = Array(4, 3)
 
   def getSpecialTokens(token: String): Array[Int] = {
     spp.getSppModel.encodeAsIds(token)
@@ -42,16 +40,16 @@ class TensorflowXlnet(val tensorflow: TensorflowWrapper,
 
     batch.map { tokenIds =>
       val diff = maxSentenceLength - tokenIds.length
-      segmentBuffers.put(Array.fill(maxSentenceLength)(0))
+      segmentBuffers.put(Array.fill(maxSentenceLength-1)(0) ++ Array(2))
 
       if (tokenIds.length >= maxSentenceLength) {
         tokenBuffers.put(tokenIds)
-        maskBuffers.put(tokenIds.map(x=> if (x == 0.0f) 0 else 1.0f))
+        maskBuffers.put(tokenIds.map(x=> if (x == 0) 0.0f else 1.0f))
       }
       else {
         val newTokenIds = tokenIds ++ Array.fill(1, diff)(0).head
         tokenBuffers.put(newTokenIds)
-        maskBuffers.put(newTokenIds.map(x=> if (x == 0.0f) 0 else 1.0f))
+        maskBuffers.put(newTokenIds.map(x=> if (x == 0) 0.0f else 1.0f))
       }
     }
 
@@ -109,11 +107,9 @@ class TensorflowXlnet(val tensorflow: TensorflowWrapper,
 
       val tokensPiece = tokenize(batch, maxSentenceLength, caseSensitive)
       val tokenIds = tokensPiece.map { sentence =>
-        sentence.flatMap(x => x.tokens.map(x => x.pieceId)) ++ specialTokenSEPId ++ specialTokenCLSId
+        sentence.flatMap(x => x.tokens.map(x => x.pieceId)) ++ tokenSEPCLSIds
       }
       val vectors = tag(tokenIds)
-      val t = vectors.length
-      t
       val tokenIdsVectors = tokenIds.zip(vectors).map { x =>
         x._1.zip(x._2).toMap
       }
@@ -144,7 +140,8 @@ class TensorflowXlnet(val tensorflow: TensorflowWrapper,
   Seq[Array[WordpieceTokenizedSentence]] = {
 
     val sentecneTokenPieces = sentences.map { s =>
-      val shrinkedSentence = s.indexedTokens.take(maxSeqLength - 1)
+      // Account for one [SEP] & one [CLS]
+      val shrinkedSentence = s.indexedTokens.take(maxSeqLength - 3)
       shrinkedSentence.map{
         case(token) =>
           val tokenContent = if (caseSensitive) token.token else token.token.toLowerCase()
