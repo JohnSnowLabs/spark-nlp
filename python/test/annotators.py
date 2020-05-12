8ac4439a5c88701a90ec2001f4a2c814c4d2f8ae
Alina <erar1@mail.ru>
2020-05-12 14:01:08 +0300

    add python ans scala tests for XlnetEmbeddings

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -1086,3 +1086,33 @@ class AlbertEmbeddingsTestSpec(unittest.TestCase):
         model = pipeline.fit(self.data)
         model.transform(self.data).show()
 
+
+class XlnetEmbeddingsTestSpec(unittest.TestCase):
+
+    def setUp(self):
+        self.data = SparkContextForTest.spark.read.option("header", "true") \
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/embeddings/sentence_embeddings.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+        sentence_detector = SentenceDetector() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
+        tokenizer = Tokenizer() \
+            .setInputCols(["sentence"]) \
+            .setOutputCol("token")
+        xlnet = XlnetEmbeddings.pretrained() \
+            .setInputCols(["sentence", "token"]) \
+            .setOutputCol("embeddings")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_detector,
+            tokenizer,
+            xlnet
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
