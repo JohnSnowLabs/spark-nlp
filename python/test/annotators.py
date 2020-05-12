c75da25046cb6d6df617713245c0ecea3002b9eb
Alina <erar1@mail.ru>
2020-05-12 20:13:37 +0300

    Added tests for the SentimentDLTestSpec in python

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -1086,3 +1086,36 @@ class AlbertEmbeddingsTestSpec(unittest.TestCase):
         model = pipeline.fit(self.data)
         model.transform(self.data).show()
 
+
+class SentimentDLTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkSessionForTest.spark.read.option("header", "true") \
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/classifier/sentiment.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+
+        sentence_embeddings = UniversalSentenceEncoder.pretrained() \
+            .setInputCols("document") \
+            .setOutputCol("sentence_embeddings")
+
+        classifier = SentimentDLApproach() \
+            .setInputCols("sentence_embeddings") \
+            .setOutputCol("category") \
+            .setLabelColumn("label")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_embeddings,
+            classifier
+        ])
+
+        model = pipeline.fit(self.data)
+        model.stages[-1].write().overwrite().save('./tmp_sentimentDL_model')
+
+        sentimentdlModel = SentimentDLModel.load("./tmp_sentimentDL_model") \
+            .setInputCols(["sentence_embeddings"]) \
+            .setOutputCol("class")
+
