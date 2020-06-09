74f173b2f0221db6ea75044863362ac709406188
rohit kumar <rohit@johnsnowlabs.com>
2020-06-09 09:14:51 +0200

    fixed databricks setGraph Path issue.

--- src/main/scala/com/johnsnowlabs/nlp/util/io/ResourceHelper.scala
+++ src/main/scala/com/johnsnowlabs/nlp/util/io/ResourceHelper.scala
@@ -1,10 +1,5 @@
 package com.johnsnowlabs.nlp.util.io
 
-import java.io._
-import java.net.{URL, URLDecoder}
-import java.nio.file.{Files, Paths}
-import java.util.jar.JarFile
-
 import com.johnsnowlabs.nlp.annotators.Tokenizer
 import com.johnsnowlabs.nlp.annotators.common.{TaggedSentence, TaggedWord}
 import com.johnsnowlabs.nlp.util.io.ReadAs._
@@ -402,7 +397,7 @@ object ResourceHelper {
           throw new FileNotFoundException("Word count dictionary for spell checker does not exist or is empty")
         wordCount
       case SPARK =>
-        import spark.implicits._
+
         val dataset = spark.read.options(externalResource.options).format(externalResource.options("format"))
           .load(externalResource.path)
         val transformation = {
@@ -446,6 +441,13 @@ object ResourceHelper {
   }
 
   def listLocalFiles(path: String): List[File] = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    if (fs.getScheme == "dbfs") {
+      //return fs.listStatus(new Path(path)).map(_.getPath().toString).map(new File(_)).toList
+      val filesPath = Option(new File(path.replace("file:", "")).listFiles())
+      val files = filesPath.getOrElse(throw new FileNotFoundException(s"folder: $path not found"))
+      return files.toList
+    }
     val filesPath = Option(new File(path).listFiles())
     val files = filesPath.getOrElse(throw new FileNotFoundException(s"folder: $path not found"))
     files.toList
