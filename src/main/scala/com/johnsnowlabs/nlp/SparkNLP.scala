a7f7b36567e20bd3df8de3039d488e379f537c98
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-06-27 01:02:42 +0200

    [skip travis] set maxResultSize to 0 for unlimited size

--- src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
+++ src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
@@ -13,7 +13,7 @@ object SparkNLP {
       .config("spark.driver.memory", "16G")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .config("spark.kryoserializer.buffer.max", "1000M")
-      .config("spark.driver.maxResultSize", "4096G")
+      .config("spark.driver.maxResultSize", "0")
     
     if(gpu){
       build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:2.5.2")
