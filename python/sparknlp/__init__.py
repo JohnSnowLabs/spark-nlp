e21f74eb89ac2054098161b985b596b85a7e2707
rohit kumar <rohit@johnsnowlabs.com>
2020-07-11 12:31:53 +0200

    changed spark start to spark version

--- python/sparknlp/__init__.py
+++ python/sparknlp/__init__.py
@@ -34,7 +34,7 @@ annotators = annotator
 embeddings = annotator
 
 
-def start(gpu=False):
+def start(gpu=False,isSpark23=False):
     builder = SparkSession.builder \
         .appName("Spark NLP") \
         .master("local[*]") \
@@ -43,11 +43,21 @@ def start(gpu=False):
         .config("spark.kryoserializer.buffer.max", "1000M") \
         .config("spark.driver.maxResultSize", "4096G")
     if gpu:
-        builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:2.5.2")
+        if isSpark23:
+            builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu-spark23_2.11:2.5.2")
+        else:
+            builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:2.5.2")
     else:
-        builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.2")
-        
+        if isSpark23:
+            builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-spark23_2.11:2.5.2")
+        else:
+            builder.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.2")
+
     return builder.getOrCreate()
+
+
+def start(gpu=False):
+    return start(gpu,False)
 
 
 def version():
--- src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
+++ src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
@@ -5,8 +5,7 @@ import org.apache.spark.sql.SparkSession
 object SparkNLP {
 
   val currentVersion = "2.5.2"
-
-  def start(gpu:Boolean = false): SparkSession = {
+  def start(gpu:Boolean=false,isSpark23:Boolean=false): SparkSession = {
     val build = SparkSession.builder()
       .appName("Spark NLP")
       .master("local[*]")
@@ -16,10 +15,21 @@ object SparkNLP {
       .config("spark.driver.maxResultSize", "4096G")
     
     if(gpu){
-      build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:2.5.2")
+      if(isSpark23){
+        build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu-spark23_2.11:2.5.2")
+      }else{
+        build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:2.5.2")
+      }
+
     }
-    else
-      build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.2")
+    else {
+      if(isSpark23){
+        build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp-spark23_2.11:2.5.2")
+      }else{
+        build.config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.2")
+      }
+
+    }
 
     build.getOrCreate()
   }
