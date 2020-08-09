1cf088608c62c43ac69e575255fb3b28391ccf8b
malikhijazi <mhijazi7@bloomberg.net>
2020-08-09 01:16:05 -0400

    no need to check for availability since its done in createNewFile

--- src/main/scala/com/johnsnowlabs/nlp/util/io/OutputHelper.scala
+++ src/main/scala/com/johnsnowlabs/nlp/util/io/OutputHelper.scala
@@ -35,9 +35,7 @@ object OutputHelper {
       writer.append(content + System.lineSeparator())
       writer.close()
     } else {
-      if (!fs.exists(targetPath)) {
-        fs.createNewFile(targetPath)
-      }
+      fs.createNewFile(targetPath)
       val fo = fs.append(targetPath)
       val writer = new PrintWriter(fo, true)
       writer.append(content + System.lineSeparator())
