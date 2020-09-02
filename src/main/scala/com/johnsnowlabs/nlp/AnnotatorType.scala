b21a51d3832e3ef0df4eaafeea1d9a76caa2b9e3
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-09-02 14:32:56 +0200

    [skip travis] Add keyword as AnnotatorType

--- src/main/scala/com/johnsnowlabs/nlp/AnnotatorType.scala
+++ src/main/scala/com/johnsnowlabs/nlp/AnnotatorType.scala
@@ -17,5 +17,6 @@ object AnnotatorType {
   val DEPENDENCY = "dependency"
   val LABELED_DEPENDENCY = "labeled_dependency"
   val LANGUAGE = "language"
+  val KEYWORD = "keyword"
   val DUMMY = "dummy"
 }
