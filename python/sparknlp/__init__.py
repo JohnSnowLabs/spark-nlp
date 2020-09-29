63f443c15bb29323df18523c1ecee91d4c0993a8
i <i.i.vankov@gmail.com>
2020-09-29 10:19:23 +0300

    python wrapping

--- python/sparknlp/__init__.py
+++ python/sparknlp/__init__.py
@@ -30,6 +30,7 @@ sys.modules['com.johnsnowlabs.nlp.annotators.classifier.dl'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.spell.context'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.ld'] = annotator
 sys.modules['com.johnsnowlabs.nlp.annotators.ld.dl'] = annotator
+sys.modules['com.johnsnowlabs.nlp.annotators.sentence_detector_dl'] = annotator
 
 annotators = annotator
 embeddings = annotator
