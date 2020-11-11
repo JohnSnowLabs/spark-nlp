8aa07894d4113bab61166691887cf498aa8af580
Ivan Vankov <i.i.vankov@gmail.com>
2020-11-11 17:32:00 +0200

    added pytorch created SBert loading functionality

--- python/sparknlp/annotator.py
+++ python/sparknlp/annotator.py
@@ -1819,6 +1819,11 @@ class BertSentenceEmbeddings(AnnotatorModel, HasEmbeddingsProperties, HasCaseSen
                       "Batch size. Large values allows faster processing but requires more memory.",
                       typeConverter=TypeConverters.toInt)
 
+    isSBert = Param(Params._dummy(),
+                      "isSBert",
+                      "Specify whether the model is SBert or not.",
+                      typeConverter=TypeConverters.toBoolean)
+
     configProtoBytes = Param(Params._dummy(),
                              "configProtoBytes",
                              "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()",
@@ -1832,6 +1837,9 @@ class BertSentenceEmbeddings(AnnotatorModel, HasEmbeddingsProperties, HasCaseSen
 
     def setBatchSize(self, value):
         return self._set(batchSize=value)
+
+    def setSBert(self, value):
+        return self._set(isSBert=value)
 
     @keyword_only
     def __init__(self, classname="com.johnsnowlabs.nlp.embeddings.BertSentenceEmbeddings", java_model=None):
