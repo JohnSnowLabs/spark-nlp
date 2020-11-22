f83a8c79ddae645e9dd99a95fe9f64ef98fdae22
stefanolori <wolliq@gmail.com>
2020-11-22 11:03:03 +0100

    Added HTML web pages unit tests

--- src/test/scala/com/johnsnowlabs/nlp/annotators/DocumentNormalizerBehaviors.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/DocumentNormalizerBehaviors.scala
@@ -1,6 +1,6 @@
 package com.johnsnowlabs.nlp.annotators
 
-import com.johnsnowlabs.nlp.{Annotation, AnnotatorBuilder, DataBuilder}
+import com.johnsnowlabs.nlp.{Annotation, AnnotatorBuilder, DataBuilder, SparkAccessor}
 import org.apache.spark.sql.{Dataset, Row}
 import org.scalatest.Matchers.{convertToAnyShouldWrapper, equal}
 import org.scalatest._
@@ -10,20 +10,38 @@ import scala.language.reflectiveCalls
 
 trait DocumentNormalizerBehaviors extends FlatSpec {
 
-  val path = ""
+  val DOC_NORMALIZER_BASE_DIR = "src/test/resources/doc-normalizer"
 
   private def loadDocNormalizerDataset(path: String): Dataset[Row] = {
     DataBuilder
       .basicDataBuild(
-        Source.fromFile("src/test/resources/doc-normalizer/scraped_text_small.txt").getLines().mkString)
+        Source.fromFile(path).getLines().mkString)
   }
 
   def fixture = new {
-    val scrapedTextDS: Dataset[Row] = loadDocNormalizerDataset("doc-normalizer/scraped_text_small.txt")
+    val scrapedTextDS: Dataset[Row] =
+      loadDocNormalizerDataset(s"$DOC_NORMALIZER_BASE_DIR/scraped_text_small.txt")
 
-    val df = AnnotatorBuilder.withDocumentNormalizerPipeline(scrapedTextDS)
+    val annotated = AnnotatorBuilder.withDocumentNormalizerPipeline(scrapedTextDS)
 
-    val normalizedDoc: Array[Annotation] = df
+    val normalizedDoc: Array[Annotation] = annotated
+      .select("normalizedDocument")
+      .collect
+      .flatMap { _.getSeq[Row](0) }
+      .map { Annotation(_) }
+  }
+
+  def fixtureMultipleDocs = new {
+    import SparkAccessor.spark.implicits._
+    val dataset =
+      SparkAccessor.spark.sparkContext
+        .wholeTextFiles(s"$DOC_NORMALIZER_BASE_DIR/webpage-samples")
+        .toDF("filename", "text")
+        .select("text")
+
+    val annotated = AnnotatorBuilder.withDocumentNormalizerPipelineForHTML(dataset)
+
+    val normalizedDoc: Array[Annotation] = annotated
       .select("normalizedDocument")
       .collect
       .flatMap { _.getSeq[Row](0) }
@@ -42,5 +60,25 @@ trait DocumentNormalizerBehaviors extends FlatSpec {
     val f = fixture
 
     Map("sentence" -> "0") should equal (f.normalizedDoc.head.metadata)
+  }
+
+  "A DocumentNormalizer" should "annotate multiple tagged documents with the correct indexes" in {
+    val f = fixtureMultipleDocs
+
+    0 should equal (f.normalizedDoc.head.begin)
+
+    13410 should equal (f.normalizedDoc.head.end)
+  }
+
+  "A DocumentNormalizer" should "annotate multiple tagged documents with the correct metadata" in {
+    val f = fixtureMultipleDocs
+
+    Map("sentence" -> "0") should equal (f.normalizedDoc.head.metadata)
+  }
+
+  "A DocumentNormalizer" should "annotate the correct number of sample documents" in {
+    val f = fixtureMultipleDocs
+
+    3 should equal (f.annotated.count)
   }
 }
