32ec26e44f6248d065e4e379a37bdc38ab75706f
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-12-03 22:12:26 +0100

    Update ClassifierDLModel with new changes

--- src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/ClassifierDLModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/ClassifierDLModel.scala
@@ -152,7 +152,7 @@ class ClassifierDLModel(override val uid: String)
 
   override def onWrite(path: String, spark: SparkSession): Unit = {
     super.onWrite(path, spark)
-    writeTensorflowModel(
+    writeTensorflowModelV2(
       path,
       spark,
       getModelIfNotSet.tensorflow,
@@ -186,7 +186,7 @@ trait ReadClassifierDLTensorflowModel extends ReadTensorflowModel {
 
   def readTensorflow(instance: ClassifierDLModel, path: String, spark: SparkSession): Unit = {
 
-    val tf = readTensorflowChkPoints(path, spark, "_classifierdl_tf", initAllTables = true)
+    val tf = readTensorflowModel(path, spark, "_classifierdl_tf", initAllTables = true)
     instance.setModelIfNotSet(spark, tf)
     // This allows for Python to access getClasses function
     val encoder = new ClassifierDatasetEncoder(instance.datasetParams.get.get)
