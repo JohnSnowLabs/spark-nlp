c88d3cf83e9cb28cce2dc66fb89938e1c9c4ced0
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2020-12-03 23:03:38 +0100

    Adopt to the new methods of write and read

--- src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/MultiClassifierDLModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/MultiClassifierDLModel.scala
@@ -178,7 +178,7 @@ class MultiClassifierDLModel(override val uid: String)
 
   override def onWrite(path: String, spark: SparkSession): Unit = {
     super.onWrite(path, spark)
-    writeTensorflowModel(
+    writeTensorflowModelV2(
       path,
       spark,
       getModelIfNotSet.tensorflow,
@@ -212,7 +212,7 @@ trait ReadMultiClassifierDLTensorflowModel extends ReadTensorflowModel {
 
   def readTensorflow(instance: MultiClassifierDLModel, path: String, spark: SparkSession): Unit = {
 
-    val tf = readTensorflowChkPoints(path, spark, "_multiclassifierdl_tf", initAllTables = true)
+    val tf = readTensorflowModel(path, spark, "_multiclassifierdl_tf", initAllTables = true)
     instance.setModelIfNotSet(spark, tf)
     // This allows for Python to access getClasses function
     val encoder = new ClassifierDatasetEncoder(instance.datasetParams.get.get)
--- src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/SentimentDLModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/SentimentDLModel.scala
@@ -110,7 +110,7 @@ class SentimentDLModel(override val uid: String)
 
   override def onWrite(path: String, spark: SparkSession): Unit = {
     super.onWrite(path, spark)
-    writeTensorflowModel(
+    writeTensorflowModelV2(
       path,
       spark,
       getModelIfNotSet.tensorflow,
@@ -144,7 +144,7 @@ trait ReadSentimentDLTensorflowModel extends ReadTensorflowModel {
 
   def readTensorflow(instance: SentimentDLModel, path: String, spark: SparkSession): Unit = {
 
-    val tf = readTensorflowChkPoints(path, spark, "_sentimentdl_tf", initAllTables = true)
+    val tf = readTensorflowModel(path, spark, "_sentimentdl_tf", initAllTables = true)
     instance.setModelIfNotSet(spark, tf)
     // This allows for Python to access getClasses function
     val encoder = new ClassifierDatasetEncoder(instance.datasetParams.get.get)
