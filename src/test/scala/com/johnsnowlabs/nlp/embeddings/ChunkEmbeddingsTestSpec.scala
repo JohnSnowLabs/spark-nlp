77e194e99ba2052e1cda9985e08696bce4d01fcd
jfernandrezj <andres@johnsnowlabs.com>
2020-12-08 12:06:42 -0300

    Full pipeline test [missing assertion] [skip ci]

--- src/test/scala/com/johnsnowlabs/nlp/embeddings/ChunkEmbeddingsTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/embeddings/ChunkEmbeddingsTestSpec.scala
@@ -1,11 +1,11 @@
 package com.johnsnowlabs.nlp.embeddings
 
-import com.johnsnowlabs.nlp.annotator.{Chunker, PerceptronModel}
+import com.johnsnowlabs.nlp.annotator.{ChunkTokenizer, Chunker, NerConverter, NerDLModel, PerceptronModel, SentenceDetectorDLModel}
 import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
 import com.johnsnowlabs.nlp.annotators.{NGramGenerator, StopWordsCleaner, Tokenizer}
 import com.johnsnowlabs.nlp.base.{DocumentAssembler, RecursivePipeline}
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.nlp.{AnnotatorBuilder, EmbeddingsFinisher, Finisher}
+import com.johnsnowlabs.nlp.{AnnotatorBuilder, Chunk2Doc, EmbeddingsFinisher, Finisher}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.functions.size
 import org.scalatest._
@@ -231,6 +231,35 @@ class ChunkEmbeddingsTestSpec extends FlatSpec {
     pipelineDF.select("embeddings").show(1)
     pipelineDF.select("sentence_embeddings").show(1)
 
+  }
+
+  "ChunkEmbeddings new" should "correctly work with empty tokens" in {
+    val rows = Array(
+          Tuple1("""In short, the patient is a 55-year-old entleman with a broken leg. The guy also has long-standing morbid obesity, resistant to nonsurgical
+      methods of weight loss with of 69.7 with comorbidities of hypertension, atrial fibrillation,
+      hyperlipidemia, possible sleep apnea, and also osteoarthritis of the lower extremities.
+      He is also an ex-smoker. Does not have diabetes at all. He is currently smoking SMOKER and he is planning to quit and
+      at least he should do this six to eight days before for multiple reasons including decreasing the DVT,
+      pulmonary embolism rates and marginal ulcer problems after surgery, which will be discussed later on.""")
+    )
+    val df = ResourceHelper.spark.createDataFrame(rows).toDF("text")
+    val dac = new DocumentAssembler().setInputCol("text").setOutputCol("doc")
+    val sd = SentenceDetectorDLModel.pretrained("sentence_detector_dl_healthcare","en","clinical/models")
+      .setInputCols("doc").setOutputCol("sentence")
+    val tk = new Tokenizer().setInputCols("sentence").setOutputCol("token")
+    val emb = WordEmbeddingsModel.pretrained("embeddings_clinical","en","clinical/models").setOutputCol("embs")
+    val ner = NerDLModel.pretrained("ner_clinical","en","clinical/models").setInputCols("sentence","token","embs").setOutputCol("ner")
+    val conv = new NerConverter().setInputCols("sentence","token","ner").setOutputCol("ner_chunk")
+    val c2d = new Chunk2Doc().setInputCols("ner_chunk").setOutputCol("chunk_doc")
+    val ctk = new ChunkTokenizer().setInputCols("ner_chunk").setOutputCol("chunk_tk") //Optional
+    val cembs = WordEmbeddingsModel.pretrained("embeddings_healthcare_100d","en","clinical/models")
+      .setInputCols("chunk_doc","chunk_tk").setOutputCol("chunk_tk_embs")
+    val aggembs = new ChunkEmbeddings().setInputCols("ner_chunk","chunk_tk_embs").setOutputCol("chunk_embs")
+      .setPoolingStrategy("AVERAGE").setSkipOOV(false)
+    val embs_pl = new Pipeline().setStages(Array(dac, sd, tk, emb, ner, conv, c2d, ctk, cembs, aggembs)).fit(df)
+    val out_df = embs_pl.transform(df)
+    out_df.selectExpr("explode(arrays_zip(ner_chunk.result, chunk_embs.embeddings)) as a").show(100, truncate=50)
+    //TODO:
   }
 
 }
