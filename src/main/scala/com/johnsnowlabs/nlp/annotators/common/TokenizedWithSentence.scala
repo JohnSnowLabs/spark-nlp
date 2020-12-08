0f91849ead32177f6e96bcb321ddcbcd7ebdf2a2
jfernandrezj <andres@johnsnowlabs.com>
2020-12-08 11:45:43 -0300

    [skip ci] if no sentence metadata at all, don't group by and assume sentence indexes to be array's indexes

--- src/main/scala/com/johnsnowlabs/nlp/annotators/common/TokenizedWithSentence.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/common/TokenizedWithSentence.scala
@@ -12,26 +12,37 @@ object TokenizedWithSentence extends Annotated[TokenizedSentence] {
       .filter(_.annotatorType == annotatorType)
       .toArray
 
+    val hasMeta = annotations.find(_.metadata.get("sentence").nonEmpty).nonEmpty
     val sentences = SentenceSplit.unpack(annotations)
 
     /** // Evaluate whether to enable this validation to check proper usage of DOCUMENT and SENTENCE within entire pipelines
-    require(tokens.map(_.metadata.getOrElse("sentence", "0").toInt).distinct.length == sentences.length,
-      "Inconsistencies found in pipeline. Tokens in sentences does not match with sentence count")
+      * require(tokens.map(_.metadata.getOrElse("sentence", "0").toInt).distinct.length == sentences.length,
+      * "Inconsistencies found in pipeline. Tokens in sentences does not match with sentence count")
       */
+    val retAnns = if (!hasMeta) {
+      sentences.map(sentence => filterTokensFromSentence(sentence, tokens))
+        .zipWithIndex.map { case (indexedTokens, index) => TokenizedSentence(indexedTokens.toArray, index) }.filter(_.indexedTokens.nonEmpty)
+    } else {
+      sentences.flatMap(sentence => filterTokensFromSentence(sentence, tokens))
+        .groupBy(_.sentenceIndex).map { case (sentenceIndex, indexedTokens) => TokenizedSentence(indexedTokens.toArray, sentenceIndex) }
+        .filter(_.indexedTokens.nonEmpty).toSeq.sortBy(_.sentenceIndex)
+    }
 
-    sentences.flatMap(sentence => {
-      val sentenceTokens = tokens.filter(token =>
-        token.begin >= sentence.start & token.end <= sentence.end
-      ).map(token => IndexedToken(token.result, token.begin, token.end, sentence.index))
-      sentenceTokens
-    }).groupBy(_.sentenceIndex).map{case (sentenceIndex, indexedTokens) => TokenizedSentence(indexedTokens.toArray, sentenceIndex)}.filter(_.indexedTokens.nonEmpty).toSeq.sortBy(_.sentenceIndex)
+    retAnns
+  }
+
+  def filterTokensFromSentence(sentence:Sentence, tokens: Seq[Annotation]): Seq[IndexedToken] = {
+    val sentenceTokens = tokens.filter(token =>
+      token.begin >= sentence.start & token.end <= sentence.end
+    ).map(token => IndexedToken(token.result, token.begin, token.end, sentence.index))
+    sentenceTokens
   }
 
   override def pack(sentences: Seq[TokenizedSentence]): Seq[Annotation] = {
     sentences.flatMap{ sentence =>
-        sentence.indexedTokens.map{token =>
+      sentence.indexedTokens.map{token =>
         Annotation(annotatorType, token.begin, token.end, token.token,
           Map("sentence" -> sentence.sentenceIndex.toString))
-    }}
+      }}
   }
 }
