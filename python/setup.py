fde24a4fe749555b917fc06e4a44eb4fa407b946
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-01-04 19:16:48 +0100

    Add conda project to spark-nlp [skip ci]

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, './../README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
