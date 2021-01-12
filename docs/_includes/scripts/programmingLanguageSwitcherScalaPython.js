9cf322e9fa042e99055ade028f4b533aa3ab8eba
Lev <agsfer@gmail.com>
2021-01-12 16:17:00 +0200

    Update

--- docs/_includes/scripts/programmingLanguageSwitcherScalaPython.js
+++ docs/_includes/scripts/programmingLanguageSwitcherScalaPython.js
@@ -1,37 +1,52 @@
 
+jQuery(document).ready(function(){  
+    $( ".scala-button" ).click(function() {
+        $(this).closest( ".tabs-box" ).find(".scala-button").removeClass('code-selector-un-active').addClass( "code-selector-active" );
+        
 
-function toggleScala() {
+        //remove  active class from all other buttons
+        $(this).closest( ".tabs-box" ).find(".java-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+        $(this).closest( ".tabs-box" ).find(".python-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+
+        //toggle language snippets
+        $(this).closest( ".tabs-box" ).find( ".language-scala" ).show();
+        $(this).closest( ".tabs-box" ).find( ".language-python, .language-java" ).hide();
+    });
+
+    $( ".python-button" ).click(function() {
+        //set current button to active class and remove unactive class
+        $(this).closest( ".tabs-box" ).find(".python-button").removeClass('code-selector-un-active').addClass( "code-selector-active" );        
+
+
+        //remove  active class from all other buttons
+        $(this).closest( ".tabs-box" ).find(".java-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+        $(this).closest( ".tabs-box" ).find(".scala-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+
+
+        //toggle language snippets
+        $(this).closest( ".tabs-box" ).find( ".language-python" ).show();
+        $(this).closest( ".tabs-box" ).find( ".language-java, .language-scala" ).hide();
+    });
+
+    $( ".java-button" ).click(function() {
+        //set current button to active class and remove unactive class
+        $(this).closest( ".tabs-box" ).find(".java-button").removeClass('code-selector-un-active').addClass( "code-selector-active" );        
+
+        //remove  active class from all other buttons
+        $(this).closest( ".tabs-box" ).find(".scala-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+        $(this).closest( ".tabs-box" ).find(".python-button").removeClass('code-selector-active').addClass('code-selector-un-active');
+
+        //toggle language snippets
+        $(this).closest( ".tabs-box" ).find( ".language-java" ).show();
+        $(this).closest( ".tabs-box" ).find( ".language-python, .language-scala" ).hide();
+    });
+});
+
+function togglePython1() {
+
     //set current button to active class and remove unactive class
-    $(".scala-button").removeClass('code-selector-un-active');
-    $( ".scala-button" ).addClass( "code-selector-active" );
-
-
-    //remove  active class from all other buttons
-    $(".python-button").removeClass('code-selector-active');
-
-    //set  unactive class from all other buttons
-    $(".python-button").addClass('code-selector-un-active');
-
-    //toggle language snippets
-    $( ".language-scala" ).show() 
-    $( ".language-python" ).hide()
-    $( ".language-java" ).hide()
-
-}
-
-function togglePython() {
-
-    //set current button to active class and remove unactive class
-    $(".python-button").removeClass('code-selector-un-active');
     $( ".python-button" ).addClass( "code-selector-active" );
 
-
-    //remove  active class from all other buttons
-    $(".scala-button").removeClass('code-selector-active');
-
-
-    //set un active class from all other buttons
-    $(".scala-button").addClass('code-selector-un-active');
 
     //toggle language snippets
     $( ".language-python" ).show() 
@@ -48,7 +63,7 @@ function defer(method) { //wait until jquery ready
 }
 
 defer(function () { // load inital language
-    togglePython()
+    togglePython1()
 });
 
 
