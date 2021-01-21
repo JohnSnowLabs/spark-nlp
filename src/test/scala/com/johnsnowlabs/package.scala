548779b54133ca6d614de449c9e7c9250ff1d45c
wolliq <wolliq@gmail.com>
2021-01-21 11:43:04 +0100

    Adding driver without sbt build plugin

--- src/test/scala/com/johnsnowlabs/package.scala
+++
@@ -1,9 +0,0 @@
-package com
-
-import org.scalatest.Tag
-import com.johnsnowlabs.build.BuildInfo
-
-package object johnsnowlabs {
-  object FastTest extends Tag(s"${BuildInfo.name}.fast")
-  object SlowTest extends Tag(s"${BuildInfo.name}.slow")
-}
