b4d300a139cc90cb7139354a5a22694887c713c2
Kanin Pavel <kanin.pavel@gmail.com>
2021-02-09 21:02:57 +0700

    Send changed posts to Elasticsearch

---
+++ docs/_plugins/search_index.rb
@@ -0,0 +1,23 @@
+require 'elasticsearch'
+
+unless ENV['ELASTICSEARCH_URL'].to_s.empty?
+  puts "Connecting to Elasticsearch..."
+  client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'], transport_options: {
+    headers: {
+      'Authorization': "Bearer #{ENV['ELASTICSEARCH_ACCESS_TOKEN']}"
+    }
+  }
+
+  posts = File.open("./posts.txt").each_line.to_a.map(&:chomp)
+  Jekyll::Hooks.register :posts, :post_render do |post|
+    next unless posts.include? post.basename
+    puts "Indexing #{post.url}..."
+    client.index index: 'models', id: post.url, body: {
+      title: post.data['title'],
+      task: post.data['task'],
+      language: post.data['language'],
+      edition: post.data['edition'],
+      date: post.data['date'].strftime('%F')
+    }
+  end
+end
