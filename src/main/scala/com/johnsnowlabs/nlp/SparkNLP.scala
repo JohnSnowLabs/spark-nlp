94c26b3bbc96f8fee96f39b7cb789a230faa11a7
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-03-04 15:30:26 +0100

    Adopt Spark NLP start function for Spark 3.0

--- src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
+++ src/main/scala/com/johnsnowlabs/nlp/SparkNLP.scala
@@ -5,28 +5,42 @@ import org.apache.spark.sql.SparkSession
 object SparkNLP {
 
   val currentVersion = "3.0.0-rc1"
-  val MavenSpark24 = s"com.johnsnowlabs.nlp:spark-nlp_2.11:$currentVersion"
-  val MavenGpuSpark24 = s"com.johnsnowlabs.nlp:spark-nlp-gpu_2.11:$currentVersion"
+  val MavenSpark30 = s"com.johnsnowlabs.nlp:spark-nlp_2.12:$currentVersion"
+  val MavenGpuSpark30 = s"com.johnsnowlabs.nlp:spark-nlp-gpu_2.12:$currentVersion"
+  val MavenSpark24 = s"com.johnsnowlabs.nlp:spark-nlp-spark24_2.11:$currentVersion"
+  val MavenGpuSpark24 = s"com.johnsnowlabs.nlp:spark-nlp-gpu-spark24_2.11:$currentVersion"
   val MavenSpark23 = s"com.johnsnowlabs.nlp:spark-nlp-spark23_2.11:$currentVersion"
   val MavenGpuSpark23 = s"com.johnsnowlabs.nlp:spark-nlp-gpu-spark23_2.11:$currentVersion"
 
-  def start(gpu:Boolean = false, spark23:Boolean = false): SparkSession = {
+  /**
+    *
+    * @param gpu start Spark NLP with GPU
+    * @param spark23 start Spark NLP on Apache Spark 2.3.x
+    * @param spark24 start Spark NLP on Apache Spark 2.4.x
+    * @param memory set driver memory for SparkSession
+    * @return SparkSession
+    */
+  def start(gpu: Boolean = false, spark23: Boolean = false, spark24: Boolean = false, memory: String = "16G"): SparkSession = {
     val build = SparkSession.builder()
       .appName("Spark NLP")
       .master("local[*]")
-      .config("spark.driver.memory", "16G")
+      .config("spark.driver.memory", memory)
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .config("spark.kryoserializer.buffer.max", "1000M")
+      .config("spark.kryoserializer.buffer.max", "2000M")
       .config("spark.driver.maxResultSize", "0")
 
     if(gpu & spark23){
       build.config("spark.jars.packages", MavenGpuSpark23)
+    } else if(gpu & spark24){
+      build.config("spark.jars.packages", MavenGpuSpark24)
     } else if(spark23){
       build.config("spark.jars.packages", MavenSpark23)
-    } else if(gpu){
-      build.config("spark.jars.packages", MavenGpuSpark24)
-    } else {
+    } else if(spark24){
       build.config("spark.jars.packages", MavenSpark24)
+    } else if(gpu){
+      build.config("spark.jars.packages", MavenGpuSpark30)
+    } else {
+      build.config("spark.jars.packages", MavenSpark30)
     }
 
     build.getOrCreate()
