e806bf099a57bbeb9f5e67ba959cdea16d39addb
stefanolori <wolliq@gmail.com>
2021-03-22 11:55:31 +0100

    Refactored build sbt

---
+++ project/Dependencies.scala
@@ -0,0 +1,98 @@
+import sbt._
+import Keys._
+
+object Dependencies {
+
+  /** ------- Spark version start ------- */
+  val spark23Ver = "2.3.4"
+  val spark24Ver = "2.4.7"
+  val spark30Ver = "3.0.2"
+
+  val is_gpu = System.getProperty("is_gpu", "false")
+  val is_opt = System.getProperty("is_opt", "false")
+  val is_spark23 = System.getProperty("is_spark23", "false")
+  val is_spark24 = System.getProperty("is_spark24", "false")
+
+  val sparkVer = getSparkVersion(is_spark23, is_spark24)
+  /** ------- Spark version end ------- */
+
+
+  /** Package attributes */
+  def getPackageName(is_spark23: String, is_spark24: String, is_gpu: String): String = {
+    if (is_gpu.equals("true") && is_spark23.equals("true")){
+      "spark-nlp-gpu-spark23"
+    }else if (is_gpu.equals("true") && is_spark24.equals("true")){
+      "spark-nlp-gpu-spark24"
+    }else if (is_gpu.equals("true") && is_spark24.equals("false")){
+      "spark-nlp-gpu"
+    }else if (is_gpu.equals("false") && is_spark23.equals("true")){
+      "spark-nlp-spark23"
+    }else if (is_gpu.equals("false") && is_spark24.equals("true")){
+      "spark-nlp-spark24"
+    }else{
+      "spark-nlp"
+    }
+  }
+
+  def getSparkVersion(is_spark23: String, is_spark24: String): String = {
+    if(is_spark24 == "true") spark24Ver
+    else
+      if(is_spark23=="true") spark23Ver
+      else spark30Ver
+  }
+
+  def getJavaTarget(is_spark23: String, is_spark24: String): String = {
+    if (is_spark24.equals("true") || is_spark23.equals("true")) {
+      "-target:jvm-1.8"
+    } else {
+      ""
+    }
+  }
+
+  /** ------- Scala version start ------- */
+  lazy val scala211 = "2.11.12"
+  lazy val scala212 = "2.12.10"
+  lazy val scalaVer = if(is_spark23 == "true" | is_spark24 == "true") scala211 else scala212
+
+  lazy val supportedScalaVersions = List(scala212, scala211)
+
+  val scalaTestVersion = "3.0.0"
+  /** ------- Scala version end ------- */
+
+  /** ------- Dependencies start------- */
+
+  // utilDependencies
+
+  val typesafeVersion = "1.3.0"
+  val typesafe = "com.typesafe" % "config" % typesafeVersion
+
+  val rocksdbjniVersion = "6.5.3"
+  val rocksdbjni= "org.rocksdb" % "rocksdbjni" % rocksdbjniVersion
+
+  val awsjavasdkbundleVersion = "1.11.603"
+  val awsjavasdkbundle = "com.amazonaws" % "aws-java-sdk-bundle" % awsjavasdkbundleVersion
+
+  val liblevenshteinVersion = "3.0.0"
+  val liblevenshtein = "com.github.universal-automata" % "liblevenshtein" % liblevenshteinVersion
+
+  val greexVersion = "1.0"
+  val greex = "com.navigamez" % "greex" % greexVersion
+
+  val json4sVersion = "3.5.3"
+  val json4s = "org.json4s" %% "json4s-ext" % json4sVersion
+
+  // typedDependencyParserDependencies
+
+  val trove4jVersion = "3.0.3"
+  val trove4j = "net.sf.trove4j" % "trove4j" % trove4jVersion
+
+  val junitVersion = "4.10"
+  val junit = "junit" % "junit" % junitVersion % Test
+
+  val tensorflowGPUVersion = "0.2.2"
+  val tensorflowGPU = "com.johnsnowlabs.nlp" %% "tensorflow-gpu" % tensorflowGPUVersion
+
+  val tensorflowCPUVersion = "0.2.2"
+  val tensorflowCPU = "com.johnsnowlabs.nlp" %% "tensorflow-cpu" % tensorflowCPUVersion
+  /** ------- Dependencies end  ------- */
+}
---
+++ project/Resolvers.scala
@@ -0,0 +1,8 @@
+import sbt._
+import Keys._
+
+object Resolvers {
+  val m2CentralRepo = "Maven Central"  at "https://repo1.maven.org/maven2/"
+
+  val m2Resolvers = Seq(m2CentralRepo)
+}
