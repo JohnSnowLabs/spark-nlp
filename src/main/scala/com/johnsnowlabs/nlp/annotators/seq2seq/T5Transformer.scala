d0012ab444e0c6c806aafd736a1ed71b63ba4e1c
Alina <erar1@mail.ru>
2021-04-07 16:32:56 +0100

    add tests and improve performance

--- src/main/scala/com/johnsnowlabs/nlp/annotators/seq2seq/T5Transformer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/seq2seq/T5Transformer.scala
@@ -5,10 +5,9 @@ import com.johnsnowlabs.ml.tensorflow.{ReadTensorflowModel, TensorflowT5, Tensor
 import com.johnsnowlabs.nlp.AnnotatorType.DOCUMENT
 import com.johnsnowlabs.nlp.{Annotation, AnnotatorModel, HasPretrained, HasSimpleAnnotate, ParamsAndFeaturesReadable, ParamsAndFeaturesWritable}
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.ml.param.{IntArrayParam, IntParam, Param}
+import org.apache.spark.ml.param.{BooleanParam, DoubleParam, IntArrayParam, IntParam, LongParam, Param}
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.SparkSession
-
 import java.io.File
 
 class T5Transformer(override val uid: String)
@@ -35,32 +34,110 @@ class T5Transformer(override val uid: String)
   val configProtoBytes = new IntArrayParam(this, "configProtoBytes", "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()")
 
   val task = new Param[String](this, "task", "Set transformer task, e.g. 'summarize'")
-
+  val minOutputLength = new IntParam(this, "minOutputLength", "Minimum length of the sequence to be generated")
   val maxOutputLength = new IntParam(this, "maxOutputLength", "Maximum length of output text")
-
+  val doSample = new BooleanParam(this, "doSample", "Whether or not to use sampling; use greedy decoding otherwise")
+  val temperature = new DoubleParam(this, "temperature", "The value used to module the next token probabilities")
+  val topK = new IntParam(this, "topK", "The number of highest probability vocabulary tokens to keep for top-k-filtering")
+  val topP = new DoubleParam(this, "topP", "If set to float < 1, only the most probable tokens with probabilities that add up to ``top_p`` or higher are kept for generation")
+  val repetitionPenalty = new DoubleParam(this, "repetitionPenalty", "The parameter for repetition penalty. 1.0 means no penalty. See `this paper <https://arxiv.org/pdf/1909.05858.pdf>`__ for more details")
+  val noRepeatNgramSize = new IntParam(this, "noRepeatNgramSize", "If set to int > 0, all ngrams of that size can only occur once")
+  var randomSeed: Option[Long] = None
   private var _tfModel: Option[Broadcast[TensorflowT5]] = None
 
   def setConfigProtoBytes(bytes: Array[Int]): T5Transformer  .this.type = set(this.configProtoBytes, bytes)
 
   def getConfigProtoBytes: Option[Array[Byte]] = get(this.configProtoBytes).map(_.map(_.toByte))
 
+  /** @group setParam **/
   def setTask(value: String): T5Transformer.this.type = {
     if (get(task).isEmpty)
       set(task, value)
     this
   }
 
+  /** @group setParam **/
+  def setMinOutputLength(value: Int): T5Transformer.this.type = {
+    set(minOutputLength, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getMinOutputLength: Int = $(this.minOutputLength)
+
+  /** @group setParam **/
   def setMaxOutputLength(value: Int): T5Transformer.this.type = {
     set(maxOutputLength, value)
     this
   }
 
+  /** @group getParam **/
   def getMaxOutputLength: Int = $(this.maxOutputLength)
 
-  setDefault(
-    task -> "",
-    maxOutputLength -> 200
-  )
+  /** @group setParam **/
+  def setDoSample(value: Boolean): T5Transformer.this.type = {
+    set(doSample, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getDoSample: Boolean = $(this.doSample)
+
+  /** @group setParam **/
+  def setTemperature(value: Double): T5Transformer.this.type = {
+    set(temperature, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getTemperature: Double = $(this.temperature)
+
+  /** @group setParam **/
+  def setTopK(value: Int): T5Transformer.this.type = {
+    set(topK, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getTopK: Int = $(this.topK)
+
+  /** @group setParam **/
+  def setTopP(value: Double): T5Transformer.this.type = {
+    set(topP, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getTopP: Double = $(this.topP)
+
+  /** @group setParam **/
+  def setRepetitionPenalty(value: Double): T5Transformer.this.type = {
+    set(repetitionPenalty, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getRepetitionPenalty: Double = $(this.repetitionPenalty)
+
+  /** @group setParam **/
+  def setNoRepeatNgramSize(value: Int): T5Transformer.this.type = {
+    set(noRepeatNgramSize, value)
+    this
+  }
+
+  /** @group getParam **/
+  def getNoRepeatNgramSize: Int = $(this.noRepeatNgramSize)
+
+  /** @group setParam **/
+  def setRandomSeed(value: Long): T5Transformer.this.type = {
+    if (randomSeed.isEmpty) {
+      this.randomSeed = Some(value)
+    }
+    this
+  }
+
+  /** @group getParam **/
+  def getRandomSeed: Option[Long] = this.randomSeed
 
   def setModelIfNotSet(spark: SparkSession, tfWrapper: TensorflowWrapper, spp: SentencePieceWrapper): this.type = {
     if (_tfModel.isEmpty) {
@@ -75,15 +152,38 @@ class T5Transformer(override val uid: String)
 
   def getModelIfNotSet: TensorflowT5 = _tfModel.get.value
 
+  setDefault(
+    task -> "",
+    minOutputLength -> 0,
+    maxOutputLength -> 20,
+    doSample -> false,
+    temperature -> 1.0,
+    topK -> 50,
+    topP -> 1.0,
+    repetitionPenalty -> 1.0,
+    noRepeatNgramSize -> 0
+  )
+
+
   override def annotate(annotations: Seq[Annotation]): Seq[Annotation] = {
 
     val nonEmptySentences = annotations.filter(_.result.nonEmpty)
 
-    if (nonEmptySentences.nonEmpty){
+    //        randomSeed = $(randomSeed),
+    if (nonEmptySentences.nonEmpty) {
       this.getModelIfNotSet.generateSeq2Seq(
         sentences = nonEmptySentences,
+        batchSize = 1,
+        minOutputLength = $(minOutputLength),
         maxOutputLength = $(maxOutputLength),
-        task = $(task)
+        doSample = $(doSample),
+        temperature = $(temperature),
+        topK = $(topK),
+        topP = $(topP),
+        repetitionPenalty = $(repetitionPenalty),
+        noRepeatNgramSize = $(noRepeatNgramSize),
+        task = $(task),
+        randomSeed = this.randomSeed
       )
     } else {
       Seq.empty[Annotation]
@@ -100,10 +200,14 @@ class T5Transformer(override val uid: String)
 
 trait ReadablePretrainedT5TransformerModel extends ParamsAndFeaturesReadable[T5Transformer] with HasPretrained[T5Transformer] {
   override val defaultModelName: Some[String] = Some("t5_small")
+
   /** Java compliant-overrides */
   override def pretrained(): T5Transformer = super.pretrained()
+
   override def pretrained(name: String): T5Transformer = super.pretrained(name)
+
   override def pretrained(name: String, lang: String): T5Transformer = super.pretrained(name, lang)
+
   override def pretrained(name: String, lang: String, remoteLoc: String): T5Transformer = super.pretrained(name, lang, remoteLoc)
 }
 
