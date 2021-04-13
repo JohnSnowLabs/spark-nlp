b1ce29fc0ab94f2397f978f783581b8694476dcb
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-04-13 19:50:16 +0200

    Make pyspark 3.0.2 default [skip ci]

--- scripts/colab_setup.sh
+++ scripts/colab_setup.sh
@@ -2,7 +2,7 @@
 
 #default values for pyspark, spark-nlp, and SPARK_HOME
 SPARKNLP="3.0.1"
-PYSPARK="3.1.1"
+PYSPARK="3.0.2"
 SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
 
 while getopts s:p: option
