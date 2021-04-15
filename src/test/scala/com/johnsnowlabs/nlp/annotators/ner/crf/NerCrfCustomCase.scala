f802383cfda0dbadaad40e3d969e2f90a864be9f
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-04-15 15:41:47 +0200

    Update NerCrf unit tests [skip ci]

--- src/test/scala/com/johnsnowlabs/nlp/annotators/ner/crf/NerCrfCustomCase.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/ner/crf/NerCrfCustomCase.scala
@@ -1,67 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.ner.crf
 
-import com.johnsnowlabs.nlp.annotator.PerceptronModel
-import com.johnsnowlabs.nlp.annotators.Tokenizer
-import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
-import com.johnsnowlabs.nlp.embeddings.WordEmbeddings
+import com.johnsnowlabs.nlp.embeddings.WordEmbeddingsModel
+import com.johnsnowlabs.nlp.training.CoNLL
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher, LightPipeline, RecursivePipeline}
+import com.johnsnowlabs.nlp.{Finisher, LightPipeline}
 import com.johnsnowlabs.tags.SlowTest
-import org.apache.spark.ml.PipelineModel
+
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.SparkSession
+
 import org.scalatest._
 
 class NerCrfCustomCase extends FlatSpec {
 
-  val spark = ResourceHelper.spark
+  val spark: SparkSession = ResourceHelper.spark
 
-  import spark.implicits._
 
   "NerCRF" should "read low trained model" taggedAs SlowTest in {
 
-    val documentAssembler = new DocumentAssembler()
-      .setInputCol("text")
-      .setOutputCol("document")
 
-    val sentenceDetector = new SentenceDetector()
-      .setInputCols(Array("document"))
-      .setOutputCol("sentence")
+    val conll = CoNLL()
+    val test_data = conll.readDataset(ResourceHelper.spark, "src/test/resources/conll2003/eng.testb")
 
-    val tokenizer = new Tokenizer()
-      .setInputCols(Array("sentence"))
-      .setOutputCol("token")
-
-    val pos = PerceptronModel.pretrained()
-      .setInputCols("sentence", "token")
-      .setOutputCol("pos")
-
-    val embeddings = new WordEmbeddings()
-      .setInputCols("pos", "token", "sentence")
+    val embeddings = WordEmbeddingsModel.pretrained()
+      .setInputCols("token", "sentence")
       .setOutputCol("embeddings")
-      .setStoragePath("./emb.bin", "BINARY")
-      .setDimension(200)
 
     val nerCrf = new NerCrfApproach()
       .setInputCols("pos", "token", "sentence", "embeddings")
       .setOutputCol("ner")
-      .setMinEpochs(50)
-      .setMaxEpochs(80)
       .setLabelColumn("label")
+      .setMaxEpochs(5)
 
     val finisher = new Finisher()
       .setInputCols("ner")
 
-    val recursivePipeline = new RecursivePipeline()
+    val pipeline = new Pipeline()
       .setStages(Array(
-        documentAssembler,
-        sentenceDetector,
-        tokenizer,
-        pos,
         embeddings,
         nerCrf,
         finisher
       ))
 
-    val model = recursivePipeline.fit(Seq.empty[String].toDF("text"))
+    val model = pipeline.fit(test_data)
 
     model.write.overwrite().save("./crfnerconll")
     model.stages(4).asInstanceOf[NerCrfModel].write.overwrite().save("./crfnerconll-single")
