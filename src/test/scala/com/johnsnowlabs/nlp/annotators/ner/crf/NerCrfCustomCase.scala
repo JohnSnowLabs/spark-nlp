988402716d5851de8ab411c41f72226a492c8191
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-04-16 11:32:34 +0200

    Update unit tests for NerCrf [skip ci]

--- src/test/scala/com/johnsnowlabs/nlp/annotators/ner/crf/NerCrfCustomCase.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/ner/crf/NerCrfCustomCase.scala
@@ -20,7 +20,6 @@ package com.johnsnowlabs.nlp.annotators.ner.crf
 import com.johnsnowlabs.nlp.embeddings.WordEmbeddingsModel
 import com.johnsnowlabs.nlp.training.CoNLL
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.nlp.{Finisher, LightPipeline}
 import com.johnsnowlabs.tags.SlowTest
 
 import org.apache.spark.ml.{Pipeline, PipelineModel}
@@ -33,11 +32,11 @@ class NerCrfCustomCase extends FlatSpec {
   val spark: SparkSession = ResourceHelper.spark
 
 
-  "NerCRF" should "read low trained model" taggedAs SlowTest in {
+  "NerCRF" should "train CoNLL dataset" taggedAs SlowTest in {
 
 
     val conll = CoNLL()
-    val test_data = conll.readDataset(ResourceHelper.spark, "src/test/resources/conll2003/eng.testb")
+    val test_data = conll.readDataset(ResourceHelper.spark, "src/test/resources/conll2003/eng.train")
 
     val embeddings = WordEmbeddingsModel.pretrained()
       .setInputCols("token", "sentence")
@@ -47,32 +46,30 @@ class NerCrfCustomCase extends FlatSpec {
       .setInputCols("pos", "token", "sentence", "embeddings")
       .setOutputCol("ner")
       .setLabelColumn("label")
-      .setMaxEpochs(5)
-
-    val finisher = new Finisher()
-      .setInputCols("ner")
+      .setMaxEpochs(50)
 
     val pipeline = new Pipeline()
       .setStages(Array(
         embeddings,
-        nerCrf,
-        finisher
+        nerCrf
       ))
 
     val model = pipeline.fit(test_data)
 
-    model.write.overwrite().save("./crfnerconll")
-    model.stages(4).asInstanceOf[NerCrfModel].write.overwrite().save("./crfnerconll-single")
+    model.write.overwrite().save("./tmp_nercrfpipeline")
+    model.stages(1).asInstanceOf[NerCrfModel].write.overwrite().save("./tmp_nercrfmodel")
 
   }
 
-  "NerCRF" should "read and predict" taggedAs SlowTest in {
-    val lp = new LightPipeline(PipelineModel.load("./crfnerconll"))
+  "NerCRF" should "load saved model and predict" taggedAs SlowTest in {
 
-    println(lp.annotate(
-      "Lung, right lower lobe, lobectomy: Grade 3"
-    ))
+    val conll = CoNLL()
+    val test_data = conll.readDataset(ResourceHelper.spark, "src/test/resources/conll2003/eng.testb")
 
+    val m = PipelineModel.load("./tmp_nercrfpipeline")
+    m.stages(1).asInstanceOf[NerCrfModel].setIncludeConfidence(false)
+
+    m.transform(test_data.limit(2)).select("ner").show(false)
   }
 
 }
