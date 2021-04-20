6b12c9a9eef8644866b13192ef365cd0c135bf0a
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-04-20 15:56:54 +0200

    Update Scaladoc

--- src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
+++ src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
@@ -1,4 +1,21 @@
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.pretrained
 
 import com.amazonaws.AmazonClientException
@@ -27,6 +44,7 @@ import com.johnsnowlabs.nlp.pretrained.ResourceDownloader.{listPretrainedResourc
 import com.johnsnowlabs.nlp.pretrained.ResourceType.ResourceType
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
 import com.johnsnowlabs.util.{Build, ConfigHelper, FileHelper, Version}
+
 import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.ml.util.DefaultParamsReadable
 import org.apache.spark.ml.{PipelineModel, PipelineStage}
