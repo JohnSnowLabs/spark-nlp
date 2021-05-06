127143e2af7ae524e064ccbf39ba9880d97f8fe2
Stefano Lori <wolliq@gmail.com>
2021-05-06 10:07:15 +0200

    Added signature map with secondary indexed sign def for input and ouput

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/sign/ModelSignatureConstants.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/sign/ModelSignatureConstants.scala
@@ -8,59 +8,116 @@ import scala.util.matching.Regex
  *
  * signature_def['serving_default']:
  * The given SavedModel SignatureDef contains the following input(s):
+ *
  * inputs['attention_mask'] tensor_info:
  * dtype: DT_INT32
  * shape: (-1, -1)
  * name: serving_default_attention_mask:0
- * inputs['input_ids'] tensor_info:
- * dtype: DT_INT32
- * shape: (-1, -1)
- * name: serving_default_input_ids:0
+ *
+   * inputs['input_ids'] tensor_info:
+   * dtype: DT_INT32
+   * shape: (-1, -1)
+   * name: serving_default_input_ids:0
+ *
  * inputs['token_type_ids'] tensor_info:
  * dtype: DT_INT32
  * shape: (-1, -1)
  * name: serving_default_token_type_ids:0
+ *
  * The given SavedModel SignatureDef contains the following output(s):
+ *
  * outputs['last_hidden_state'] tensor_info:
  * dtype: DT_FLOAT
  * shape: (-1, -1, 768)
  * name: StatefulPartitionedCall:0
+ *
  * outputs['pooler_output'] tensor_info:
  * dtype: DT_FLOAT
  * shape: (-1, 768)
  * name: StatefulPartitionedCall:1
+ *
  * Method name is: tensorflow/serving/predict*
 */
 
 
 object ModelSignatureConstants {
 
+  sealed trait TFInfoDescriptor {
+    protected val key: String
+  }
+
+  case object Name extends TFInfoDescriptor {
+    override val key: String = "name"
+  }
+
+  case object DType extends TFInfoDescriptor {
+    override val key: String = "dtype"
+  }
+
+  case object DimCount extends TFInfoDescriptor {
+    override val key: String = "dim_count"
+  }
+
+  case object ShapeDimList extends TFInfoDescriptor {
+    override val key: String = "shape_dim_list"
+  }
+
+  case object SerializedSize extends TFInfoDescriptor {
+    override val key: String = "serialized_size"
+  }
+
+
   sealed trait TFInfoNameMapper {
     protected val key: String
     protected val value: String
   }
 
-  case object TokenIds extends TFInfoNameMapper {
+  case object InputIds extends TFInfoNameMapper {
     override val key: String = "input_ids"
     override val value: String = "input_ids:0"
   }
 
-  case object MaskIds extends TFInfoNameMapper {
+  case object InputIdsV1 extends TFInfoNameMapper {
+    override val key: String = "input_ids"
+    override val value: String = "input_ids:0"
+  }
+
+  case object AttentionMask extends TFInfoNameMapper {
+    override val key: String = "attention_mask"
+    override val value: String = "attention_mask:0"
+  }
+
+  case object AttentionMaskV1 extends TFInfoNameMapper {
     override val key: String = "attention_mask"
     override val value: String = "input_mask:0"
   }
 
-  case object SegmentIds extends TFInfoNameMapper {
+  case object TokenTypeIds extends TFInfoNameMapper {
+    override val key: String = "token_type_ids"
+    override val value: String = "token_type_ids:0"
+  }
+
+  case object TokenTypeIdsV1 extends TFInfoNameMapper {
     override val key: String = "token_type_ids"
     override val value: String = "segment_ids:0"
   }
 
   case object LastHiddenState extends TFInfoNameMapper {
     override val key: String = "last_hidden_state"
+    override val value: String = "StatefulPartitionedCall:0"
+  }
+
+  case object LastHiddenStateV1 extends TFInfoNameMapper {
+    override val key: String = "last_hidden_state"
     override val value: String = "sequence_output:0"
   }
 
   case object PoolerOutput extends TFInfoNameMapper {
+    override val key: String = "pooler_output"
+    override val value: String = "StatefulPartitionedCall:1"
+  }
+
+  case object PoolerOutputV1 extends TFInfoNameMapper {
     override val key: String = "pooler_output"
     override val value: String = "pooled_output:0"
   }
@@ -90,21 +147,21 @@ object ModelSignatureConstants {
           "(input)(.*)(ids)".r,
           "(attention)(.*)(mask)".r,
           "(token_type)(.*)(ids)".r,
-          "(StatefulPartitionedCall)".r)
+          "(last_hidden_state)".r)
 
       case _ => throw new Exception("Unknown model provider! Please provide one in between JSL, TF2 or HF.")
     }
     referenceKeys
   }
 
-  /** Convert signatures key names to adopted naming conventions for BERT keys mapping */
+  /** Convert signatures key names to adopted Hugging Face naming conventions */
   def toAdoptedKeys(keyName: String): String = {
     keyName match {
-      case "input_ids" | "input_word_ids" => TokenIds.key
-      case "input_mask" | "attention_mask" => MaskIds.key
-      case "segment_ids" | "input_type_ids" | "token_type_ids" => SegmentIds.key
+      case "input_ids" | "input_word_ids" => InputIds.key
+      case "input_mask" | "attention_mask" => AttentionMask.key
+      case "segment_ids" | "input_type_ids" | "token_type_ids" => TokenTypeIds.key
       case "sequence_output" | "bert_encoder" | "last_hidden_state" => LastHiddenState.key
-      case "pooled_output" => PoolerOutput.key
+      case "pooler_output" => PoolerOutput.key
       case k => k
     }
   }
