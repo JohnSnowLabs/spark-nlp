635970e0670f34f7461e6477693a9ac8a07fd4db
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-05-16 21:12:19 +0200

    Add RoBertaEmbeddings to annotator import [skip ci]

--- src/main/scala/com/johnsnowlabs/nlp/annotator.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotator.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp
 
 import com.johnsnowlabs.ml.tensorflow.sentencepiece.ReadSentencePieceModel
@@ -17,6 +34,7 @@ import com.johnsnowlabs.nlp.annotators.ld.dl.{ReadLanguageDetectorDLTensorflowMo
 import com.johnsnowlabs.nlp.annotators.sentence_detector_dl.{ReadablePretrainedSentenceDetectorDL, ReadsSentenceDetectorDLGraph}
 import com.johnsnowlabs.nlp.annotators.seq2seq.{ReadMarianMTTensorflowModel, ReadablePretrainedMarianMTModel, ReadT5TransformerTensorflowModel, ReadablePretrainedT5TransformerModel}
 import com.johnsnowlabs.nlp.annotators.ws.ReadablePretrainedWordSegmenter
+
 import org.apache.spark.ml.util.DefaultParamsReadable
 
 package object annotator {
@@ -209,5 +227,8 @@ package object annotator {
 
   type DistilBertEmbeddings = com.johnsnowlabs.nlp.embeddings.DistilBertEmbeddings
   object DistilBertEmbeddings extends ReadablePretrainedDistilBertModel with ReadDistilBertTensorflowModel
+
+  type RoBertaEmbeddings = com.johnsnowlabs.nlp.embeddings.RoBertaEmbeddings
+  object RoBertaEmbeddings extends ReadablePretrainedRobertaModel with ReadRobertaTensorflowModel
 
 }
