ba6601645692ffe0d82b0fea11306d9af06a1668
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-05-26 11:29:29 +0200

    Doc: Added Examples for Chunk2Doc, Doc2Chunk, DocumentAssembler, TokenAssembler

--- src/main/scala/com/johnsnowlabs/nlp/TokenAssembler.scala
+++ src/main/scala/com/johnsnowlabs/nlp/TokenAssembler.scala
@@ -1,22 +1,117 @@
 package com.johnsnowlabs.nlp
 
-import com.johnsnowlabs.nlp.annotators.common.NerTagged
-import com.johnsnowlabs.nlp.annotators.ner.NerTagsEncoding
 import org.apache.spark.ml.param.BooleanParam
 import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
 
 import scala.collection.mutable.ArrayBuffer
 
+/**
+  * This transformer reconstructs a `DOCUMENT` type annotation from tokens, usually after these have been normalized,
+  * lemmatized, normalized, spell checked, etc, in order to use this document annotation in further annotators.
+  * Requires `DOCUMENT` and `TOKEN` type annotations as input.
+  *
+  * For more extended examples on document pre-processing see the
+  * [[https://github.com/JohnSnowLabs/spark-nlp-workshop/blob/master/tutorials/Certification_Trainings/Public/databricks_notebooks/2.Text_Preprocessing_with_SparkNLP_Annotators_Transformers_v3.0.ipynb Spark NLP Workshop]].
+  *
+  * ==Example==
+  * {{{
+  * import spark.implicits._
+  * import com.johnsnowlabs.nlp.DocumentAssembler
+  * import com.johnsnowlabs.nlp.annotator.SentenceDetector
+  * import com.johnsnowlabs.nlp.annotator.Tokenizer
+  * import com.johnsnowlabs.nlp.annotator.{Normalizer, StopWordsCleaner}
+  * import com.johnsnowlabs.nlp.TokenAssembler
+  * import org.apache.spark.ml.Pipeline
+  *
+  * val documentAssembler = new DocumentAssembler()
+  *   .setInputCol("text")
+  *   .setOutputCol("document")
+  *
+  * val sentenceDetector = new SentenceDetector()
+  *   .setInputCols("document")
+  *   .setOutputCol("sentences")
+  *
+  * val tokenizer = new Tokenizer()
+  *   .setInputCols("sentences")
+  *   .setOutputCol("token")
+  *
+  * val normalizer = new Normalizer()
+  *   .setInputCols("token")
+  *   .setOutputCol("normalized")
+  *   .setLowercase(false)
+  *
+  * val stopwordsCleaner = new StopWordsCleaner()
+  *   .setInputCols("normalized")
+  *   .setOutputCol("cleanTokens")
+  *   .setCaseSensitive(false)
+  *
+  * val tokenAssembler = new TokenAssembler()
+  *   .setInputCols("sentences", "cleanTokens")
+  *   .setOutputCol("cleanText")
+  *
+  * val data = Seq("Spark NLP is an open-source text processing library for advanced natural language processing.")
+  *   .toDF("text")
+  *
+  * val pipeline = new Pipeline().setStages(Array(
+  *   documentAssembler,
+  *   sentenceDetector,
+  *   tokenizer,
+  *   normalizer,
+  *   stopwordsCleaner,
+  *   tokenAssembler
+  * )).fit(data)
+  *
+  * val result = pipeline.transform(data)
+  * result.select("cleanText").show(false)
+  * +---------------------------------------------------------------------------------------------------------------------------+
+  * |cleanText                                                                                                                  |
+  * +---------------------------------------------------------------------------------------------------------------------------+
+  * |[[document, 0, 80, Spark NLP opensource text processing library advanced natural language processing, [sentence -> 0], []]]|
+  * +---------------------------------------------------------------------------------------------------------------------------+
+  * }}}
+  *
+  * @see [[DocumentAssembler]] on the data structure
+  * @param uid required uid for storing annotator to disk
+  * @groupname anno Annotator types
+  * @groupdesc anno Required input and expected output annotator types
+  * @groupname Ungrouped Members
+  * @groupname param Parameters
+  * @groupname setParam Parameter setters
+  * @groupname getParam Parameter getters
+  * @groupname Ungrouped Members
+  * @groupprio param  1
+  * @groupprio anno  2
+  * @groupprio Ungrouped 3
+  * @groupprio setParam  4
+  * @groupprio getParam  5
+  * @groupdesc param A list of (hyper-)parameter keys this annotator can take. Users can set and get the parameter values through setters and getters, respectively.
+  */
 class TokenAssembler(override val uid: String) extends AnnotatorModel[TokenAssembler] with HasSimpleAnnotate[TokenAssembler] {
 
   import com.johnsnowlabs.nlp.AnnotatorType._
 
+  /**
+    * Output annotator types: DOCUMENT
+    * @group anno
+    */
   override val outputAnnotatorType: AnnotatorType = DOCUMENT
 
+  /**
+    * Input annotator types: DOCUMENT, TOKEN
+    * @group anno
+    */
   override val inputAnnotatorTypes: Array[String] = Array(DOCUMENT, TOKEN)
 
+  /**
+    * Whether to preserve the actual position of the tokens or reduce them to one space (Default: `false`)
+    * @group param
+    */
   val preservePosition: BooleanParam = new BooleanParam(this, "preservePosition", "Whether to preserve the actual position of the tokens or reduce them to one space")
 
+  /**
+    * Whether to preserve the actual position of the tokens or reduce them to one space (Default: `false`)
+    * @group setParam
+    */
   def setPreservePosition(value: Boolean): this.type = set(preservePosition, value)
 
   setDefault(
