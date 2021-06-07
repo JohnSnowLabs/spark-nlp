6b9cda1aeee7f5808e8be496a6edb47f360dddec
Stefano Lori <wolliq@gmail.com>
2021-06-07 14:19:43 +0200

    Added new dictionary mapping for it

--- src/main/scala/com/johnsnowlabs/nlp/annotators/DateMatcherTranslator.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/DateMatcherTranslator.scala
@@ -2,7 +2,7 @@ package com.johnsnowlabs.nlp.annotators
 
 object DateMatcherTranslator extends Serializable {
 
-  val Language = "it"
+  val Language = "it2"
   val DictionaryPath = s"src/main/resources/date-matcher/date_translation_data/$Language.json"
 //  val DictionaryPath = s"/home/wolliqeonii/workspace/dev/jsl/spark-nlp/src/main/resources/date-matcher/date_translation_data/$Language.json"
 
@@ -15,39 +15,20 @@ object DateMatcherTranslator extends Serializable {
 
     // reading a file
     val jsonString = Source.fromFile(DictionaryPath).mkString
-    println(jsonString)
+//    println(jsonString)
 
     val json = parse(jsonString)
 
-//    println(json)
-
-    // Converting from JOjbect to plain object
     implicit val formats = DefaultFormats
-    val myOldMap = json.extract[Map[String, Any]]
+    val jsonMap = json.extract[Map[String, Any]]
 
-    println(myOldMap)
+    println(jsonMap)
   }
 
   val dictionary = loadTranslationDictionary()
 
-//  val dictionary = SparkSession.builder().getOrCreate()
-//    .read
-//    .option("multiline", true)
-//    //      .option("mode", "PERMISSIVE")
-//    .json(DictionaryPath)
-
   def searchLanguage(text: String) = {
-    //    val dictionary = JSON.parseFull(DictionaryPath)
 
-//    val url = ClassLoader.getSystemResource(DictionaryPath)
-//    val schemaSource = Source.fromFile(url.getFile).getLines.mkString
-//    val schemaFromJson = DataType.fromJson(schemaSource).asInstanceOf[StructType]
-//    val dictionary = spark.read.schema(schemaFromJson)
-//      .json(DictionaryPath)
-//    dictionary.printSchema()
-//    dictionary.show(false)
-//
-//    dictionary.show()
   }
 
   def detectLanguage(text: String, sourceLanguage: String) = {
