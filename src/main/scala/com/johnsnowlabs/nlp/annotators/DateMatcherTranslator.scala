8b5ad3bbd960456f9ffcc0b55f5ba7f378f6e802
Stefano Lori <wolliq@gmail.com>
2021-06-08 11:46:19 +0200

    Added language auto-detection mode

--- src/main/scala/com/johnsnowlabs/nlp/annotators/DateMatcherTranslator.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/DateMatcherTranslator.scala
@@ -7,10 +7,8 @@ import scala.io.Source
 
 object DateMatcherTranslator extends Serializable {
 
-  val Language = "it_new"
-  val DictionaryPath = s"src/main/resources/date-matcher/date_translation_data/$Language.json"
-
-  def loadTranslationDictionary() = {
+  def loadTranslationDictionary(language: String = "en") = {
+    val DictionaryPath = s"src/main/resources/date-matcher/date_translation_data/$language.json"
     val jsonString = Source.fromFile(DictionaryPath).mkString
 
     val json = parse(jsonString)
@@ -18,42 +16,84 @@ object DateMatcherTranslator extends Serializable {
     implicit val formats = DefaultFormats
     val jsonMap: Map[String, Any] = json.extract[Map[String, Any]]
 
-//    println(jsonMap)
+    //    println(jsonMap)
     jsonMap
   }
 
-  val dictionary: Map[String, Any] = loadTranslationDictionary()
+  //  val dictionary: Map[String, Any] = loadTranslationDictionary()
 
-  def monthFlatter(any: Any): List[Any] =
+  def listFlattener(any: Any): List[Any] =
     any match {
       case i: List[_] => i
       case _ => List(any)
     }
 
-  def searchLanguage(text: String) = {
-    // tokenize and search token in keys
-    val tokens = text.split(" ").map(_.toLowerCase).toSet
+  def detectLanguage(text: String) = {
+    val filePath = "src/main/resources/date-matcher/supported_languages.txt"
+    val languages = scala.io.Source.fromFile(filePath, "utf-8").getLines.toList
 
-    val months = dictionary
-      .getOrElse("month", Map.empty[String, Any])
+    val matched =
+      languages
+        .map(l => searchForLanguageMatches(text, l))
+        .filterNot(_._1.equals("NF"))
+
+    val mappedDetected: Map[String, Set[String]] = matched.toMap
+    println(mappedDetected)
+    val matchesLengths: Map[String, Int] = mappedDetected.map{case(k, v) => (k, v.size)}
+
+    val maxValue = matchesLengths.values.max
+    val detected = matchesLengths.filter(_._2 == maxValue).toList
+
+    detected
+  }
+
+  private def searchForLanguageMatches(text: String, language: String) = {
+    val dictionary: Map[String, Any] = loadTranslationDictionary(language)
+    // tokenize and search token in keys
+    val tokens = text.split(" ").filterNot(_.size <= 2).map(_.toLowerCase).toSet
+
+    val dictionaryValues = dictionary
       .asInstanceOf[Map[String, Any]]
-      .values.flatten(monthFlatter).asInstanceOf[List[String]]
+      .values.flatten(listFlattener).asInstanceOf[List[String]]
       .toSet
 
-    println(months)
+    val res: (String, Set[String]) =
+      if(!(tokens intersect dictionaryValues isEmpty))
+        dictionary
+          .asInstanceOf[Map[String, Any]]
+          .getOrElse("name", "NA")
+          .toString -> (tokens intersect dictionaryValues)
+      else
+        "NF" -> Set.empty
 
-    !(tokens intersect months isEmpty)
+    res
   }
 
   def detectSourceLanguage(text: String) = {
-    val _sourceLanguage = searchLanguage(text)
-    _sourceLanguage
+    val detected: List[(String, Int)] = detectLanguage(text)
+
+    if(detected.size != 1)
+      throw new Exception(s"" +
+        s"Detected multiple languages. " +
+        s"Please specify which one to use using the setSourceLanguage method in the DateMatcher annotator. " +
+        s"Detected: ${detected.map(_._1).mkString(", ")} .")
+
+    detected
   }
 
-  def translateLanguage(text: String, sourceLanguage: String, destinationLanguage: String = "en") = {
-    // detect source language
-    val _sourceLanguage = if(!sourceLanguage.equals("en")) detectSourceLanguage(text) else "en"
-    // apply translation
-    ""
+  def translateLanguage(text: String,
+                        sourceLanguage: String,
+                        destinationLanguage: String = "en") = {
+
+    // 1. detect source language if None
+    val detectedLanguage =
+      if(sourceLanguage.isEmpty)
+        detectSourceLanguage(text)
+      else
+        sourceLanguage
+
+    // 2. apply translation
+    println(s"Translating from $sourceLanguage to $destinationLanguage...")
+    "translated"
   }
 }
