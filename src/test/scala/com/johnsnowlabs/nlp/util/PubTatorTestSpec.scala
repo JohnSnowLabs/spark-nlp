4186c11c5a6fe7b47816ffd6d327c122444586de
xusliebana <jesus@johnsnowlabs.com>
2021-06-18 19:32:29 +0200

    python pubtator fix

--- src/test/scala/com/johnsnowlabs/nlp/util/PubTatorTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/util/PubTatorTestSpec.scala
@@ -17,8 +17,19 @@ class PubTatorTestSpec extends FlatSpec{
     //remove file if it's already there
     val directory = new Directory(new File("./pubtator-conll-test"))
     directory.deleteRecursively()
-    val df = PubTator.readDataset(ResourceHelper.spark, "./src/test/resources/corpus_pubtator_sample.txt")
+    val df = PubTator().readDataset(ResourceHelper.spark, "./src/test/resources/corpus_pubtator_sample.txt")
     CoNLLGenerator.exportConllFiles(df, "pubtator-conll-test")
+    directory.deleteRecursively()
+
+  }
+
+  "PubTator.readDataset" should "create conll-friendly dataframe with not padding" taggedAs FastTest in {
+
+    //remove file if it's already there
+    val directory = new Directory(new File("./pubtator_not_padding-conll-test"))
+    directory.deleteRecursively()
+    val df = PubTator().readDataset(ResourceHelper.spark, "src/test/resources/corpus_pubtator_not_padding.txt",false)
+    CoNLLGenerator.exportConllFiles(df, "pubtator_not_padding-conll-test")
     directory.deleteRecursively()
 
   }
