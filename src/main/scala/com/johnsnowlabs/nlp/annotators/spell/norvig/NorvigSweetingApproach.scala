beba49d668d42a059556a6058563b16e72055a15
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-06-18 14:11:08 +0200

    Docs: Added examples for NorvigSweeting*, SymmetricDelete*

--- src/main/scala/com/johnsnowlabs/nlp/annotators/spell/norvig/NorvigSweetingApproach.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/spell/norvig/NorvigSweetingApproach.scala
@@ -8,30 +8,77 @@ import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
 import org.apache.spark.sql.{AnalysisException, Dataset}
 
-/** This annotator retrieves tokens and makes corrections automatically if not found in an English dictionary. Inspired by Norvig model
-  *
-  * Inspired on [[https://github.com/wolfgarbe/SymSpell]]
-  *
-  * The Symmetric Delete spelling correction algorithm reduces the complexity of edit candidate generation and
-  * dictionary lookup for a given Damerau-Levenshtein distance. It is six orders of magnitude faster
-  * (than the standard approach with deletes + transposes + replaces + inserts) and language independent.
-  *
-  * See [[https://github.com/JohnSnowLabs/spark-nlp/blob/master/src/test/scala/com/johnsnowlabs/nlp/annotators/spell/norvig/NorvigSweetingTestSpec.scala]] for further reference on how to use this API
-  *
-  * @groupname anno Annotator types
-  * @groupdesc anno Required input and expected output annotator types
-  * @groupname Ungrouped Members
-  * @groupname param Parameters
-  * @groupname setParam Parameter setters
-  * @groupname getParam Parameter getters
-  * @groupname Ungrouped Members
-  * @groupprio param  1
-  * @groupprio anno  2
-  * @groupprio Ungrouped 3
-  * @groupprio setParam  4
-  * @groupprio getParam  5
-  * @groupdesc param A list of (hyper-)parameter keys this annotator can take. Users can set and get the parameter values through setters and getters, respectively.
-  **/
+/** Trains annotator, that retrieves tokens and makes corrections automatically if not found in an English dictionary.
+ *
+ * The Symmetric Delete spelling correction algorithm reduces the complexity of edit candidate generation and
+ * dictionary lookup for a given Damerau-Levenshtein distance. It is six orders of magnitude faster
+ * (than the standard approach with deletes + transposes + replaces + inserts) and language independent.
+ * A dictionary of correct spellings must be provided with `setDictionary` either in the form of a text file or directly
+ * as an [[com.johnsnowlabs.nlp.util.io.ExternalResource ExternalResource]], where each word is parsed by a regex pattern.
+ *
+ * Inspired by Norvig model and [[https://github.com/wolfgarbe/SymSpell SymSpell]].
+ *
+ * For instantiated/pretrained models, see [[NorvigSweetingModel]].
+ *
+ * For extended examples of usage, see the [[https://github.com/JohnSnowLabs/spark-nlp-workshop/blob/master/jupyter/training/english/vivekn-sentiment/VivekNarayanSentimentApproach.ipynb Spark NLP Workshop]]
+ * and the [[https://github.com/JohnSnowLabs/spark-nlp/blob/master/src/test/scala/com/johnsnowlabs/nlp/annotators/spell/norvig/NorvigSweetingTestSpec.scala NorvigSweetingTestSpec]].
+ *
+ * ==Example==
+ * In this example, the dictionary `"words.txt"` has the form of
+ * {{{
+ * ...
+ * gummy
+ * gummic
+ * gummier
+ * gummiest
+ * gummiferous
+ * ...
+ * }}}
+ * This dictionary is then set to be the basis of the spell checker.
+ * {{{
+ * import com.johnsnowlabs.nlp.base.DocumentAssembler
+ * import com.johnsnowlabs.nlp.annotators.Tokenizer
+ * import com.johnsnowlabs.nlp.annotators.spell.norvig.NorvigSweetingApproach
+ * import org.apache.spark.ml.Pipeline
+ *
+ * val documentAssembler = new DocumentAssembler()
+ *   .setInputCol("text")
+ *   .setOutputCol("document")
+ *
+ * val tokenizer = new Tokenizer()
+ *   .setInputCols("document")
+ *   .setOutputCol("token")
+ *
+ * val spellChecker = new NorvigSweetingApproach()
+ *   .setInputCols("token")
+ *   .setOutputCol("spell")
+ *   .setDictionary("src/test/resources/spell/words.txt")
+ *
+ * val pipeline = new Pipeline().setStages(Array(
+ *   documentAssembler,
+ *   tokenizer,
+ *   spellChecker
+ * ))
+ *
+ * val pipelineModel = pipeline.fit(trainingData)
+ * }}}
+ *
+ * @see [[com.johnsnowlabs.nlp.annotators.spell.symmetric.SymmetricDeleteApproach SymmetricDeleteApproach]] for an alternative approach to spell checking
+ * @see [[com.johnsnowlabs.nlp.annotators.spell.context.ContextSpellCheckerApproach ContextSpellCheckerApproach]] for a DL based approach
+ * @groupname anno Annotator types
+ * @groupdesc anno Required input and expected output annotator types
+ * @groupname Ungrouped Members
+ * @groupname param Parameters
+ * @groupname setParam Parameter setters
+ * @groupname getParam Parameter getters
+ * @groupname Ungrouped Members
+ * @groupprio param  1
+ * @groupprio anno  2
+ * @groupprio Ungrouped 3
+ * @groupprio setParam  4
+ * @groupprio getParam  5
+ * @groupdesc param A list of (hyper-)parameter keys this annotator can take. Users can set and get the parameter values through setters and getters, respectively.
+ * */
 class NorvigSweetingApproach(override val uid: String)
   extends AnnotatorApproach[NorvigSweetingModel]
     with NorvigSweetingParams {
@@ -40,11 +87,21 @@ class NorvigSweetingApproach(override val uid: String)
 
   /** Spell checking algorithm inspired on Norvig model */
   override val description: String = "Spell checking algorithm inspired on Norvig model"
-  /** file with a list of correct words
-    *
-    * @group param
-    **/
-  val dictionary = new ExternalResourceParam(this, "dictionary", "file with a list of correct words")
+  /** External dictionary to be used, which needs `"tokenPattern"` (Default: `\S+`) for parsing the resource.
+   * ==Example==
+   * {{{
+   * ...
+   * gummy
+   * gummic
+   * gummier
+   * gummiest
+   * gummiferous
+   * ...
+   * }}}
+   *
+   * @group param
+   * */
+  val dictionary = new ExternalResourceParam(this, "dictionary", "File with a list of correct words")
 
   setDefault(
     caseSensitive -> true,
@@ -58,19 +115,33 @@ class NorvigSweetingApproach(override val uid: String)
     vowelSwapLimit -> 6
   )
 
-  /** External path to file with properly spelled words, tokenPattern is the regex pattern to identify them in text, readAs LINE_BY_LINE or SPARK_DATASET, with options passed to Spark reader if the latter is set. dictionary needs 'tokenPattern' regex in dictionary for separating words
-    *
-    * @group setParam
-    **/
+  /** External dictionary already in the form of [[ExternalResource]], for which the Map member `options`
+   * has an entry defined for `"tokenPattern"`.
+   * ==Example==
+   * {{{
+   * val resource = ExternalResource(
+   *   "src/test/resources/spell/words.txt",
+   *   ReadAs.TEXT,
+   *   Map("tokenPattern" -> "\\S+")
+   * )
+   * val spellChecker = new NorvigSweetingApproach()
+   *   .setInputCols("token")
+   *   .setOutputCol("spell")
+   *   .setDictionary(resource)
+   * }}}
+   * @group setParam
+   * */
   def setDictionary(value: ExternalResource): this.type = {
     require(value.options.contains("tokenPattern"), "dictionary needs 'tokenPattern' regex in dictionary for separating words")
     set(dictionary, value)
   }
 
-  /** Path to file with properly spelled words, tokenPattern is the regex pattern to identify them in text, readAs LINE_BY_LINE or SPARK_DATASET, with options passed to Spark reader if the latter is set. dictionary needs 'tokenPattern' regex in dictionary for separating words
-    *
-    * @group setParam
-    **/
+  /** Path to file with properly spelled words, `tokenPattern` is the regex pattern to identify them in text,
+   * readAs can be `ReadAs.TEXT` or `ReadAs.SPARK`, with options passed to Spark reader if the latter is set.
+   * Dictionary needs `tokenPattern` regex for separating words.
+   *
+   * @group setParam
+   * */
   def setDictionary(path: String,
                     tokenPattern: String = "\\S+",
                     readAs: ReadAs.Format = ReadAs.TEXT,
@@ -78,14 +149,14 @@ class NorvigSweetingApproach(override val uid: String)
     set(dictionary, ExternalResource(path, readAs, options ++ Map("tokenPattern" -> tokenPattern)))
 
   /** Output annotator type : TOKEN
-    *
-    * @group anno
-    **/
+   *
+   * @group anno
+   * */
   override val outputAnnotatorType: AnnotatorType = TOKEN
-  /** Output annotator type : TOKEN
-    *
-    * @group anno
-    **/
+  /** Input annotator type : TOKEN
+   *
+   * @group anno
+   * */
   override val inputAnnotatorTypes: Array[AnnotatorType] = Array(TOKEN)
 
   def this() = this(Identifiable.randomUID("SPELL"))
@@ -96,12 +167,12 @@ class NorvigSweetingApproach(override val uid: String)
     val loadWords = ResourceHelper.getWordCount($(dictionary)).toMap
     val corpusWordCount: Map[String, Long] = {
 
-        dataset.select(getInputCols.head).as[Array[Annotation]]
-          .flatMap(_.map(_.result))
-          .groupBy("value").count
-          .as[(String, Long)]
-          .collect.toMap
-      }
+      dataset.select(getInputCols.head).as[Array[Annotation]]
+        .flatMap(_.map(_.result))
+        .groupBy("value").count
+        .as[(String, Long)]
+        .collect.toMap
+    }
 
     new NorvigSweetingModel()
       .setWordSizeIgnore($(wordSizeIgnore))
@@ -130,6 +201,7 @@ class NorvigSweetingApproach(override val uid: String)
   }
 
 }
+
 /**
  * This is the companion object of [[NorvigSweetingApproach]]. Please refer to that class for the documentation.
  */
