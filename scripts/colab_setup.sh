a30464955c298a506a37eed1daf6479e6f6ee9e6
C-K-Loan <christian.kasim.loan@gmail.com>
2021-06-28 01:49:59 +0200

    updated script for colab

--- scripts/colab_setup.sh
+++ scripts/colab_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #default values for pyspark, spark-nlp, and SPARK_HOME
-SPARKNLP="3.1.1"
-PYSPARK="3.0.3"
-SPARKHOME="/content/spark-3.1.2-bin-hadoop2.7"
+SPARKNLP="3.1.0"
+PYSPARK="3.0.2"
+SPARKHOME="/content/spark-3.0.3-bin-hadoop2.7"
 
 while getopts s:p: option
 do
