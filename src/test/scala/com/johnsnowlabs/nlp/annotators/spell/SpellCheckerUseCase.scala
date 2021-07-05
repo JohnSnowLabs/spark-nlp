0aa19a17e28e2395d74841046badd6bd11fcfb33
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-07-05 21:08:11 +0200

    Update Code styling in unit test [skip ci]

--- src/test/scala/com/johnsnowlabs/nlp/annotators/spell/SpellCheckerUseCase.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/spell/SpellCheckerUseCase.scala
@@ -1,10 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.spell
+
 import com.johnsnowlabs.nlp.annotator.RecursiveTokenizer
 import com.johnsnowlabs.nlp.{DocumentAssembler, SparkAccessor}
 import com.johnsnowlabs.nlp.annotators.spell.context.{ContextSpellCheckerApproach, ContextSpellCheckerModel}
 import org.apache.spark.ml.Pipeline
 
-object SpellCheckerUseCase extends App  {
+object SpellCheckerUseCase extends App {
 
   // ==================================
   // Getting and cleaning all the data
@@ -13,7 +31,7 @@ object SpellCheckerUseCase extends App  {
   // Let's use the Paisa corpus
   // https://clarin.eurac.edu/repository/xmlui/bitstream/handle/20.500.12124/3/paisa.raw.utf8.gz
   // (update with the path you downloaded the file to)
-  val paisaCorpusPath = "/home/jose/Downloads/paisa/paisa.raw.utf8"
+  val paisaCorpusPath = "/tmp/paisa/paisa.raw.utf8"
 
   // do some brief DS exploration, and preparation to get clean text
   val df = SparkAccessor.spark.read.text(paisaCorpusPath)
@@ -23,26 +41,27 @@ object SpellCheckerUseCase extends App  {
     filter(!df("value").startsWith("#")).
     limit(10000)
 
-  dataset.show(truncate=false)
+  dataset.show(truncate = false)
 
-  val names = List( "Achille", "Achillea", "Achilleo",  "Achillina", "Achiropita", "Acilia", "Acilio", "Acquisto",
-  "Acrisio", "Ada", "Adalberta", "Adalberto", "Adalciso", "Adalgerio", "Adalgisa")
+  val names = List("Achille", "Achillea", "Achilleo", "Achillina", "Achiropita", "Acilia", "Acilio", "Acquisto",
+    "Acrisio", "Ada", "Adalberta", "Adalberto", "Adalciso", "Adalgerio", "Adalgisa")
 
   import scala.collection.JavaConverters._
+
   val javaNames = new java.util.ArrayList[String](names.asJava)
 
   // ==================================
   // all the pipeline & training
   // ==================================
   val assembler = new DocumentAssembler()
-  .setInputCol("value")
-  .setOutputCol("document")
+    .setInputCol("value")
+    .setOutputCol("document")
 
   val tokenizer = new RecursiveTokenizer()
-  .setInputCols("document")
-  .setOutputCol("token")
-  .setPrefixes(Array("\"", "“", "(", "[", "\n", ".", "l’", "dell’", "nell’", "sull’", "all’", "d’", "un’"))
-  .setSuffixes(Array("\"", "”", ".", ",", "?", ")", "]", "!", ";", ":"))
+    .setInputCols("document")
+    .setOutputCol("token")
+    .setPrefixes(Array("\"", "“", "(", "[", "\n", ".", "l’", "dell’", "nell’", "sull’", "all’", "d’", "un’"))
+    .setSuffixes(Array("\"", "”", ".", ",", "?", ")", "]", "!", ";", ":"))
 
   val spellCheckerModel = new ContextSpellCheckerApproach()
     .setInputCols("token")
@@ -54,7 +73,7 @@ object SpellCheckerUseCase extends App  {
 
   val pipelineTok = new Pipeline().setStages(Array(assembler, tokenizer)).fit(dataset)
   val tokenized = pipelineTok.transform(dataset)
-  spellCheckerModel.fit(tokenized).write.save("src/test/resources/spell/contextSpellCheckerModel")
-  val spellChecker = ContextSpellCheckerModel.load("src/test/resources/spell/contextSpellCheckerModel")
+  spellCheckerModel.fit(tokenized).write.save("./tmp_contextSpellCheckerModel")
+  val spellChecker = ContextSpellCheckerModel.load("./tmp_contextSpellCheckerModel")
 
 }
