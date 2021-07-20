5faefa8c26fa9ef3e6ccb8f5ef8911bd997b0ee0
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-07-20 09:37:39 +0200

    Add NorvigSweetingModelTestSpec to Python [skip ci]

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -510,6 +510,35 @@ class SpellCheckerTestSpec(unittest.TestCase):
         checked.show()
 
 
+class NorvigSweetingModelTestSpec(unittest.TestCase):
+
+    def setUp(self):
+        self.data = SparkContextForTest.spark \
+            .createDataFrame([["I saw a girl with a telescope"]]).toDF("text")
+
+    def runTest(self):
+
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+
+        tokenizer = Tokenizer() \
+            .setInputCols(["document"]) \
+            .setOutputCol("token")
+
+        spell_checker = NorvigSweetingModel.pretrained() \
+            .setInputCols(["token"]) \
+            .setOutputCol("spell")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            tokenizer,
+            spell_checker
+        ])
+
+        pipelineDF = pipeline.fit(self.data).transform(self.data)
+        pipelineDF.show()
+
 class SymmetricDeleteTestSpec(unittest.TestCase):
 
     def setUp(self):
