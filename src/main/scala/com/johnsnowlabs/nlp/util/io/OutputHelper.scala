8017a03baad245d3f9f4d7f148f9ef6fb593bfa6
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-08-02 09:57:08 +0200

    Update code styling [skip ci]

--- src/main/scala/com/johnsnowlabs/nlp/util/io/OutputHelper.scala
+++ src/main/scala/com/johnsnowlabs/nlp/util/io/OutputHelper.scala
@@ -1,7 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.util.io
 
 import com.johnsnowlabs.util.{ConfigHelper, ConfigLoader}
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.Path
 
 import java.io.{File, FileWriter, PrintWriter}
 import scala.language.existentials
@@ -20,7 +37,7 @@ object OutputHelper {
 
     if (isDBFS) {
       if (!new File(targetFolder).exists()) new File(targetFolder).mkdirs()
-    }else{
+    } else {
       if (!fileSystem.exists(new Path(targetFolder))) fileSystem.mkdirs(new Path(targetFolder))
     }
 
--- src/main/scala/com/johnsnowlabs/util/ConfigLoader.scala
+++ src/main/scala/com/johnsnowlabs/util/ConfigLoader.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.util
 
 import org.apache.hadoop.fs.FileSystem
@@ -16,22 +33,22 @@ object ConfigLoader {
   private lazy val configData: Map[String, String] = {
 
     getConfigInfo(ConfigHelper.pretrainedS3BucketKey, "auxdata.johnsnowlabs.com") ++
-    getConfigInfo(ConfigHelper.pretrainedCommunityS3BucketKey, "community.johnsnowlabs.com") ++
-    getConfigInfo(ConfigHelper.pretrainedS3PathKey, "") ++
-    getConfigInfo(ConfigHelper.pretrainedCacheFolder, homeDirectory + "/cache_pretrained") ++
-    getConfigInfo(ConfigHelper.annotatorLogFolder, homeDirectory + "/annotator_logs") ++
-    getConfigInfo(ConfigHelper.accessKeyId, "") ++
-    getConfigInfo(ConfigHelper.secretAccessKey, "") ++
-    getConfigInfo(ConfigHelper.awsProfileName, "") ++
-    getConfigInfo(ConfigHelper.s3SocketTimeout, "0") ++
-    getConfigInfo(ConfigHelper.storageTmpDir, hadoopTmpDir) ++
-    getConfigInfo(ConfigHelper.serializationMode, "object") ++
-    getConfigInfo(ConfigHelper.useBroadcast, "true")
+      getConfigInfo(ConfigHelper.pretrainedCommunityS3BucketKey, "community.johnsnowlabs.com") ++
+      getConfigInfo(ConfigHelper.pretrainedS3PathKey, "") ++
+      getConfigInfo(ConfigHelper.pretrainedCacheFolder, homeDirectory + "/cache_pretrained") ++
+      getConfigInfo(ConfigHelper.annotatorLogFolder, homeDirectory + "/annotator_logs") ++
+      getConfigInfo(ConfigHelper.accessKeyId, "") ++
+      getConfigInfo(ConfigHelper.secretAccessKey, "") ++
+      getConfigInfo(ConfigHelper.awsProfileName, "") ++
+      getConfigInfo(ConfigHelper.s3SocketTimeout, "0") ++
+      getConfigInfo(ConfigHelper.storageTmpDir, hadoopTmpDir) ++
+      getConfigInfo(ConfigHelper.serializationMode, "object") ++
+      getConfigInfo(ConfigHelper.useBroadcast, "true")
 
   }
 
   private def getConfigInfo(property: String, defaultValue: String): Map[String, String] = {
-    if (property== ConfigHelper.storageTmpDir) {
+    if (property == ConfigHelper.storageTmpDir) {
       val path = ConfigHelper.getConfigValueOrElse(property, defaultValue)
       val tmpLocation = path + "/" + UUID.randomUUID().toString.takeRight(12) + "_cdx"
       Map(property -> tmpLocation)
