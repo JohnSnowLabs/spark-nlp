041ae346db9fc1ec59530e49b6a25754d7470f89
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-08-03 15:38:20 +0200

    PyDoc: NerOverwriter

--- python/sparknlp/annotator.py
+++ python/sparknlp/annotator.py
@@ -6638,8 +6638,10 @@ class ChunkEmbeddings(AnnotatorModel):
 class NerOverwriter(AnnotatorModel):
     """Overwrites entities of specified strings.
 
-    The input for this Annotator have to be entities that are already extracted, Annotator type ``NAMED_ENTITY``.
-    The strings specified with ``setStopWords`` will have new entities assigned to, specified with ``setNewResult``.
+    The input for this Annotator have to be entities that are already extracted,
+    Annotator type ``NAMED_ENTITY``. The strings specified with
+    :meth:`.setStopWords` will have new entities assigned to, specified with
+    :meth:`.setNewResult`.
 
     ====================== ======================
     Input Annotation types Output Annotation type
@@ -6657,91 +6659,79 @@ class NerOverwriter(AnnotatorModel):
 
     Examples
     --------
+    >>> import sparknlp
+    >>> from sparknlp.base import *
+    >>> from sparknlp.annotator import *
+    >>> from pyspark.ml import Pipeline
 
-    .. code-block:: python
+    First extract the prerequisite Entities
 
-        import sparknlp
-        from sparknlp.base import *
-        from sparknlp.common import *
-        from sparknlp.annotator import *
-        from sparknlp.training import *
-        from pyspark.ml import Pipeline
+    >>> documentAssembler = DocumentAssembler() \\
+    ...     .setInputCol("text") \\
+    ...     .setOutputCol("document")
+    >>> sentence = SentenceDetector() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentence")
+    >>> tokenizer = Tokenizer() \\
+    ...     .setInputCols(["sentence"]) \\
+    ...     .setOutputCol("token")
+    >>> embeddings = WordEmbeddingsModel.pretrained() \\
+    ...     .setInputCols(["sentence", "token"]) \\
+    ...     .setOutputCol("bert")
+    >>> nerTagger = NerDLModel.pretrained() \\
+    ...     .setInputCols(["sentence", "token", "bert"]) \\
+    ...     .setOutputCol("ner")
+    >>> pipeline = Pipeline().setStages([
+    ...     documentAssembler,
+    ...     sentence,
+    ...     tokenizer,
+    ...     embeddings,
+    ...     nerTagger
+    ... ])
+    >>> data = spark.createDataFrame([["Spark NLP Crosses Five Million Downloads, John Snow Labs Announces."]]).toDF("text")
+    >>> result = pipeline.fit(data).transform(data)
+    >>> result.selectExpr("explode(ner)").show(truncate=False)
+    +------------------------------------------------------+
+    |col                                                   |
+    +------------------------------------------------------+
+    |[named_entity, 0, 4, B-ORG, [word -> Spark], []]      |
+    |[named_entity, 6, 8, I-ORG, [word -> NLP], []]        |
+    |[named_entity, 10, 16, O, [word -> Crosses], []]      |
+    |[named_entity, 18, 21, O, [word -> Five], []]         |
+    |[named_entity, 23, 29, O, [word -> Million], []]      |
+    |[named_entity, 31, 39, O, [word -> Downloads], []]    |
+    |[named_entity, 40, 40, O, [word -> ,], []]            |
+    |[named_entity, 42, 45, B-ORG, [word -> John], []]     |
+    |[named_entity, 47, 50, I-ORG, [word -> Snow], []]     |
+    |[named_entity, 52, 55, I-ORG, [word -> Labs], []]     |
+    |[named_entity, 57, 65, I-ORG, [word -> Announces], []]|
+    |[named_entity, 66, 66, O, [word -> .], []]            |
+    +------------------------------------------------------+
 
-        # First extract the prerequisite Entities
-        documentAssembler = DocumentAssembler() \\
-            .setInputCol("text") \\
-            .setOutputCol("document")
+    The recognized entities can then be overwritten
 
-        sentence = SentenceDetector() \\
-            .setInputCols(["document"]) \\
-            .setOutputCol("sentence")
-
-        tokenizer = Tokenizer() \\
-            .setInputCols(["sentence"]) \\
-            .setOutputCol("token")
-
-        embeddings = WordEmbeddingsModel.pretrained() \\
-            .setInputCols(["sentence", "token"]) \\
-            .setOutputCol("bert")
-
-        nerTagger = NerDLModel.pretrained() \\
-            .setInputCols(["sentence", "token", "bert"]) \\
-            .setOutputCol("ner")
-
-        pipeline = Pipeline().setStages([
-            documentAssembler,
-            sentence,
-            tokenizer,
-            embeddings,
-            nerTagger
-        ])
-
-        data = spark.createDataFrame([["Spark NLP Crosses Five Million Downloads, John Snow Labs Announces."]]).toDF("text")
-        result = pipeline.fit(data).transform(data)
-
-        result.selectExpr("explode(ner)").show(truncate=False)
-        # +------------------------------------------------------+
-        # |col                                                   |
-        # +------------------------------------------------------+
-        # |[named_entity, 0, 4, B-ORG, [word -> Spark], []]      |
-        # |[named_entity, 6, 8, I-ORG, [word -> NLP], []]        |
-        # |[named_entity, 10, 16, O, [word -> Crosses], []]      |
-        # |[named_entity, 18, 21, O, [word -> Five], []]         |
-        # |[named_entity, 23, 29, O, [word -> Million], []]      |
-        # |[named_entity, 31, 39, O, [word -> Downloads], []]    |
-        # |[named_entity, 40, 40, O, [word -> ,], []]            |
-        # |[named_entity, 42, 45, B-ORG, [word -> John], []]     |
-        # |[named_entity, 47, 50, I-ORG, [word -> Snow], []]     |
-        # |[named_entity, 52, 55, I-ORG, [word -> Labs], []]     |
-        # |[named_entity, 57, 65, I-ORG, [word -> Announces], []]|
-        # |[named_entity, 66, 66, O, [word -> .], []]            |
-        # +------------------------------------------------------+
-
-        # The recognized entities can then be overwritten
-        nerOverwriter = NerOverwriter() \\
-            .setInputCols(["ner"]) \\
-            .setOutputCol("ner_overwritten") \\
-            .setStopWords(["Million"]) \\
-            .setNewResult("B-CARDINAL")
-
-        nerOverwriter.transform(result).selectExpr("explode(ner_overwritten)").show(truncate=False)
-        +---------------------------------------------------------+
-        |col                                                      |
-        +---------------------------------------------------------+
-        |[named_entity, 0, 4, B-ORG, [word -> Spark], []]         |
-        |[named_entity, 6, 8, I-ORG, [word -> NLP], []]           |
-        |[named_entity, 10, 16, O, [word -> Crosses], []]         |
-        |[named_entity, 18, 21, O, [word -> Five], []]            |
-        |[named_entity, 23, 29, B-CARDINAL, [word -> Million], []]|
-        |[named_entity, 31, 39, O, [word -> Downloads], []]       |
-        |[named_entity, 40, 40, O, [word -> ,], []]               |
-        |[named_entity, 42, 45, B-ORG, [word -> John], []]        |
-        |[named_entity, 47, 50, I-ORG, [word -> Snow], []]        |
-        |[named_entity, 52, 55, I-ORG, [word -> Labs], []]        |
-        |[named_entity, 57, 65, I-ORG, [word -> Announces], []]   |
-        |[named_entity, 66, 66, O, [word -> .], []]               |
-        +---------------------------------------------------------+
-
+    >>> nerOverwriter = NerOverwriter() \\
+    ...     .setInputCols(["ner"]) \\
+    ...     .setOutputCol("ner_overwritten") \\
+    ...     .setStopWords(["Million"]) \\
+    ...     .setNewResult("B-CARDINAL")
+    >>> nerOverwriter.transform(result).selectExpr("explode(ner_overwritten)").show(truncate=False)
+    +---------------------------------------------------------+
+    |col                                                      |
+    +---------------------------------------------------------+
+    |[named_entity, 0, 4, B-ORG, [word -> Spark], []]         |
+    |[named_entity, 6, 8, I-ORG, [word -> NLP], []]           |
+    |[named_entity, 10, 16, O, [word -> Crosses], []]         |
+    |[named_entity, 18, 21, O, [word -> Five], []]            |
+    |[named_entity, 23, 29, B-CARDINAL, [word -> Million], []]|
+    |[named_entity, 31, 39, O, [word -> Downloads], []]       |
+    |[named_entity, 40, 40, O, [word -> ,], []]               |
+    |[named_entity, 42, 45, B-ORG, [word -> John], []]        |
+    |[named_entity, 47, 50, I-ORG, [word -> Snow], []]        |
+    |[named_entity, 52, 55, I-ORG, [word -> Labs], []]        |
+    |[named_entity, 57, 65, I-ORG, [word -> Announces], []]   |
+    |[named_entity, 66, 66, O, [word -> .], []]               |
+    +---------------------------------------------------------+
     """
     name = "NerOverwriter"
 
@@ -6758,9 +6748,24 @@ class NerOverwriter(AnnotatorModel):
                       typeConverter=TypeConverters.toString)
 
     def setStopWords(self, value):
+        """Sets the words to be overwritten.
+
+        Parameters
+        ----------
+        value : List[str]
+            The words to be overwritten
+        """
         return self._set(stopWords=value)
 
     def setNewResult(self, value):
+        """Sets new NER class to apply to those stopwords, by default
+        I-OVERWRITE.
+
+        Parameters
+        ----------
+        value : str
+            NER class to apply the stopwords to
+        """
         return self._set(newResult=value)
 
 
