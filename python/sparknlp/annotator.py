82053ffe2037181a3a688f148d8a8959d2a34a97
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-08-04 12:45:06 +0200

    PyDoc: SentenceDetectorDL

--- python/sparknlp/annotator.py
+++ python/sparknlp/annotator.py
@@ -10262,26 +10262,25 @@ class YakeModel(AnnotatorModel):
 class SentenceDetectorDLModel(AnnotatorModel):
     """Annotator that detects sentence boundaries using a deep learning approach.
 
-    Instantiated Model of the SentenceDetectorDLApproach.
+    Instantiated Model of the :class:`.SentenceDetectorDLApproach`.
     Detects sentence boundaries using a deep learning approach.
 
     Pretrained models can be loaded with :meth:`.pretrained` of the companion
     object:
 
-    .. code-block:: python
+    >>> sentenceDL = SentenceDetectorDLModel.pretrained() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentencesDL")
 
-        sentenceDL = SentenceDetectorDLModel.pretrained() \\
-            .setInputCols(["document"]) \\
-            .setOutputCol("sentencesDL")
+    The default model is ``"sentence_detector_dl"``, if no name is provided. For
+    available pretrained models please see the `Models Hub
+    <https://nlp.johnsnowlabs.com/models?task=Sentence+Detection>`__.
 
+    Each extracted sentence can be returned in an Array or exploded to separate
+    rows, if ``explodeSentences`` is set to ``true``.
 
-    The default model is ``"sentence_detector_dl"``, if no name is provided.
-    For available pretrained models please see the `Models Hub <https://nlp.johnsnowlabs.com/models?task=Sentence+Detection>`__.
-
-    Each extracted sentence can be returned in an Array or exploded to separate rows,
-    if ``explodeSentences`` is set to ``true``.
-
-    For extended examples of usage, see the `Spark NLP Workshop <https://github.com/JohnSnowLabs/spark-nlp-workshop/blob/master/tutorials/Certification_Trainings/Public/2.Text_Preprocessing_with_SparkNLP_Annotators_Transformers.ipynb>`__.
+    For extended examples of usage, see the `Spark NLP Workshop
+    <https://github.com/JohnSnowLabs/spark-nlp-workshop/blob/master/tutorials/Certification_Trainings/Public/2.Text_Preprocessing_with_SparkNLP_Annotators_Transformers.ipynb>`__.
 
     ====================== ======================
     Input Annotation types Output Annotation type
@@ -10299,62 +10298,51 @@ class SentenceDetectorDLModel(AnnotatorModel):
 
     Examples
     --------
+    In this example, the normal `SentenceDetector` is compared to the
+    `SentenceDetectorDLModel`. In a pipeline, `SentenceDetectorDLModel` can be
+    used as a replacement for the `SentenceDetector`.
 
-    .. code-block:: python
-
-        import sparknlp
-        from sparknlp.base import *
-        from sparknlp.common import *
-        from sparknlp.annotator import *
-        from sparknlp.training import *
-        from pyspark.ml import Pipeline
-        # In this example, the normal `SentenceDetector` is compared to the `SentenceDetectorDLModel`. In a pipeline,
-        # `SentenceDetectorDLModel` can be used as a replacement for the `SentenceDetector`.
-
-        documentAssembler = DocumentAssembler() \\
-            .setInputCol("text") \\
-            .setOutputCol("document")
-
-        sentence = SentenceDetector() \\
-            .setInputCols(["document"]) \\
-            .setOutputCol("sentences")
-
-        sentenceDL = SentenceDetectorDLModel \\
-            .pretrained("sentence_detector_dl", "en") \\
-            .setInputCols(["document"]) \\
-            .setOutputCol("sentencesDL")
-
-        pipeline = Pipeline().setStages([
-            documentAssembler,
-            sentence,
-            sentenceDL
-        ])
-
-        data = spark.createDataFrame([[\"\"\"John loves Mary.Mary loves Peter
-            Peter loves Helen .Helen loves John;
-            Total: four people involved.\"\"\"]]).toDF("text")
-        result = pipeline.fit(data).transform(data)
-
-        result.selectExpr("explode(sentences.result) as sentences").show(truncate=False)
-        +----------------------------------------------------------+
-        |sentences                                                 |
-        +----------------------------------------------------------+
-        |John loves Mary.Mary loves Peter\n     Peter loves Helen .|
-        |Helen loves John;                                         |
-        |Total: four people involved.                              |
-        +----------------------------------------------------------+
-
-        result.selectExpr("explode(sentencesDL.result) as sentencesDL").show(truncate=False)
-        +----------------------------+
-        |sentencesDL                 |
-        +----------------------------+
-        |John loves Mary.            |
-        |Mary loves Peter            |
-        |Peter loves Helen .         |
-        |Helen loves John;           |
-        |Total: four people involved.|
-        +----------------------------+
-
+    >>> import sparknlp
+    >>> from sparknlp.base import *
+    >>> from sparknlp.annotator import *
+    >>> from pyspark.ml import Pipeline
+    >>> documentAssembler = DocumentAssembler() \\
+    ...     .setInputCol("text") \\
+    ...     .setOutputCol("document")
+    >>> sentence = SentenceDetector() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentences")
+    >>> sentenceDL = SentenceDetectorDLModel \\
+    ...     .pretrained("sentence_detector_dl", "en") \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentencesDL")
+    >>> pipeline = Pipeline().setStages([
+    ...     documentAssembler,
+    ...     sentence,
+    ...     sentenceDL
+    ... ])
+    >>> data = spark.createDataFrame([[\"\"\"John loves Mary.Mary loves Peter
+    ...     Peter loves Helen .Helen loves John;
+    ...     Total: four people involved.\"\"\"]]).toDF("text")
+    >>> result = pipeline.fit(data).transform(data)
+    >>> result.selectExpr("explode(sentences.result) as sentences").show(truncate=False)
+    +----------------------------------------------------------+
+    |sentences                                                 |
+    +----------------------------------------------------------+
+    |John loves Mary.Mary loves Peter\\n     Peter loves Helen .|
+    |Helen loves John;                                         |
+    |Total: four people involved.                              |
+    +----------------------------------------------------------+
+    >>> result.selectExpr("explode(sentencesDL.result) as sentencesDL").show(truncate=False)
+    +----------------------------+
+    |sentencesDL                 |
+    +----------------------------+
+    |John loves Mary.            |
+    |Mary loves Peter            |
+    |Peter loves Helen .         |
+    |Helen loves John;           |
+    |Total: four people involved.|
+    +----------------------------+
     """
     name = "SentenceDetectorDLModel"
 
@@ -10367,9 +10355,24 @@ class SentenceDetectorDLModel(AnnotatorModel):
                              TypeConverters.toBoolean)
 
     def setModel(self, modelArchitecture):
+        """Sets the Model architecture. Currently only ``"cnn"`` is available.
+
+        Parameters
+        ----------
+        model_architecture : str
+            Model architecture
+        """
         return self._set(modelArchitecture=modelArchitecture)
 
     def setExplodeSentences(self, value):
+        """Sets whether to explode each sentence into a different row, for
+        better parallelization, by default False.
+
+        Parameters
+        ----------
+        value : bool
+            Whether to explode each sentence into a different row
+        """
         return self._set(explodeSentences=value)
 
     def __init__(self, classname="com.johnsnowlabs.nlp.annotators.sentence_detector_dl.SentenceDetectorDLModel",
@@ -10403,19 +10406,16 @@ class SentenceDetectorDLModel(AnnotatorModel):
 
 
 class SentenceDetectorDLApproach(AnnotatorApproach):
-    """Trains an annotator that detects sentence boundaries using a deep learning approach.
+    """Trains an annotator that detects sentence boundaries using a deep
+    learning approach.
+
+    Currently, only the CNN model is supported for training, but in the future
+    the architecture of the model can be set with :meth:`.setModel`.
 
     For pretrained models see :class:`.SentenceDetectorDLModel`.
 
-    Currently, only the CNN model is supported for training, but in the future the architecture of the model can
-    be set with ``setModelArchitecture``.
-
-    The default model ``"cnn"`` is based on the paper
-    `Deep-EOS: General-Purpose Neural Networks for Sentence Boundary Detection (2020, Stefan Schweter, Sajawel Ahmed) <https://konvens.org/proceedings/2019/papers/KONVENS2019_paper_41.pdf>`__
-    using a CNN architecture. We also modified the original implementation a little bit to cover broken sentences and some impossible end of line chars.
-
-    Each extracted sentence can be returned in an Array or exploded to separate rows,
-    if ``explodeSentences`` is set to ``true``.
+    Each extracted sentence can be returned in an Array or exploded to separate
+    rows, if ``explodeSentences`` is set to ``True``.
 
     ====================== ======================
     Input Annotation types Output Annotation type
@@ -10429,53 +10429,57 @@ class SentenceDetectorDLApproach(AnnotatorApproach):
     modelArchitecture
         Model architecture (CNN)
     impossiblePenultimates
-        Impossible penultimates - list of strings which a sentence can't end with
+        Impossible penultimates - list of strings which a sentence can't end
+        with
     validationSplit
-        Choose the proportion of training dataset to be validated against the model on each
+        Choose the proportion of training dataset to be validated against the
+        model on each
     epochsNumber
         Number of epochs for the optimization process
     outputLogsPath
-        Path to folder where logs will be saved. If no path is specified, no logs are generated
+        Path to folder where logs will be saved. If no path is specified, no
+        logs are generated
     explodeSentences
-        whether to explode each sentence into a different row, for better parallelization. Defaults to false.
+        Whether to explode each sentence into a different row, for better
+        parallelization. Defaults to False.
+
+    References
+    ----------
+    The default model ``"cnn"`` is based on the paper `Deep-EOS: General-Purpose
+    Neural Networks for Sentence Boundary Detection (2020, Stefan Schweter,
+    Sajawel Ahmed)
+    <https://konvens.org/proceedings/2019/papers/KONVENS2019_paper_41.pdf>`__
+    using a CNN architecture. We also modified the original implementation a
+    little bit to cover broken sentences and some impossible end of line chars.
 
     Examples
     --------
+    The training process needs data, where each data point is a sentence.
+    In this example the ``train.txt`` file has the form of::
 
-    .. code-block:: python
+        ...
+        Slightly more moderate language would make our present situation – namely the lack of progress – a little easier.
+        His political successors now have great responsibilities to history and to the heritage of values bequeathed to them by Nelson Mandela.
+        ...
 
-        import sparknlp
-        from sparknlp.base import *
-        from sparknlp.common import *
-        from sparknlp.annotator import *
-        from sparknlp.training import *
-        from pyspark.ml import Pipeline
-        # The training process needs data, where each data point is a sentence.
-        # In this example the `train.txt` file has the form of
-        #
-        # ...
-        # Slightly more moderate language would make our present situation – namely the lack of progress – a little easier.
-        # His political successors now have great responsibilities to history and to the heritage of values bequeathed to them by Nelson Mandela.
-        # ...
-        #
-        # where each line is one sentence.
-        # Training can then be started like so:
+    where each line is one sentence.
 
-        trainingData = spark.read.text("train.txt").toDF("text")
+    Training can then be started like so:
 
-        documentAssembler = DocumentAssembler() \\
-            .setInputCol("text") \\
-            .setOutputCol("document")
-
-        sentenceDetector = SentenceDetectorDLApproach() \\
-            .setInputCols(["document"]) \\
-            .setOutputCol("sentences") \\
-            .setEpochsNumber(100)
-
-        pipeline = Pipeline().setStages([documentAssembler, sentenceDetector])
-
-        model = pipeline.fit(trainingData)
-
+    >>> import sparknlp
+    >>> from sparknlp.base import *
+    >>> from sparknlp.annotator import *
+    >>> from pyspark.ml import Pipeline
+    >>> trainingData = spark.read.text("train.txt").toDF("text")
+    >>> documentAssembler = DocumentAssembler() \\
+    ...     .setInputCol("text") \\
+    ...     .setOutputCol("document")
+    >>> sentenceDetector = SentenceDetectorDLApproach() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentences") \\
+    ...     .setEpochsNumber(100)
+    >>> pipeline = Pipeline().setStages([documentAssembler, sentenceDetector])
+    >>> model = pipeline.fit(trainingData)
     """
 
     name = "SentenceDetectorDLApproach"
@@ -10512,6 +10516,13 @@ class SentenceDetectorDLApproach(AnnotatorApproach):
                              TypeConverters.toBoolean)
 
     def setModel(self, model_architecture):
+        """Sets the Model architecture. Currently only ``"cnn"`` is available.
+
+        Parameters
+        ----------
+        model_architecture : str
+            Model architecture
+        """
         return self._set(modelArchitecture=model_architecture)
 
     def setValidationSplit(self, validation_split):
@@ -10527,6 +10538,13 @@ class SentenceDetectorDLApproach(AnnotatorApproach):
         return self._set(validationSplit=validation_split)
 
     def setEpochsNumber(self, epochs_number):
+        """Sets number of epochs to train.
+
+        Parameters
+        ----------
+        epochs_number : int
+            Number of epochs
+        """
         return self._set(epochsNumber=epochs_number)
 
     def setOutputLogsPath(self, output_logs_path):
@@ -10540,9 +10558,26 @@ class SentenceDetectorDLApproach(AnnotatorApproach):
         return self._set(outputLogsPath=output_logs_path)
 
     def setImpossiblePenultimates(self, impossible_penultimates):
+        """Sets impossible penultimates - list of strings which a sentence can't
+        end with
+
+        Parameters
+        ----------
+        impossible_penultimates : List[str]
+            List of strings which a sentence can't end with
+
+        """
         return self._set(impossiblePenultimates=impossible_penultimates)
 
     def setExplodeSentences(self, value):
+        """Sets whether to explode each sentence into a different row, for
+        better parallelization, by default False.
+
+        Parameters
+        ----------
+        value : bool
+            Whether to explode each sentence into a different row
+        """
         return self._set(explodeSentences=value)
 
     def _create_model(self, java_model):
