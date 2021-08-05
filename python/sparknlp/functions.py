7f31e61a7f3fdc7c7e77fd7947aedaa768c268d1
Trung Duc Ha <duc.hatrung95@gmail.com>
2021-08-05 17:15:30 +0200

    PyDoc: function; annotation

--- python/sparknlp/functions.py
+++ python/sparknlp/functions.py
@@ -29,13 +29,39 @@ def map_annotations(f, output_type: DataType):
     ----------
     f : function
         The function to be applied over the results
-    output_type : DataType
+    output_type : :class:`pyspark.sql.types.DataType`
         Output type of the data
 
     Returns
     -------
-    :class:`UserDefinedFunction`
+    :func:`pyspark.sql.functions.udf`
         Spark UserDefinedFunction (udf)
+
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+
+    The array type must be provided in order to tell Spark the expected output
+    type of our column. We are using an Annotation array here.
+
+    >>> from sparknlp.functions import *
+    >>> def nnp_tokens(annotations: List[Row]):
+    ...     return list(
+    ...         filter(lambda annotation: annotation.result == 'NNP', annotations)
+    ...     )
+    >>> result.select(
+    ...     map_annotations(nnp_tokens, Annotation.arrayType())('pos').alias("nnp")
+    ... ).selectExpr("explode(nnp) as nnp").show(truncate=False)
+    +-----------------------------------------+
+    |nnp                                      |
+    +-----------------------------------------+
+    |[pos, 0, 2, NNP, [word -> U.N], []]      |
+    |[pos, 14, 18, NNP, [word -> Epeus], []]  |
+    |[pos, 30, 36, NNP, [word -> Baghdad], []]|
+    +-----------------------------------------+
     """
     return udf(
         lambda content: [ Annotation.toRow(a) for a in f([Annotation.fromRow(r) for r in content])],
@@ -49,12 +75,12 @@ def map_annotations_array(f, output_type: DataType):
     ----------
     f : function
         The function to be applied over the results
-    output_type : DataType
+    output_type : :class:`pyspark.sql.types.DataType`
         Output type of the data
 
     Returns
     -------
-    :class:`UserDefinedFunction`
+    :func:`pyspark.sql.functions.udf`
         Spark UserDefinedFunction (udf)
     """
     return udf(
@@ -63,8 +89,8 @@ def map_annotations_array(f, output_type: DataType):
     )
 
 def map_annotations_strict(f):
-    """Creates a Spark UDF to map over an Annotator's results, for which the return type is explicitly defined as
-    a :func:`Annotation.dataType`.
+    """Creates a Spark UDF to map over an Annotator's results, for which the
+    return type is explicitly defined as a `Annotation.dataType()`.
 
     Parameters
     ----------
@@ -73,8 +99,29 @@ def map_annotations_strict(f):
 
     Returns
     -------
-    :class:`UserDefinedFunction`
+    :func:`pyspark.sql.functions.udf`
         Spark UserDefinedFunction (udf)
+
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+    >>> def nnp_tokens(annotations):
+    ...     return list(
+    ...         filter(lambda annotation: annotation.result == 'NNP', annotations)
+    ...     )
+    >>> result.select(
+    ...     map_annotations_strict(nnp_tokens)('pos').alias("nnp")
+    ... ).selectExpr("explode(nnp) as nnp").show(truncate=False)
+    +-----------------------------------------+
+    |nnp                                      |
+    +-----------------------------------------+
+    |[pos, 0, 2, NNP, [word -> U.N], []]      |
+    |[pos, 14, 18, NNP, [word -> Epeus], []]  |
+    |[pos, 30, 36, NNP, [word -> Baghdad], []]|
+    +-----------------------------------------+
     """
     return udf(
         lambda content: [ Annotation.toRow(a) for a in f([Annotation.fromRow(r) for r in content])],
@@ -84,49 +131,125 @@ def map_annotations_strict(f):
 
 def map_annotations_col(dataframe: DataFrame, f, column: str, output_column: str, annotatyon_type: str,
                         output_type: DataType = Annotation.arrayType()):
-    """Creates a Spark UDF to map over an Annotator's array results, for which the return type is explicitly defined as
-    a :class:`Annotation`.
+    """Creates a Spark UDF to map over a column of Annotation results.
 
     Parameters
     ----------
+    dataframe : DataFrame
+        Input DataFrame
     f : function
-        The function to be applied over the results
-    output_type : DataType
-        Output type of the data
+        Function to apply to the column
+    column : str
+        Name of the input column
+    output_column : str
+        Name of the output column
+    annotatyon_type : str
+        Annotator type
+    output_type : DataType, optional
+        Output type, by default Annotation.arrayType()
 
     Returns
     -------
-    :class:`UserDefinedFunction`
-        Spark UserDefinedFunction (udf)
+    :class:`pyspark.sql.DataFrame`
+        Transformed DataFrame
+
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> from sparknlp.functions import *
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+    >>> chunks_df = map_annotations_col(
+    ...     result,
+    ...     lambda x: [
+    ...         Annotation("chunk", a.begin, a.end, a.result, a.metadata, a.embeddings)
+    ...         for a in x
+    ...     ],
+    ...     "pos",
+    ...     "pos_chunk",
+    ...     "chunk",
+    ... )
+    >>> chunks_df.selectExpr("explode(pos_chunk)").show()
+    +--------------------+
+    |                 col|
+    +--------------------+
+    |[chunk, 0, 2, NNP...|
+    |[chunk, 3, 3, ., ...|
+    |[chunk, 5, 12, JJ...|
+    |[chunk, 14, 18, N...|
+    |[chunk, 20, 24, V...|
+    |[chunk, 26, 28, I...|
+    |[chunk, 30, 36, N...|
+    |[chunk, 37, 37, ....|
+    +--------------------+
     """
     return dataframe.withColumn(output_column, map_annotations(f, output_type)(column).alias(output_column, metadata={
         'annotatorType': annotatyon_type}))
 
 def map_annotations_cols(dataframe: DataFrame, f, columns: list, output_column: str, annotatyon_type: str,
                         output_type: DataType = Annotation.arrayType()):
-    """Maps a function over a column of :class:`sparknlp.annotation.Annotation` to create a new column.
+    """Creates a Spark UDF to map over multiple columns of Annotation results.
 
     Parameters
     ----------
     dataframe : DataFrame
-        The Spark Dataframe containing output Annotations
+        Input DataFrame
     f : function
-        The function to be applied over the results
+        Function to apply to the column
     columns : list
-        Columns to apply the filter over
+        Name of the input column
     output_column : str
-        Name of the output Column
+        Name of the output column
     annotatyon_type : str
-        Annotator Type
+        Annotator type
     output_type : DataType, optional
-        Output Annotation schema, by default Annotation.arrayType()
+        Output type, by default Annotation.arrayType()
 
     Returns
     -------
-    :class:`DataFrame`
-        Spark DataFrame with new column.
-    """
+    :class:`pyspark.sql.DataFrame`
+        Transformed DataFrame
 
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> from sparknlp.functions import *
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+    >>> chunks_df = map_annotations_cols(
+    ...     result,
+    ...     lambda x: [
+    ...         Annotation("tag", a.begin, a.end, a.result, a.metadata, a.embeddings)
+    ...         for a in x
+    ...     ],
+    ...     ["pos", "ner"],
+    ...     "tags",
+    ...     "chunk"
+    ... )
+    >>> chunks_df.selectExpr("explode(tags)").show(truncate=False)
+    +-------------------------------------------+
+    |col                                        |
+    +-------------------------------------------+
+    |[tag, 0, 2, NNP, [word -> U.N], []]        |
+    |[tag, 3, 3, ., [word -> .], []]            |
+    |[tag, 5, 12, JJ, [word -> official], []]   |
+    |[tag, 14, 18, NNP, [word -> Epeus], []]    |
+    |[tag, 20, 24, VBZ, [word -> heads], []]    |
+    |[tag, 26, 28, IN, [word -> for], []]       |
+    |[tag, 30, 36, NNP, [word -> Baghdad], []]  |
+    |[tag, 37, 37, ., [word -> .], []]          |
+    |[tag, 0, 2, B-ORG, [word -> U.N], []]      |
+    |[tag, 3, 3, O, [word -> .], []]            |
+    |[tag, 5, 12, O, [word -> official], []]    |
+    |[tag, 14, 18, B-PER, [word -> Ekeus], []]  |
+    |[tag, 20, 24, O, [word -> heads], []]      |
+    |[tag, 26, 28, O, [word -> for], []]        |
+    |[tag, 30, 36, B-LOC, [word -> Baghdad], []]|
+    |[tag, 37, 37, O, [word -> .], []]          |
+    +-------------------------------------------+
+    """
     return dataframe.withColumn(output_column, map_annotations_array(f, output_type)(array(*columns)).alias(output_column, metadata={
         'annotatorType': annotatyon_type}))
 
@@ -136,15 +259,37 @@ def filter_by_annotations_col(dataframe, f, column):
 
     Parameters
     ----------
+    dataframe : DataFrame
+        Input DataFrame
     f : function
-        The function to be applied over the results.
+        Filter function
     column : str
-        Name of the column.
+        Name of the column
 
     Returns
     -------
-    :class:`DataFrame`
-        The filtered Dataframe
+    :class:`pyspark.sql.DataFrame`
+        Filtered DataFrame
+
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> from sparknlp.functions import *
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+    >>> def filter_pos(annotation: Annotation):
+    ...     return annotation.result == "NNP"
+    >>> filter_by_annotations_col(
+    ...     explode_annotations_col(result, "pos", "pos"), filter_pos, "pos"
+    ... ).select("pos").show(truncate=False)
+    +-----------------------------------------+
+    |pos                                      |
+    +-----------------------------------------+
+    |[pos, 0, 2, NNP, [word -> U.N], []]      |
+    |[pos, 14, 18, NNP, [word -> Epeus], []]  |
+    |[pos, 30, 36, NNP, [word -> Baghdad], []]|
+    +-----------------------------------------+
     """
     this_udf = udf(
         lambda content: f(content),
@@ -167,8 +312,35 @@ def explode_annotations_col(dataframe: DataFrame, column, output_column):
 
     Returns
     -------
-    :class:`DataFrame`
-        The filtered Dataframe
+    :class:`pyspark.sql.DataFrame`
+        Transformed DataFrame
+
+    Examples
+    --------
+    >>> from sparknlp.pretrained import PretrainedPipeline
+    >>> from sparknlp.functions import *
+    >>> explain_document_pipeline = PretrainedPipeline("explain_document_dl")
+    >>> data = spark.createDataFrame([["U.N. official Ekeus heads for Baghdad."]]).toDF("text")
+    >>> result = explain_document_pipeline.transform(data)
+    >>> result.select("pos.result").show(truncate=False)
+    +----------------------------------+
+    |result                            |
+    +----------------------------------+
+    |[NNP, ., JJ, NNP, VBZ, IN, NNP, .]|
+    +----------------------------------+
+    >>> explode_annotations_col(result, "pos", "pos").select("pos.result").show()
+    +------+
+    |result|
+    +------+
+    |   NNP|
+    |     .|
+    |    JJ|
+    |   NNP|
+    |   VBZ|
+    |    IN|
+    |   NNP|
+    |     .|
+    +------+
     """
     from pyspark.sql.functions import explode
     return dataframe.withColumn(output_column, explode(column))
