59c8753b3a6a48de4b9746f6fffeaf9f36eb08c1
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-08-21 11:49:43 +0200

    Add unit tests to RoBertaSentenceEmbeddings [skip test]

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -1813,3 +1813,26 @@ class BertForTokenClassificationTestSpec(unittest.TestCase):
         model = pipeline.fit(self.data)
         model.transform(self.data).show()
 
+
+class RoBertaSentenceEmbeddingsTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkContextForTest.spark.read.option("header", "true") \
+            .csv(path="file:///" + os.getcwd() + "/../src/test/resources/embeddings/sentence_embeddings.csv")
+
+    def runTest(self):
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+
+        sentence_embeddings = RoBertaSentenceEmbeddings.pretrained() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence_embeddings")
+
+        pipeline = Pipeline(stages=[
+            document_assembler,
+            sentence_embeddings
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
+
