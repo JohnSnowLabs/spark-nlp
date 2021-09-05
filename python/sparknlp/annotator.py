4fc8ba103095a26fe35597cf1a24b7e560ac7ac8
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-09-05 18:51:14 +0200

    Create TensorflowAlbertClassification Python APIs [skip test]

--- python/sparknlp/annotator.py
+++ python/sparknlp/annotator.py
@@ -12391,7 +12391,7 @@ class XlmRoBertaEmbeddings(AnnotatorModel,
         name : str, optional
             Name of the pretrained model, by default "xlm_roberta_base"
         lang : str, optional
-            Language of the pretrained model, by default "en"
+            Language of the pretrained model, by default "xx"
         remote_loc : str, optional
             Optional remote address of the resource, by default None. Will use
             Spark NLPs repositories otherwise.
@@ -13613,15 +13613,15 @@ class XlmRoBertaSentenceEmbeddings(AnnotatorModel,
         return XlmRoBertaSentenceEmbeddings(java_model=jModel)
 
     @staticmethod
-    def pretrained(name="sent_xlm_roberta_base", lang="en", remote_loc=None):
+    def pretrained(name="sent_xlm_roberta_base", lang="xx", remote_loc=None):
         """Downloads and loads a pretrained model.
 
         Parameters
         ----------
         name : str, optional
-            Name of the pretrained model, by default "sent_roberta_base"
+            Name of the pretrained model, by default "sent_xlm_roberta_base"
         lang : str, optional
-            Language of the pretrained model, by default "en"
+            Language of the pretrained model, by default "xx"
         remote_loc : str, optional
             Optional remote address of the resource, by default None. Will use
             Spark NLPs repositories otherwise.
@@ -13922,7 +13922,7 @@ class XlmRoBertaForTokenClassification(AnnotatorModel,
         ----------
         name : str, optional
             Name of the pretrained model, by default
-            "roberta_base_token_classifier_conll03"
+            "xlm_roberta_base_token_classifier_conll03"
         lang : str, optional
             Language of the pretrained model, by default "en"
         remote_loc : str, optional
@@ -13935,3 +13935,155 @@ class XlmRoBertaForTokenClassification(AnnotatorModel,
         """
         from sparknlp.pretrained import ResourceDownloader
         return ResourceDownloader.downloadModel(XlmRoBertaForTokenClassification, name, lang, remote_loc)
+
+
+class AlbertForTokenClassification(AnnotatorModel,
+                                   HasCaseSensitiveProperties,
+                                   HasBatchedAnnotate):
+    """XlmRoBertaForTokenClassification can load ALBERT Models with a token
+    classification head on top (a linear layer on top of the hidden-states
+    output) e.g. for Named-Entity-Recognition (NER) tasks.
+    Pretrained models can be loaded with :meth:`.pretrained` of the companion
+    object:
+    >>> token_classifier = AlbertForTokenClassification.pretrained() \\
+    ...     .setInputCols(["token", "document"]) \\
+    ...     .setOutputCol("label")
+    The default model is ``"albert_base_token_classifier_conll03"``, if no name is
+    provided.
+    For available pretrained models please see the `Models Hub
+    <https://nlp.johnsnowlabs.com/models?task=Text+Classification>`__.
+    Models from the HuggingFace 🤗 Transformers library are also compatible with
+    Spark NLP 🚀. To see which models are compatible and how to import them see
+    `Import Transformers into Spark NLP 🚀
+    <https://github.com/JohnSnowLabs/spark-nlp/discussions/5669>`_.
+    ====================== ======================
+    Input Annotation types Output Annotation type
+    ====================== ======================
+    ``DOCUMENT, TOKEN``    ``NAMED_ENTITY``
+    ====================== ======================
+    Parameters
+    ----------
+    batchSize
+        Batch size. Large values allows faster processing but requires more
+        memory, by default 8
+    caseSensitive
+        Whether to ignore case in tokens for embeddings matching, by default
+        True
+    configProtoBytes
+        ConfigProto from tensorflow, serialized into byte array.
+    maxSentenceLength
+        Max sentence length to process, by default 128
+    Examples
+    --------
+    >>> import sparknlp
+    >>> from sparknlp.base import *
+    >>> from sparknlp.annotator import *
+    >>> from pyspark.ml import Pipeline
+    >>> documentAssembler = DocumentAssembler() \\
+    ...     .setInputCol("text") \\
+    ...     .setOutputCol("document")
+    >>> tokenizer = Tokenizer() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("token")
+    >>> tokenClassifier = AlbertForTokenClassification.pretrained() \\
+    ...     .setInputCols(["token", "document"]) \\
+    ...     .setOutputCol("label") \\
+    ...     .setCaseSensitive(True)
+    >>> pipeline = Pipeline().setStages([
+    >>> ...     documentAssembler,
+    >>> ...     tokenizer,
+    >>> ...     tokenClassifier
+    ... ])
+    >>> data = spark.createDataFrame([["John Lenon was born in London and lived in Paris. My name is Sarah and I live in London"]]).toDF("text")
+    >>> result = pipeline.fit(data).transform(data)
+    >>> result.select("label.result").show(truncate=False)
+    +------------------------------------------------------------------------------------+
+    |result
+    |
+    +------------------------------------------------------------------------------------+
+    |[B-PER, I-PER, O, O, O, B-LOC, O, O, O, B-LOC, O, O, O, O, B-PER, O, O, O,
+    O, B-LOC]|
+    +------------------------------------------------------------------------------------+
+    """
+
+    name = "AlbertForTokenClassification"
+
+    maxSentenceLength = Param(Params._dummy(),
+                              "maxSentenceLength",
+                              "Max sentence length to process",
+                              typeConverter=TypeConverters.toInt)
+
+    configProtoBytes = Param(Params._dummy(),
+                             "configProtoBytes",
+                             "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()",
+                             TypeConverters.toListString)
+
+    def setConfigProtoBytes(self, b):
+        """Sets configProto from tensorflow, serialized into byte array.
+        Parameters
+        ----------
+        b : List[str]
+            ConfigProto from tensorflow, serialized into byte array
+        """
+        return self._set(configProtoBytes=b)
+
+    def setMaxSentenceLength(self, value):
+        """Sets max sentence length to process, by default 128.
+        Parameters
+        ----------
+        value : int
+            Max sentence length to process
+        """
+        return self._set(maxSentenceLength=value)
+
+    @keyword_only
+    def __init__(self, classname="com.johnsnowlabs.nlp.annotators.classifier.dl.AlbertForTokenClassification",
+                 java_model=None):
+        super(AlbertForTokenClassification, self).__init__(
+            classname=classname,
+            java_model=java_model
+        )
+        self._setDefault(
+            batchSize=8,
+            maxSentenceLength=128,
+            caseSensitive=True
+        )
+
+    @staticmethod
+    def loadSavedModel(folder, spark_session):
+        """Loads a locally saved model.
+        Parameters
+        ----------
+        folder : str
+            Folder of the saved model
+        spark_session : pyspark.sql.SparkSession
+            The current SparkSession
+        Returns
+        -------
+        AlbertForTokenClassification
+            The restored model
+        """
+        from sparknlp.internal import _AlbertTokenClassifierLoader
+        jModel = _AlbertTokenClassifierLoader(folder, spark_session._jsparkSession)._java_obj
+        return AlbertForTokenClassification(java_model=jModel)
+
+    @staticmethod
+    def pretrained(name="albert_base_token_classifier_conll03", lang="en", remote_loc=None):
+        """Downloads and loads a pretrained model.
+        Parameters
+        ----------
+        name : str, optional
+            Name of the pretrained model, by default
+            "albert_base_token_classifier_conll03"
+        lang : str, optional
+            Language of the pretrained model, by default "en"
+        remote_loc : str, optional
+            Optional remote address of the resource, by default None. Will use
+            Spark NLPs repositories otherwise.
+        Returns
+        -------
+        XlmRoBertaForTokenClassification
+            The restored model
+        """
+        from sparknlp.pretrained import ResourceDownloader
+        return ResourceDownloader.downloadModel(AlbertForTokenClassification, name, lang, remote_loc)
