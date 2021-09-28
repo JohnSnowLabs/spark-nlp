a47e6d865db7b003cce9b5228eebe086f737f4cf
Danilo Burbano <danilo@johnsnowlabs.com>
2021-09-28 10:39:59 -0500

    Adding EntityRuler option to read patterns as Spark dataframes

--- src/main/scala/com/johnsnowlabs/nlp/annotators/er/EntityRulerModel.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/er/EntityRulerModel.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2021 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.er
 
 import com.johnsnowlabs.nlp.AnnotatorType.{CHUNK, DOCUMENT, TOKEN}
@@ -27,8 +43,8 @@ class EntityRulerModel(override val uid: String) extends AnnotatorModel[EntityRu
     val patternsReader = getReader(Database.ENTITY_PATTERNS).asInstanceOf[PatternsReader]
     val tokenizedWithSentences = TokenizedWithSentence.unpack(annotations)
 
-    val entities = tokenizedWithSentences.par.flatMap{ tokenizedWithSentence =>
-     tokenizedWithSentence.indexedTokens.par.flatMap{ indexedToken =>
+    val entities = tokenizedWithSentences.flatMap{ tokenizedWithSentence =>
+     tokenizedWithSentence.indexedTokens.flatMap{ indexedToken =>
        val entity = patternsReader.lookup(indexedToken.token)
        val annotation = if (entity.isDefined) {
          Some(Annotation(CHUNK, indexedToken.begin, indexedToken.end, indexedToken.token,
@@ -38,7 +54,7 @@ class EntityRulerModel(override val uid: String) extends AnnotatorModel[EntityRu
      }
     }
 
-    entities.toList
+    entities
   }
 
   override protected val databases: Array[Name] = EntityRulerModel.databases
