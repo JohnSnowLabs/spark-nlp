8f5b50b4d072d8e9ddafb5807ba023cd2dfe7ec6
Danilo Burbano <danilo@johnsnowlabs.com>
2021-09-28 10:39:59 -0500

    Adding EntityRuler option to read patterns as Spark dataframes

--- src/main/scala/com/johnsnowlabs/nlp/annotators/er/EntityRuler.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/er/EntityRuler.scala
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2017-2021 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.er
 
 import com.johnsnowlabs.nlp.AnnotatorApproach
 import com.johnsnowlabs.nlp.AnnotatorType.{CHUNK, DOCUMENT, TOKEN}
 import com.johnsnowlabs.nlp.annotators.param.ExternalResourceParam
+import com.johnsnowlabs.nlp.util.io.ResourceHelper.spark
 import com.johnsnowlabs.nlp.util.io.{ExternalResource, ReadAs, ResourceHelper}
 import com.johnsnowlabs.storage.Database.Name
 import com.johnsnowlabs.storage.{Database, HasStorage, RocksDBConnection, StorageWriter}
@@ -29,6 +46,8 @@ class EntityRuler(override val uid: String) extends AnnotatorApproach[EntityRule
     patterns -> null
   )
 
+  private val AVAILABLE_FORMATS = Array("JSON", "CSV")
+
   override def train(dataset: Dataset[_], recursivePipeline: Option[PipelineModel]): EntityRulerModel = {
 
     new EntityRulerModel()
@@ -44,16 +63,27 @@ class EntityRuler(override val uid: String) extends AnnotatorApproach[EntityRule
                                writers: Map[Name, StorageWriter[_]], readOptions: Option[Map[String, String]]): Unit = {
 
     require($(patterns) != null, "patterns parameter required")
+    require(AVAILABLE_FORMATS.contains($(patterns).options.getOrElse("format", "").toUpperCase()),
+      "format option parameter required with either JSON or CSV values")
+    if ($(patterns).options("format") != "JSON") {
+      require($(patterns).options.getOrElse("delimiter", "") != "", "delimiter option parameter required")
+    }
+    require($(patterns).readAs != null, "readAs parameter required")
 
     val patternsReaderWriter = writers(Database.ENTITY_PATTERNS).asInstanceOf[PatternsReadWriter]
 
-    entityPatterns.foreach{ case (pattern, entity) =>
-      patternsReaderWriter.lookup(pattern).getOrElse(patternsReaderWriter.add(pattern, entity))
+    resourceFormats match {
+      case "JSON&TEXT" => storeEntityPatternsFromJson(patternsReaderWriter)
+      case "JSON&SPARK" => storeEntityPatternsFromJSONDataFrame(patternsReaderWriter)
+      case "CSV&TEXT" => storeEntityPatternsFromCSV(patternsReaderWriter)
+      case "CSV&SPARK" => storeEntityPatternsFromCSVDataFrame(patternsReaderWriter)
     }
+
   }
 
-  private lazy val entityPatterns: Map[String, String] = {
+  private lazy val resourceFormats: String = $(patterns).options("format").toUpperCase() + "&" +  $(patterns).readAs
 
+  private def storeEntityPatternsFromJson(patternsReaderWriter: PatternsReadWriter): Unit = {
     import io.circe.generic.auto._
 
     val stream =  ResourceHelper.getResourceStream($(patterns).path)
@@ -61,9 +91,52 @@ class EntityRuler(override val uid: String) extends AnnotatorApproach[EntityRule
     val jsonParser = new JsonParser[EntityPattern]
     val entityPatterns: Array[EntityPattern] = jsonParser.readJsonArray(jsonContent)
 
-    entityPatterns.flatMap{ entityPattern =>
-      entityPattern.pattern.split("\\s").flatMap(pattern => Map(pattern -> entityPattern.label))
-    }.toMap
+    entityPatterns.foreach{ entityPattern =>
+      storePatterns(entityPattern.pattern, entityPattern.label, patternsReaderWriter)
+    }
+  }
+
+  private def storeEntityPatternsFromCSV(patternsReaderWriter: PatternsReadWriter): Unit = {
+    val entityPatterns: Map[String, String] = ResourceHelper.parseKeyValueText($(patterns))
+    entityPatterns.foreach(entityPattern => storePatterns(entityPattern._1, entityPattern._2, patternsReaderWriter))
+  }
+
+  private def storeEntityPatternsFromCSVDataFrame(patternsReaderWriter: PatternsReadWriter): Unit = {
+
+    val patternsResource = $(patterns)
+    val patternsDataFrame = spark.read.options(patternsResource.options)
+      .format(patternsResource.options("format"))
+      .options(patternsResource.options)
+      .option("delimiter", patternsResource.options("delimiter"))
+      .load(patternsResource.path)
+      .toDF("pattern", "entity")
+
+    patternsDataFrame.rdd.toLocalIterator.foreach{ row =>
+      val pattern = row.getAs[String]("pattern")
+      val entity = row.getAs[String]("entity")
+      storePatterns(pattern, entity, patternsReaderWriter)
+    }
+
+  }
+
+  private def storeEntityPatternsFromJSONDataFrame(patternsReaderWriter: PatternsReadWriter): Unit = {
+
+    val patternsResource = $(patterns)
+    val patternsDataFrame = spark.read.option("multiline", "true")
+      .json(patternsResource.path)
+
+    patternsDataFrame.printSchema()
+
+    patternsDataFrame.rdd.toLocalIterator.foreach{ row =>
+      val pattern = row.getAs[String]("pattern")
+      val entity = row.getAs[String]("label")
+      storePatterns(pattern, entity, patternsReaderWriter)
+    }
+
+  }
+
+  private def storePatterns(pattern: String, entity: String, patternsReaderWriter: PatternsReadWriter): Unit = {
+    pattern.split("\\s").foreach(p => patternsReaderWriter.lookup(p).getOrElse(patternsReaderWriter.add(p, entity)))
   }
 
   override protected def createWriter(database: Name, connection: RocksDBConnection): StorageWriter[_] = {
