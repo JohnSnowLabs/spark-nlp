528dc4c8b6a4e8e67e6154c39974ff2342afc6e2
xegulon <mapx3a@gmail.com>
2021-11-10 19:25:42 +0100

    fix: improved the script to install spark in the root sagemaker directory

--- scripts/sagemaker_setup.sh
+++ scripts/sagemaker_setup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Java setup on SageMaker
+echo "Setup SageMaker for PySpark $PYSPARK and Spark NLP $SPARKNLP"
+JAVA_8=$(alternatives --display java | grep 'jre-1.8.0-openjdk.x86_64/bin/java' | cut -d' ' -f1)
+sudo alternatives --set java $JAVA_8
+
 # Default values for pyspark, spark-nlp, and SPARK_HOME
 SPARKNLP="3.3.2"
 PYSPARK="3.1.2"
 
 SPARK_FOLDER_NAME="spark-$PYSPARK-bin-hadoop2.7"
-SPARKHOME="/home/ec2-user/SageMaker/$SPARK_FOLDER_NAME"
+LOCAL_ROOT_DIR="/home/ec2-user/SageMaker"
+SPARKHOME="$LOCAL_ROOT_DIR/$SPARK_FOLDER_NAME"
 APACHE_DL_URL="https://downloads.apache.org/spark"
 TARGZ_URL_TO_DOWNLOAD="$APACHE_DL_URL/spark-$PYSPARK/$SPARK_FOLDER_NAME.tgz"
 
-echo "Setup SageMaker for PySpark $PYSPARK and Spark NLP $SPARKNLP"
-JAVA_8=$(alternatives --display java | grep 'jre-1.8.0-openjdk.x86_64/bin/java' | cut -d' ' -f1)
-sudo alternatives --set java $JAVA_8
-
-echo "Beginning download of Spark..."
-wget -q "$APACHE_DL_URL/spark-$PYSPARK/$SPARK_FOLDER_NAME.tgz" > /dev/null
-echo "Download done, will extract now."
-tar -xvf "$SPARK_FOLDER_NAME.tgz" >/dev/null
-echo "Spark has been downloaded and extracted."
+echo "Beginning download of Spark"
+wget -q "$APACHE_DL_URL/spark-$PYSPARK/$SPARK_FOLDER_NAME.tgz" >/dev/null
+echo "Download done, beginning extraction."
+tar -xvf "$SPARK_FOLDER_NAME.tgz" -C $LOCAL_ROOT_DIR >/dev/null
+echo "Spark has been downloaded and extracted, removing tgz file."
+rm "$SPARK_FOLDER_NAME.tgz"
 
 export SPARK_HOME=$SPARKHOME
 
