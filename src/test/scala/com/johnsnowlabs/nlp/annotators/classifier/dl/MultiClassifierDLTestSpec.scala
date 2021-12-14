d8c3abff3a6efb9a0d96d6e6a08c6ea96e424697
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2021-12-14 17:15:52 +0100

    Enable a fast test for SentimentDLApproach

--- src/test/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/MultiClassifierDLTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/MultiClassifierDLTestSpec.scala
@@ -19,7 +19,7 @@ package com.johnsnowlabs.nlp.annotators.classifier.dl
 import com.johnsnowlabs.nlp.annotator.BertSentenceEmbeddings
 import com.johnsnowlabs.nlp.base._
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.tags.SlowTest
+import com.johnsnowlabs.tags.FastTest
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, udf}
@@ -29,7 +29,7 @@ class MultiClassifierDLTestSpec extends AnyFlatSpec {
 
   val spark: SparkSession = ResourceHelper.getActiveSparkSession
 
-  "MultiClassifierDL" should "correctly train E2E Challenge" taggedAs SlowTest in {
+  "MultiClassifierDL" should "correctly train E2E Challenge" taggedAs FastTest in {
     def splitAndTrim = udf { labels: String =>
       labels.split(", ").map(x => x.trim)
     }
@@ -44,7 +44,6 @@ class MultiClassifierDLTestSpec extends AnyFlatSpec {
 
     println("count of training dataset: ", smallCorpus.count)
     smallCorpus.select("labels").show(1)
-    //    smallCorpus.printSchema()
 
     val documentAssembler = new DocumentAssembler()
       .setInputCol("ref")
--- src/test/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/SentimentDLTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/SentimentDLTestSpec.scala
@@ -19,14 +19,15 @@ package com.johnsnowlabs.nlp.annotators.classifier.dl
 import com.johnsnowlabs.nlp.annotator._
 import com.johnsnowlabs.nlp.base._
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import com.johnsnowlabs.tags.SlowTest
+import com.johnsnowlabs.tags.{FastTest, SlowTest}
 import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.SparkSession
 import org.scalatest.flatspec.AnyFlatSpec
 
 class SentimentDLTestSpec extends AnyFlatSpec {
-  val spark = ResourceHelper.spark
+  val spark: SparkSession = ResourceHelper.spark
 
-  "SentimentDL" should "correctly train on a test dataset" taggedAs SlowTest in {
+  "SentimentDL" should "correctly train on a test dataset" taggedAs FastTest in {
 
     val smallCorpus = ResourceHelper.spark.read.option("header", "true").csv("src/test/resources/classifier/sentiment.csv")
 
@@ -35,7 +36,7 @@ class SentimentDLTestSpec extends AnyFlatSpec {
       .setInputCol("text")
       .setOutputCol("document")
 
-    val useEmbeddings = UniversalSentenceEncoder.pretrained()
+    val sentenceEmbeddings = BertSentenceEmbeddings.pretrained("sent_small_bert_L2_128")
       .setInputCols("document")
       .setOutputCol("sentence_embeddings")
 
@@ -53,7 +54,7 @@ class SentimentDLTestSpec extends AnyFlatSpec {
       .setStages(
         Array(
           documentAssembler,
-          useEmbeddings,
+          sentenceEmbeddings,
           docClassifier
         )
       )
