ffdf6e56fe29a4d39fb30f53a63e1031bc3cdd4f
Danilo Burbano <danilo@johnsnowlabs.com>
2021-12-29 14:40:13 -0500

    Adding deepLearningEngine parameter in Python

--- python/sparknlp/annotator.py
+++ python/sparknlp/annotator.py
@@ -6478,6 +6478,11 @@ class BertEmbeddings(AnnotatorModel,
                              "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()",
                              TypeConverters.toListInt)
 
+    deepLearningEngine = Param(Params._dummy(),
+                               "deepLearningEngine",
+                               "Deep Learning engine for creating embeddings [tensorflow|pytorch]",
+                               typeConverter=TypeConverters.toString)
+
     def setConfigProtoBytes(self, b):
         """Sets configProto from tensorflow, serialized into byte array.
 
@@ -6497,6 +6502,9 @@ class BertEmbeddings(AnnotatorModel,
             Max sentence length to process
         """
         return self._set(maxSentenceLength=value)
+
+    def setDeepLearningEngine(self, value):
+        return self._set(deepLearningEngine=value)
 
     @keyword_only
     def __init__(self, classname="com.johnsnowlabs.nlp.embeddings.BertEmbeddings", java_model=None):
@@ -8395,6 +8403,11 @@ class AlbertEmbeddings(AnnotatorModel,
                               "Max sentence length to process",
                               typeConverter=TypeConverters.toInt)
 
+    deepLearningEngine = Param(Params._dummy(),
+                               "deepLearningEngine",
+                               "Deep Learning engine for creating embeddings [tensorflow|pytorch]",
+                               typeConverter=TypeConverters.toString)
+
     def setConfigProtoBytes(self, b):
         """Sets configProto from tensorflow, serialized into byte array.
 
@@ -8414,6 +8427,9 @@ class AlbertEmbeddings(AnnotatorModel,
             Max sentence length to process
         """
         return self._set(maxSentenceLength=value)
+
+    def setDeepLearningEngine(self, value):
+        return self._set(deepLearningEngine=value)
 
     @keyword_only
     def __init__(self, classname="com.johnsnowlabs.nlp.embeddings.AlbertEmbeddings", java_model=None):
