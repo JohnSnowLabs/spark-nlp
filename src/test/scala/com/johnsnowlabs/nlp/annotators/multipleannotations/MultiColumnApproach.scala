0181bac5ecce1a17c3a3f1f7e2887ec4dd3650b7
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-01-01 12:49:44 +0100

    Update coding style and License headers

--- src/test/scala/com/johnsnowlabs/nlp/annotators/multipleannotations/MultiColumnApproach.scala
+++ src/test/scala/com/johnsnowlabs/nlp/annotators/multipleannotations/MultiColumnApproach.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2022 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.multipleannotations
 
 import com.johnsnowlabs.nlp.{AnnotatorApproach, HasMultipleInputAnnotationCols}
@@ -7,9 +23,10 @@ import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.Dataset
 
 
-class MultiColumnApproach(override val uid: String) extends AnnotatorApproach[MultiColumnsModel] with HasMultipleInputAnnotationCols{
+class MultiColumnApproach(override val uid: String) extends AnnotatorApproach[MultiColumnsModel] with HasMultipleInputAnnotationCols {
 
-  def this() = this(Identifiable.randomUID("multiplecolums"))
+  def this() = this(Identifiable.randomUID("MultiColumnApproach"))
+
   override val description: String = "Example multiple columns"
 
   /**
@@ -22,7 +39,6 @@ class MultiColumnApproach(override val uid: String) extends AnnotatorApproach[Mu
    *
    */
   override val inputAnnotatorType: AnnotatorType = DOCUMENT
-
 
 
   override def train(dataset: Dataset[_], recursivePipeline: Option[PipelineModel]): MultiColumnsModel = {
