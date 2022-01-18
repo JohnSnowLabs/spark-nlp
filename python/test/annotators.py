308532c5a96b41aab407d1fe17ec2d1ab39e8cac
Stefano Lori <s.lori@izicap.com>
2022-01-18 19:31:03 +0100

    Added Python interface to Regex Tok new params

--- python/test/annotators.py
+++ python/test/annotators.py
@@ -312,6 +312,47 @@ class DocumentNormalizerSpec(unittest.TestCase):
         ds.select("normalizedDocument").show()
 
 
+class RegexTokenizerTestSpec(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = SparkSessionForTest.spark.createDataFrame(
+            ["AL 123456!, TX 54321-4444, AL :55555-4444, 12345-4444, 12345"]
+        )
+
+    def runTest(self) -> None:
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("document")
+
+        sentence_detector = SentenceDetector() \
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
+
+        pattern = "^(\\s+)|(?=[\\s+\"\'\|:;<=>!?~{}*+,$)\(&%\\[\\]])|(?<=[\\s+\"\'\|:;<=>!?~{}*+,$)\(&%\\[\\]])|(?=\.$)"
+
+        regex_tok = RegexTokenizer() \
+            .setInputCols(["sentence"]) \
+            .setOutputCol("regex_token") \
+            .setPattern(pattern) \
+            .setTrimWhiteSpaces(False) \
+            .setPreserveIndexes(True)
+
+        pipeline = Pipeline(
+            stages=[
+                document_assembler,
+                sentence_detector,
+                regex_tok
+            ]
+        )
+
+        pipeline_model = pipeline.fit(self.data)
+
+        pipe_path = "file:///" + os.getcwd() + "/tmp_regextok_pipeline"
+        pipeline_model.write().overwrite().save(pipe_path)
+
+        loaded_pipeline: PipelineModel = Pipeline.read().load(pipe_path)
+        loaded_pipeline.transform(self.data).show()
+
+
 class PerceptronApproachTestSpec(unittest.TestCase):
 
     def setUp(self):
