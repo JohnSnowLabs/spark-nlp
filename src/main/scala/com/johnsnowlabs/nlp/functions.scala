9a72ae81d8b59f1dda4bcd3819261428f2c636ca
Stefano Lori <s.lori@izicap.com>
2022-01-31 18:44:47 +0100

    Fixing functions for Annotation type

--- src/main/scala/com/johnsnowlabs/nlp/functions.scala
+++ src/main/scala/com/johnsnowlabs/nlp/functions.scala
@@ -22,58 +22,56 @@ import org.apache.spark.sql.functions.{array, col, explode, udf}
 import org.apache.spark.sql.types.MetadataBuilder
 import scala.reflect.runtime.universe._
 
+
 object functions {
 
+  type JSLAnnotation = com.johnsnowlabs.nlp.Annotation
+
   implicit class FilterAnnotations(dataset: DataFrame) {
-    def filterByAnnotationsCol(column: String, function: Seq[Annotation] => Boolean): DataFrame = {
+    def filterByAnnotationsCol(column: String, function: collection.Seq[JSLAnnotation] => Boolean): DataFrame = {
       val meta = dataset.schema(column).metadata
       val func = udf {
-        annotatorProperties: Seq[Row] =>
-          function(annotatorProperties.map(Annotation(_)))
+        annotatorProperties: collection.Seq[Row] => function(annotatorProperties.map(com.johnsnowlabs.nlp.Annotation(_)))
       }
       dataset.filter(func(col(column)).as(column, meta))
     }
   }
 
-  def mapAnnotations(function: Seq[Annotation] => Seq[Annotation]): UserDefinedFunction =
-    udf { annotatorProperties: Seq[Row] => function(annotatorProperties.map(Annotation(_))) }
+  def mapAnnotations(function: Seq[JSLAnnotation] => Seq[JSLAnnotation]): UserDefinedFunction =
+    udf { annotatorProperties: Seq[Row] => function(annotatorProperties.map(com.johnsnowlabs.nlp.Annotation(_))) }
 
-  def mapAnnotationsStrict(function: Seq[Annotation] => Seq[Annotation]): UserDefinedFunction =
-    udf { annotatorProperties: Seq[Row] => function(annotatorProperties.map(Annotation(_))) }
+  // FIXME eliminate or create facade or declare one as deprecated?
+  def mapAnnotationsStrict(function: Seq[JSLAnnotation] => Seq[JSLAnnotation]): UserDefinedFunction =
+    udf { annotatorProperties: Seq[Row] => function(annotatorProperties.map(com.johnsnowlabs.nlp.Annotation(_))) }
 
   implicit class MapAnnotations(dataset: DataFrame) {
-    def mapAnnotationsCol[T: TypeTag](column: String, outputCol: String, annotatorType: String, function: Seq[Annotation] => T): DataFrame = {
+    def mapAnnotationsCol[T: TypeTag](column: String, outputCol: String, annotatorType: String, function: Seq[JSLAnnotation] => T): DataFrame = {
       val metadataBuilder: MetadataBuilder = new MetadataBuilder()
       val meta = metadataBuilder.putString("annotatorType", annotatorType).build()
       val func = udf {
-        annotatorProperties: Seq[Row] =>
-          function(annotatorProperties.map(Annotation(_)))
+        annotatorProperties: Seq[Row] => function(annotatorProperties.map(com.johnsnowlabs.nlp.Annotation(_)))
       }
       dataset.withColumn(outputCol, func(col(column)).as(outputCol, meta))
     }
 
-    def mapAnnotationsCol[T: TypeTag](cols: Seq[String], outputCol: String, annotatorType: String, function: Seq[Annotation] => T): DataFrame = {
+    def mapAnnotationsCol[T: TypeTag](cols: Seq[String], outputCol: String, annotatorType: String, function: Seq[JSLAnnotation] => T): DataFrame = {
       val metadataBuilder: MetadataBuilder = new MetadataBuilder()
       val meta = metadataBuilder.putString("annotatorType", annotatorType).build()
       val func = udf {
-        (cols: Seq[Seq[Row]]) =>
-          function {
-            cols.flatMap(aa => aa.map(Annotation(_)))
-          }
+        (cols: Seq[Seq[Row]]) => function { cols.flatMap(aa => aa.map(com.johnsnowlabs.nlp.Annotation(_))) }
       }
       val inputCols = cols.map(col)
       dataset.withColumn(outputCol, func(array(inputCols: _*)).as(outputCol, meta))
     }
-
-
   }
+
 
   implicit class EachAnnotations(dataset: DataFrame) {
 
     import dataset.sparkSession.implicits._
 
-    def eachAnnotationsCol[T: TypeTag](column: String, function: Seq[Annotation] => Unit): Unit = {
-      dataset.select(column).as[Array[Annotation]].foreach(function(_))
+    def eachAnnotationsCol[T: TypeTag](column: String, function: Seq[JSLAnnotation] => Unit): Unit = {
+      dataset.select(column).as[Array[JSLAnnotation]].foreach(function(_))
     }
   }
 
@@ -85,5 +83,4 @@ object functions {
         withColumn(outputCol, array(col(outputCol)).as(outputCol, meta))
     }
   }
-
 }
