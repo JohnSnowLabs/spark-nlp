fab8c30c80aaa5de9d6e0abddb9607082b8a67eb
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-05-12 10:03:33 +0200

    Implement batching and change to CHUNK in predictSpan [skip test]

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowForClassification.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowForClassification.scala
@@ -155,42 +155,49 @@ trait TensorflowForClassification {
       maxSentenceLength: Int,
       caseSensitive: Boolean): Seq[Annotation] = {
 
-    val questionAnnot = Seq(documents.head)
-    val contextAnnot = documents.drop(1)
+    documents.zipWithIndex
+      .grouped(batchSize)
+      .flatMap { batch =>
+        val questionAnnot = Seq(documents.head)
+        val contextAnnot = documents.drop(1)
 
-    val wordPieceTokenizedQuestion =
-      tokenizeDocument(questionAnnot, maxSentenceLength, caseSensitive)
-    val wordPieceTokenizedContext =
-      tokenizeDocument(contextAnnot, maxSentenceLength, caseSensitive)
+        val wordPieceTokenizedQuestion =
+          tokenizeDocument(questionAnnot, maxSentenceLength, caseSensitive)
+        val wordPieceTokenizedContext =
+          tokenizeDocument(contextAnnot, maxSentenceLength, caseSensitive)
 
-    val encodedInput =
-      encodeSequence(wordPieceTokenizedQuestion, wordPieceTokenizedContext, maxSentenceLength)
-    val (startLogits, endLogits) = tagSpan(encodedInput)
+        val encodedInput =
+          encodeSequence(wordPieceTokenizedQuestion, wordPieceTokenizedContext, maxSentenceLength)
+        val (startLogits, endLogits) = tagSpan(encodedInput)
 
-    val startScores = startLogits.transpose.map(_.sum / startLogits.length)
-    val endScores = endLogits.transpose.map(_.sum / endLogits.length)
+        val startScores = startLogits.transpose.map(_.sum / startLogits.length)
+        val endScores = endLogits.transpose.map(_.sum / endLogits.length)
 
-    val startIndex = startScores.zipWithIndex.maxBy(_._1)
-    val endIndex = endScores.zipWithIndex.maxBy(_._1)
+        val startIndex = startScores.zipWithIndex.maxBy(_._1)
+        val endIndex = endScores.zipWithIndex.maxBy(_._1)
 
-    val allTokenPieces =
-      wordPieceTokenizedQuestion.head.tokens ++ wordPieceTokenizedContext.flatMap(x => x.tokens)
-    val decodedAnswer = allTokenPieces.slice(startIndex._2 - 2, endIndex._2 - 1)
-    val content = decodedAnswer.filter(_.isWordStart).map(x => x.token).mkString(" ")
+        val allTokenPieces =
+          wordPieceTokenizedQuestion.head.tokens ++ wordPieceTokenizedContext.flatMap(x =>
+            x.tokens)
+        val decodedAnswer = allTokenPieces.slice(startIndex._2 - 2, endIndex._2 - 1)
+        val content = decodedAnswer.filter(_.isWordStart).map(x => x.token).mkString(" ")
 
-    Seq(
-      Annotation(
-        annotatorType = AnnotatorType.DOCUMENT,
-        begin = 0,
-        end = if (content.isEmpty) 0 else content.length - 1,
-        result = content,
-        metadata = Map(
-          "sentence" -> "0",
-          "start" -> startIndex._2.toString,
-          "start_score" -> startIndex._1.toString,
-          "end" -> endIndex._2.toString,
-          "end_score" -> endIndex._1.toString,
-          "score" -> ((startIndex._1 + endIndex._1) / 2).toString)))
+        Seq(
+          Annotation(
+            annotatorType = AnnotatorType.CHUNK,
+            begin = 0,
+            end = if (content.isEmpty) 0 else content.length - 1,
+            result = content,
+            metadata = Map(
+              "sentence" -> "0",
+              "chunk" -> "0",
+              "start" -> startIndex._2.toString,
+              "start_score" -> startIndex._1.toString,
+              "end" -> endIndex._2.toString,
+              "end_score" -> endIndex._1.toString,
+              "score" -> ((startIndex._1 + endIndex._1) / 2).toString)))
+      }
+      .toSeq
 
   }
 
