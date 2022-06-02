3bfe9bc368d91ba91e01374f5718c91c8cd3eaba
Danilo Burbano <danilo@johnsnowlabs.com>
2022-06-02 17:09:55 -0500

    Adding annotateJava methods to PretrainedPipeline

--- src/main/scala/com/johnsnowlabs/nlp/pretrained/PretrainedPipeline.scala
+++ src/main/scala/com/johnsnowlabs/nlp/pretrained/PretrainedPipeline.scala
@@ -95,8 +95,13 @@ case class PretrainedPipeline(
 
   def annotate(target: String): Map[String, Seq[String]] = lightModel.annotate(target)
 
-  def annotate(target: Array[String]): Array[Map[String, Seq[String]]] =
-    lightModel.annotate(target)
+  def annotate(target: Array[String]): Array[Map[String, Seq[String]]] = lightModel.annotate(target)
+
+  def annotateJava(target: String): java.util.Map[String, java.util.List[String]] = lightModel.annotateJava(target)
+
+  def annotateJava(targets: java.util.ArrayList[String]): java.util.List[java.util.Map[String, java.util.List[String]]] = {
+    lightModel.annotateJava(targets)
+  }
 
   def transform(dataFrame: DataFrame): DataFrame = model.transform(dataFrame)
 
