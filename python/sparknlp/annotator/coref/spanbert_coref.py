a5d51bd8657763440dbaed699663646c6305a8da
Ivan Vankov <i.i.vankov@gmail.com>
2022-06-14 13:51:19 +0300

    docs and updated tests to use pretrained model

--- python/sparknlp/annotator/coref/spanbert_coref.py
+++ python/sparknlp/annotator/coref/spanbert_coref.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""Contains classes for the SpanBertCorefModel."""
 
 from sparknlp.common import *
 
@@ -20,7 +21,86 @@ class SpanBertCorefModel(AnnotatorModel,
                      HasCaseSensitiveProperties,
                      HasStorageRef):
     """
-    TODO
+    A coreference resolution model based on SpanBert.
+
+    A coreference resolution model identifies expressions which refer to the same entity in a text. For example, given
+    a sentence "John told Mary he would like to borrow a book from her." the model will link "he" to "John" and "her"
+    to "Mary".
+
+    This model is based on SpanBert, which is fine-tuned on the OntoNotes 5.0 data set.
+
+    Pretrained models can be loaded with :meth:`.pretrained` of the companion object:
+
+    >>> corefResolution = SpanBertCorefModel.pretrained() \\
+    ...     .setInputCols(["sentence", "token"]) \\
+    ...     .setOutputCol("coref")
+
+    The default model is ``"spanbert_base_coref"``, if no name is provided. For available
+    pretrained models please see the `Models Hub
+    <https://nlp.johnsnowlabs.com/models?q=coref>`__.
+
+    ====================== ======================
+    Input Annotation types Output Annotation type
+    ====================== ======================
+    ``DOCUMENT, TOKEN`     ``DEPENDENCY``
+    ====================== ======================
+
+    Parameters
+    ----------
+    maxSentenceLength
+        Maximum sentence length to process
+    maxSegmentLength
+        Maximum segment length
+    textGenre
+        Text genre.  One of the following values:
+        "bc", // Broadcast conversation, default
+        "bn", // Broadcast news
+        "nw", // News wire
+        "pt", // Pivot text: Old Testament and New Testament text
+        "tc", // Telephone conversation
+        "wb" // Web data
+
+    Examples
+    --------
+    >>> import sparknlp
+    >>> from sparknlp.base import *
+    >>> from sparknlp.annotator import *
+    >>> from pyspark.ml import Pipeline
+    >>> documentAssembler = DocumentAssembler() \\
+    ...     .setInputCol("text") \\
+    ...     .setOutputCol("document")
+    >>> sentence = SentenceDetector() \\
+    ...     .setInputCols(["document"]) \\
+    ...     .setOutputCol("sentence")
+    >>> tokenizer = Tokenizer() \\
+    ...     .setInputCols(["sentence"]) \\
+    ...     .setOutputCol("token")
+    >>> corefResolution = SpanBertCorefModel() \\
+    ...     .pretrained() \\
+    ...     .setInputCols(["sentence", "token"]) \\
+    ...     .setOutputCol("corefs") \\
+    >>> pipeline = Pipeline().setStages([
+    ...     documentAssembler,
+    ...     sentence,
+    ...     tokenizer,
+    ...     corefResolution
+    ... ])
+    >>> data = spark.createDataFrame([
+    ...     ["John told Mary he would like to borrow a book from her."]
+    ... ]).toDF("text")
+    >>> results = pipeline.fit(data).transform(data))
+    >>> results\
+    ...     .selectExpr("explode(corefs) AS coref")
+    ...     .selectExpr("coref.result as token", "coref.metadata")
+    ...     .show(truncate=False)
+    +-----+------------------------------------------------------------------------------------+
+    |token|metadata                                                                            |
+    +-----+------------------------------------------------------------------------------------+
+    |John |{head.sentence -> -1, head -> ROOT, head.begin -> -1, head.end -> -1, sentence -> 0}|
+    |he   |{head.sentence -> 0, head -> John, head.begin -> 0, head.end -> 3, sentence -> 0}   |
+    |Mary |{head.sentence -> -1, head -> ROOT, head.begin -> -1, head.end -> -1, sentence -> 0}|
+    |her  |{head.sentence -> 0, head -> Mary, head.begin -> 10, head.end -> 13, sentence -> 0} |
+    +-----+------------------------------------------------------------------------------------|
     """
 
     name = "SpanBertCorefModel"
