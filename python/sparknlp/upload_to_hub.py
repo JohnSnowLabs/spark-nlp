d9388e226f5e799f0fa19c513214d6c4d2f256d1
ahmedlone127 <ahmedlone127@gmail.com>
2022-08-03 02:27:09 +0500

    Adding tests for uploading to hub and more doc creation utils

--- python/sparknlp/upload_to_hub.py
+++ python/sparknlp/upload_to_hub.py
@@ -1,16 +1,119 @@
 import requests
 import json
+from typing import List
+import sparknlp
+import os
+import zipfile
 
+
+                
 
 class PushToHub:
-    @staticmethod
-    def upload_to_modelshub_and_fill_form_API(model_data:dict,GIT_TOKEN:str):
-        """Pushes model to Hub .
+
+    list_of_tasks = [ # List  of available tasks in Modelhub
+    "Named Entity Recognition",
+    'Text Classification',
+    'Text Generation',
+    'Sentiment Analysis',
+    'Translation',
+    'Question Answering',
+    'Summarization',
+    'Sentence Detection',
+    'Embeddings',
+    'Language Detection',
+    'Stop Words Removal',
+    'Word Segmentation',
+    'Part of Speech Tagging',
+    'Lemmatization',
+    'Chunk Mapping',
+    'Relation Extraction',
+    'Spell Check',
+    'Dependency Parser',
+    'Assertion Status',
+    'Entity Resolution',
+    'De-identification',
+    'Pipeline Public',
+    'Pipeline Healthcare']
+
+    def zip_directory(folder_path:str, zip_path:str):
+        """Zips folder for pushing to hub.
+
+        folder_path:Path to the folder to zip.
+        zip_path:Path of the zip file to create."""
+
+        with zipfile.ZipFile(zip_path, mode='w') as zipf:
+            len_dir_path = len(folder_path)
+            for root, _, files in os.walk(folder_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, file_path[len_dir_path:])
+
+    def unzip_directory(zip_path:str):
+        """Unzips Model to check for required files for upload.
 
         Keyword Arguments:
-        model_data:Dictionary containing info about the model such as Name and Language
-        GIT_TOKEN: Token required for pushing to hub."""
-        
+        zip_path:Zip Path to unzip.
+        """
+
+    def check_for_required_info(model_data:dict) :
+        """Checks if the required fields exist in given dictionary  and fills any remaining fields.
+
+        Keyword Arguments: 
+        model_data: The model data to check.
+        """
+
+        list_of_required_fields = ['name','task','language','pythonCode','model_zip_path']
+
+        if model_data['task'] not in PushToHub.list_of_tasks:
+            list_of_tasks_string_version = "\n".join(PushToHub.list_of_tasks)
+            raise ValueError(f"""Invalid task, please pick one of the following tasks\n{list_of_tasks_string_version}""")
+        for field in list_of_required_fields :
+            if field not in model_data.keys():
+                raise ValueError(f"The dicionary entered does not contain the field '{field}'")
+
+        if model_data['model_zip_path'].endswith(".zip"):
+            with zipfile.ZipFile(model_data['model_zip_path']) as modelfile:
+                if 'metadata/part-00000' not in modelfile.namelist():
+                    raise ValueError("The Model is not a Spark Saved Model.")
+        else:
+            if not os.path.exists(f"{model_data['model_zip_path']}/metadata/part-00000"):
+                raise ValueError("The Model is not a Spark Saved Model.")
+
+
+
+    def push_to_hub(name:str,
+        language:str,
+        model_zip_path:str,
+        task:str,
+        pythonCode:str,
+        GIT_TOKEN:str,
+        title:str = None,
+        tags:List[str] = None,
+        dependencies:str = None,
+        description:str = None,
+        predictedEntities:str = None,
+        sparknlpVersion:str = None,
+        howToUse:str = None,
+        liveDemo:str = None,
+        runInColab:str = None,
+        scalaCode:str = None,
+        nluCode:str = None,
+        results:str = None,
+        dataSource:str = None,
+        includedModels:str = None,
+        benchmarking:str = None,
+        ) ->str:
+        """Pushes model to Hub.
+
+        Keyword Arguments:
+        model_data:Dictionary containing info about the model such as Name and Language.
+        GIT_TOKEN: Token required for pushing to hub.
+        """
+            
+        model_data = {item: value for (item, value) in locals().items() if value !=None}
+        PushToHub.check_for_required_info(model_data)        
+        model_data  = PushToHub.create_docs(model_data)
+
         r1 = requests.post('https://modelshub.johnsnowlabs.com/api/v1/models', data=json.dumps(model_data), headers={
                 'Content-type': 'application/json',
                 'Authorization': f'Bearer {GIT_TOKEN}'
@@ -29,29 +132,28 @@ class PushToHub:
             print(f"Something Went Wrong During the Upload. Got Status Code: {r1.status_code}")
 
 
-    @staticmethod
-    def create_docs(name:str, task:str, title:str, ):
+    def create_docs(dicionary_for_upload:dict) -> dict:
+        """Adds fields in the dicinary for pushing to hub.
 
+        Keyword Argumnets:
+        dicionary_for_upload: The dicionary to add keys to.
+        """
 
-from python.sparknlp.upload_to_hub import PushToHub
-sample_upload = {
-    "name":"analyze_sentiment_ml",
-    "task":'Sentiment Analysis',
-    'title':'Analyze Sentiment Machine Learning ',
-    'sparkVersion':"3.0",
-    'sparknlpVersion':'Spark NLP 4.0.0',
-    'language':'en',
-    'license':'Open Source',
-    'description':'''The analyze_sentiment_ml is a pretrained pipeline that we can use to process text with a simple pipeline that performs basic processing steps and predicts sentiment  .
-         It performs most of the common text processing tasks on your dataframe''',
-    'pythonCode':'''from sparknlp.pretrained import PretrainedPipeline
-pipeline = PretrainedPipeline("analyze_sentiment_ml", "en")
+        dicionary_for_upload['sparkVersion'] = "3.0"
+        dicionary_for_upload['license'] = 'Open Source'
+        dicionary_for_upload['supported'] = False
 
-result = pipeline.annotate("""I love johnsnowlabs!  """)''',
-'model_zip_path':'pos_ud_bokmaal_nb_3.4.0_3.0_1641902661339.zip'
+        if 'sparknlpVersion' not in dicionary_for_upload.keys():
+            dicionary_for_upload['sparknlpVersion'] ="Spark NLP "+sparknlp.version()
 
-}
+        if 'description' not in dicionary_for_upload.keys():
+            dicionary_for_upload['description'] =f"This model is ussed for {dicionary_for_upload['task']} and this model works with {dicionary_for_upload['language']} language"
+        
+        if 'title' not in dicionary_for_upload.keys():
+            dicionary_for_upload['title'] =f"{dicionary_for_upload['task']} for {dicionary_for_upload['language']} language"
 
-PushToHub.upload_to_modelshub_and_fill_form_API(sample_upload,GitToken )
-
+        if os.path.isdir(dicionary_for_upload['model_zip_path']) : 
+            PushToHub.zip_directory(dicionary_for_upload['model_zip_path'],f"{dicionary_for_upload['model_zip_path']}.zip")
+            dicionary_for_upload['model_zip_path']=dicionary_for_upload['model_zip_path']+'.zip'
+        return dicionary_for_upload
 
