187cc842229a8ed149e56ce350e444c6a2b349ed
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-08-04 11:32:26 +0200

    Add unit test for ViTForImageClassification in Python

---
+++ python/test/annotator/cv/vit_for_image_classification_test.py
@@ -0,0 +1,48 @@
+#  Copyright 2017-2022 John Snow Labs
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+import unittest
+
+import pytest
+
+from sparknlp.annotator import *
+from sparknlp.base import *
+from test.util import SparkSessionForTest
+
+
+@pytest.mark.fast
+class ViTForImageClassificationTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.data = SparkSessionForTest.spark.read.format("image") \
+            .load(path="file:///" + os.getcwd() + "/../src/test/resources/image/")
+
+    def runTest(self):
+        image_assembler = ImageAssembler() \
+            .setInputCol("image") \
+            .setOutputCol("image_assembler")
+
+        imageClassifier = ViTForImageClassification \
+            .pretrained() \
+            .setInputCols("image_assembler") \
+            .setOutputCol("class")
+
+        pipeline = Pipeline(stages=[
+            image_assembler,
+            imageClassifier,
+        ])
+
+        model = pipeline.fit(self.data)
+        model.transform(self.data).show()
+
+
