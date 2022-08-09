1dc44f829cfa605eca98afc9bd73da8a11a0c4e1
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-08-09 15:50:31 +0200

    Refactor TF Session warmup
    - loading an actual image from resources to warm up the session
    - compatible with JAR/Maven package

--- src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowViTClassifier.scala
+++ src/main/scala/com/johnsnowlabs/ml/tensorflow/TensorflowViTClassifier.scala
@@ -17,10 +17,10 @@
 package com.johnsnowlabs.ml.tensorflow
 
 import com.johnsnowlabs.ml.tensorflow.sign.{ModelSignatureConstants, ModelSignatureManager}
+import com.johnsnowlabs.nlp.annotators.cv.feature_extractor.Preprocessor
 import com.johnsnowlabs.nlp.annotators.cv.util.io.ImageIOUtils
 import com.johnsnowlabs.nlp.annotators.cv.util.transform.ImageResizeUtils
-import com.johnsnowlabs.nlp.{AnnotationImage, _}
-import com.johnsnowlabs.nlp.annotators.cv.feature_extractor.Preprocessor
+import com.johnsnowlabs.nlp._
 
 import scala.collection.JavaConverters._
 
@@ -35,18 +35,20 @@ class TensorflowViTClassifier(
     signatures.getOrElse(ModelSignatureManager.apply())
 
   private def sessionWarmup(): Unit = {
-    val content = Array[Byte](50, 2, 20, 1, 56, 110, 50, 2, 20, 1, 56, 110, 50, 2, 20, 1, 56, 110)
+    val image =
+      ImageIOUtils.loadImage(getClass.getResourceAsStream("/image/ox.JPEG"))
+    val bytes = ImageIOUtils.BufferedImageToByte(image)
     val preprocessor =
       Preprocessor(
-        do_normalize = false,
-        do_resize = false,
+        do_normalize = true,
+        do_resize = true,
         "",
         Array(0.5d, 0.5d, 0.5d),
         Array(0.5d, 0.5d, 0.5d),
         2,
-        2)
+        224)
     val images =
-      Array(AnnotationImage("image", "test.jpeg", 2, 2, 3, 1, content, Map("image" -> "0")))
+      Array(AnnotationImage("image", "ox.JPEG", 265, 360, 3, 16, bytes, Map("image" -> "0")))
     val encoded = encode(images, preprocessor)
     tag(encoded)
   }
