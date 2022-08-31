ed20c35f6d2f9c4266a7546c92f752133018f710
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-08-31 09:47:07 +0200

    Re-structure files
    - use audio package under annotators: this will host the annotators used for speech-to-text, maybe audio classifiers, etc.
    - move assembler and annotation outside into nlp

--- src/main/scala/com/johnsnowlabs/nlp/annotators/audio/AudioProcessors.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/audio/AudioProcessors.scala
@@ -1,11 +1,27 @@
-package com.johnsnowlabs.nlp.audio
+/*
+ * Copyright 2017-2022 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.johnsnowlabs.nlp.AnnotatorType
+package com.johnsnowlabs.nlp.annotators.audio
+
+import com.johnsnowlabs.nlp.{AnnotationAudio, AnnotatorType}
 
 import java.io.{ByteArrayInputStream, InputStream}
 
 /** Utils to check audio files and parse audio byte arrays. */
-private[audio] object AudioProcessors {
+private[nlp] object AudioProcessors {
 
   private final val MAGIC_WAV: Array[Byte] = "RIFF".getBytes
   private final val MAGIC_FLAC: Array[Byte] = "fLaC".getBytes
