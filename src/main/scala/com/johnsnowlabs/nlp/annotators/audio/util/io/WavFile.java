4063ca2fc4477faae3411558b41b73dcfdfbd369
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-09-03 09:31:01 +0200

    SPARKNLP-261 Add JLibrosa to process wav files

--- src/main/scala/com/johnsnowlabs/nlp/annotators/audio/util/io/WavFile.java
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/audio/util/io/WavFile.java
@@ -25,6 +25,7 @@ import java.io.*;
  * Source based on <a href="https://github.com/Subtitle-Synchronizer/jlibrosa/blob/master/src/main/java/com/jlibrosa/audio/wavFile/WavFile.java">jlibrosa</a>
  */
 public class WavFile {
+    private File file;
     private enum IOState {READING, WRITING, CLOSED}
 
     private final static int BUFFER_SIZE = 4096;
@@ -101,6 +102,118 @@ public class WavFile {
 
     public long getFileSize() {
         return fileSize;
+    }
+
+
+    public static WavFile openWavFile(File file) throws IOException, WavFileException {
+        WavFile wavFile = new WavFile();
+        wavFile.file = file;
+        wavFile.iStream = new FileInputStream(file);
+        int bytesRead = wavFile.iStream.read(wavFile.buffer, 0, 12);
+        if (bytesRead != 12) {
+            throw new WavFileException("Not enough wav file bytes for header");
+        } else {
+            long riffChunkID = getLE(wavFile.buffer, 0, 4);
+            long chunkSize = getLE(wavFile.buffer, 4, 4);
+            long riffTypeID = getLE(wavFile.buffer, 8, 4);
+            if (riffChunkID != 1179011410L) {
+                throw new WavFileException("Invalid Wav Header data, incorrect riff chunk ID");
+            } else if (riffTypeID != 1163280727L) {
+                throw new WavFileException("Invalid Wav Header data, incorrect riff type ID");
+            } else if (file.length() != chunkSize + 8L) {
+                throw new WavFileException("Header chunk size (" + chunkSize + ") does not match file size (" + file.length() + ")");
+            } else {
+                wavFile.fileSize = chunkSize;
+                boolean foundFormat = false;
+                boolean foundData = false;
+
+                while(true) {
+                    bytesRead = wavFile.iStream.read(wavFile.buffer, 0, 8);
+                    if (bytesRead == -1) {
+                        throw new WavFileException("Reached end of file without finding format chunk");
+                    }
+
+                    if (bytesRead != 8) {
+                        throw new WavFileException("Could not read chunk header");
+                    }
+
+                    long chunkID = getLE(wavFile.buffer, 0, 4);
+                    chunkSize = getLE(wavFile.buffer, 4, 4);
+                    long numChunkBytes = chunkSize % 2L == 1L ? chunkSize + 1L : chunkSize;
+                    if (chunkID == 544501094L) {
+                        foundFormat = true;
+                        bytesRead = wavFile.iStream.read(wavFile.buffer, 0, 16);
+                        int compressionCode = (int)getLE(wavFile.buffer, 0, 2);
+                        if (compressionCode != 1) {
+                            throw new WavFileException("Compression Code " + compressionCode + " not supported");
+                        }
+
+                        wavFile.numChannels = (int)getLE(wavFile.buffer, 2, 2);
+                        wavFile.sampleRate = getLE(wavFile.buffer, 4, 4);
+                        wavFile.blockAlign = (int)getLE(wavFile.buffer, 12, 2);
+                        wavFile.validBits = (int)getLE(wavFile.buffer, 14, 2);
+                        if (wavFile.numChannels == 0) {
+                            throw new WavFileException("Number of channels specified in header is equal to zero");
+                        }
+
+                        if (wavFile.blockAlign == 0) {
+                            throw new WavFileException("Block Align specified in header is equal to zero");
+                        }
+
+                        if (wavFile.validBits < 2) {
+                            throw new WavFileException("Valid Bits specified in header is less than 2");
+                        }
+
+                        if (wavFile.validBits > 64) {
+                            throw new WavFileException("Valid Bits specified in header is greater than 64, this is greater than a long can hold");
+                        }
+
+                        wavFile.bytesPerSample = (wavFile.validBits + 7) / 8;
+                        if (wavFile.bytesPerSample * wavFile.numChannels != wavFile.blockAlign) {
+                            throw new WavFileException("Block Align does not agree with bytes required for validBits and number of channels");
+                        }
+
+                        numChunkBytes -= 16L;
+                        if (numChunkBytes > 0L) {
+                            wavFile.iStream.skip(numChunkBytes);
+                        }
+                    } else {
+                        if (chunkID == 1635017060L) {
+                            if (!foundFormat) {
+                                throw new WavFileException("Data chunk found before Format chunk");
+                            }
+
+                            if (chunkSize % (long)wavFile.blockAlign != 0L) {
+                                throw new WavFileException("Data Chunk size is not multiple of Block Align");
+                            }
+
+                            wavFile.numFrames = chunkSize / (long)wavFile.blockAlign;
+                            foundData = true;
+                            if (!foundData) {
+                                throw new WavFileException("Did not find a data chunk");
+                            }
+
+                            if (wavFile.validBits > 8) {
+                                wavFile.floatOffset = 0.0F;
+                                wavFile.floatScale = (float)(1 << wavFile.validBits - 1);
+                            } else {
+                                wavFile.floatOffset = -1.0F;
+                                wavFile.floatScale = 0.5F * (float)((1 << wavFile.validBits) - 1);
+                            }
+
+                            wavFile.bufferPointer = 0;
+                            wavFile.bytesRead = 0;
+                            wavFile.frameCounter = 0L;
+                            wavFile.ioState = WavFile.IOState.READING;
+                            wavFile.totalNumFrames = wavFile.numFrames;
+                            return wavFile;
+                        }
+
+                        wavFile.iStream.skip(numChunkBytes);
+                    }
+                }
+            }
+        }
     }
 
     /**
