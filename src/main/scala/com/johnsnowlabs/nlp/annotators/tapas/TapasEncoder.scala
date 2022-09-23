8ffa4fb27e9422e3bb3c492f44e886bd8a89399b
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-09-23 10:09:52 +0200

    Add default to Scala and update coding style
    - disable TAPAS unit test
    - add default parameters to Scala
    - update coding style and imports
    - add copyright headers

--- src/main/scala/com/johnsnowlabs/nlp/annotators/tapas/TapasEncoder.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/tapas/TapasEncoder.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017-2022 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.johnsnowlabs.nlp.annotators.tapas
 
 import com.johnsnowlabs.nlp.annotators.common.{IndexedToken, Sentence, TableData}
@@ -5,9 +21,8 @@ import com.johnsnowlabs.nlp.annotators.tokenizer.wordpiece.{BasicTokenizer, Word
 
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoField
-import scala.util.matching.Regex
-
 import scala.util.Try
+import scala.util.matching.Regex
 
 case class TapasCellDate(day: Option[Int], month: Option[Int], year: Option[Int]) {
   def isDefined: Boolean = {
