58cfc82bafbde4129ce6883c623ec52a13939ced
C-K-Loan <christian.kasim.loan@gmail.com>
2022-09-24 06:19:18 +0200

    Improved Warning message if no jars are set in Spark Configs

--- python/sparknlp/internal/py4j_utils.py
+++ python/sparknlp/internal/py4j_utils.py
@@ -2,6 +2,7 @@
 FAIL = '\033[91m'
 ENDC = '\033[0m'
 
+
 def check_obj_exist(java_class):
     # only call this after JVM is turned on
     from pyspark import SparkContext
@@ -19,7 +20,10 @@ def check_obj_exist(java_class):
     # see JavaPackage.__dir__ and JavaPackage.__apply__ methods for more infos
     if UserHelpAutoCompletion.KEY in dir(java_obj):
         bck = '\n'
-        print(
-            f"🚨 It looks like the Java Class {FAIL + java_class + ENDC} is missing in JVM! 🚨\n"
-            f"Currently loaded Jars are: \n",
-            f'{bck.join(sc.getConf().get("spark.jars").split(";"))}')
+        print(f"🚨 It looks like the Java Class {FAIL + java_class + ENDC} is missing in JVM! 🚨\n")
+        if sc.getConf().get("spark.jars"):
+            print(f"Currently loaded Jars are: \n",
+                  f'{bck.join(sc.getConf().get("spark.jars").split(";"))}')
+        else:
+            print(f'There are no custom Jars loaded into this Spark Session! '
+                  f'Make sure {FAIL}spark.jar{ENDC} SparkSetting is pointing to the correct jars!')
