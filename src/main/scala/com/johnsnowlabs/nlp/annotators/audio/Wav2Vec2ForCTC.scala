8b72b68ca8fec60049f1246d6a744178f6fd6d13
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-10-15 20:53:25 +0200

    Refactor loadSavedModel in audio, cv, coref, and ld modules [skip test]

--- src/main/scala/com/johnsnowlabs/nlp/annotators/audio/Wav2Vec2ForCTC.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/audio/Wav2Vec2ForCTC.scala
@@ -22,6 +22,8 @@ import com.johnsnowlabs.ml.tensorflow.{
   TensorflowWrapper,
   WriteTensorflowModel
 }
+import com.johnsnowlabs.ml.util.LoadExternalModel.{modelSanityCheck, notSupportedEngineError}
+import com.johnsnowlabs.ml.util.ModelEngine
 import com.johnsnowlabs.nlp.AnnotatorType.{AUDIO, DOCUMENT}
 import com.johnsnowlabs.nlp._
 import com.johnsnowlabs.nlp.annotators.audio.feature_extractor.Preprocessor
@@ -311,21 +313,12 @@ trait ReadWav2Vec2ForAudioTensorflowModel extends ReadTensorflowModel {
 
   addReader(readTensorflow)
 
-  def loadSavedModel(tfModelPath: String, spark: SparkSession): Wav2Vec2ForCTC = {
+  def loadSavedModel(modelPath: String, spark: SparkSession): Wav2Vec2ForCTC = {
 
-    val f = new File(tfModelPath)
-    val savedModel = new File(tfModelPath, "saved_model.pb")
+    val detectedEngine = modelSanityCheck(modelPath)
 
-    require(f.exists, s"Folder $tfModelPath not found")
-    require(f.isDirectory, s"File $tfModelPath is not folder")
-    require(
-      savedModel.exists(),
-      s"savedModel file saved_model.pb not found in folder $tfModelPath")
-
-    val vocabPath = new File(tfModelPath + "/assets", "vocab.json")
-    require(
-      vocabPath.exists(),
-      s"Labels file vocab.json not found in folder $tfModelPath/assets/")
+    val vocabPath = new File(modelPath + "/assets", "vocab.json")
+    require(vocabPath.exists(), s"Labels file vocab.json not found in folder $modelPath/assets/")
 
     val vocabStream = ResourceHelper.getResourceStream(vocabPath.getAbsolutePath)
     val vocabJsonContent = Source.fromInputStream(vocabStream).mkString
@@ -333,27 +326,17 @@ trait ReadWav2Vec2ForAudioTensorflowModel extends ReadTensorflowModel {
       parse(vocabJsonContent, useBigIntForLong = true).values
         .asInstanceOf[Map[String, BigInt]]
 
-    val preprocessorConfigPath = new File(tfModelPath + "/assets", "preprocessor_config.json")
+    val preprocessorConfigPath = new File(modelPath + "/assets", "preprocessor_config.json")
     require(
       preprocessorConfigPath.exists(),
-      s"Labels file preprocessor_config.json not found in folder $tfModelPath/assets/")
+      s"Labels file preprocessor_config.json not found in folder $modelPath/assets/")
 
     val preprocessorConfig =
       Preprocessor.loadPreprocessorConfig(preprocessorConfigPath.getAbsolutePath)
 
-    val (wrapper, signatures) =
-      TensorflowWrapper.read(tfModelPath, zipped = false, useBundle = true)
-
-    val _signatures = signatures match {
-      case Some(s) => s
-      case None => throw new Exception("Cannot load signature definitions from model!")
-    }
-
-    /** the order of setSignatures is important if we use getSignatures inside setModelIfNotSet */
-    new Wav2Vec2ForCTC()
+    /*Universal parameters for all engines*/
+    val annotatorModel = new Wav2Vec2ForCTC()
       .setVocabulary(vocabJsonMap)
-      .setSignatures(_signatures)
-      .setModelIfNotSet(spark, wrapper)
       .setDoNormalize(preprocessorConfig.do_normalize)
       .setFeatureSize(preprocessorConfig.feature_size)
       .setPaddingSide(preprocessorConfig.padding_side)
@@ -361,6 +344,28 @@ trait ReadWav2Vec2ForAudioTensorflowModel extends ReadTensorflowModel {
       .setReturnAttentionMask(preprocessorConfig.return_attention_mask)
       .setSamplingRate(preprocessorConfig.sampling_rate)
 
+    detectedEngine match {
+      case ModelEngine.tensorflow =>
+        val (wrapper, signatures) =
+          TensorflowWrapper.read(modelPath, zipped = false, useBundle = true)
+
+        val _signatures = signatures match {
+          case Some(s) => s
+          case None => throw new Exception("Cannot load signature definitions from model!")
+        }
+
+        /** the order of setSignatures is important if we use getSignatures inside
+          * setModelIfNotSet
+          */
+        annotatorModel
+          .setSignatures(_signatures)
+          .setModelIfNotSet(spark, wrapper)
+
+      case _ =>
+        throw new Exception(notSupportedEngineError)
+    }
+
+    annotatorModel
   }
 }
 
