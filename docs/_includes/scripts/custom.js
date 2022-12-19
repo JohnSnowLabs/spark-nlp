6d858d6d92d897bbdcaaa54e5c1f6ffd91e35f7a
Lev <agsfer@gmail.com>
2022-12-19 19:51:40 +0200

    Update Install buttons (#13262)

--- docs/_includes/scripts/custom.js
+++ docs/_includes/scripts/custom.js
@@ -123,3 +123,27 @@ if(document.querySelector('.btn.disable')) {
     });
   });
 }
+
+
+// Ancor click
+const anchors = [].slice.call(document.querySelectorAll('.btn-box-install a')),
+      animationTime = 300,
+      framesCount = 20;
+
+anchors.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset -100;
+    
+        let scroller = setInterval(function() {
+            let scrollBy = coordY / framesCount;
+      
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+          window.scrollBy(0, scrollBy);
+      } else {
+                window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+        }, animationTime / framesCount);
+  });
+}); 
