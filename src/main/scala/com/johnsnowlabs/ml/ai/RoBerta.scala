365cb4555707900296ea00c4a6893c8b343bfd55
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2022-12-29 16:46:59 +0100

    SPARKNLP-697 refactor more duplicate codes in transformer embeddings

--- src/main/scala/com/johnsnowlabs/ml/ai/RoBerta.scala
+++ src/main/scala/com/johnsnowlabs/ml/ai/RoBerta.scala
@@ -16,11 +16,11 @@
 
 package com.johnsnowlabs.ml.ai
 
+import com.johnsnowlabs.ml.ai.util.PrepareEmbeddings
 import com.johnsnowlabs.ml.tensorflow.sign.{ModelSignatureConstants, ModelSignatureManager}
 import com.johnsnowlabs.ml.tensorflow.{TensorResources, TensorflowWrapper}
 import com.johnsnowlabs.nlp.annotators.common._
 import com.johnsnowlabs.nlp.{Annotation, AnnotatorType}
-import org.tensorflow.ndarray.buffer.IntDataBuffer
 
 import scala.collection.JavaConverters._
 
@@ -49,52 +49,27 @@ private[johnsnowlabs] class RoBerta(
   val _tfRoBertaSignatures: Map[String, String] =
     signatures.getOrElse(ModelSignatureManager.apply())
 
-  /** Encode the input sequence to indexes IDs adding padding where necessary */
-  def encode(
-      sentences: Seq[(WordpieceTokenizedSentence, Int)],
-      maxSequenceLength: Int): Seq[Array[Int]] = {
-    val maxSentenceLength =
-      Array(
-        maxSequenceLength - 2,
-        sentences.map { case (wpTokSentence, _) =>
-          wpTokSentence.tokens.length
-        }.max).min
-
-    sentences
-      .map { case (wpTokSentence, _) =>
-        val tokenPieceIds = wpTokSentence.tokens.map(t => t.pieceId)
-        val padding = Array.fill(maxSentenceLength - tokenPieceIds.length)(padTokenId)
-
-        Array(sentenceStartTokenId) ++ tokenPieceIds.take(maxSentenceLength) ++ Array(
-          sentenceEndTokenId) ++ padding
-      }
-  }
-
   def tag(batch: Seq[Array[Int]]): Seq[Array[Array[Float]]] = {
-    val tensors = new TensorResources()
 
-    val maxSentenceLength = batch.map(encodedSentence => encodedSentence.length).max
+    val maxSentenceLength = batch.map(pieceIds => pieceIds.length).max
     val batchLength = batch.length
 
-    val tokenBuffers: IntDataBuffer = tensors.createIntBuffer(batchLength * maxSentenceLength)
-    val maskBuffers: IntDataBuffer = tensors.createIntBuffer(batchLength * maxSentenceLength)
+    val tensors = new TensorResources()
 
-    // [nb of encoded sentences , maxSentenceLength]
-    val shape = Array(batch.length.toLong, maxSentenceLength)
-
-    batch.zipWithIndex
-      .foreach { case (sentence, idx) =>
-        val offset = idx * maxSentenceLength
-        tokenBuffers.offset(offset).write(sentence)
-        maskBuffers.offset(offset).write(sentence.map(x => if (x == padTokenId) 0 else 1))
-      }
+    val (tokenTensors, maskTensors) =
+      PrepareEmbeddings.prepareBatchTensors(
+        tensors = tensors,
+        batch = batch,
+        maxSentenceLength = maxSentenceLength,
+        batchLength = batchLength,
+        sentencePadTokenId = padTokenId)
 
     val runner = tensorflowWrapper
-      .getTFSessionWithSignature(configProtoBytes = configProtoBytes, initAllTables = false)
+      .getTFSessionWithSignature(
+        configProtoBytes = configProtoBytes,
+        savedSignatures = signatures,
+        initAllTables = false)
       .runner
-
-    val tokenTensors = tensors.createIntBufferTensor(shape, tokenBuffers)
-    val maskTensors = tensors.createIntBufferTensor(shape, maskBuffers)
 
     runner
       .feed(
@@ -111,25 +86,16 @@ private[johnsnowlabs] class RoBerta(
     val outs = runner.run().asScala
     val embeddings = TensorResources.extractFloats(outs.head)
 
-    outs.foreach(_.close())
+    tokenTensors.close()
+    maskTensors.close()
     tensors.clearSession(outs)
     tensors.clearTensors()
 
-    val dim = embeddings.length / (batchLength * maxSentenceLength)
-    val shrinkedEmbeddings: Array[Array[Array[Float]]] =
-      embeddings.grouped(dim).toArray.grouped(maxSentenceLength).toArray
-
-    val emptyVector = Array.fill(dim)(0f)
-
-    batch.zip(shrinkedEmbeddings).map { case (ids, embeddings) =>
-      if (ids.length > embeddings.length) {
-        embeddings.take(embeddings.length - 1) ++
-          Array.fill(embeddings.length - ids.length)(emptyVector) ++
-          Array(embeddings.last)
-      } else {
-        embeddings
-      }
-    }
+    PrepareEmbeddings.prepareBatchWordEmbeddings(
+      batch,
+      embeddings,
+      maxSentenceLength,
+      batchLength)
 
   }
 
@@ -139,23 +105,19 @@ private[johnsnowlabs] class RoBerta(
     *   batches of vectors for each sentence
     */
   def tagSequence(batch: Seq[Array[Int]]): Array[Array[Float]] = {
-    val tensors = new TensorResources()
-    val tensorsMasks = new TensorResources()
 
-    val maxSentenceLength = batch.map(x => x.length).max
+    val maxSentenceLength = batch.map(pieceIds => pieceIds.length).max
     val batchLength = batch.length
 
-    val tokenBuffers = tensors.createIntBuffer(batchLength * maxSentenceLength)
-    val maskBuffers = tensorsMasks.createIntBuffer(batchLength * maxSentenceLength)
+    val tensors = new TensorResources()
 
-    val shape = Array(batchLength.toLong, maxSentenceLength)
-
-    batch.zipWithIndex.foreach { case (sentence, idx) =>
-      val offset = idx * maxSentenceLength
-
-      tokenBuffers.offset(offset).write(sentence)
-      maskBuffers.offset(offset).write(sentence.map(x => if (x == 0) 0 else 1))
-    }
+    val (tokenTensors, maskTensors) =
+      PrepareEmbeddings.prepareBatchTensors(
+        tensors = tensors,
+        batch = batch,
+        maxSentenceLength = maxSentenceLength,
+        batchLength = batchLength,
+        sentencePadTokenId = padTokenId)
 
     val runner = tensorflowWrapper
       .getTFSessionWithSignature(
@@ -163,9 +125,6 @@ private[johnsnowlabs] class RoBerta(
         savedSignatures = signatures,
         initAllTables = false)
       .runner
-
-    val tokenTensors = tensors.createIntBufferTensor(shape, tokenBuffers)
-    val maskTensors = tensorsMasks.createIntBufferTensor(shape, maskBuffers)
 
     runner
       .feed(
@@ -182,6 +141,8 @@ private[johnsnowlabs] class RoBerta(
     val outs = runner.run().asScala
     val embeddings = TensorResources.extractFloats(outs.head)
 
+    tokenTensors.close()
+    maskTensors.close()
     tensors.clearSession(outs)
     tensors.clearTensors()
 
@@ -201,7 +162,12 @@ private[johnsnowlabs] class RoBerta(
     sentences.zipWithIndex
       .grouped(batchSize)
       .flatMap { batch =>
-        val encoded = encode(batch, maxSentenceLength)
+        val encoded = PrepareEmbeddings.prepareBatchInputsWithPadding(
+          batch,
+          maxSentenceLength,
+          sentenceStartTokenId,
+          sentenceEndTokenId,
+          padTokenId)
         val vectors = tag(encoded)
 
         /*Combine tokens and calculated embeddings*/
@@ -262,7 +228,12 @@ private[johnsnowlabs] class RoBerta(
       .flatMap { batch =>
         val tokensBatch = batch.map(x => (x._1._1, x._2))
         val sentencesBatch = batch.map(x => x._1._2)
-        val encoded = encode(tokensBatch, maxSentenceLength)
+        val encoded = PrepareEmbeddings.prepareBatchInputsWithPadding(
+          tokensBatch,
+          maxSentenceLength,
+          sentenceStartTokenId,
+          sentenceEndTokenId,
+          padTokenId)
         val embeddings = tagSequence(encoded)
 
         sentencesBatch.zip(embeddings).map { case (sentence, vectors) =>
