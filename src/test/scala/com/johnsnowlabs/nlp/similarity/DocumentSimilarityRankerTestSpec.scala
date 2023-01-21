439ba66e3bedd6245a29874da8ecfc5463599e8f
Stefano Lori <s.lori@izicap.com>
2023-01-21 14:22:57 +0100

    Added doc similarity ranker annotator template

---
+++ src/test/scala/com/johnsnowlabs/nlp/similarity/DocumentSimilarityRankerTestSpec.scala
@@ -0,0 +1,74 @@
+package com.johnsnowlabs.nlp.similarity
+
+import com.johnsnowlabs.nlp.AnnotatorType.{DOC_SIMILARITY_RANKINGS, SENTENCE_EMBEDDINGS}
+import com.johnsnowlabs.nlp.annotators.Tokenizer
+import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
+import com.johnsnowlabs.nlp.annotators.similarity.DocumentSimilarityRanker
+import com.johnsnowlabs.nlp.base.DocumentAssembler
+import com.johnsnowlabs.nlp.embeddings.SentenceEmbeddings
+import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import com.johnsnowlabs.nlp.{AnnotatorBuilder, EmbeddingsFinisher}
+import com.johnsnowlabs.tags.SlowTest
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.SparkSession
+import org.scalatest.flatspec.AnyFlatSpec
+
+class DocumentSimilarityRankerTestSpec extends AnyFlatSpec {
+  val spark: SparkSession = ResourceHelper.spark
+
+  "DocumentSimilarityRanker" should "should rank document similarity" taggedAs SlowTest in {
+    val smallCorpus = ResourceHelper.spark.read
+      .option("header", "true")
+      .csv("src/test/resources/embeddings/sentence_embeddings.csv")
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val sentence = new SentenceDetector()
+      .setInputCols("document")
+      .setOutputCol("sentence")
+
+    val tokenizer = new Tokenizer()
+      .setInputCols(Array("document"))
+      .setOutputCol("token")
+
+    val embeddings = AnnotatorBuilder
+      .getGLoveEmbeddings(smallCorpus)
+      .setInputCols("document", "token")
+      .setOutputCol("embeddings")
+      .setCaseSensitive(false)
+
+    val embeddingsSentence = new SentenceEmbeddings()
+      .setInputCols(Array("document", "embeddings"))
+      .setOutputCol("sentence_embeddings")
+      .setPoolingStrategy("AVERAGE")
+
+    val sentenceFinisher = new EmbeddingsFinisher()
+      .setInputCols("sentence_embeddings")
+      .setOutputCols("finished_sentence_embeddings")
+      .setCleanAnnotations(false)
+
+    val similarityRanker = new DocumentSimilarityRanker()
+      .setInputCols("sentence_embeddings")
+      .setOutputCol(DOC_SIMILARITY_RANKINGS)
+      .setSimilarityMethod("brp")
+      .setQuery("ciao")
+      .setNumberOfNeighbours(10)
+
+    val pipeline = new Pipeline()
+      .setStages(
+        Array(
+          documentAssembler,
+          sentence,
+          tokenizer,
+          embeddings,
+          embeddingsSentence,
+          sentenceFinisher,
+          similarityRanker))
+
+    val pipelineDF = pipeline.fit(smallCorpus).transform(smallCorpus)
+
+    pipelineDF.show
+  }
+}
