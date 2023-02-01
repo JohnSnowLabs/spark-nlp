d9f6de8b61665d755312a47840d432cc4d021629
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2023-02-01 14:43:25 +0100

    Sparknlp 736 Implement Date2Chunk annotator (#13447)
    * Implement DateMatcher annotator
    
    * Add Date2Chunk annotator and unit test to Python
    
    - Move Token2Chunk to the base module
    - Add Date2Chunk to the Python APIs
    - Add Date2Chunk unit tests

--- python/sparknlp/annotator/token/__init__.py
+++ python/sparknlp/annotator/token/__init__.py
@@ -16,5 +16,4 @@
 from sparknlp.annotator.token.chunk_tokenizer import *
 from sparknlp.annotator.token.recursive_tokenizer import *
 from sparknlp.annotator.token.regex_tokenizer import *
-from sparknlp.annotator.token.token2_chunk import *
 from sparknlp.annotator.token.tokenizer import *
