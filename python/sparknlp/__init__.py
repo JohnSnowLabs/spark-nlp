00bff535b2b91cbdde5ac84dfa27724243d8d6af
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2023-03-14 14:16:12 +0100

    Update CHANGELOG and bump to 4.3.2 [run doc]

--- python/sparknlp/__init__.py
+++ python/sparknlp/__init__.py
@@ -123,7 +123,7 @@ def start(gpu=False,
         The initiated Spark session.
 
     """
-    current_version = "4.3.1"
+    current_version = "4.3.2"
 
     if params is None:
         params = {}
@@ -293,4 +293,4 @@ def version():
     str
         The current Spark NLP version.
     """
-    return '4.3.1'
+    return '4.3.2'
