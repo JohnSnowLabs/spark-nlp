195550418d4d97085326035c9ce972347dd1d1cc
Prabod Rathnayaka <prabod@rathnayaka.me>
2023-03-23 09:57:10 +0000

    WIP: changed BartTransformer methods to include beam size and added description

--- src/main/scala/com/johnsnowlabs/nlp/annotators/seq2seq/BartTransformer.scala
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/seq2seq/BartTransformer.scala
@@ -37,36 +37,41 @@ import org.apache.spark.ml.param._
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.SparkSession
 
-/** GPT-2: the OpenAI Text-To-Text Transformer
+/** BART: Denoising Sequence-to-Sequence Pre-training for Natural Language Generation,
+  * Translation, and Comprehension Transformer
   *
-  * GPT-2 is a large transformer-based language model with 1.5 billion parameters, trained on a
-  * dataset of 8 million web pages. GPT-2 is trained with a simple objective: predict the next
-  * word, given all of the previous words within some text. The diversity of the dataset causes
-  * this simple goal to contain naturally occurring demonstrations of many tasks across diverse
-  * domains. GPT-2 is a direct scale-up of GPT, with more than 10X the parameters and trained on
-  * more than 10X the amount of data.
+  * The Facebook BART (Bidirectional and Auto-Regressive Transformer) model is a state-of-the-art
+  * language generation model that was introduced by Facebook AI in 2019. It is based on the
+  * transformer architecture and is designed to handle a wide range of natural language processing
+  * tasks such as text generation, summarization, and machine translation.
   *
-  * GPT-2 displays a broad set of capabilities, including the ability to generate conditional
-  * synthetic text samples of unprecedented quality, where we prime the model with an input and
-  * have it generate a lengthy continuation. In addition, GPT-2 outperforms other language models
-  * trained on specific domains (like Wikipedia, news, or books) without needing to use these
-  * domain-specific training datasets. On language tasks like question answering, reading
-  * comprehension, summarization, and translation, GPT-2 begins to learn these tasks from the raw
-  * text, using no task-specific training data. While scores on these downstream tasks are far
-  * from state-of-the-art, they suggest that the tasks can benefit from unsupervised techniques,
-  * given sufficient (unlabeled) data and compute.
+  * BART is unique in that it is both bidirectional and auto-regressive, meaning that it can
+  * generate text both from left-to-right and from right-to-left. This allows it to capture
+  * contextual information from both past and future tokens in a sentence,resulting in more
+  * accurate and natural language generation.
+  *
+  * The model was trained on a large corpus of text data using a combination of unsupervised and
+  * supervised learning techniques. It incorporates pretraining and fine-tuning phases, where the
+  * model is first trained on a large unlabeled corpus of text, and then fine-tuned on specific
+  * downstream tasks.
+  *
+  * BART has achieved state-of-the-art performance on a wide range of NLP tasks, including
+  * summarization, question-answering, and language translation. Its ability to handle multiple
+  * tasks and its high performance on each of these tasks make it a versatile and valuable tool
+  * for natural language processing applications.
   *
   * Pretrained models can be loaded with `pretrained` of the companion object:
   * {{{
-  * val gpt2 = GPT2Transformer.pretrained()
+  * val bart = BartTransformer.pretrained()
   *   .setInputCols("document")
   *   .setOutputCol("generation")
   * }}}
-  * The default model is `"gpt2"`, if no name is provided. For available pretrained models please
-  * see the [[https://nlp.johnsnowlabs.com/models?q=gpt2 Models Hub]].
+  * //TODO Update model names here The default model is `"gpt2"`, if no name is provided. For
+  * available pretrained models please see the
+  * [[https://nlp.johnsnowlabs.com/models?q=gpt2 Models Hub]].
   *
   * For extended examples of usage, see
-  * [[https://github.com/JohnSnowLabs/spark-nlp/blob/master/src/test/scala/com/johnsnowlabs/nlp/annotators/seq2seq/GPT2TestSpec.scala GPT2TestSpec]].
+  * [[https://github.com/JohnSnowLabs/spark-nlp/blob/master/src/test/scala/com/johnsnowlabs/nlp/annotators/seq2seq/BartTestSpec.scala BartTestSpec]].
   *
   * '''References:'''
   *   - [[https://d4mucfpksywv.cloudfront.net/better-language-models/language_models_are_unsupervised_multitask_learners.pdf Language Models are Unsupervised Multitask Learners]]
@@ -357,6 +362,20 @@ class BartTransformer(override val uid: String)
   /** @group getParam */
   def getIgnoreTokenIds: Array[Int] = $(ignoreTokenIds)
 
+  /** Beam size for the beam search algorithm (Default: `4`)
+    *
+    * @group param
+    */
+  var beamSize = new IntParam(this, "beamSize", "Number of beams for beam search.")
+
+  /** @group setParam */
+  def setBeamSize(beamNum: Int): BartTransformer.this.type = {
+    set(beamSize, beamNum)
+  }
+
+  /** @group getParam */
+  def getBeamSize: Int = $(beamSize)
+
   /** ConfigProto from tensorflow, serialized into byte array. Get with
     * config_proto.SerializeToString()
     *
@@ -446,7 +465,8 @@ class BartTransformer(override val uid: String)
     repetitionPenalty -> 1.0,
     noRepeatNgramSize -> 0,
     ignoreTokenIds -> Array(),
-    batchSize -> 1)
+    batchSize -> 1,
+    beamSize -> 4)
 
   override def batchAnnotate(batchedAnnotations: Seq[Array[Annotation]]): Seq[Seq[Annotation]] = {
 
@@ -471,7 +491,8 @@ class BartTransformer(override val uid: String)
         noRepeatNgramSize = $(noRepeatNgramSize),
         task = $(task),
         randomSeed = this.randomSeed,
-        ignoreTokenIds = $(ignoreTokenIds))
+        ignoreTokenIds = $(ignoreTokenIds),
+        beamSize = $(beamSize))
     } else {
       Seq()
     }
