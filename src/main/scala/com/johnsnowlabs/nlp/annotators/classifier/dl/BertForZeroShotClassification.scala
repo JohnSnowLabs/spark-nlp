8f758492897909cbe8ec71fa57e2d81852526c7b
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2023-03-31 12:05:28 +0200

    SPARKNLP-763 Create a new BertForZeroShotClassification annotator
    Signed-off-by: Maziyar Panahi <maziyar.panahi@iscpif.fr>

---
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/BertForZeroShotClassification.scala
@@ -0,0 +1,454 @@
+/*
+ * Copyright 2017-2022 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.johnsnowlabs.nlp.annotators.classifier.dl
+
+import com.johnsnowlabs.ml.ai.BertClassification
+import com.johnsnowlabs.ml.tensorflow._
+import com.johnsnowlabs.ml.util.LoadExternalModel.{
+  loadSentencePieceAsset,
+  loadTextAsset,
+  modelSanityCheck,
+  notSupportedEngineError
+}
+import com.johnsnowlabs.ml.util.ModelEngine
+import com.johnsnowlabs.nlp._
+import com.johnsnowlabs.nlp.annotators.common._
+import com.johnsnowlabs.nlp.serialization.MapFeature
+import com.johnsnowlabs.nlp.util.io.{ExternalResource, ReadAs, ResourceHelper}
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.ml.param.{BooleanParam, IntArrayParam, IntParam}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.SparkSession
+
+import java.io.File
+
+/** BertForZeroShotClassification using a `ModelForSequenceClassification` trained on NLI (natural
+  * language inference) tasks. Equivalent of `BertForSequenceClassification` models, but these
+  * models don't require a hardcoded number of potential classes, they can be chosen at runtime.
+  * It usually means it's slower but it is much** more flexible.
+  *
+  * Any combination of sequences and labels can be passed and each combination will be posed as a
+  * premise/hypothesis pair and passed to the pretrained model.
+  *
+  * Pretrained models can be loaded with `pretrained` of the companion object:
+  * {{{
+  * val sequenceClassifier = BertForZeroShotClassification.pretrained()
+  *   .setInputCols("token", "document")
+  *   .setOutputCol("label")
+  * }}}
+  * The default model is `"bert_base_sequence_classifier_imdb"`, if no name is provided.
+  *
+  * For available pretrained models please see the
+  * [[https://nlp.johnsnowlabs.com/models?task=Text+Classification Models Hub]].
+  *
+  * To see which models are compatible and how to import them see
+  * [[https://github.com/JohnSnowLabs/spark-nlp/discussions/5669]] and to see more extended
+  * examples, see
+  * [[https://github.com/JohnSnowLabs/spark-nlp/blob/master/src/test/scala/com/johnsnowlabs/nlp/annotators/classifier/dl/BertForZeroShotClassification.scala BertForZeroShotClassification]].
+  *
+  * ==Example==
+  * {{{
+  * import spark.implicits._
+  * import com.johnsnowlabs.nlp.base._
+  * import com.johnsnowlabs.nlp.annotator._
+  * import org.apache.spark.ml.Pipeline
+  *
+  * val documentAssembler = new DocumentAssembler()
+  *   .setInputCol("text")
+  *   .setOutputCol("document")
+  *
+  * val tokenizer = new Tokenizer()
+  *   .setInputCols("document")
+  *   .setOutputCol("token")
+  *
+  * val sequenceClassifier = BertForZeroShotClassification.pretrained()
+  *   .setInputCols("token", "document")
+  *   .setOutputCol("label")
+  *   .setCaseSensitive(true)
+  *
+  * val pipeline = new Pipeline().setStages(Array(
+  *   documentAssembler,
+  *   tokenizer,
+  *   sequenceClassifier
+  * ))
+  *
+  * val data = Seq("I loved this movie when I was a child.", "It was pretty boring.").toDF("text")
+  * val result = pipeline.fit(data).transform(data)
+  *
+  * result.select("label.result").show(false)
+  * +------+
+  * |result|
+  * +------+
+  * |[pos] |
+  * |[neg] |
+  * +------+
+  * }}}
+  *
+  * @see
+  *   [[BertForZeroShotClassification]] for sequence-level classification
+  * @see
+  *   [[https://nlp.johnsnowlabs.com/docs/en/annotators Annotators Main Page]] for a list of
+  *   transformer based classifiers
+  * @param uid
+  *   required uid for storing annotator to disk
+  * @groupname anno Annotator types
+  * @groupdesc anno
+  *   Required input and expected output annotator types
+  * @groupname Ungrouped Members
+  * @groupname param Parameters
+  * @groupname setParam Parameter setters
+  * @groupname getParam Parameter getters
+  * @groupname Ungrouped Members
+  * @groupprio param  1
+  * @groupprio anno  2
+  * @groupprio Ungrouped 3
+  * @groupprio setParam  4
+  * @groupprio getParam  5
+  * @groupdesc param
+  *   A list of (hyper-)parameter keys this annotator can take. Users can set and get the
+  *   parameter values through setters and getters, respectively.
+  */
+class BertForZeroShotClassification(override val uid: String)
+    extends AnnotatorModel[BertForZeroShotClassification]
+    with HasBatchedAnnotate[BertForZeroShotClassification]
+    with WriteTensorflowModel
+    with HasCaseSensitiveProperties
+    with HasClassifierActivationProperties
+    with HasEngine
+    with HasCandidateLabelsProperties {
+
+  /** Annotator reference id. Used to identify elements in metadata or to refer to this annotator
+    * type
+    */
+  def this() = this(Identifiable.randomUID("BERT_FOR_ZERO_SHOT_CLASSIFICATION"))
+
+  /** Input Annotator Types: DOCUMENT, TOKEN
+    *
+    * @group anno
+    */
+  override val inputAnnotatorTypes: Array[String] =
+    Array(AnnotatorType.DOCUMENT, AnnotatorType.TOKEN)
+
+  /** Output Annotator Types: CATEGORY
+    *
+    * @group anno
+    */
+  override val outputAnnotatorType: AnnotatorType = AnnotatorType.CATEGORY
+
+  /** @group setParam */
+  def sentenceStartTokenId: Int = {
+    $$(vocabulary)("[CLS]")
+  }
+
+  /** @group setParam */
+  def sentenceEndTokenId: Int = {
+    $$(vocabulary)("[SEP]")
+  }
+
+  /** Vocabulary used to encode the words to ids with WordPieceEncoder
+    *
+    * @group param
+    */
+  val vocabulary: MapFeature[String, Int] = new MapFeature(this, "vocabulary")
+
+  /** @group setParam */
+  def setVocabulary(value: Map[String, Int]): this.type = {
+    if (get(vocabulary).isEmpty)
+      set(vocabulary, value)
+    this
+  }
+
+  /** Labels used to decode predicted IDs back to string tags
+    *
+    * @group param
+    */
+  val labels: MapFeature[String, Int] = new MapFeature(this, "labels")
+
+  /** @group setParam */
+  def setLabels(value: Map[String, Int]): this.type = {
+    if (get(labels).isEmpty)
+      set(labels, value)
+    this
+  }
+
+  /** Returns labels used to train this model */
+  def getClasses: Array[String] = {
+    $$(labels).keys.toArray
+  }
+
+  /** Instead of 1 class per sentence (if inputCols is '''sentence''') output 1 class per document
+    * by averaging probabilities in all sentences (Default: `false`).
+    *
+    * Due to max sequence length limit in almost all transformer models such as BERT (512 tokens),
+    * this parameter helps feeding all the sentences into the model and averaging all the
+    * probabilities for the entire document instead of probabilities per sentence.
+    *
+    * @group param
+    */
+  val coalesceSentences = new BooleanParam(
+    this,
+    "coalesceSentences",
+    "If sets to true the output of all sentences will be averaged to one output instead of one output per sentence. Default to true.")
+
+  /** @group setParam */
+  def setCoalesceSentences(value: Boolean): this.type = set(coalesceSentences, value)
+
+  /** @group getParam */
+  def getCoalesceSentences: Boolean = $(coalesceSentences)
+
+  /** ConfigProto from tensorflow, serialized into byte array. Get with
+    * `config_proto.SerializeToString()`
+    *
+    * @group param
+    */
+  val configProtoBytes = new IntArrayParam(
+    this,
+    "configProtoBytes",
+    "ConfigProto from tensorflow, serialized into byte array. Get with config_proto.SerializeToString()")
+
+  /** @group setParam */
+  def setConfigProtoBytes(bytes: Array[Int]): BertForZeroShotClassification.this.type =
+    set(this.configProtoBytes, bytes)
+
+  /** @group getParam */
+  def getConfigProtoBytes: Option[Array[Byte]] = get(this.configProtoBytes).map(_.map(_.toByte))
+
+  /** Max sentence length to process (Default: `128`)
+    *
+    * @group param
+    */
+  val maxSentenceLength =
+    new IntParam(this, "maxSentenceLength", "Max sentence length to process")
+
+  /** @group setParam */
+  def setMaxSentenceLength(value: Int): this.type = {
+    require(
+      value <= 512,
+      "BERT models do not support sequences longer than 512 because of trainable positional embeddings.")
+    require(value >= 1, "The maxSentenceLength must be at least 1")
+    set(maxSentenceLength, value)
+    this
+  }
+
+  /** @group getParam */
+  def getMaxSentenceLength: Int = $(maxSentenceLength)
+
+  /** It contains TF model signatures for the laded saved model
+    *
+    * @group param
+    */
+  val signatures = new MapFeature[String, String](model = this, name = "signatures")
+
+  /** @group setParam */
+  def setSignatures(value: Map[String, String]): this.type = {
+    if (get(signatures).isEmpty)
+      set(signatures, value)
+    this
+  }
+
+  /** @group getParam */
+  def getSignatures: Option[Map[String, String]] = get(this.signatures)
+
+  private var _model: Option[Broadcast[BertClassification]] = None
+
+  /** @group setParam */
+  def setModelIfNotSet(
+      spark: SparkSession,
+      tensorflowWrapper: TensorflowWrapper): BertForZeroShotClassification = {
+    if (_model.isEmpty) {
+      _model = Some(
+        spark.sparkContext.broadcast(
+          new BertClassification(
+            tensorflowWrapper,
+            sentenceStartTokenId,
+            sentenceEndTokenId,
+            configProtoBytes = getConfigProtoBytes,
+            tags = $$(labels),
+            signatures = getSignatures,
+            $$(vocabulary))))
+    }
+
+    this
+  }
+
+  /** @group getParam */
+  def getModelIfNotSet: BertClassification = _model.get.value
+
+  /** Whether to lowercase tokens or not (Default: `true`).
+    *
+    * @group setParam
+    */
+  override def setCaseSensitive(value: Boolean): this.type = {
+    if (get(caseSensitive).isEmpty)
+      set(this.caseSensitive, value)
+    this
+  }
+
+  setDefault(
+    batchSize -> 8,
+    maxSentenceLength -> 128,
+    caseSensitive -> true,
+    coalesceSentences -> false)
+
+  /** takes a document and annotations and produces new annotations of this annotator's annotation
+    * type
+    *
+    * @param batchedAnnotations
+    *   Annotations that correspond to inputAnnotationCols generated by previous annotators if any
+    * @return
+    *   any number of annotations processed for every input annotation. Not necessary one to one
+    *   relationship
+    */
+  override def batchAnnotate(batchedAnnotations: Seq[Array[Annotation]]): Seq[Seq[Annotation]] = {
+    batchedAnnotations.map(annotations => {
+      val sentences = SentenceSplit.unpack(annotations).toArray
+      val tokenizedSentences = TokenizedWithSentence.unpack(annotations).toArray
+
+      if (tokenizedSentences.nonEmpty) {
+        getModelIfNotSet.predictSequenceWithZeroShot(
+          tokenizedSentences,
+          sentences,
+          $(candidateLabels),
+          $(entailmentIdParam),
+          $(contradictionIdParam),
+          $(batchSize),
+          $(maxSentenceLength),
+          $(caseSensitive),
+          $(coalesceSentences),
+          $$(labels),
+          $(activation))
+
+      } else {
+        Seq.empty[Annotation]
+      }
+    })
+  }
+
+  override def onWrite(path: String, spark: SparkSession): Unit = {
+    super.onWrite(path, spark)
+    writeTensorflowModelV2(
+      path,
+      spark,
+      getModelIfNotSet.tensorflowWrapper,
+      "_bert_classification",
+      BertForZeroShotClassification.tfFile,
+      configProtoBytes = getConfigProtoBytes)
+  }
+
+}
+
+trait ReadablePretrainedBertForZeroShotModel
+    extends ParamsAndFeaturesReadable[BertForZeroShotClassification]
+    with HasPretrained[BertForZeroShotClassification] {
+  override val defaultModelName: Some[String] = Some("bert_base_sequence_classifier_imdb")
+
+  /** Java compliant-overrides */
+  override def pretrained(): BertForZeroShotClassification = super.pretrained()
+
+  override def pretrained(name: String): BertForZeroShotClassification = super.pretrained(name)
+
+  override def pretrained(name: String, lang: String): BertForZeroShotClassification =
+    super.pretrained(name, lang)
+
+  override def pretrained(
+      name: String,
+      lang: String,
+      remoteLoc: String): BertForZeroShotClassification = super.pretrained(name, lang, remoteLoc)
+}
+
+trait ReadBertForZeroShotDLModel extends ReadTensorflowModel {
+  this: ParamsAndFeaturesReadable[BertForZeroShotClassification] =>
+
+  override val tfFile: String = "bert_classification_tensorflow"
+
+  def readModel(
+      instance: BertForZeroShotClassification,
+      path: String,
+      spark: SparkSession): Unit = {
+
+    val tf = readTensorflowModel(path, spark, "_bert_classification_tf", initAllTables = false)
+    instance.setModelIfNotSet(spark, tf)
+  }
+
+  addReader(readModel)
+
+  def loadSavedModel(modelPath: String, spark: SparkSession): BertForZeroShotClassification = {
+
+    val (localModelPath, detectedEngine) = modelSanityCheck(modelPath)
+
+    val vocabs = loadTextAsset(localModelPath, "vocab.txt").zipWithIndex.toMap
+    val labels = loadTextAsset(localModelPath, "labels.txt").zipWithIndex.toMap
+
+    val entailmentIds = labels.filter(x => x._1.toLowerCase().startsWith("entail")).values.toArray
+    val contradictionIds =
+      labels.filter(x => x._1.toLowerCase().startsWith("contradict")).values.toArray
+
+    require(
+      entailmentIds.length == 1 && contradictionIds.length == 1,
+      s"""This annotator supports classifiers trained on NLI datasets. You must have only at least 2 or maximum 3 labels in your dataset:
+         
+          example with 3 labels: 'contradict', 'neutral', 'entailment'
+          example with 2 labels: 'contradict', 'entailment'
+
+          You can modify assets/labels.txt file to match the above format.
+
+          Current labels: ${labels.keys.mkString(", ")}
+          """)
+
+    val annotatorModel = new BertForZeroShotClassification()
+      .setVocabulary(vocabs)
+      .setLabels(labels)
+      .setCandidateLabels(labels.keys.toArray)
+
+    /* set the entailment id */
+    annotatorModel.set(annotatorModel.entailmentIdParam, entailmentIds.head)
+    /* set the contradiction id */
+    annotatorModel.set(annotatorModel.contradictionIdParam, contradictionIds.head)
+    /* set the engine */
+    annotatorModel.set(annotatorModel.engine, detectedEngine)
+
+    detectedEngine match {
+      case ModelEngine.tensorflow =>
+        val (wrapper, signatures) =
+          TensorflowWrapper.read(localModelPath, zipped = false, useBundle = true)
+
+        val _signatures = signatures match {
+          case Some(s) => s
+          case None => throw new Exception("Cannot load signature definitions from model!")
+        }
+
+        /** the order of setSignatures is important if we use getSignatures inside
+          * setModelIfNotSet
+          */
+        annotatorModel
+          .setSignatures(_signatures)
+          .setModelIfNotSet(spark, wrapper)
+
+      case _ =>
+        throw new Exception(notSupportedEngineError)
+    }
+
+    annotatorModel
+  }
+}
+
+/** This is the companion object of [[BertForZeroShotClassification]]. Please refer to that class
+  * for the documentation.
+  */
+object BertForZeroShotClassification
+    extends ReadablePretrainedBertForZeroShotModel
+    with ReadBertForZeroShotDLModel
