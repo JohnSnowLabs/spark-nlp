08cad55eba3a1dcfd622b91d4fc8465361f9f8a2
Stefano Lori <wolliq@users.noreply.github.com>
2023-07-03 09:10:20 +0200

    Feature/doc similarity ranker (#13858)
    * Added doc similarity ranker annotator template
    
    * Created ranker model
    
    * gitignore modified
    
    * Added params to LSH models
    
    * Added BRP LSH as annotator engine
    
    * Added replace features col with embeddings
    
    * Added LSH logic on vector cast
    
    * Added skeleton for lsh doc sim ranker - WIP
    
    * Fixed mh3 hash calculation
    
    * Fixed dataset assertions id vs neghbours
    
    * Converting neighbours result string to map
    
    * Added finisher to extract lsh id and neighbors
    
    * Labels refactoring
    
    * Added distance param to show in rankings
    
    * Added logic to select nearest neighbor
    
    * Added identity ranking for debugging
    
    * Adding Python interface to doc sim ranker approach and model
    
    * WIP - Python interface
    
    * WIP - fixed umbalanced embeddings Py test
    
    * Added MinHash engine to doc sim ranker
    
    * Fixed serde for ranker map params
    
    * Clean up pytests
    
    * Added doc sim ranker finisher Python interface
    
    * stabilized tests for doc sim ranker
    
    * Moved and enriched test for doc sim ranker
    
    * Bumped version 5.0.0 in doc sim ranker test
    
    ---------
    
    Co-authored-by: Stefano Lori <s.lori@izicap.com>

---
+++ src/main/scala/com/johnsnowlabs/nlp/annotators/similarity/DocumentSimilarityRankerApproach.scala
@@ -0,0 +1,223 @@
+package com.johnsnowlabs.nlp.annotators.similarity
+
+import com.johnsnowlabs.nlp.AnnotatorType.{DOC_SIMILARITY_RANKINGS, SENTENCE_EMBEDDINGS}
+import com.johnsnowlabs.nlp.{AnnotatorApproach, HasEnableCachingProperties}
+import com.johnsnowlabs.storage.HasStorageRef
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.feature.{
+  BucketedRandomProjectionLSH,
+  BucketedRandomProjectionLSHModel,
+  MinHashLSH
+}
+import org.apache.spark.ml.functions.array_to_vector
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.param.{BooleanParam, Param}
+import org.apache.spark.ml.util.{DefaultParamsReadable, Identifiable}
+import org.apache.spark.sql.functions.{col, flatten, udf}
+import org.apache.spark.sql.{DataFrame, Dataset}
+
+import scala.util.hashing.MurmurHash3
+
+sealed trait NeighborAnnotation {
+  def neighbors: Array[_]
+}
+
+case class IndexedNeighbors(neighbors: Array[Int]) extends NeighborAnnotation
+
+case class IndexedNeighborsWithDistance(neighbors: Array[(Int, Double)])
+    extends NeighborAnnotation
+
+case class NeighborsResultSet(result: (Int, NeighborAnnotation))
+
+class DocumentSimilarityRankerApproach(override val uid: String)
+    extends AnnotatorApproach[DocumentSimilarityRankerModel]
+    with HasEnableCachingProperties {
+
+  override val description: AnnotatorType = "LSH based document similarity annotator"
+
+  /** Annotator reference id. Used to identify elements in metadata or to refer to this annotator
+    * type
+    */
+  def this() = this(Identifiable.randomUID("DocumentSimilarityRankerApproach"))
+
+  override val inputAnnotatorTypes: Array[AnnotatorType] = Array(SENTENCE_EMBEDDINGS)
+
+  override val outputAnnotatorType: AnnotatorType = DOC_SIMILARITY_RANKINGS
+
+  val LSH_INPUT_COL_NAME = "features"
+
+  val LSH_OUTPUT_COL_NAME = "hashes"
+
+  val INDEX_COL_NAME = "index"
+
+  val DISTANCE = "distCol"
+
+  val INPUT_EMBEDDINGS = "sentence_embeddings.embeddings"
+
+  val TEXT = "text"
+
+  /** The similarity method used to calculate the neighbours. (Default: `"brp"`, Bucketed Random
+    * Projection for Euclidean Distance)
+    *
+    * @group param
+    */
+  val similarityMethod = new Param[String](
+    this,
+    "similarityMethod",
+    """The similarity method used to calculate the neighbours.
+      |(Default: `"brp"`, Bucketed Random Projection for Euclidean Distance)
+      |""".stripMargin)
+
+  def setSimilarityMethod(value: String): this.type = set(similarityMethod, value)
+
+  def getSimilarityMethod: String = $(similarityMethod)
+
+  /** The number of neighbours the model will return (Default:`"10"`).
+    *
+    * @group param
+    */
+  val numberOfNeighbours = new Param[Int](
+    this,
+    "numberOfNeighbours",
+    """The number of neighbours the model will return for each document (Default:`"10"`)""")
+
+  def setNumberOfNeighbours(value: Int): this.type = set(numberOfNeighbours, value)
+
+  def getNumberOfNeighbours: Int = $(numberOfNeighbours)
+
+  val bucketLength = new Param[Double](
+    this,
+    "bucketLength",
+    """The bucket length that controls the average size of hash buckets.
+      |A larger bucket length (i.e., fewer buckets) increases the probability of features being hashed
+      |to the same bucket (increasing the numbers of true and false positives)
+      |""".stripMargin)
+
+  def setBucketLength(value: Double): this.type = set(bucketLength, value)
+
+  def getBucketLength: Double = $(bucketLength)
+
+  val numHashTables = new Param[Int](
+    this,
+    "numHashTables",
+    """number of hash tables, where increasing number of hash tables lowers the false negative rate,
+      |and decreasing it improves the running performance.
+      |""".stripMargin)
+
+  def setNumHashTables(value: Int): this.type = set(numHashTables, value)
+
+  val visibleDistances = new BooleanParam(
+    this,
+    "visibleDistances",
+    "Whether to set visibleDistances in ranking output (Default: `false`)")
+
+  def setVisibleDistances(value: Boolean): this.type = set(visibleDistances, value)
+
+  def getVisibleDistances: Boolean = $(visibleDistances)
+
+  val identityRanking = new BooleanParam(
+    this,
+    "identityRanking",
+    "Whether to include identity in ranking result set. Useful for debug. (Default: `false`)")
+
+  def setIdentityRanking(value: Boolean): this.type = set(identityRanking, value)
+
+  def getIdentityRanking: Boolean = $(identityRanking)
+
+  setDefault(
+    similarityMethod -> "brp",
+    numberOfNeighbours -> 10,
+    bucketLength -> 2.0,
+    numHashTables -> 3,
+    visibleDistances -> false,
+    identityRanking -> false)
+
+  def getNeighborsResultSet(
+      query: (Int, Vector),
+      similarityDataset: DataFrame): NeighborsResultSet = {
+
+    val lsh = $(similarityMethod) match {
+      case "brp" =>
+        new BucketedRandomProjectionLSH()
+          .setBucketLength($(bucketLength))
+          .setNumHashTables($(numHashTables))
+          .setInputCol(LSH_INPUT_COL_NAME)
+          .setOutputCol(LSH_OUTPUT_COL_NAME)
+      case "mh" =>
+        new MinHashLSH()
+          .setNumHashTables($(numHashTables))
+          .setInputCol(LSH_INPUT_COL_NAME)
+          .setOutputCol(LSH_OUTPUT_COL_NAME)
+      case _ =>
+        throw new IllegalArgumentException(s"${$(similarityMethod)} is not a valid value.")
+    }
+
+    val model = lsh.fit(similarityDataset)
+
+    query match {
+      case (index, queryVector) =>
+        val _similarityDataset =
+          if (getIdentityRanking) {
+            similarityDataset
+          } else {
+            similarityDataset.where(col("index") =!= index)
+          }
+
+        val similarRankedDocs =
+          model.approxNearestNeighbors(_similarityDataset, queryVector, getNumberOfNeighbours)
+
+        if (getVisibleDistances) {
+          val rankedNeighboursWithDistances = similarRankedDocs
+            .select(INDEX_COL_NAME, DISTANCE)
+            .collect()
+            .map(row => (row.getInt(0), row.getDouble(1)))
+
+          NeighborsResultSet((index, IndexedNeighborsWithDistance(rankedNeighboursWithDistances)))
+        } else {
+          val rankedNeighbours = similarRankedDocs
+            .select(INDEX_COL_NAME)
+            .collect()
+            .map(_.getInt(0))
+
+          NeighborsResultSet(index, IndexedNeighbors(rankedNeighbours))
+        }
+      case _ => throw new IllegalArgumentException("query is not of type (Int, DenseVector)")
+    }
+  }
+
+  override def train(
+      dataset: Dataset[_],
+      recursivePipeline: Option[PipelineModel]): DocumentSimilarityRankerModel = {
+
+    val embeddingsDataset = dataset.withColumn(LSH_INPUT_COL_NAME, col(INPUT_EMBEDDINGS))
+
+    val similarityDataset: DataFrame = embeddingsDataset
+      .withColumn(s"$LSH_INPUT_COL_NAME", flatten(col(s"$LSH_INPUT_COL_NAME")))
+      .withColumn(s"$LSH_INPUT_COL_NAME", array_to_vector(col(s"$LSH_INPUT_COL_NAME")))
+
+    val mh3UDF = udf { (s: String) => MurmurHash3.stringHash(s, MurmurHash3.stringSeed) }
+
+    val similarityDatasetWithIndex =
+      similarityDataset.withColumn(INDEX_COL_NAME, mh3UDF(col(TEXT)))
+
+    val indexedVectorTuples = similarityDatasetWithIndex
+      .select(INDEX_COL_NAME, LSH_INPUT_COL_NAME)
+      .rdd
+      .map(x => (x.getAs[Int](INDEX_COL_NAME), x.getAs[Vector](LSH_INPUT_COL_NAME)))
+      .collect()
+
+    val similarityMappings: Map[Int, NeighborAnnotation] = indexedVectorTuples
+      .map(query => getNeighborsResultSet(query, similarityDatasetWithIndex))
+      .map(_.result)
+      .toMap
+
+    new DocumentSimilarityRankerModel()
+      .setSimilarityMappings(Map("similarityMappings" -> similarityMappings))
+  }
+}
+
+/** This is the companion object of [[DocumentSimilarityRankerApproach]]. Please refer to that
+  * class for the documentation.
+  */
+object DocumentSimilarityRankerApproach
+    extends DefaultParamsReadable[DocumentSimilarityRankerApproach]
