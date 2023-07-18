eac9cb01ccff37471a18a4c37291f766ec4ebb7c
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2023-07-18 11:11:02 +0000

    Add unit test for driver cores in start function

--- src/test/scala/com/johnsnowlabs/nlp/SparkNLPTestSpec.scala
+++ src/test/scala/com/johnsnowlabs/nlp/SparkNLPTestSpec.scala
@@ -1,6 +1,6 @@
 package com.johnsnowlabs.nlp
 
-import com.johnsnowlabs.tags.SlowTest
+import com.johnsnowlabs.tags.FastTest
 import com.johnsnowlabs.util.ConfigHelper.{awsJavaSdkVersion, hadoopAwsVersion}
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -8,14 +8,16 @@ class SparkNLPTestSpec extends AnyFlatSpec {
 
   behavior of "SparkNLPTestSpec"
 
-  it should "start with extra parameters" taggedAs SlowTest ignore {
+  it should "start with extra parameters" taggedAs FastTest in {
     val extraParams: Map[String, String] = Map(
       "spark.jars.packages" -> ("org.apache.hadoop:hadoop-aws:" + hadoopAwsVersion + ",com.amazonaws:aws-java-sdk:" + awsJavaSdkVersion),
-      "spark.hadoop.fs.s3a.path.style.access" -> "true")
+      "spark.hadoop.fs.s3a.path.style.access" -> "true",
+      "spark.driver.cores" -> "2")
 
     val spark = SparkNLP.start(params = extraParams)
 
     assert(spark.conf.get("spark.hadoop.fs.s3a.path.style.access") == "true")
+    assert(spark.conf.get("spark.master") == "local[2]")
 
     Seq(
       "com.johnsnowlabs.nlp:spark-nlp",
