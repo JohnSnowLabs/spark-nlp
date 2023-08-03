957a17fcda44e5dd093f31d47d15fff64b8b3142
Prabod Rathnayaka <prabod@rathnayaka.me>
2023-08-03 15:23:58 +0000

    Introducing MPNet Sentence Embeddings

---
+++ src/test/scala/com/johnsnowlabs/nlp/embeddings/MPNetEmbeddingsTestSpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017-2022 John Snow Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.johnsnowlabs.nlp.embeddings
+
+import com.johnsnowlabs.nlp.base.DocumentAssembler
+import com.johnsnowlabs.nlp.util.io.ResourceHelper
+import com.johnsnowlabs.tags.{FastTest, SlowTest}
+import org.apache.spark.ml.Pipeline
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MPNetEmbeddingsTestSpec extends AnyFlatSpec {
+
+  "E5 Embeddings" should "correctly embed multiple sentences" taggedAs FastTest in {
+
+    import ResourceHelper.spark.implicits._
+
+    val ddd = Seq("This is an example sentence", "Each sentence is converted")
+      .toDF("text")
+
+    val document = new DocumentAssembler()
+      .setInputCol("text")
+      .setOutputCol("document")
+
+    val embeddings = MPNetEmbeddings
+      .pretrained()
+      .setInputCols(Array("document"))
+      .setOutputCol("e5")
+
+    val pipeline = new Pipeline().setStages(Array(document, embeddings))
+
+    val pipelineDF = pipeline.fit(ddd).transform(ddd)
+    pipelineDF.select("e5.embeddings").show(truncate = false)
+
+  }
+}
