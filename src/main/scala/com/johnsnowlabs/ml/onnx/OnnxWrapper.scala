31feb194df041889896fb5b2923c5393c0ca56da
ahmedlone127 <ahmedlone127@gmail.com>
2023-09-06 15:17:22 +0500

    fixing formatting

--- src/main/scala/com/johnsnowlabs/ml/onnx/OnnxWrapper.scala
+++ src/main/scala/com/johnsnowlabs/ml/onnx/OnnxWrapper.scala
@@ -79,8 +79,8 @@ object OnnxWrapper {
 
   // TODO: make sure this.synchronized is needed or it's not a bottleneck
   private def withSafeOnnxModelLoader(
-                                       onnxModel: Array[Byte],
-                                       sessionOptions: Option[SessionOptions] = None): (OrtSession, OrtEnvironment) =
+     onnxModel: Array[Byte],
+     sessionOptions: Option[SessionOptions] = None): (OrtSession, OrtEnvironment) =
     this.synchronized {
       val env = OrtEnvironment.getEnvironment()
 
@@ -118,11 +118,11 @@ object OnnxWrapper {
     }
 
   def read(
-            modelPath: String,
-            zipped: Boolean = true,
-            useBundle: Boolean = false,
-            modelName: String = "model",
-            sessionOptions: Option[SessionOptions] = None): OnnxWrapper = {
+      modelPath: String,
+      zipped: Boolean = true,
+      useBundle: Boolean = false,
+      modelName: String = "model",
+      sessionOptions: Option[SessionOptions] = None): OnnxWrapper = {
 
     // 1. Create tmp folder
     val tmpFolder = Files
@@ -163,8 +163,8 @@ object OnnxWrapper {
   }
 
   case class EncoderDecoderWrappers(
-                                     encoder: OnnxWrapper,
-                                     decoder: OnnxWrapper,
-                                     decoderWithPast: OnnxWrapper)
+       encoder: OnnxWrapper,
+       decoder: OnnxWrapper,
+       decoderWithPast: OnnxWrapper)
 
 }
