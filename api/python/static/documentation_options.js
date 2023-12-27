3b2952f91786032168e2ecdfdb9403475b286dcc
maziyarpanahi <maziyarpanahi@users.noreply.github.com>
2023-12-27 21:35:37 +0000

    deploy: 02ef1a83d06bf3dd567505e5f9cb992a9584973a

--- api/python/static/documentation_options.js
+++ api/python/static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.2.0',
+    VERSION: '5.2.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
