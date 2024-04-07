41006b2e8e45f50637886820f208328a8426367d
Abdullah mubeen <77073730+AbdullahMubeenAnwar@users.noreply.github.com>
2024-04-08 02:55:58 +0500

    Update streamlit_app.py

--- examples/demos/streamlit/Text Finder EN/streamlit_app.py
+++ examples/demos/streamlit/Text Finder EN/streamlit_app.py
@@ -14,6 +14,7 @@ from sparknlp.pretrained import PretrainedPipeline
     
 spark = sparknlp.start()
 
+@st.cache_resource
 def create_pipeline(model, matches):
     documentAssembler = DocumentAssembler()\
         .setInputCol("text")\
