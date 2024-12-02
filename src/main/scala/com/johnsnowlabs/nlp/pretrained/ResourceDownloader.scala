15e3ac6ccc19d3601fea5a6fe879da00866a97a9
ahmedlone127 <ahmedlone127@gmail.com>
2024-12-02 17:45:14 +0500

    Update ResourceDownloader.scala

--- src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
+++ src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
@@ -691,7 +691,11 @@ object PythonResourceDownloader {
     "CamemBertForZeroShotClassification" -> CamemBertForZeroShotClassification,
     "BertForMultipleChoice" -> BertForMultipleChoice,
     "PromptAssembler" -> PromptAssembler,
-    "CPMTransformer"-> CPMTransformer)
+    "CPMTransformer"-> CPMTransformer,
+    "NomicEmbeddings" -> NomicEmbeddings,
+    "NLLBTransformer" -> NLLBTransformer,
+    "Phi3Transformer" -> Phi3Transformer,
+    "QwenTransformer" -> QwenTransformer)
 
   // List pairs of types such as the one with key type can load a pretrained model from the value type
   val typeMapper: Map[String, String] = Map("ZeroShotNerModel" -> "RoBertaForQuestionAnswering")
