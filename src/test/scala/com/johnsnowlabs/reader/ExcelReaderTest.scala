465749085378e1bff16af7f83aa43e355107842a
Danilo Burbano <danilo@johnsnowlabs.com>
2024-12-17 18:58:18 -0500

    [SPARKNLP-1102] Adding support to read Excel files

---
+++ src/test/scala/com/johnsnowlabs/reader/ExcelReaderTest.scala
@@ -0,0 +1,35 @@
+package com.johnsnowlabs.reader
+
+import com.johnsnowlabs.tags.FastTest
+import org.apache.spark.sql.functions.col
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ExcelReaderTest extends AnyFlatSpec {
+
+  val docDirectory = "src/test/resources/reader/xls"
+
+  "ExcelReader" should "read an excel file" taggedAs FastTest in {
+    val excelReader = new ExcelReader()
+    val excelDf = excelReader.xls(s"$docDirectory/2023-half-year-analyses-by-segment.xlsx")
+    excelDf.select("xls").show(false)
+
+    assert(!excelDf.select(col("xls").getItem(0)).isEmpty)
+  }
+
+  "ExcelReader" should "read a directory of excel files" taggedAs FastTest in {
+    val excelReader = new ExcelReader()
+    val excelDf = excelReader.xls(docDirectory)
+    excelDf.select("xls") show (false)
+
+    assert(!excelDf.select(col("xls").getItem(0)).isEmpty)
+  }
+
+  "ExcelReader" should "read a directory of excel files with custom cell separator" taggedAs FastTest in {
+    val excelReader = new ExcelReader(cellSeparator = "\t")
+    val excelDf = excelReader.xls(s"$docDirectory/vodafone.xlsx")
+    excelDf.select("xls").show(false)
+
+    assert(!excelDf.select(col("xls").getItem(0)).isEmpty)
+  }
+
+}
