9752516a87a32fd28129e6fcc2b16703cb7488c8
Prabod Rathnayaka <prabod@rathnayaka.me>
2025-02-13 03:57:23 +0000

    update documentation and resource downloader entry
    Signed-off-by: Prabod Rathnayaka <prabod@rathnayaka.me>

--- src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
+++ src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
@@ -697,7 +697,8 @@ object PythonResourceDownloader {
     "NLLBTransformer" -> NLLBTransformer,
     "Phi3Transformer" -> Phi3Transformer,
     "QwenTransformer" -> QwenTransformer,
-    "AutoGGUFEmbeddings" -> AutoGGUFEmbeddings)
+    "AutoGGUFEmbeddings" -> AutoGGUFEmbeddings,
+    "Phi3Vision" -> Phi3Vision)
 
   // List pairs of types such as the one with key type can load a pretrained model from the value type
   val typeMapper: Map[String, String] = Map("ZeroShotNerModel" -> "RoBertaForQuestionAnswering")
