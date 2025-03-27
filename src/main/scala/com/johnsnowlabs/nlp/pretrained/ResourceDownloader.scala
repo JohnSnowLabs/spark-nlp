8ba1d4cea0321426fb229605a01bab8d3f8f9de4
Prabod Rathnayaka <prabod@rathnayaka.me>
2025-03-27 03:53:17 +0000

    add janus to resourcedownloader
    Signed-off-by: Prabod Rathnayaka <prabod@rathnayaka.me>

--- src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
+++ src/main/scala/com/johnsnowlabs/nlp/pretrained/ResourceDownloader.scala
@@ -704,7 +704,8 @@ object PythonResourceDownloader {
     "CoHereTransformer" -> CoHereTransformer,
     "LLAVAForMultiModal" -> LLAVAForMultiModal,
     "Phi3Vision" -> Phi3Vision,
-    "OLMoTransformer" -> OLMoTransformer)
+    "OLMoTransformer" -> OLMoTransformer,
+    "JanusForMultiModal" -> JanusForMultiModal)
 
   // List pairs of types such as the one with key type can load a pretrained model from the value type
   val typeMapper: Map[String, String] = Map("ZeroShotNerModel" -> "RoBertaForQuestionAnswering")
