a25e481160a59c0353737fc4b0b015a591291a63
Danilo Burbano <37355249+danilojsl@users.noreply.github.com>
2025-05-12 10:54:07 -0500

    [SPARKNLP-1158] Adding Parameters Options to PDF Reader (#14562)
    * Adding splitPage and onlyPageNum parameters
    
    * [SPARKNLP-4098] Adding split page feature
    * [SPARKNLP-1098] Adding onlyPageNum parameter
    
    * Adding textStripper and sort parameters to PDF Reader
    
    * [SPARKNLP-4098] Adding split page feature
    * [SPARKNLP-1098] Adding onlyPageNum parameter
    * [SPARKNLP-1098] Adding textStripper parameter
    * [SPARKNLP-1098] Adding a unit tests for PDF document with scattered text
    
    * [SPARKNLP-1158] Formatting PdfToText
    
    * [SPARKNLP-1164] Updating python 3.7 to 3.8 for jobs spark34 and spark33

--- python/sparknlp/reader/pdf_to_text.py
+++ python/sparknlp/reader/pdf_to_text.py
@@ -4,6 +4,8 @@ from pyspark.ml.param.shared import HasInputCol, HasOutputCol
 from pyspark.ml.util import JavaMLReadable, JavaMLWritable
 from pyspark.ml.wrapper import JavaTransformer
 
+from sparknlp.reader.enums import TextStripperType
+
 
 class PdfToText(JavaTransformer, HasInputCol, HasOutputCol,
                 JavaMLReadable, JavaMLWritable):
@@ -25,6 +27,22 @@ class PdfToText(JavaTransformer, HasInputCol, HasOutputCol,
                              "Force to store splitted pdf.",
                              typeConverter=TypeConverters.toBoolean)
 
+    splitPage = Param(Params._dummy(), "splitPage",
+                      "Param for enable/disable splitting document per page",
+                      typeConverter=TypeConverters.toBoolean)
+
+    textStripper = Param(Params._dummy(), "textStripper",
+                         "Text stripper type used for output layout and formatting",
+                         typeConverter=TypeConverters.toString)
+
+    sort = Param(Params._dummy(), "sort",
+                 "Param for enable/disable sort lines",
+                 typeConverter=TypeConverters.toBoolean)
+
+    onlyPageNum = Param(Params._dummy(), "onlyPageNum",
+                        "Force to extract only number of pages",
+                        typeConverter=TypeConverters.toBoolean)
+
     @keyword_only
     def __init__(self):
         """
@@ -32,7 +50,6 @@ class PdfToText(JavaTransformer, HasInputCol, HasOutputCol,
         """
         super(PdfToText, self).__init__()
         self._java_obj = self._new_java_obj("com.johnsnowlabs.reader.PdfToText", self.uid)
-
 
     def setInputCol(self, value):
         """
@@ -63,3 +80,32 @@ class PdfToText(JavaTransformer, HasInputCol, HasOutputCol,
         Sets the value of :py:attr:`storeSplittedPdf`.
         """
         return self._set(storeSplittedPdf=value)
+
+    def setSplitPage(self, value):
+        """
+        Sets the value of :py:attr:`splitPage`.
+        """
+        return self._set(splitPage=value)
+
+    def setOnlyPageNum(self, value):
+        """
+        Sets the value of :py:attr:`onlyPageNum`.
+        """
+        return self._set(onlyPageNum=value)
+
+    def setTextStripper(self, value):
+        """
+        Sets the value of :py:attr:`textStripper`.
+        """
+        if isinstance(value, TextStripperType):
+            value = value.value
+        if value not in [i.value for i in TextStripperType]:
+            type_value = type(value)
+            raise ValueError(f"Param textStripper must be a 'TextStripperType' enum but got {type_value}.")
+        return self._set(textStripper=str(value))
+
+    def setSort(self, value):
+        """
+        Sets the value of :py:attr:`sort`.
+        """
+        return self._set(sort=value)
