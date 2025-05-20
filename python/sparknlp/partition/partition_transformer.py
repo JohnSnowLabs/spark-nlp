e3d420cf7db76378611689635b2046edc756e275
paulamibhattacharya <paulamixbhattacharya@gmail.com>
2025-05-20 15:00:43 -0700

    reverted changes in imports

--- python/sparknlp/partition/partition_transformer.py
+++ python/sparknlp/partition/partition_transformer.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from sparknlp.common import *
-from partition.partition_properties import *
+from sparknlp.partition.partition_properties import *
 
 class PartitionTransformer(
     AnnotatorModel,
