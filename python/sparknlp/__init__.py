a96d8f11ed256da1f112dca0d31291e225d92847
Devin Ha <devin@trungducha.de>
2025-05-26 16:43:12 +0200

    Bump Version to 6.0.2 [skip test] [run doc]

--- python/sparknlp/__init__.py
+++ python/sparknlp/__init__.py
@@ -66,7 +66,7 @@ sys.modules['com.johnsnowlabs.ml.ai'] = annotator
 annotators = annotator
 embeddings = annotator
 
-__version__ = "6.0.1"
+__version__ = "6.0.2"
 
 
 def start(gpu=False,
--- src/main/scala/com/johnsnowlabs/util/Build.scala
+++ src/main/scala/com/johnsnowlabs/util/Build.scala
@@ -17,5 +17,5 @@
 package com.johnsnowlabs.util
 
 object Build {
-  val version: String = "6.0.1"
+  val version: String = "6.0.2"
 }
