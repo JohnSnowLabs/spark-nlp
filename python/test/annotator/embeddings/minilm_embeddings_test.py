865293262e6fd23b460e75fcf62cdb597343aa07
Prabod Rathnayaka <prabod@rathnayaka.me>
2025-06-20 11:02:57 +0000

    Add MiniLM embeddings support by introducing MiniLMEmbeddings class and related test cases. Update imports to include MiniLM in the embeddings module.

---
+++ python/test/annotator/embeddings/minilm_embeddings_test.py
@@ -0,0 +1,51 @@
+#  Copyright 2017-2022 John Snow Labs
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+import unittest
+
+import pytest
+
+from sparknlp.annotator import *
+from sparknlp.base import *
+from test.annotator.common.has_max_sentence_length_test import HasMaxSentenceLengthTests
+from test.util import SparkContextForTest
+
+
+@pytest.mark.slow
+class MiniLMEmbeddingsTestSpec(unittest.TestCase):
+    def setUp(self):
+        self.spark = SparkContextForTest.spark
+        self.tested_annotator = MiniLMEmbeddings \
+            .loadSavedModel("file:///mnt/research/Projects/ModelZoo/MiniLM/models/int4/sentence-transformers/all-MiniLM-L6-v2",self.spark) \
+            .setInputCols(["documents"]) \
+            .setOutputCol("minilm")
+
+    def runTest(self):
+        data = self.spark.createDataFrame([
+            [1, "This is a sample sentence for embedding generation."],
+            [2, "Another example sentence to demonstrate MiniLM embeddings."],
+            [3, "MiniLM is a lightweight and efficient sentence embedding model that can generate text embeddings for various NLP tasks."],
+            [4, "The model achieves comparable results with BERT-base while being much smaller and faster."]
+        ]).toDF("id", "text")
+
+        document_assembler = DocumentAssembler() \
+            .setInputCol("text") \
+            .setOutputCol("documents")
+
+        minilm = self.tested_annotator
+
+        pipeline = Pipeline().setStages([document_assembler, minilm])
+        results = pipeline.fit(data).transform(data)
+
+        results.select("minilm.embeddings").show(truncate=False)
